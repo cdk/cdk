9833399e1687102b767ab79bc7068cb67481877d
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2001-09-03 11:01:38 +0000

    Changes in the website sources.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@218 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- doc/source/bin/uploadPages.bat
+++ doc/source/bin/uploadPages.bat
@@ -1,2 +1,3 @@
 set JCP_HOME=z:\develop\cdk
+move %JCP_HOME%\doc\htdocs\index.html %JCP_HOME%\doc\htdocs\index_template.html
 z:\bin\pscp -r %JCP_HOME%\doc\htdocs\* steinbeck@cdk.sourceforge.net:/home/groups/c/cd/cdk/htdocs
