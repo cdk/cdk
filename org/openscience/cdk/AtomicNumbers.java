898b638b779176fb1c4561caebb34d5d8abf8a69
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2002-02-06 12:45:45 +0000

    Added convenience class to be able to do: atom.getAtomicNumber() == AtomicNumbers.CARBON
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@358 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ org/openscience/cdk/AtomicNumbers.java
@@ -0,0 +1,47 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 1997-2001  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: steinbeck@ice.mpg.de, gezelter@maul.chem.nd.edu, egonw@sci.kun.nl
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+package org.openscience.cdk;
+
+/**
+ * Implements the idea of an element in the periodic table.
+ * Use an element factory to get a ready-to-use element by name or number
+ */
+
+public class AtomicNumbers {
+
+    public static final int HYDROGEN       = 1;
+    public static final int HELIUM         = 2;
+    public static final int LITHIUM        = 3;
+    public static final int BORON          = 5;
+    public static final int CARBON         = 6;
+    public static final int NITROGEN       = 7;
+    public static final int OXYGEN         = 8;
+    public static final int FLUOUR         = 9;
+    public static final int PHOSPHORUS     = 15;
+    public static final int SULPHUR        = 16;
+    public static final int CHLORINE       = 17;
+
+}
