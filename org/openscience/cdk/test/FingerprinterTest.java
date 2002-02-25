daa39213462ca726231257628c19c716da3c4be4
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2002-02-25 14:59:36 +0000

    Added a test class for the fingerprinter. I still need to add some methods to the test which actually search substructures based on the fingerprint.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@366 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ org/openscience/cdk/test/FingerprinterTest.java
@@ -0,0 +1,54 @@
+/* FingerprinterTest.java
+ *
+ * $RCSfile$    $Author$    $Date$    $Revision$
+ * 
+ * Copyright (C) 1997-2001  The JChemPaint project
+ * 
+ * Contact: steinbeck@ice.mpg.de
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All I ask is that proper credit is given for my work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ * 
+ */
+
+package org.openscience.cdk.test;
+
+
+import org.openscience.cdk.*;
+import org.openscience.cdk.tools.*;
+import org.openscience.cdk.fingerprint.*;
+import org.openscience.cdk.io.*;
+import java.io.*;
+import java.util.*;
+
+public class FingerprinterTest
+{
+	public static boolean test()
+	{
+		Molecule mol = MoleculeFactory.makeAlphaPinene();
+		BitSet bs = Fingerprinter.getFingerprint(mol);
+		System.out.println(bs);
+		return false;
+	}
+
+	public static void main(String[] args)
+	{
+		FingerprinterTest fpt = new FingerprinterTest();
+		fpt.test();
+	}
+}
