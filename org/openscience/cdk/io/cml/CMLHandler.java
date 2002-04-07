c540de51e51d7eae510a8ef15eda8e65315c0d81
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2002-04-07 05:25:39 +0000

    Applied a workaround to have GNUJAXP not resolve SYSTEM ids when not validating.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@423 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- org/openscience/cdk/io/cml/CMLHandler.java
+++ org/openscience/cdk/io/cml/CMLHandler.java
@@ -1,24 +1,22 @@
 /*
  * @(#)CMLHandler.java   0.2 2000/01/06
  *
- * Information can be found at http://www.openscience.org/egonw/cml/
+ * Copyright (c) 2001 The CDK Project
  *
- * Copyright (c) 2000 E.L. Willighagen (egonw@sci.kun.nl)
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
  **/
 
 package org.openscience.cdk.io.cml;
