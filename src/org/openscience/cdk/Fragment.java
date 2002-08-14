e305fd15a04e1a062a432a331d48212b09127231
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2002-08-14 11:48:12 +0000

    Added Fragment class, implementing wishlist #591476.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@550 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/Fragment.java
@@ -0,0 +1,84 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 1997-2002  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: steinbeck@ice.mpg.de, gezelter@maul.chem.nd.edu, egonw@sci.kun.nl
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk;
+
+/**
+ * Represents the concept of a fragment with free valences.
+ * An example use would be a set of templates with amino acid
+ * residues in a protein generator.
+ *
+ * @author     egonw
+ * @created    August 14th 2002
+ *
+ * @keyword    fragment
+ * @keyword    free valence
+ */
+public class Fragment extends AtomContainer {
+
+    private String title;
+    
+    // number of bonds this fragment can still make
+    private int free_valences;
+
+    /**
+     *  Creates an empty Fragment
+     */
+    public Fragment() {
+        super();
+    }
+
+    /**
+     * Returns the title of this Fragment
+     *
+     * @return    The title of this Fragment
+     */
+    public String getTitle() {
+        return this.title;
+    }
+
+    /**
+     * Returns the title of this Molecule
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Returns the title of this Fragment
+     *
+     * @return    The title of this Fragment
+     */
+    public int getFreeValences() {
+        return this.free_valences;
+    }
+
+    /**
+     * Returns the title of this Molecule
+     */
+    public void setFreeValences(int count) {
+        this.free_valences = count;
+    }
+}
+
+
