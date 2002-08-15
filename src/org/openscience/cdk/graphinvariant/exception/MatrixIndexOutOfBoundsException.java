f1017452704f9f45757a4cf623104da41faf39d9
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2002-08-15 09:33:23 +0000

    created graphinvariant package and some content
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@554 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/graphinvariant/exception/MatrixIndexOutOfBoundsException.java
@@ -0,0 +1,7 @@
+package org.openscience.cdk.graphinvariant.exception;
+
+
+public class MatrixIndexOutOfBoundsException extends Exception
+{
+
+}
---
+++ src/org/openscience/cdk/graphinvariant/exception/MatrixNotInvertibleException.java
@@ -0,0 +1,7 @@
+package org.openscience.cdk.graphinvariant.exception;
+
+
+public class MatrixNotInvertibleException extends Exception
+{
+
+} 
