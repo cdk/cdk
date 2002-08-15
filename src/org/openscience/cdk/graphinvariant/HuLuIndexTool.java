938148270f328153bb325fe6262245ed5a74f95b
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2002-08-15 09:51:13 +0000

    Added class to calculate topological index according to Chang-Yu Hu and Lu Xu, "On Highly Discriminating Molecular Topological Index",    * J. Chem. Inf. Comput. Sci. 1996, 29, 82-90.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@557 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/graphinvariant/HuLuIndexTool.java
@@ -0,0 +1,332 @@
+/* 
+ *
+ * $RCSfile$    $Author$    $Date$    $Revision$
+ * 
+ * Copyright (C) 1997-2002  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: steinbeck@ice.mpg.de, gezelter@maul.chem.nd.edu, egonw@sci.kun.nl
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ * 
+ */
+
+package org.openscience.cdk.graphinvariant;
+
+import org.openscience.cdk.*;
+import org.openscience.cdk.tools.*;
+import org.openscience.cdk.graphinvariant.exception.*;
+import org.openscience.cdk.exception.*;
+import java.util.*;
+/**
+ * Collection of methods for the calculation of topological indices of a molecular graph
+ */
+ 
+public class HuLuIndexTool implements CDKConstants
+{
+	private static final int INFINITE = Integer.MAX_VALUE;
+	
+   /**
+   * Calculates the extended adjacency matrix index.
+   * An implementation of the algorithm published in
+   * Chang-Yu Hu and Lu Xu, "On Highly Discriminating Molecular Topological Index",
+   * J. Chem. Inf. Comput. Sci. 1996, 29, 82-90.
+   */
+	public static double getEAIDNumber(AtomContainer atomContainer) throws org.openscience.cdk.exception.NoSuchAtomException,
+			BadMatrixFormatException,MatrixIndexOutOfBoundsException
+	{
+		boolean debug = false;
+		
+		Matrix matrix = new Matrix(getExtendedAdjacenyMatrix(atomContainer));
+		
+		Matrix tempMatrix = matrix;
+		Matrix fixedMatrix = matrix;
+		for (int i = 2; i < atomContainer.getAtomCount(); i++)
+		{
+			tempMatrix = tempMatrix.multiply(fixedMatrix);
+			matrix = matrix.add(tempMatrix);			
+		}
+		
+		for (int i = 0; i < atomContainer.getAtomCount(); i++)
+		{
+			matrix.setValueAt(i,i,matrix.getValueAt(i,i)+1);		
+		}
+		double eaid = matrix.trace();
+		
+		if (debug)
+		{
+			System.out.println();
+			System.out.println("final matrix - the sum of the powers of EA matrix: ");
+			System.out.println();
+			displayMatrix(matrix.getArrayValue());
+			System.out.println();		
+			System.out.println("eaid number: "+ eaid);
+			System.out.println();
+		}
+		
+		return eaid;
+	}
+	
+	
+	public static double[][] getExtendedAdjacenyMatrix(AtomContainer atomContainer) throws org.openscience.cdk.exception.NoSuchAtomException
+	{
+		boolean debug = false;
+		double[][] adjaMatrix = getAdjacenyMatrix(atomContainer);
+		if (debug) 
+		{
+			System.out.println("adjacency matrix: ");
+			System.out.println();
+			displayMatrix(adjaMatrix);
+			System.out.println();
+		}
+	
+		double[] atomWeights = getAtomWeights(atomContainer);
+
+		
+		for (int i = 0; i < adjaMatrix.length; i++)
+		{
+			for (int j = 0; j < adjaMatrix.length; j++)
+			{
+				if (i==j)
+				{
+					if (atomContainer.getAtomAt(i).getSymbol()=="O")
+					{
+						adjaMatrix[i][j] = Math.sqrt(0.74)/6;						
+					}
+					else
+					{
+						adjaMatrix[i][j] = Math.sqrt(0.74)/6;
+					}					
+				}
+				else
+				{
+					adjaMatrix[i][j] = (Math.sqrt(atomWeights[i]/atomWeights[j]) + Math.sqrt(atomWeights[j]/atomWeights[i])) * Math.sqrt(adjaMatrix[i][j])/6;					
+				}			
+			}
+		}
+		
+		if (debug) 
+		{
+			System.out.println("extended adjacency matrix: ");
+			System.out.println();
+			displayMatrix(adjaMatrix);
+			System.out.println();
+		}
+		return adjaMatrix;
+	}
+	
+	public static double[] getAtomWeights(AtomContainer atomContainer) throws org.openscience.cdk.exception.NoSuchAtomException
+	{
+		boolean debug = false;
+		Atom atom,headAtom,endAtom;
+		Bond bond;
+		int headAtomPosition,endAtomPosition;
+
+		int k = 0;
+		double[] weightArray = new double[atomContainer.getAtomCount()];
+		double[][] adjaMatrix = getAdjacenyMatrix(atomContainer);
+		
+		int[][] apspMatrix = PathTools.computeFloydAPSP(adjaMatrix);		
+		int[] atomLayers = getAtomLayers(apspMatrix);
+		
+		int[] valenceSum;
+		int[] interLayerBondSum;
+		
+		if (debug) 
+		{
+			System.out.println("adjacency matrix: ");
+			System.out.println();
+			displayMatrix(adjaMatrix);
+			System.out.println();
+			System.out.println("all-pairs-shortest-path matrix: ");
+			System.out.println();
+			displayMatrix(apspMatrix);
+			System.out.println();
+			System.out.println("atom layers: ");
+			displayArray(atomLayers);
+			System.out.println();
+		}
+		
+		for (int i = 0; i < atomContainer.getAtomCount(); i++)
+		{
+			atom = atomContainer.getAtomAt(i);
+			
+			valenceSum = new int[atomLayers[i]];			
+			for (int v = 0; v < valenceSum.length; v++)
+			{
+				valenceSum[v] = 0;	
+			}
+			
+			interLayerBondSum = new int[atomLayers[i]-1];
+			for (int v = 0; v < interLayerBondSum.length; v++)
+			{
+				interLayerBondSum[v] = 0;			
+			}
+			
+			
+			//weightArray[k] = atom.getValenceElectronsCount() - atom.getHydrogenCount(); // method unfinished
+			if(atom.getSymbol()=="O")
+				weightArray[i] = 6 - atom.getHydrogenCount();
+			else
+				weightArray[i] = 4 - atom.getHydrogenCount();			
+			 	
+			
+			
+			for (int j = 0; j < apspMatrix.length; j++)
+			{
+				if(atomContainer.getAtomAt(j).getSymbol()=="O")
+					valenceSum[apspMatrix[j][i]] += 6 - atomContainer.getAtomAt(j).getHydrogenCount();
+				else
+					valenceSum[apspMatrix[j][i]] += 4 - atomContainer.getAtomAt(j).getHydrogenCount();					
+			}
+			
+			for (int j = 0; j < atomContainer.getBondCount(); j++)
+			{
+				bond = atomContainer.getBondAt(j);
+				headAtom = bond.getAtomAt(0);
+				endAtom = bond.getAtomAt(1);
+				
+				
+				headAtomPosition = atomContainer.getAtomNumber(headAtom);
+				endAtomPosition = atomContainer.getAtomNumber(endAtom);
+				
+				
+				
+
+				if (Math.abs(apspMatrix[i][headAtomPosition] - apspMatrix[i][endAtomPosition]) == 1)
+				{
+					int min = Math.min(apspMatrix[i][headAtomPosition],apspMatrix[i][endAtomPosition]);
+					interLayerBondSum[min] += bond.getOrder();
+				}
+			}
+			
+			
+			for (int j = 0; j < interLayerBondSum.length; j++)
+			{
+				weightArray[i] += interLayerBondSum[j] * valenceSum[j+1] * Math.pow(10, -(j+1));
+			}
+			
+			if (debug) 
+			{
+				System.out.println("valence sum: ");
+				displayArray(valenceSum);
+				System.out.println();
+				System.out.println("inter-layer bond sum: ");
+				displayArray(interLayerBondSum);
+				System.out.println();
+			}			
+		}
+		
+		if (debug) 
+		{
+			System.out.println("weight array: ");
+			System.out.println();
+			displayArray(weightArray);
+			System.out.println();
+		}
+		return weightArray;
+	}
+	
+	
+	
+	public static double[][] getAdjacenyMatrix(AtomContainer atomContainer)
+	{
+		int dim =atomContainer.getAtomCount();
+		double[][] adjaMatrix = new double[dim][dim];
+		
+		for (int j = 0; j < dim; j++){
+			for (int i = 0; i < dim; i++)
+				adjaMatrix[i][j] = 0;
+		}		
+		
+		for (int f = 0; f < atomContainer.getBondCount(); f++)
+		{
+			Bond bond = atomContainer.getBondAt(f);
+			try
+			{
+				int i = atomContainer.getAtomNumber(bond.getAtomAt(0));
+				int j = atomContainer.getAtomNumber(bond.getAtomAt(1));
+				adjaMatrix[i][j] = (int)bond.getOrder();
+				adjaMatrix[j][i] = (int)bond.getOrder();
+			}
+			catch(NoSuchAtomException e){}			
+		}
+		return adjaMatrix;
+	}	
+		
+	public static int[] getAtomLayers(int[][]apspMatrix)
+	{
+		int[] atomLayers  = new int[apspMatrix.length];
+		for(int i = 0; i < apspMatrix.length; i++)
+		{
+			atomLayers[i] = 0;
+			for(int j = 0; j < apspMatrix.length; j++)
+			{
+				if(atomLayers[i] < 1+ apspMatrix[j][i] )
+					atomLayers[i] = 1+ apspMatrix[j][i]; 			
+			}
+			
+		}
+		return atomLayers;
+	}
+	
+	/** Lists a 2D double matrix to the System console */
+	public static void displayMatrix(double[][] matrix){
+		String line;
+		for (int f = 0; f < matrix.length; f++)
+		{
+			line  = "";
+			for (int g = 0; g < matrix.length; g++)
+			{
+				line += matrix[g][f] + " | ";
+			}
+			System.out.println(line);
+		}
+	}
+	
+	/** Lists a 2D int matrix to the System console */
+	public static void displayMatrix(int[][] matrix){
+		String line;
+		for (int f = 0; f < matrix.length; f++)
+		{
+			line  = "";
+			for (int g = 0; g < matrix.length; g++)
+			{
+				line += matrix[g][f] + " | ";
+			}
+			System.out.println(line);
+		}
+	}
+	
+	/** Lists a 1D array to the System console */
+	public static void displayArray(int[] array){
+		String line  = "";
+		for (int f = 0; f < array.length; f++)
+		{
+			line += array[f] + " | ";			
+		}
+		System.out.println(line);
+	}
+	
+	/** Lists a 1D array to the System console */
+	public static void displayArray(double[] array){
+		String line  = "";
+		for (int f = 0; f < array.length; f++)
+		{
+			line += array[f] + " | ";			
+		}
+		System.out.println(line);
+	}	
+
+}  
