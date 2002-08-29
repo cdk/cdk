8298d6e7d2892641b70c4b65a4e551b1074fa75c
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2002-08-29 15:13:41 +0000

    Removed more rudimentary stuff.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@625 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/applications/PublicJmol.java
+++ src/org/openscience/cdk/applications/PublicJmol.java
@@ -1,4 +1,8 @@
-/*
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
  * Copyright 2002 The Jmol Development Team
  *
  *  This library is free software; you can redistribute it and/or
@@ -84,7 +88,7 @@ import javax.swing.SwingConstants;
 import javax.swing.JOptionPane;
 
 /**
- * An adapted Jmol version with menu, toolbars etc for simple viewing.
+ * An adapted Jmol version without menu, toolbars etc for simple viewing.
  *
  * @author Bradley A. Smith (bradley@baysmith.com)
  * @author Peter Murray-Rust
@@ -95,7 +99,6 @@ public class PublicJmol extends JPanel {
   private JScrollPane scroller;
   private JViewport port;
   static DisplayPanel display;
-  StatusBar status;
   static AtomTypeTable atomTypeTable;
   protected static JFrame frame;
   private ChemFile chemFile;
@@ -107,13 +110,8 @@ public class PublicJmol extends JPanel {
 
   static File UserPropsFile;
   static File UserAtypeFile;
-  private static HistoryFile historyFile;
 
   Splash splash;
-
-  public static HistoryFile getHistoryFile() {
-    return historyFile;
-  }
 
   private static JFrame consoleframe;
 
@@ -123,12 +121,6 @@ public class PublicJmol extends JPanel {
   public String currentFileName = "";
 
   static {
-    if (System.getProperty("javawebstart.version") != null) {
-
-      // If the property is found, Jmol is running with Java Web Start. To fix
-      // bug 4621090, the security manager is set to null.
-      System.setSecurityManager(null);
-    }
     if (System.getProperty("user.home") == null) {
       System.err.println(
           "Error starting Jmol: the property 'user.home' is not defined.");
@@ -139,8 +131,6 @@ public class PublicJmol extends JPanel {
     ujmoldir.mkdirs();
     UserPropsFile = new File(ujmoldir, "properties");
     UserAtypeFile = new File(ujmoldir, "AtomTypes");
-    historyFile = new HistoryFile(new File(ujmoldir, "history"),
-        "Jmol's persistent values");
   }
 
   public PublicJmol(Splash splash) {
@@ -172,9 +162,8 @@ public class PublicJmol extends JPanel {
       // just use the viewport default
     }
 
-    status = (StatusBar) createStatusBar();
     splash.showStatus("Initializing 3D display...");
-    display = new DisplayPanel(status, settings);
+    display = new DisplayPanel(new StatusBar(), settings);
     port.add(display);
 
     JPanel panel = new JPanel();
@@ -202,13 +191,9 @@ public class PublicJmol extends JPanel {
     Splash splash = new Splash(frame, splash_image);
     splash.setCursor(new Cursor(Cursor.WAIT_CURSOR));
     splash.showStatus("Creating main window...");
-    frame.setTitle(jrh.getString("Jmol.Title"));
-    frame.setBackground(Color.lightGray);
-    frame.getContentPane().setLayout(new BorderLayout());
     splash.showStatus("Initializing Jmol...");
     PublicJmol window = new PublicJmol(splash);
     frame.getContentPane().add("Center", window);
-    frame.addWindowListener(new PublicJmol.AppCloser());
     frame.pack();
     frame.setSize(400, 400);
     frame.show();
@@ -231,8 +216,10 @@ public class PublicJmol extends JPanel {
     return chemFile;
   }
 
-
-  void setChemFile(ChemFile chemFile) {
+  /**
+   *  Method for setting the ChemFile to show.
+   */
+  public void setChemFile(ChemFile chemFile) {
 
     ChemFile oldChemFile = this.chemFile;
     this.chemFile = chemFile;
@@ -249,19 +236,6 @@ public class PublicJmol extends JPanel {
   }
 
   /**
-   * To shutdown when run as an application.  This is a
-   * fairly lame implementation.   A more self-respecting
-   * implementation would at least check to see if a save
-   * was needed.
-   */
-  protected static final class AppCloser extends WindowAdapter {
-
-    public void windowClosing(WindowEvent e) {
-      System.exit(0);
-    }
-  }
-
-  /**
    * Find the hosting frame, for the file-chooser dialog.
    */
   protected Frame getFrame() {
@@ -272,216 +246,6 @@ public class PublicJmol extends JPanel {
       }
     }
     return null;
-  }
-
-  /**
-   * This is the hook through which all menu items are
-   * created.  It registers the result with the menuitem
-   * hashtable so that it can be fetched with getMenuItem().
-   * @see #getMenuItem
-   */
-  protected JMenuItem createMenuItem(String cmd, boolean isRadio) {
-
-    JMenuItem mi;
-    if (isRadio) {
-      mi = new JRadioButtonMenuItem(JmolResourceHandler.getInstance()
-          .getString("Jmol." + cmd + labelSuffix));
-    } else {
-      String checked = JmolResourceHandler.getInstance().getString("Jmol."
-                         + cmd + checkSuffix);
-      if (checked != null) {
-        boolean c = false;
-        if (checked.equals("true")) {
-          c = true;
-        }
-        mi = new JCheckBoxMenuItem(JmolResourceHandler.getInstance()
-            .getString("Jmol." + cmd + labelSuffix), c);
-      } else {
-        mi = new JMenuItem(JmolResourceHandler.getInstance().getString("Jmol."
-            + cmd + labelSuffix));
-      }
-    }
-    String mnem = JmolResourceHandler.getInstance().getString("Jmol." + cmd
-                    + mnemonicSuffix);
-    if (mnem != null) {
-      char mn = mnem.charAt(0);
-      mi.setMnemonic(mn);
-    }
-
-    /*        String accel = JmolResourceHandler.getInstance().getString("Jmol." + cmd + acceleratorSuffix);
-    if (accel != null) {
-            if (accel.startsWith("Ctrl-")) {
-                    char ac = accel.charAt(5);
-                    mi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1,
-                                                                                                     ActionEvent.CTRL_MASK));
-            }
-            if (accel.startsWith("Alt-")) {
-                    char ac = accel.charAt(4);
-                    mi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1,
-                                                                                                     ActionEvent.ALT_MASK));
-            }
-    }
-    */
-    ImageIcon f = JmolResourceHandler.getInstance().getIcon("Jmol." + cmd
-                    + imageSuffix);
-    if (f != null) {
-      mi.setHorizontalTextPosition(JButton.RIGHT);
-      mi.setIcon(f);
-    }
-    String astr = JmolResourceHandler.getInstance().getString("Jmol." + cmd
-                    + actionSuffix);
-    if (astr == null) {
-      astr = cmd;
-    }
-    mi.setActionCommand(astr);
-    Action a = getAction(astr);
-    if (a != null) {
-      mi.addActionListener(a);
-      a.addPropertyChangeListener(new ActionChangedListener(mi));
-      mi.setEnabled(a.isEnabled());
-    } else {
-      mi.setEnabled(false);
-    }
-    menuItems.put(cmd, mi);
-    return mi;
-  }
-
-  /**
-   * Fetch the menu item that was created for the given
-   * command.
-   * @param cmd  Name of the action.
-   * @return item created for the given command or null
-   *  if one wasn't created.
-   */
-  protected JMenuItem getMenuItem(String cmd) {
-    return (JMenuItem) menuItems.get(cmd);
-  }
-
-  /**
-   * Fetch the action that was created for the given
-   * command.
-   * @param cmd  Name of the action.
-   */
-  protected Action getAction(String cmd) {
-    return (Action) commands.get(cmd);
-  }
-
-  /**
-   * Create the toolbar.  By default this reads the
-   * resource file for the definition of the toolbars.
-   */
-  private Component createToolbar() {
-
-    /* toolbar = new JToolBar();
-    String[] tool1Keys =
-      tokenize(JmolResourceHandler.getInstance().getString("Jmol.toolbar"));
-    for (int i = 0; i < tool1Keys.length; i++) {
-      if (tool1Keys[i].equals("-")) {
-        toolbar.addSeparator();
-      } else {
-        toolbar.add(createTool(tool1Keys[i]));
-      }
-    }
-
-    //Action handler implementation would go here.
-    toolbar.add(Box.createHorizontalGlue());
-
-    */
-    return toolbar;
-  }
-
-  /**
-   * Hook through which every toolbar item is created.
-   */
-  protected Component createTool(String key) {
-    return createToolbarButton(key);
-  }
-
-  /**
-   * Create a button to go inside of the toolbar.  By default this
-   * will load an image resource.  The image filename is relative to
-   * the classpath (including the '.' directory if its a part of the
-   * classpath), and may either be in a JAR file or a separate file.
-   *
-   * @param key The key in the resource file to serve as the basis
-   *  of lookups.
-   */
-  protected AbstractButton createToolbarButton(String key) {
-
-    ImageIcon ii = JmolResourceHandler.getInstance().getIcon("Jmol." + key
-                     + imageSuffix);
-    AbstractButton b = new JButton(ii);
-    String isToggleString =
-      JmolResourceHandler.getInstance().getString("Jmol." + key + "Toggle");
-    if (isToggleString != null) {
-      boolean isToggle = Boolean.valueOf(isToggleString).booleanValue();
-      if (isToggle) {
-        b = new JToggleButton(ii);
-        toolbarButtonGroup.add(b);
-        String isSelectedString =
-          JmolResourceHandler.getInstance().getString("Jmol." + key
-            + "ToggleSelected");
-        if (isSelectedString != null) {
-          boolean isSelected =
-            Boolean.valueOf(isSelectedString).booleanValue();
-          b.setSelected(isSelected);
-        }
-      }
-    }
-    b.setRequestFocusEnabled(false);
-    b.setMargin(new Insets(1, 1, 1, 1));
-
-    String astr = JmolResourceHandler.getInstance().getString("Jmol." + key
-                    + actionSuffix);
-    if (astr == null) {
-      astr = key;
-    }
-    Action a = getAction(astr);
-    if (a != null) {
-
-      // b = new JButton(a);
-      b.setActionCommand(astr);
-      b.addActionListener(a);
-      a.addPropertyChangeListener(new ActionChangedListener(b));
-      b.setEnabled(a.isEnabled());
-    } else {
-      b.setEnabled(false);
-    }
-
-    String tip = JmolResourceHandler.getInstance().getString("Jmol." + key
-                   + tipSuffix);
-    if (tip != null) {
-      b.setToolTipText(tip);
-    }
-
-    return b;
-  }
-
-  /**
-   * Take the given string and chop it up into a series
-   * of strings on whitespace boundries.  This is useful
-   * for trying to get an array of strings out of the
-   * resource file.
-   */
-  protected String[] tokenize(String input) {
-
-    Vector v = new Vector();
-    StringTokenizer t = new StringTokenizer(input);
-    String cmd[];
-
-    while (t.hasMoreTokens()) {
-      v.addElement(t.nextToken());
-    }
-    cmd = new String[v.size()];
-    for (int i = 0; i < cmd.length; i++) {
-      cmd[i] = (String) v.elementAt(i);
-    }
-
-    return cmd;
-  }
-
-  protected Component createStatusBar() {
-    return new StatusBar();
   }
 
   private class ActionChangedListener implements PropertyChangeListener {
@@ -507,66 +271,6 @@ public class PublicJmol extends JPanel {
       }
     }
   }
-
-  private Hashtable commands;
-  private Hashtable menuItems;
-  private JMenuBar menubar;
-  private JToolBar toolbar;
-
-
-  /**
-   * Suffix applied to the key used in resource file
-   * lookups for an image.
-   */
-  private static final String imageSuffix = "Image";
-
-  /**
-   * Suffix applied to the key used in resource file
-   * lookups for a label.
-   */
-  private static final String labelSuffix = "Label";
-
-  /**
-   * Suffix applied to the key used in resource file
-   * lookups for a checkbox menu item.
-   */
-  private static final String checkSuffix = "Check";
-
-  /**
-   * Suffix applied to the key used in resource file
-   * lookups for a radio group.
-   */
-  private static final String radioSuffix = "Radio";
-
-  /**
-   * Suffix applied to the key used in resource file
-   * lookups for a selected member of a radio group.
-   */
-  private static final String selectedSuffix = "Selected";
-
-  /**
-   * Suffix applied to the key used in resource file
-   * lookups for a popup menu.
-   */
-  private static final String popupSuffix = "Popup";
-
-  /**
-   * Suffix applied to the key used in resource file
-   * lookups for an action.
-   */
-  private static final String actionSuffix = "Action";
-
-  /**
-   * Suffix applied to the key used in resource file
-   * lookups for tooltip text.
-   */
-  private static final String tipSuffix = "Tooltip";
-
-  /**
-   * Suffix applied to the key used in resource file
-   * lookups for Mnemonics.
-   */
-  private static final String mnemonicSuffix = "Mnemonic";
 
   /**
    * Returns a new File referenced by the property 'user.dir', or null
--- src/org/openscience/cdk/applications/Viewer.java
+++ src/org/openscience/cdk/applications/Viewer.java
@@ -85,9 +85,13 @@ public class Viewer {
             if (useJmol) {
                 logger.debug(".. trying Jmol viewer");
                 try {
+                    frame.getContentPane().setLayout(new BorderLayout());
                     org.openscience.jmol.PublicJmol jmol = org.openscience.jmol.PublicJmol.getJmol(frame);
                     jmol.showChemFrame(Convertor.convert(m));
+                    // frame.setSize(400, 400);
+                    // frame.show();
 
+                    frame.getContentPane().add(jmol, BorderLayout.CENTER);
                     logger.debug(".. done");
 
                     viewed = true;
@@ -131,10 +135,10 @@ public class Viewer {
             System.exit(1);
         }
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        if (!useJmol) {
+        // if (!useJmol) {
             frame.setSize(500,500);
             frame.setVisible(true);
-        }
+        // }
         frame.addWindowListener(new AppCloser());
     }
 
