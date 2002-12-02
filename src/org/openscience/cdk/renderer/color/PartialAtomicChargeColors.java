370f9eabb95c819d27ee0cc8b450878b961d52b5
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2002-12-02 14:22:06 +0000

    Added a color scheme for atoms based on the partial atomic charge.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@831 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/renderer/color/PartialAtomicChargeColors.java
@@ -0,0 +1,65 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 1997-2002  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: steinbeck@ice.mpg.de, gezelter@maul.chem.nd.edu, egonw@sci.kun.nl
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk.renderer.color;
+
+import org.openscience.cdk.*;
+import java.awt.Color;
+
+/**
+ * Class defining the color which with atoms are colored.
+ *
+ * <p>This scheme used the atomic partial charge to determine
+ * the Atom's color:
+ * uncharged atoms are colored white, positively charged
+ * atoms are blue, and negatively charge atoms are red.
+ */
+public class PartialAtomicChargeColors implements AtomColorer {
+
+    /**
+     * Returns the color for a certain atom type
+     */
+    public Color getAtomColor(Atom a) {
+        Color c = Color.white;
+        if (ct != null) {
+            double charge = a.getCharge();
+            if (charge > 0.0) {
+                if (charge < 1.0) {
+                    int index = 255 - (int)(charge*255.0);
+                    c = new Color(index, index, 255);
+                } else {
+                    c = Color.blue;
+                }
+            } else if (charge < 0.0) {
+                if (charge > -1.0) {
+                    int index = 255 + (int)(charge*255.0);
+                    c = new Color(255, index, index);
+                } else {
+                    c = Color.red;
+                }
+            }
+        }
+        return c;
+    }
+}
