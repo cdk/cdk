739679da1015d3481ddabf357afccba3066f3477
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2002-12-29 21:59:20 +0000

    Add new class for displaying a ChemObject. It is cool! (Try JCPCDK from cvs with the option -Dshow.cot=true).
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@859 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/renderer/ChemObjectTree.java
@@ -0,0 +1,153 @@
+/*  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 2002  The Chemistry Development Kit (CDK) project
+ *
+ *  Contact: steinbeck@ice.mpg.de, gezelter@maul.chem.nd.edu, egonw@sci.kun.nl
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk.renderer;
+
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.tools.LoggingTool;
+import javax.swing.JTree;
+import javax.swing.JPanel;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.lang.reflect.*;
+
+/**
+ * Displays a ChemObject as a tree. An example tree looks like this:
+ * <pre>
+ *   ChemModel
+ *    +- SetOfMolecules
+ *        +- Molecule
+ *        |   +- Atom
+ *        |   +- Atom
+ *        |   +- Bond
+ *        +- Molecule
+ * </pre>
+ *
+ * <p>For each ChemObject it will add child nodes for each variable
+ * it has that is either a ChemObject or a ChemObject[].
+ *
+ * @author     egonw
+ * @created    December 29, 2002
+ * @keyword    tree, ChemObject
+ */
+public class ChemObjectTree extends JPanel {
+
+    private LoggingTool logger;
+
+	private ChemObject chemObject;
+    private JTree tree;
+
+	/**
+	 * Constructs a JPanel showing a ChemObject in a tree.
+	 */
+	public ChemObjectTree() {
+        logger = new LoggingTool(this.getClass().getName());
+        this.tree = new JTree(new DefaultMutableTreeNode("empty"));
+        this.add(tree);
+	}
+
+    /**
+     * Displays the ChemObject in tree format.
+     */
+    public void paintChemObject(ChemObject object) {
+        // make a new tree
+        this.removeAll();
+        if (object == null) {
+            logger.info("Making empty ChemObjectTree");
+            this.tree = new JTree(new DefaultMutableTreeNode("empty"));
+        } else {
+            logger.info("Making ChemObjectTree for " + object.getClass().getName());
+            DefaultMutableTreeNode top = getTree(object);
+            this.tree = new JTree(top);
+        }
+        this.add(this.tree);
+    }
+    
+    /**
+     * Creates a node with the object name.
+     */
+    private DefaultMutableTreeNode getObjectName(Object object) {
+        return new DefaultMutableTreeNode(object.getClass().getName());
+    }
+    
+    /**
+     * Generate a tree of ChemObject's.
+     */
+    private DefaultMutableTreeNode getTree(ChemObject object) {
+        DefaultMutableTreeNode node = getObjectName(object);
+        Class reflectedClass = object.getClass();
+        Field[] fields = getFields(reflectedClass);
+        logger.debug(reflectedClass.getName() + " #fields: " + fields.length); 
+        for (int i=0; i<fields.length; i++) {
+            Field f = fields[i];
+            f.setAccessible(true);
+            logger.debug("Field name: " + f.getName());
+            logger.debug("Field type: " + f.getType().getName());
+            try {
+                Object fieldObject = f.get(object);
+                if (fieldObject != null) {
+                    logger.debug("Field value: " + fieldObject.getClass().getName());
+                    if (fieldObject instanceof ChemObject) {
+                        // yes, found a ChemObject!
+                        logger.debug("Recursing into this object");
+                        node.add(getTree((ChemObject)fieldObject));
+                    } else if (fieldObject instanceof ChemObject[]) {
+                        // yes, found a Array!
+                        logger.debug("Recursing into this Array");
+                        ChemObject[] objects = (ChemObject[])fieldObject;
+                        for (int j=0; j<objects.length; j++) {
+                            if (objects[i] != null) {
+                                node.add(getTree(objects[i]));
+                            }
+                        }
+                    }
+                } else {
+                    logger.debug("Field value: null");
+                }
+            } catch (Exception e) {
+                logger.debug(e.toString());
+            }
+        }
+        return node;
+    }
+    
+    private Field[] getFields(Class reflectedClass) {
+        Field[] fields = reflectedClass.getDeclaredFields();
+        if (fields.length == 0) {
+            // try its super class (as long as it is still a ChemObject
+            Class superClass = reflectedClass.getSuperclass();
+            try {
+                if (superClass.newInstance() instanceof ChemObject) {
+                    fields = getFields(superClass);
+                }
+            } catch (IllegalAccessException e) {
+            } catch (InstantiationException e) {};
+        }
+        return fields;
+    }
+}
+
