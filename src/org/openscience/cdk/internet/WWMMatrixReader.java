eb658ed23972ddb2adb5bbfa658b3be6dd90f536
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-02-05 15:51:47 +0000

    Added collection setting and kegg name support.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@940 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/internet/WWMMatrixReader.java
+++ src/org/openscience/cdk/internet/WWMMatrixReader.java
@@ -48,10 +48,11 @@ import javax.vecmath.*;
  */
 public class WWMMatrixReader implements ChemObjectReader {
 
-    String server = "wwmm.cam.ac.uk:8080";
+    String server = "wwmm.ch.cam.ac.uk:8080";
+    String collection = "g2";
     
-    String index = "IChI";
-	String query = "C4,";
+    private String index = "ichi";
+	private String query = "C4,";
 
     private org.openscience.cdk.tools.LoggingTool logger;
 
@@ -71,14 +72,22 @@ public class WWMMatrixReader implements ChemObjectReader {
 	 * @param   value   Index of molecule to download (e.g. 'C4,')
 	 */
 	public void setQuery(String index, String value) {
-		this.index = index;
+		this.index = index.toLowerCase();
 		this.query = value;
 	}
     
+	public void setCollection(String collection) {
+		this.collection = collection;
+	}
     
     public ChemObject read(ChemObject object) throws UnsupportedChemObjectException {
         if (object instanceof Molecule) {
-		    return (ChemObject)readMolecule(server, query);
+            try {
+                return (ChemObject)readMolecule();
+            } catch (Exception exc) {
+                logger.error("Error while reading molecule: " + exc.toString());
+                return object;
+            }
 		} else {
 		    throw new UnsupportedChemObjectException("Only supported is Molecule.");
 		}
@@ -86,18 +95,25 @@ public class WWMMatrixReader implements ChemObjectReader {
     
     public static void main(String[] args) throws Exception {
         WWMMatrixReader wwmm = new WWMMatrixReader();
-        if (args.length != 2) {
-            System.out.println("WWMMatrixReader <server> <IChI>");
+        if (args.length != 4) {
+            System.out.println("WWMMatrixReader <server> <collection> <index> <query>");
             System.out.println();
-            System.out.println("   e.g. wwmm.ch.cam.ac.uk:8080 'C4,'");
+            System.out.println("   e.g. wwmm.ch.cam.ac.uk:8080 g2 ichi 'C4,'");
+            System.out.println("   e.g. wwmm.ch.cam.ac.uk:8080 kegg kegg 'C00001'");
             System.exit(1);
         }
         String server = args[0];
-        String ichi = args[1];
-        System.out.println("Server: " + server);
-        System.out.println("IChI  : " + ichi);
+        String coll = args[1];
+        String index = args[2];
+        String query = args[3];
+        System.out.println("Server    : " + server);
+        System.out.println("Collection: " + coll);
+        System.out.println("Index     : " + index);
+        System.out.println("Query     : " + query);
         
-        Molecule m = wwmm.readMolecule(server, ichi);
+        wwmm.setCollection(coll);
+        wwmm.setQuery(index, query);
+        Molecule m = (Molecule)wwmm.read(new Molecule());
         if (!GeometryTools.has2DCoordinates(m)) {
 			StructureDiagramGenerator sdg = new StructureDiagramGenerator();
 			try {
@@ -112,14 +128,31 @@ public class WWMMatrixReader implements ChemObjectReader {
         
         MoleculeListViewer moleculeListViewer = new MoleculeListViewer();
         MoleculeViewer2D mv = new MoleculeViewer2D(m);
-        moleculeListViewer.addStructure(mv, ichi);
+        moleculeListViewer.addStructure(mv, index + "=" + query);
     }
         
-    private Molecule readMolecule(String server, String ichi) throws Exception {
-        String xpath = URLEncoder.encode("//molecule[./identifier/basic='" + ichi + "']");
-        String colname = URLEncoder.encode("//wwmm/g2");
+    /**
+     * This methods reads molecule from the WWMM based on queries where the index 
+     * is <i>ichi</i> or <i>kegg</i>.
+     *
+     * @returns null if the index is not recognized.
+     */
+    private Molecule readMolecule() throws Exception {
+        String xpath = "";
+        if (index.equals("ichi")) {
+            xpath = URLEncoder.encode("//molecule[./identifier/basic='" + query + "']");
+        } else if (index.equals("kegg")) {
+            xpath = URLEncoder.encode("//molecule[./@name='" + query + "' and ./@dictRef='KEGG']");
+        } else {
+            logger.error("Did not recognize index type: " + index);
+            return null;
+        }
+        String colname = URLEncoder.encode("//wwmm/" + this.collection);
+        
+        logger.info("Doing query: " + xpath + " in collection " + colname);
         
         URL url = new URL("http://" + server + "/Bob/QueryXindice");
+        logger.info("Connection to server: " + url.toString());
         URLConnection connection = url.openConnection();
         connection.setDoOutput(true);
         
