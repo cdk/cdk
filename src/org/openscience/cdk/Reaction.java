b02c03a1502b2df111f6c30dbc4f9762961a0f77
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-02-14 17:30:46 +0000

    Adding required classes for reading reaction in CML.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@982 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/Reaction.java
@@ -0,0 +1,103 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: steinbeck@ice.mpg.de, gezelter@maul.chem.nd.edu, egonw@sci.kun.nl
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk;
+
+import javax.vecmath.*;
+
+/**
+ * Represents the idea of a chemical reaction. The reaction consists of 
+ * a set of reactants and a set of products.
+ *
+ * @author     Egon Willighagen <elw38@cam.ac.uk>
+ * @created    2003-02-13
+ * @keyword    reaction
+ */
+public class Reaction extends ChemObject implements Cloneable {
+
+	protected int growArraySize = 2;
+
+    protected Molecule[] reactants;
+    protected int reactantCount;    
+    protected Molecule[] products;    
+    protected int productCount;    
+    
+    /**
+     * Constructs an empty reaction;
+     */
+    public Reaction() {
+        this.reactants = new Molecule[growArraySize];
+        reactantCount = 0;
+        this.products = new Molecule[growArraySize];
+        productCount = 0;
+    }
+    
+    public int getReactantCount() {
+        return reactantCount;
+    }
+    
+    public int getProductCount() {
+        return productCount;
+    }
+
+    public Molecule[] getReactants() {
+        Molecule[] returnReactants = new Molecule[getReactantCount()];
+        System.arraycopy(this.reactants, 0, returnReactants, 0, returnReactants.length);
+        return returnReactants;
+    }
+
+    public Molecule[] getProducts() {
+        Molecule[] returnProducts = new Molecule[getProductCount()];
+        System.arraycopy(this.products, 0, returnProducts, 0, returnProducts.length);
+        return returnProducts;
+    }
+    
+    public void addReactant(Molecule reactant) {
+        if (reactantCount + 1 >= reactants.length) growReactantArray();
+        reactants[reactantCount] = reactant;
+        reactantCount++;
+    }
+    
+    public void addProduct(Molecule product) {
+        if (productCount + 1 >= products.length) growProductArray();
+        products[productCount] = product;
+        productCount++;
+    }
+    
+    protected void growReactantArray() {
+        Molecule[] newReactants = new Molecule[reactants.length + growArraySize];
+        System.arraycopy(reactants, 0, newReactants, 0, reactants.length);
+        reactants = newReactants;
+    }
+    
+    protected void growProductArray() {
+        Molecule[] newProducts = new Molecule[products.length + growArraySize];
+        System.arraycopy(products, 0, newProducts, 0, products.length);
+        reactants = newProducts;
+    }
+}
---
+++ src/org/openscience/cdk/SetOfReactions.java
@@ -0,0 +1,127 @@
+/* $RCSfile$    
+ * $Author$    
+ * $Date$    
+ * $Revision$
+ * 
+ * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: steinbeck@ice.mpg.de, gezelter@maul.chem.nd.edu, egonw@sci.kun.nl
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ */
+
+package org.openscience.cdk;
+
+
+/** 
+ * A set of reactions, for example those taking part in a reaction.
+ *
+ * To retrieve the reactions from the set, there are two options:
+ *
+ * <pre>
+ * Reaction[] reactions = setOfReactions.getReactions();
+ * for (int i=0; i < reactions.length; i++) {
+ *     Reaction reaction = reactions[i];
+ * }
+ * </pre>
+ *
+ * and
+ *
+ * <pre>
+ * for (int i=0; i < setOfReactions.getReactionCount(); i++) {
+ *    Reaction reaction = setOfReactions.getReaction(i);
+ * }
+ * </pre>
+ *
+ * @keyword reaction
+ * @keyword reaction
+ */
+public class SetOfReactions extends ChemObject {
+
+	/**
+	 *  Array of Reactions.
+	 */
+	protected Reaction[] reactions;
+	
+	/**
+	 *  Number of Reactions contained by this container.
+	 */
+	protected int reactionCount;
+
+	/**
+	 *  Amount by which the Reactions array grows when elements are added and
+	 *  the array is not large enough for that. 
+	 */
+	protected int growArraySize = 5;
+
+
+	/**
+	 *  Constructs an empty SetOfReactions.
+	 */
+	public SetOfReactions() {
+		reactionCount = 0;
+		reactions = new Reaction[growArraySize];
+	}
+
+
+	
+	/**
+	 *  Adds an reaction to this container.
+	 *
+	 * @param  reaction  The reaction to be added to this container 
+	 */
+	public void addReaction(Reaction reaction) {
+		if (reactionCount + 1 >= reactions.length) growReactionArray();
+		reactions[reactionCount] = reaction;
+		reactionCount++;
+	}
+
+
+	/**
+	 *  Returns the array of Reactions of this container.
+	 *
+	 * @return    The array of Reactions of this container 
+	 */
+	public Reaction[] getReactions() {
+        Reaction[] result = new Reaction[reactionCount];
+        for (int i=0; i < reactionCount; i++) {
+            result[i] = reactions[i];
+        }
+		return result;
+	}
+	
+	/**
+	 *  Grows the reaction array by a given size.
+	 *
+	 * @see    growArraySize
+	 */
+	protected void growReactionArray() {
+		growArraySize = reactions.length;
+		Reaction[] newreactions = new Reaction[reactions.length + growArraySize];
+		System.arraycopy(reactions, 0, newreactions, 0, reactions.length);
+		reactions = newreactions;
+	}
+	
+
+	/**
+	 * Returns the number of Reactions in this Container.
+	 *
+	 * @return     The number of Reactions in this Container
+	 */
+	public int getReactionCount() {
+		return this.reactionCount;
+	}
+
+}
