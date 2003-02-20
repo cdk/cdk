aba74b9ae5f548a16351463d8fa95b3e8b3b6061
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-02-20 09:54:01 +0000

    XYZWriter added
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1005 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/io/XYZWriter.java
@@ -0,0 +1,130 @@
+/* $RCSfile$
+ * $Author$ 
+ * $Date$
+ * $Revision$
+ * 
+ * Copyright (C) 2002  The Jmol Development Team
+ * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: steinbeck@ice.mpg.de, gezelter@maul.chem.nd.edu, egonw@sci.kun.nl
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ */
+package org.openscience.cdk.io;
+
+import org.openscience.cdk.*;
+import org.openscience.cdk.exception.*;
+import javax.vecmath.Point3d;
+import java.io.Writer;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ *  @author  Bradley A. Smith (bradley@baysmith.com)
+ *  @author  J. Daniel Gezelter
+ *  @author  Egon Willighagen
+ */
+public class XYZWriter implements ChemObjectWriter {
+  
+    static BufferedWriter writer;
+
+    /**
+    * Constructor.
+    * @param cf the ChemFile to dump.
+    * @param out the stream to write the XYZ file to.
+    */
+    public XYZWriter(Writer out) throws IOException {
+        writer = new BufferedWriter(out);
+    }
+
+    /**
+     * Flushes the output and closes this object.
+     */
+    public void close() throws IOException {
+        writer.close();
+    }
+    
+    public void write(ChemObject object) throws UnsupportedChemObjectException {
+        if (object instanceof Molecule) {
+            try {
+                    writeMolecule((Molecule)object);
+            } catch(Exception ex) {
+                System.err.println(ex.getMessage());
+            }
+        } else {
+            throw new UnsupportedChemObjectException("Only supported are ChemFile and Molecule.");
+        }
+    }
+
+    public ChemObject highestSupportedChemObject() {
+        return new Molecule();
+    }
+    
+    /**
+    * writes a single frame in XYZ format to the Writer.
+    * @param cf the ChemFrame to write
+    * @param w the Writer to write it to
+    */
+    public void writeMolecule(Molecule mol) throws IOException {
+        
+        int na = 0;
+        String info = "";
+        String st = "";
+        boolean writecharge = true;
+        
+        try {
+            
+            String s1 = new Integer(mol.getAtomCount()).toString();
+            writer.write(s1, 0, s1.length());
+            writer.newLine();
+            
+            String s2 = null; // FIXME: add some interesting comment
+            if (s2 != null) {
+                writer.write(s2, 0, s2.length());
+            }
+            writer.newLine();
+            
+            // Loop through the atoms and write them out:
+            Atom[] atoms = mol.getAtoms();
+            for (int i = 0; i < atoms.length; i++) {
+                
+                Atom a = atoms[i];
+                st = a.getSymbol();
+                
+                Point3d p3 = a.getPoint3D();
+                if (p3 != null) {
+                    st = st + "\t" + new Double(p3.x).toString() + "\t"
+                            + new Double(p3.y).toString() + "\t"
+                            + new Double(p3.z).toString();
+                }
+                
+                if (writecharge) {
+                    double ct = a.getCharge();
+                    st = st + "\t" + ct;
+                }
+                
+                writer.write(st, 0, st.length());
+                writer.newLine();
+                
+            }
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+}
+
+
