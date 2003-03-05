0abe9629f471b709d9b101667933cce2ab3c6dac
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-03-05 18:22:24 +0000

    Added dirty (but expected to be working) implementation of a PseudoAtom entity.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1104 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/PseudoAtom.java
@@ -0,0 +1,139 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 1997-2003  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: steinbeck@ice.mpg.de, gezelter@maul.chem.nd.edu, egonw@sci.kun.nl
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk;
+
+/**
+ * Represents the idea of a non-chemical atom-like entity, like Me,
+ * R, X, Phe, His, etc.
+ *
+ * <p>This should be replaced by the mechanism explained in RFC #8.
+ *
+ * @see  Atom
+ */
+public class PseudoAtom extends Atom implements Cloneable {
+
+    private String label;
+    
+    /**
+     * Constructs an Atom from a String containing an element symbol.
+     *
+     * @param   label  The String describing the PseudoAtom
+     */
+    public PseudoAtom(String label) {
+        super("H");
+        this.label = label;
+        this.point3D = null;
+        this.point2D = null;
+        // set these default, unchangable values
+        super.setHydrogenCount(0);
+        super.setStereoParity(-1);
+        super.setExactMass(0.0);
+        super.setFormalCharge(0);
+        super.setCharge(0.0);
+    }
+
+    /**
+     * Constructs an Atom from an Element and a Point3D.
+     *
+     * @param   label  The String describing the PseudoAtom
+     * @param   point3D         The 3D coordinates of the atom
+     */
+    public PseudoAtom(String label, javax.vecmath.Point3d point3D) {
+        this(label);
+        this.point3D = point3D;
+    }
+
+    /**
+     * Constructs an Atom from an Element and a Point2D.
+     *
+     * @param   label  The String describing the PseudoAtom
+     * @param   point2D         The Point
+     */
+    public PseudoAtom(String label, javax.vecmath.Point2d point2D) {
+        this(label);
+        this.point2D = point2D;
+    }
+
+    /**
+     * Dummy method: the exact mass is 0, final. 
+     */
+    public void setExactMass(double mass) {
+        // hydrogen count = 0, always
+    }
+
+    /**
+     * Dummy method: the hydrogen count is 0, final. 
+     */
+    public void setHydrogenCount(int hydrogenCount) {
+        // hydrogen count = 0, always
+    }
+
+    /**
+     * Dummy method: the formal charge is 0, final. 
+     */
+    public void setFormalCharge(int charge) {
+        // formal charge = 0, always
+    }
+
+    /**
+     * Dummy method: the partial charge is 0, final. 
+     */
+    public void setCharge(double charge) {
+        // formal charge = 0, always
+    }
+    /**
+     * Dummy method: the stereo parity is undefined, final
+     */
+    public void setStereoParity(int stereoParity) {
+        // this is undefined, always
+    }
+
+    /**
+     * Returns a one line string representation of this Atom.
+     * Methods is conform RFC #9.
+     *
+     * @return  The string representation of this Atom
+     */
+    public String toString() {
+        StringBuffer description = new StringBuffer();
+        description.append("PseudoAtom(");
+        description.append(this.hashCode() + ", ");
+        description.append(getSymbol() + ", ");
+        description.append("2D:[" + getPoint2D() + "], ");
+        description.append("3D:[" + getPoint3D() + "], ");
+        description.append(")");
+        return description.toString();
+    }
+
+}
+
+
+
+
+
