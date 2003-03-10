ed6f8abaeb87ee24944d4943b793c17cebeeddb6
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-03-10 16:04:21 +0000

    Added convenient classes for relaying higher level methods the don't exist for higher level ChemObjects, but that do exist for lower level ChemObjects (only removeAtomAndConnectedElectronContainers(Atom) has been implmented yet)
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1136 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/tools/ReactionManipulator.java
@@ -0,0 +1,56 @@
+/* $RCSfile$
+ * $Author$ 
+ * $Date$
+ * $Revision$
+ * 
+ * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  */
+package org.openscience.cdk.tools;
+
+import org.openscience.cdk.*;
+import org.openscience.cdk.exception.*;
+
+/**
+ * @see org.openscience.cdk.tools.ChemModelManipulator
+ */
+public class ReactionManipulator {
+    
+    public static void removeAtomAndConnectedElectronContainers(Reaction reaction, Atom atom) {
+        Molecule[] reactants = reaction.getReactants();
+        for (int i=0; i<reactants.length; i++) {
+            Molecule mol = reactants[i];
+            if (mol.contains(atom)) {
+                mol.removeAtomAndConnectedElectronContainers(atom);
+            }
+        }
+        Molecule[] products = reaction.getProducts();
+        for (int i=0; i<products.length; i++) {
+            Molecule mol = products[i];
+            if (mol.contains(atom)) {
+                mol.removeAtomAndConnectedElectronContainers(atom);
+            }
+        }
+    }
+    
+}
+
