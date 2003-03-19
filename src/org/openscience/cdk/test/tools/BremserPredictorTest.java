d6b3e3fd68eba7b8dcfc4d1561f9028924783503
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-03-19 09:15:50 +0000

    A new test for the BremserOneSphereHOSECodePredictor
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1175 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/test/tools/BremserPredictorTest.java
@@ -0,0 +1,279 @@
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 1997-2002  The Chemistry Development Kit (CKD) project
+ *
+ *  Contact: steinbeck@ice.mpg.de
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All I ask is that proper credit is given for my work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk.test.tools;
+
+import org.openscience.cdk.*;
+import org.openscience.cdk.tools.*;
+import org.openscience.cdk.io.*;
+import org.openscience.cdk.renderer.*;
+import org.openscience.cdk.layout.*;
+import org.openscience.cdk.templates.*;
+import org.openscience.cdk.geometry.*;
+import org.openscience.cdk.aromaticity.*;
+import org.openscience.cdk.smiles.*;
+
+import java.io.*;
+import javax.vecmath.*;
+import javax.swing.*;
+import javax.swing.tree.*;
+import java.util.*;
+import java.awt.*;
+
+import junit.framework.*;
+
+/**
+ *  Tests the HOSECode genertor. This is *not* a JUnit test class!
+ *
+ *@author     steinbeck
+ *@created    November 16, 2002
+ */
+public class BremserPredictorTest extends TestCase
+{
+	
+	static boolean standAlone = false;
+	
+
+	/**
+	 *  Constructor for the HOSECodeTest object
+	 *
+	 *@param  name  Description of the Parameter
+	 */
+	public BremserPredictorTest(String name) {
+		super(name);
+	}
+
+		/**
+	 *  A unit test suite for JUnit
+	 *
+	 *@return    The test suite
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(BremserPredictorTest.class);
+	}
+
+	
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@return    Description of the Return Value
+	 */
+	public void testConstructor()
+	{
+		BremserOneSphereHOSECodePredictor bp = new BremserOneSphereHOSECodePredictor();
+		assertTrue(bp != null);
+	}
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@return    Description of the Return Value
+	 */
+	public void testPrediction()
+	{
+		String[] data = { 
+     "=C(//)",
+     "=OCC(//)",
+     "CC(//)",
+     "CC(//)",
+     "CCC(//)",
+     "CC(//)",
+     "CC(//)",
+     "CCC(//)",
+     "CCC(//)",
+     "CC(//)",
+     "CC(//)",
+     "CC(//)",
+     "CC(//)",
+     "CCO(//)",
+     "CC(//)",
+     "CCO(//)",
+     "CCO(//)",
+     "CC(//)",
+     "O(//)",
+     "CC(//)",
+     "CCC(//)",
+     "CCC(//)",
+     "CCC(//)"
+		};
+		
+			double[] result = { 
+     112.6,
+     198.6,
+     29.6,
+     29.6,
+     40.1,
+     29.6,
+     29.6,
+     40.1,
+     40.1,
+     29.6,
+     29.6,
+     29.6,
+     29.6,
+     73.1,
+     29.6,
+     73.1,
+     73.1,
+     29.6,
+     54.7,
+     29.6,
+     40.1,
+     40.1,
+     40.1
+		};
+		
+		try{
+			double prediction;
+			BremserOneSphereHOSECodePredictor bp = new BremserOneSphereHOSECodePredictor();
+			for (int f = 0; f < data.length; f++)
+			{
+				prediction = bp.predict(data[f]);
+				//System.out.println("\"" + prediction + "\",");
+				assertTrue(prediction == result[f]);	
+			}
+		}catch(org.openscience.cdk.exception.CDKException exc)
+		{
+			fail("CDKException thrown when trying to predict Shift with BremserOneSphereHOSECodePredictor");	
+		}
+		
+	}
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@return    Description of the Return Value
+	 */
+	public void testGetConfidenceLimit()
+	{
+double[] result = { 
+     28.5,
+     25.7,
+     28.5,
+     34.9,
+     28.5,
+     25.7,
+     25.4,
+     28.5,
+     28.5,
+     14.8,
+     13.3,
+     23.0,
+     34.9,
+     25.7,
+     25.7,
+     28.5,
+     25.7,
+     25.7,
+     13.3,
+     14.4,
+     14.4,
+     8.9,
+     14.8,
+     14.8,
+     13.3,
+     13.3,
+     13.3,
+     14.4,
+     14.4,
+     13.3,
+     14.4,
+     14.4,
+     8.9,
+     14.8,
+     14.8,
+     13.3,
+     13.3,
+     13.3,
+     14.4,
+     14.4,
+     13.3
+		};
+		Molecule molecule = null;
+		try
+		{
+			String filename = "data/BremserPredictionTest.mol";
+			InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+			MDLReader reader = new MDLReader(new InputStreamReader(ins));
+			molecule = (Molecule)reader.read((ChemObject)new Molecule());
+			double prediction;
+			BremserOneSphereHOSECodePredictor bp = new BremserOneSphereHOSECodePredictor();
+			HOSECodeGenerator hcg = new HOSECodeGenerator();
+			String s = null;
+			removeHydrogens(molecule);
+			//System.out.println("Molecule has " + molecule.getAtomCount() + " atoms.");
+			for (int f = 0; f < molecule.getAtomCount(); f++)
+			{
+				s = hcg.getHOSECode(molecule, molecule.getAtomAt(f), 1);
+				prediction = bp.getConfidenceLimit(hcg.makeBremserCompliant(s));
+				//System.out.println("\"" + prediction + "\",");
+				assertTrue(prediction == result[f]);	
+			}
+		}
+		catch (Exception exc)
+		{
+			//System.out.println("failure");
+			fail("CDKException thrown when trying to predict Shift with BremserOneSphereHOSECodePredictor");	
+		}
+		
+	}
+
+	private void removeHydrogens(AtomContainer ac)
+	{
+		Atom atom = null;
+		int f = ac.getAtomCount() - 1;
+		
+		do{
+			atom = ac.getAtomAt(f);
+			if (atom.getSymbol().equals("H"))
+			{
+				ac.removeAtomAndConnectedElectronContainers(atom);
+			}
+			f--;
+		}
+		while(f >= 0);
+	}
+	
+	/**
+	 *  The main program for the HOSECodeTest class
+	 *
+	 *@param  args  The command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		standAlone = true;
+		BremserPredictorTest bpt = new BremserPredictorTest("BremserPredictorTest");
+		//bpt.testConstructor();
+		//bpt.testPrediction();
+		bpt.testGetConfidenceLimit();
+	}
+}
+
