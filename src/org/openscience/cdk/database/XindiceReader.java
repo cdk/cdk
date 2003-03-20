226eb80652540f890c6976cf71ddaa1999be4a44
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-03-20 12:05:07 +0000

    Added framework for Xindice interface
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1183 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/database/XindiceReader.java
@@ -0,0 +1,64 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ * 
+ * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ * 
+ */
+
+package org.openscience.cdk.database;
+
+
+import java.sql.*;
+import java.io.*;
+import org.openscience.cdk.*;
+import org.openscience.cdk.io.*;
+import org.openscience.cdk.exception.*;
+
+/**
+ * @author Yong Zhang <yz237@cam.ac.uk>
+ */
+public class XindiceReader implements ChemObjectReader {
+    
+    public XindiceReader(String collection) {
+        this.con = con;
+    }
+
+    public ChemObject read(ChemObject object) throws CDKException {
+        if (object instanceof Molecule) {
+            return (ChemObject)readMolecule();
+        } else {
+            throw new CDKException("Only supported Molecule.");
+        }
+    }
+    
+    private Molecule readMolecule() throws CDKException{
+        return new Molecule();
+    }
+
+    public void setQuery(String xpath) {
+        this.query = query;
+    }
+    
+}
