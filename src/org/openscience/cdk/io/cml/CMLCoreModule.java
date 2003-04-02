610cd04489785615da5c5f7f96fc14e3b29cfe7d
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-04-02 13:38:12 +0000

    Don't require bond ids
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1209 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/io/cml/CMLCoreModule.java
+++ src/org/openscience/cdk/io/cml/CMLCoreModule.java
@@ -904,7 +904,9 @@ public class CMLCoreModule implements ModuleInterface {
 
             while (orders.hasMoreElements()) {
                 cdo.startObject("Bond");
-                cdo.setObjectProperty("Bond", "id", (String)ids.nextElement());
+                if (ids.hasMoreElements()) {
+                    cdo.setObjectProperty("Bond", "id", (String)ids.nextElement());
+                }
                 cdo.setObjectProperty("Bond", "atom1", 
                                       new Integer(elid.indexOf(
                                                           (String)bar1s.nextElement())).toString());
