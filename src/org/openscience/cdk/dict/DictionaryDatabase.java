9175686c850d5a7b1dc31d844224eed70a276f27
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-04-15 05:55:19 +0000

    Added infrastructure for dictionary references
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1247 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/dict/DictionaryDatabase.java
@@ -0,0 +1,102 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk.dict;
+
+import net.sf.cml.stmml.Dictionary;
+import net.sf.cml.stmml.Entry;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+
+/**
+ * Database of dictionaries listing entries with compounds, fragments
+ * and entities.
+ *
+ * @author     Egon Willighagen
+ * @created    2003-04-06
+ * @keyword    dictionary
+ */
+public class DictionaryDatabase {
+
+    private org.openscience.cdk.tools.LoggingTool logger;
+    
+    private String[] dictionaryNames = {
+        "chemical"
+    };
+    
+    private Hashtable dictionaries;
+
+    public DictionaryDatabase() {
+        logger = new org.openscience.cdk.tools.LoggingTool(this.getClass().getName());
+        
+        // read dictionaries distributed with CDK
+        dictionaries = new Hashtable();
+        for (int i=0; i<dictionaryNames.length; i++) {
+            String name = dictionaryNames[i];
+            dictionaries.put(name, readDatabase("org.openscience.cdk.dict.data." + name + ".xml"));
+            logger.info("Read dictionary: " + name);
+        }
+    }
+
+    private Dictionary readDatabase(String databaseLocator) {
+        Dictionary dictionary = null;
+        try {
+            InputStreamReader reader = new InputStreamReader(
+                this.getClass().getClassLoader().getResourceAsStream(databaseLocator));
+            dictionary = Dictionary.unmarshal(reader);
+        } catch (Exception exception) {
+            dictionary = new Dictionary();
+        }
+        return dictionary;
+    };
+
+    /**
+     * Returns a String[] with the names of the known dictionaries.
+     */
+    public String[] getDictionaryNames() {
+        return dictionaryNames;
+    }
+    
+    /**
+     * Returns a String[] with the id's of all entries in the specified database.
+     */
+    public String[] getDictionaryEntries(String dictionaryName) {
+        Dictionary dictionary = (Dictionary)dictionaries.get(dictionaryName);
+        if (dictionary == null) {
+            return new String[0];
+        } else {
+            // FIXME: dummy method that needs an implementation
+            Entry[] entries = dictionary.getEntry();
+            String[] entryNames = new String[entries.length];
+            for (int i=0; i<entries.length; i++) {
+                entryNames[i] = entries[i].getTerm();
+            }
+            return entryNames;
+        }
+    }
+}
