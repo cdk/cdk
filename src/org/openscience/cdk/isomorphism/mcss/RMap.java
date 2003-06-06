c5d31307e0b1b4ce399db86986e75388704f2123
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-06-06 15:21:10 +0000

    Added an equals method needed for the new atom mapping method in UniversalIsomorphismTester
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1310 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/isomorphism/mcss/RMap.java
+++ src/org/openscience/cdk/isomorphism/mcss/RMap.java
@@ -10,7 +10,7 @@
  *  and Thierry Hanser from IXELIS mail@ixelis.net
  *
  *  IXELIS sarl - Semantic Information Systems
- *  17 rue des Cèdres 67200 Strasbourg, France
+ *  17 rue des Cï¿½dres 67200 Strasbourg, France
  *  Tel/Fax : +33(0)3 88 27 81 39 Email: mail@ixelis.net
  *
  *  CDK Contact: cdk-devel@lists.sf.net
@@ -36,70 +36,78 @@ package org.openscience.cdk.isomorphism.mcss;
  *  An RMap implements the association between an edge (bond) in G1 and an edge
  *  (bond) in G2, G1 and G2 being the compared graphs in a RGraph context.
  *
- *@author     Stephane Werner from IXELIS mail@ixelis.net
- *@created    24 juillet 2002
+ * @author     Stephane Werner from IXELIS mail@ixelis.net
+ * @created    24 juillet 2002
  */
-public class RMap
-{
-	int id1 = 0;
-	int id2 = 0;
+public class RMap {
+  int id1 = 0;
+  int id2 = 0;
 
 
-	/**
-	 *  Constructor for the RMap
-	 *
-	 *@param  id1  number of the edge (bond) in the graphe 1
-	 *@param  id2  number of the edge (bond) in the graphe 2
-	 */
-	public RMap(int id1, int id2)
-	{
-		this.id1 = id1;
-		this.id2 = id2;
-	}
+  /**
+   *  Constructor for the RMap
+   *
+   * @param  id1  number of the edge (bond) in the graphe 1
+   * @param  id2  number of the edge (bond) in the graphe 2
+   */
+  public RMap(int id1, int id2) {
+    this.id1 = id1;
+    this.id2 = id2;
+  }
 
 
-	/**
-	 *  Sets the id1 attribute of the RMap object
-	 *
-	 *@param  id1  The new id1 value
-	 */
-	public void setId1(int id1)
-	{
-		this.id1 = id1;
-	}
+  /**
+   *  Sets the id1 attribute of the RMap object
+   *
+   * @param  id1  The new id1 value
+   */
+  public void setId1(int id1) {
+    this.id1 = id1;
+  }
 
 
-	/**
-	 *  Sets the id2 attribute of the RMap object
-	 *
-	 *@param  id2  The new id2 value
-	 */
-	public void setId2(int id2)
-	{
-		this.id2 = id2;
-	}
+  /**
+   *  Sets the id2 attribute of the RMap object
+   *
+   * @param  id2  The new id2 value
+   */
+  public void setId2(int id2) {
+    this.id2 = id2;
+  }
 
 
-	/**
-	 *  Gets the id1 attribute of the RMap object
-	 *
-	 *@return    The id1 value
-	 */
-	public int getId1()
-	{
-		return id1;
-	}
+  /**
+   *  Gets the id1 attribute of the RMap object
+   *
+   * @return    The id1 value
+   */
+  public int getId1() {
+    return id1;
+  }
 
 
-	/**
-	 *  Gets the id2 attribute of the RMap object
-	 *
-	 *@return    The id2 value
-	 */
-	public int getId2()
-	{
-		return id2;
-	}
+  /**
+   *  Gets the id2 attribute of the RMap object
+   *
+   * @return    The id2 value
+   */
+  public int getId2() {
+    return id2;
+  }
 
+
+  /**
+   *  The equals method.
+   *
+   * @param  o  The object to compare.
+   * @return    true=if both ids equal, else false.
+   */
+  public boolean equals(Object o) {
+    if (((RMap) o).id1 == id1 && ((RMap) o).id2 == id2) {
+      return (true);
+    } else {
+      return (false);
+    }
+  }
 }
 
