e7ac1d240c93aac21bc19861e11a01ffe84c0c81
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-06-12 14:54:03 +0000

    Added Writer that outputs a Gaussian input file which can be feeded to Gaussian 98 (and possibly others). Note that it is the first Reader/Writer takes makes use of the new features of the IO API.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1338 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/io/program/GaussianInputWriter.java
@@ -0,0 +1,187 @@
+/* $RCSfile$
+ * $Author$ 
+ * $Date$
+ * $Revision$
+ * 
+ * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ */
+package org.openscience.cdk.io.program;
+
+import org.openscience.cdk.*;
+import org.openscience.cdk.io.*;
+import org.openscience.cdk.io.setting.*;
+import org.openscience.cdk.exception.CDKException;
+import javax.vecmath.Point3d;
+import java.io.Writer;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Vector;
+
+/**
+ * File writer thats generates input files for Gaussian calculation
+ * jobs. It was tested with Gaussian98.
+ *
+ * @author  Egon Willighagen <egonw@sci.kun.nl>
+ *
+ * @keyword Gaussian (tm), input file
+ */
+public class GaussianInputWriter extends DefaultChemObjectWriter {
+  
+    static BufferedWriter writer;
+
+    IOSetting method;
+    IOSetting basis;
+    IOSetting comment;
+    IOSetting command;
+    BooleanIOSetting shell;
+    
+    /**
+    * Constructs a new writer that produces input files to run a
+    * Gaussian QM job.
+    */
+    public GaussianInputWriter(Writer out) throws IOException {
+        writer = new BufferedWriter(out);
+    }
+
+    public void close() throws IOException {
+        writer.close();
+    }
+    
+    public void write(ChemObject object) throws CDKException {
+        if (object instanceof Molecule) {
+            try {
+                writeMolecule((Molecule)object);
+            } catch(Exception ex) {
+                throw new CDKException("Error while writing Gaussian input file: " + ex.getMessage());
+            }
+        } else {
+            throw new CDKException("GaussianInputWriter only supports output of Molecule classes.");
+        }
+    }
+
+    public ChemObject highestSupportedChemObject() {
+        return new Molecule();
+    }
+    
+    /**
+    * writes a single frame in XYZ format to the Writer.
+    * @param cf the ChemFrame to write
+    * @param w the Writer to write it to
+    */
+    public void writeMolecule(Molecule mol) throws IOException {
+        
+        customizeJob();
+        
+        // write the command line
+        writer.write("# " + method.getSetting() + 
+                     "/" + basis.getSetting() +  
+                     " ");
+        String commandString = command.getSetting();
+        if (commandString.equals("energy calculation")) {
+        } else if (commandString.equals("geometry optimization")) {
+            writer.write("opt");
+        } else if (commandString.equals("IR frequency calculation")) {
+            writer.write("freq");
+        }
+        writer.newLine();
+        
+        // next line is empty
+        writer.newLine();
+        
+        // next line is comment
+        writer.write(comment.getSetting() + "\n");
+        
+        // next line is empty
+        writer.newLine();
+
+        /* next line contains two digits
+         * the first is the total charge
+         * the second is boolean indicating:
+         *   0 = open shell
+         *   1 = closed shell
+         */
+        writer.write("0 "); // FIXME: should write total charge of molecule
+        if (shell.isSet()) {
+            writer.write("0");
+        } else {
+            writer.write("1");
+        }
+        writer.newLine();
+        
+        // then come all the atoms. 
+        // Loop through the atoms and write them out:
+        Atom[] atoms = mol.getAtoms();
+        for (int i = 0; i < atoms.length; i++) {
+            Atom a = atoms[i];
+            String st = a.getSymbol();
+            
+            // export Eucledian coordinates (indicated by the 0)
+            st = st + " 0 ";
+            
+            // export the 3D coordinates
+            Point3d p3 = a.getPoint3D();
+            if (p3 != null) {
+                st = st + " 0 " + new Double(p3.x).toString() + " "
+                        + new Double(p3.y).toString() + " "
+                        + new Double(p3.z).toString();
+            }
+            
+            writer.write(st, 0, st.length());
+            writer.newLine();
+        }
+    }
+    
+    private void customizeJob() {
+        Vector basisOptions = new Vector();
+        basisOptions.add("6-31g");
+        basis = new OptionIOSetting("Basis", IOSetting.MEDIUM,
+          "Which basis set do you want to use?", basisOptions, "6-31g");
+        fireWriterSettingQuestion(basis);
+
+        Vector methodOptions = new Vector();
+        methodOptions.add("b3lyp");
+        methodOptions.add("rhf");
+        method = new OptionIOSetting("Method", IOSetting.MEDIUM,
+          "Which method do you want to use?", methodOptions, "b3lyp");
+        fireWriterSettingQuestion(method);
+        
+        Vector commandOptions = new Vector();
+        commandOptions.add("energy calculation");
+        commandOptions.add("geometry optimization");
+        commandOptions.add("IR frequency calculation");
+        command = new OptionIOSetting("Command", IOSetting.HIGH,
+          "What kind of job do you want to perform?", commandOptions, 
+          "energy calculation");
+        fireWriterSettingQuestion(command);
+        
+        comment = new StringIOSetting("Comment", IOSetting.LOW,
+          "What comment should be put in the file?", 
+          "Generated with Jmol (http://jmol.sf.net/)");
+        fireWriterSettingQuestion(comment);
+        
+        shell = new BooleanIOSetting("OpenShell", IOSetting.MEDIUM,
+          "Should the calculation be open shell?", 
+          "false");
+        fireWriterSettingQuestion(shell);
+    }
+}
+
+
