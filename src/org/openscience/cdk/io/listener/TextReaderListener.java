57891b4831b87275164ec5f0181e85bba3612671
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-06-20 18:08:58 +0000

    Moved listener code into sub package
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1360 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/io/listener/TextReaderListener.java
@@ -0,0 +1,62 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2003  The Jmol Development Team
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ */
+package org.openscience.cdk.io.listener;
+
+import org.openscience.cdk.io.setting.*;
+import org.openscience.cdk.io.ReaderEvent;
+import java.util.EventListener;
+
+/**
+ * Allows monitoring of progress of file reader activities, and
+ * processing of ReaderSetting quesions which are passed to the user
+ * by using the System.out and System.in.
+ *
+ * @author Egon Willighagen <egonw@sci.kun.nl>
+ */
+public class TextReaderListener implements ReaderListener {
+
+    private int level = 0;
+    
+    /**
+     * 0 = ask no questions
+     * 3 = ask all questions
+     */
+    public TextReaderListener(int level) {
+        this.level = level;
+    }
+    
+    public void frameRead(ReaderEvent event) {
+    };
+    
+    public void processReaderSettingQuestion(IOSetting setting){
+        if (setting.getLevel() < this.level) {
+            System.out.println(setting.getQuestion());
+        }
+    };
+  
+}
+
+
+
