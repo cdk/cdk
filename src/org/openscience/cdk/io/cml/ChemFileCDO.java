e4173f38d45a73c742763b4f272c3d2ae513ae14
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-07-01 07:11:53 +0000

    Moved file in correct package
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1398 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/io/cml/ChemFileCDO.java
+++ src/org/openscience/cdk/io/cml/ChemFileCDO.java
@@ -21,10 +21,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
  */
-package org.openscience.cdk.io;
+package org.openscience.cdk.io.cml;
 
 import org.openscience.cdk.*;
-import org.openscience.cdk.io.cml.*;
+import org.openscience.cdk.io.*;
 import org.openscience.cdk.io.cml.cdopi.*;
 import java.util.*;
 
