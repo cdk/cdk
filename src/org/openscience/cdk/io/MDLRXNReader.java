8e605de2b1f0477b265efbf7342cfcbfaec36d82
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-08-07 22:06:14 +0000

    Added implementation
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1543 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/io/MDLRXNReader.java
+++ src/org/openscience/cdk/io/MDLRXNReader.java
@@ -35,7 +35,7 @@ import java.util.*;
 import javax.vecmath.*;
 
 /**
- *  Reads a molecule from an MDL RXN file.
+ * Reads a molecule from an MDL RXN file.
  *
  * @author     Egon Willighagen
  * @created    2003-07-24
@@ -74,8 +74,14 @@ public class MDLRXNReader extends DefaultChemObjectReader {
      public ChemObject read(ChemObject object) throws CDKException {
          if (object instanceof Reaction) {
              return (ChemObject) readReaction();
+         } else if (object instanceof ChemModel) {
+             ChemModel model = new ChemModel();
+             SetOfReactions reactionSet = new SetOfReactions();
+             reactionSet.addReaction(readReaction());
+             model.setSetOfReactions(reactionSet);
+             return model;
          } else {
-             throw new CDKException("Only supported is Reaction.");
+             throw new CDKException("Only supported are Reaction and ChemModel.");
          }
      }
 
@@ -87,6 +93,90 @@ public class MDLRXNReader extends DefaultChemObjectReader {
      */
     private Reaction readReaction() throws CDKException {
         Reaction reaction = new Reaction();
+        try {
+            String firstLine = input.readLine(); // should be $RXN
+            String secondLine = input.readLine();
+            String thirdLine = input.readLine();
+            String fourthLine = input.readLine();
+        } catch (IOException exception) {
+            logger.debug(exception);
+            throw new CDKException("Error while reading header of RXN file");
+        }
+
+        int reactantCount = 0;
+        int productCount = 0;
+        try {
+            String countsLine = input.readLine();
+            /* this line contains the number of reactants
+               and products */
+            StringTokenizer tokenizer = new StringTokenizer(countsLine);
+            reactantCount = Integer.valueOf(tokenizer.nextToken()).intValue();
+            logger.info("Expecting " + reactantCount + " reactants in file");
+            productCount = Integer.valueOf(tokenizer.nextToken()).intValue();
+            logger.info("Expecting " + productCount + " products in file");            
+        } catch (Exception exception) {
+            logger.debug(exception);
+            throw new CDKException("Error while counts line of RXN file");
+        }
+        
+        // now read the reactants
+        try {
+            for (int i=1; i<=reactantCount; i++) {
+                StringBuffer molFile = new StringBuffer();
+                String announceMDLFileLine = input.readLine();
+                String molFileLine = "";
+                do {
+                    molFileLine = input.readLine();
+                    molFile.append(molFileLine);
+                    molFile.append("\n");
+                } while (!molFileLine.equals("M  END"));
+                
+                // read MDL molfile content
+                MDLReader reader = new MDLReader(
+                  new StringReader(molFile.toString()));
+                Molecule reactant = (Molecule)reader.read(
+                  new Molecule());
+                  
+                // add reactant
+                reaction.addReactant(reactant);
+            }
+        } catch (CDKException exception) {
+            // rethrow exception from MDLReader
+            throw exception;
+        } catch (Exception exception) {
+            logger.debug(exception);
+            throw new CDKException("Error while reading reactant");
+        }
+        
+        // now read the products
+        try {
+            for (int i=1; i<=productCount; i++) {
+                StringBuffer molFile = new StringBuffer();
+                String announceMDLFileLine = input.readLine();
+                String molFileLine = "";
+                do {
+                    molFileLine = input.readLine();
+                    molFile.append(molFileLine);
+                    molFile.append("\n");
+                } while (!molFileLine.equals("M  END"));
+                
+                // read MDL molfile content
+                MDLReader reader = new MDLReader(
+                  new StringReader(molFile.toString()));
+                Molecule product = (Molecule)reader.read(
+                  new Molecule());
+                  
+                // add reactant
+                reaction.addProduct(product);
+            }
+        } catch (CDKException exception) {
+            // rethrow exception from MDLReader
+            throw exception;
+        } catch (Exception exception) {
+            logger.debug(exception);
+            throw new CDKException("Error while reading products");
+        }
+        
         return reaction;
     }
     
