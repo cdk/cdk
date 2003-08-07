54242cd9dd402c2c0ba927589abd0874cc2b41e9
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-08-07 16:28:57 +0000

    Fixed HydrogenPlacerTest for single atom. Added test case for whole molecule.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1535 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/layout/HydrogenPlacerTest.java
+++ src/org/openscience/cdk/test/layout/HydrogenPlacerTest.java
@@ -30,6 +30,7 @@ import org.openscience.cdk.layout.*;
 import org.openscience.cdk.renderer.*;
 import org.openscience.cdk.smiles.*;
 import org.openscience.cdk.templates.*;
+import org.openscience.cdk.tools.*;
 import java.util.*;
 import java.io.*;
 import java.net.URL;
@@ -57,13 +58,13 @@ public class HydrogenPlacerTest extends TestCase {
         Point2d carbonPos = new Point2d(0.0,0.0);
         carbon.setPoint2D(carbonPos);
         Atom h1 = new Atom("H");
-        Point2d h1Pos = new Point2d(1.0,0.0);
-        h1.setPoint2D(h1Pos);
         Atom h2 = new Atom("H");
-        Point2d h2Pos = new Point2d(-1.0,0.0);
-        h2.setPoint2D(h2Pos);
         Atom cl1 = new Atom("Cl");
+        Point2d cl1Pos = new Point2d(0.0,-1.0);
+        cl1.setPoint2D(cl1Pos);
         Atom cl2 = new Atom("Cl");
+        Point2d cl2Pos = new Point2d(-1.0,0.0);
+        cl2.setPoint2D(cl2Pos);
         dichloromethane.addAtom(carbon);
         dichloromethane.addAtom(h1);
         dichloromethane.addAtom(h2);
@@ -76,11 +77,11 @@ public class HydrogenPlacerTest extends TestCase {
 
         // generate new coords
         HydrogenPlacer.placeHydrogens2D(dichloromethane, carbon);
-        
+        if (standAlone) MoleculeViewer2D.display(dichloromethane, false);
         // check that previously set coordinates are kept
         assertEquals(carbonPos, carbon.getPoint2D(), 0.01);
-        assertEquals(h1Pos, h1.getPoint2D(), 0.01);
-        assertEquals(h2Pos, h2.getPoint2D(), 0.01);
+        assertEquals(cl1Pos, cl1.getPoint2D(), 0.01);
+        assertEquals(cl2Pos, cl2.getPoint2D(), 0.01);
     }
     
     private void assertEquals(Point2d p1, Point2d p2, double error) throws Exception {
@@ -88,12 +89,37 @@ public class HydrogenPlacerTest extends TestCase {
         assertEquals(p1.y, p2.y, error);
     }
     
-    	public static void main(String[] args)
+    
+    /* This one tests adding hydrogens to all atoms of a molecule and doing the layout for them
+    */
+	public void testFullMolecule2D()
+	{
+                String filename = "data/mdl/hydrogenplacertest.mol";
+		InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+		try {
+		    MDLReader reader = new MDLReader(new InputStreamReader(ins));
+		    ChemFile chemFile = (ChemFile)reader.read((ChemObject)new ChemFile());
+		    ChemSequence seq = chemFile.getChemSequence(0);
+		    ChemModel model = seq.getChemModel(0);
+		    Molecule mol = model.getSetOfMolecules().getMolecule(0);
+		    HydrogenAdder ha = new HydrogenAdder();
+		    ha.addExplicitHydrogensToSatisfyValency(mol);
+		    HydrogenPlacer.placeHydrogens2D(mol);
+		    MoleculeViewer2D.display(mol, false);
+		} catch (Exception e) 
+		{
+		    e.printStackTrace();
+		    fail(e.toString());
+		}
+	}
+
+       	public static void main(String[] args)
 	{
 		try{
 			HydrogenPlacerTest hpt = new HydrogenPlacerTest("HydrogenPlacerTest");
 			hpt.standAlone = true;
-			hpt.testplaceHydrogens2D();			
+			//hpt.testplaceHydrogens2D();
+			hpt.testFullMolecule2D();			
 		}
 		catch(Exception exc)
 		{
@@ -101,3 +127,4 @@ public class HydrogenPlacerTest extends TestCase {
 		}
 	}
 }
+
