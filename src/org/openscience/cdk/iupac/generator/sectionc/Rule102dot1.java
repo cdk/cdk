e5a8aaf6a6ba26ca0c54a458d4d9ca3256935fa7
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-08-07 15:48:40 +0000

    Updated headers
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1532 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/iupac/generator/sectionc/Rule102dot1.java
+++ src/org/openscience/cdk/iupac/generator/sectionc/Rule102dot1.java
@@ -3,9 +3,9 @@
  * $Date$    
  * $Revision$
  *
- * Copyright (C) 2002  The Chemistry Development Kit (CDK) project
+ * Copyright (C) 2002-2003  The Chemistry Development Kit (CDK) project
  *
- * Contact: steinbeck@ice.mpg.de, gezelter@maul.chem.nd.edu, egonw@sci.kun.nl
+ * Contact: cdk-devel@lists.sourceforge.net
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
