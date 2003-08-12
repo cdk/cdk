39758fd82f8454d7b4deecd6d135ce9b08247a82
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-08-12 13:35:02 +0000

    Added class for associations, e.g. hydrogen bond, implementing RFC #22
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1582 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/test/AssociationTest.java
@@ -0,0 +1,101 @@
+/* $RCSfile$
+ * $Author$    
+ * $Date$    
+ * $Revision$
+ * 
+ * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ * 
+ */
+package org.openscience.cdk.test;
+
+import org.openscience.cdk.*;
+import java.util.*;
+import junit.framework.*;
+import javax.vecmath.*;
+
+/**
+ * Checks the functionality of the Association class.
+ *
+ * @see org.openscience.cdk.Association
+ */
+public class AssociationTest extends TestCase {
+
+    public AssociationTest(String name) {
+        super(name);
+    }
+
+    public void setUp() {}
+
+    public static Test suite() {
+        return new TestSuite(AssociationTest.class);
+    }
+    
+    public void testAssociation() {
+        Association association = new Association();
+        assertEquals(0, association.getElectronCount());
+        assertEquals(0, association.getAtomCount());
+    }
+    
+    public void testAssociation_Atom_Atom() {
+        Association association = new Association(new Atom("C"), new Atom("C"));
+        assertEquals(0, association.getElectronCount());
+        assertEquals(2, association.getAtomCount());
+    }
+    
+    /** Test for RFC #9 */
+    public void testToString() {
+        Association association = new Association();
+        String description = association.toString();
+        for (int i=0; i< description.length(); i++) {
+            assertTrue(description.charAt(i) != '\n');
+            assertTrue(description.charAt(i) != '\r');
+        }
+    }
+
+    public void testContains() {
+        Atom c = new Atom("C");
+        Atom o = new Atom("O");
+        
+        Association association = new Association(c, o);
+        
+        assertTrue(association.contains(c));
+        assertTrue(association.contains(o));
+    }
+    
+    public void testGetAtomCount() {
+        Atom c = new Atom("C");
+        Atom o = new Atom("O");
+        
+        Association association = new Association(c, o);
+        
+        assertEquals(2, association.getAtomCount());
+    }
+    
+    public void testGetAtoms() {
+        Atom c = new Atom("C");
+        Atom o = new Atom("O");
+        
+        Association association = new Association(c, o);
+        
+        Atom[] atoms = association.getAtoms();
+        assertEquals(2, atoms.length);
+        assertNotNull(atoms[0]);
+        assertNotNull(atoms[1]);
+    }
+}
