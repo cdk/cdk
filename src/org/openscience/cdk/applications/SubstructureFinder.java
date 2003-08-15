94356b1548491b736c00afb3d8e530e419ba94a7
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-08-15 07:43:23 +0000

    Added two new command line tools
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1618 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/applications/SubstructureFinder.java
@@ -0,0 +1,83 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk.applications;
+
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.smiles.*;
+import org.openscience.cdk.io.*;
+import org.openscience.cdk.isomorphism.mcss.*;
+import freeware.PrintfFormat;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Vector;
+import java.io.*;
+
+/**
+ * Command line utility that will generate fingerprints for a set of files.
+ *
+ * @author  Egon Willighagen
+ * @created 2003-08-14
+ */
+public class SubstructureFinder {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("syntax: SubstructureFinder <SMILES> <file> <file2> ...");
+            System.exit(0);
+        }
+
+        SmilesParser sp = new SmilesParser();
+        Molecule substructure = null;
+        try {
+            substructure = sp.parseSmiles(args[0]);
+        } catch (Exception exc) {
+            System.err.println("Problem parsing SMILES: " + args[0]); 
+            System.err.println("Error: " + exc.toString());
+            System.exit(-1);
+        }
+        
+        for (int i=1; i<args.length; i++) {
+            String ifilename = args[i];
+            try {
+                File input = new File(ifilename);
+                if (!input.isDirectory()) {
+                    ChemObjectReader reader = new ReaderFactory().createReader(new FileReader(input));
+                    if (reader.accepts(new Molecule())) {
+                        Molecule molecule = (Molecule)reader.read(new Molecule());
+                        if (molecule != null) {
+                            boolean matches = RTools.isSubgraph(molecule, substructure);
+                            if (matches) {
+                                System.out.println(ifilename + ": matches!");
+                            }
+                        }
+                    }
+                }
+            } catch (Exception exception) {
+                System.err.println(ifilename + ": error=");
+                exception.printStackTrace();
+            }
+        }
+    }
+
+}
