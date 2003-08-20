12a5ff448af882e361f8504dabe0ca8cda211604
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-08-20 07:46:56 +0000

    Added more helper methods for working with Crystal unit cells
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1680 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/geometry/CrystalGeometryTools.java
+++ src/org/openscience/cdk/geometry/CrystalGeometryTools.java
@@ -29,15 +29,63 @@ package org.openscience.cdk.geometry;
 
 import javax.vecmath.*;
 import org.openscience.cdk.*;
+import org.openscience.cdk.exception.*;
 import java.awt.Dimension;
 import java.util.Vector;
 
 /**
  * A set of static methods for working with crystal coordinates.
  *
+ * @author  Egon Willighagen <egonw@sci.kun.nl>
+ *
  * @keyword fractional coordinates, crystal
  */
 public class CrystalGeometryTools {
+
+    /**
+     * Inverts three cell axes.
+     *
+     * @return         a 3x3 matrix with the three cartesian vectors representing
+     *                 the unit cell axes. The a axis is the first row.
+     */
+    public static double[][] calcInvertedAxes(double[] a, double[] b, double[] c) {
+         double det = a[0]*b[1]*c[2] -
+                      a[0]*b[2]*c[1] -
+                      a[1]*b[0]*c[2] +
+                      a[1]*b[2]*c[0] +
+                      a[2]*b[0]*c[1] -
+                      a[2]*b[1]*c[0];
+         double invaxis[][] = new double[3][3];
+         invaxis[0][0] = (b[1]*c[2] - b[2]*c[1])/det;
+         invaxis[0][1] = (b[2]*c[0] - b[0]*c[2])/det;
+         invaxis[0][2] = (b[0]*c[1] - b[1]*c[0])/det;
+
+         invaxis[1][0] = (a[2]*c[1] - a[1]*c[2])/det;
+         invaxis[1][1] = (a[0]*c[2] - a[2]*c[0])/det;
+         invaxis[1][2] = (a[1]*c[0] - a[0]*c[1])/det;
+
+         invaxis[2][0] = (a[1]*b[2] - a[2]*b[1])/det;
+         invaxis[2][1] = (a[2]*b[0] - a[0]*b[2])/det;
+         invaxis[2][2] = (a[0]*b[1] - a[1]*b[0])/det;
+         return invaxis;
+    }
+
+    /**
+     * Converts real coordinate (x,y,z) to a fractional coordinates
+     * (xf, yf, zf).
+     */
+    public static double[] cartesianToFractional(double[] a, double[] b, double[] c,
+                                                 double[] cart) {
+         double[] fractCoords = new double[3];
+         double[][] invaxis = calcInvertedAxes(a,b,c);
+         fractCoords[0] = invaxis[0][0]*cart[0] + invaxis[0][1]*cart[1] +
+                          invaxis[0][2]*cart[2];
+         fractCoords[1] = invaxis[1][0]*cart[0] + invaxis[1][1]*cart[1] +
+                          invaxis[1][2]*cart[2];
+         fractCoords[2] = invaxis[2][0]*cart[0] + invaxis[2][1]*cart[1] +
+                          invaxis[2][2]*cart[2];
+         return fractCoords;
+    };
 
     /**
      * Method that transforms fractional coordinates into cartesian coordinates.
@@ -51,6 +99,8 @@ public class CrystalGeometryTools {
      *
      * @keyword     cartesian coordinates
      * @keyword     fractional coordinates
+     *
+     * @see #cartesianToFractional(double[], double[], double[], double[])
      */
     public static double[] fractionalToCartesian(double[] a, double[] b, double[] c,
                                                  double[] frac) {
@@ -62,7 +112,7 @@ public class CrystalGeometryTools {
     }
     
     /**
-     * Calculate cartesian vectors for unit cell axes from axes lengths and angles
+     * Calculates cartesian vectors for unit cell axes from axes lengths and angles
      * between axes.
      *
      * <p>To calculate cartesian coordinates, it places the a axis on the x axes,
@@ -85,13 +135,6 @@ public class CrystalGeometryTools {
     public static double[][] notionalToCartesian(double alength, double blength,
                                                  double clength, double alpha,
                                                  double beta, double gamma) {
-        System.out.println("Creating cartesian from:");
-        System.out.println("  a: " + alength);
-        System.out.println("  b: " + blength);
-        System.out.println("  c: " + blength);
-        System.out.println("  alpha: " + alpha);
-        System.out.println("  beta: " + beta);
-        System.out.println("  gamma: " + gamma);
         double[][] axes = new double[3][3];
         
         /* 1. align the a axis with x axis */
@@ -121,25 +164,128 @@ public class CrystalGeometryTools {
                              cosbeta*cosbeta -
                              cosgamma*cosgamma +
                              2.0*cosalpha*cosbeta*cosgamma);
-        System.out.println("Volume: " + V);
-        System.out.println("    a*b*c = " + alength * blength * clength);
-        System.out.println("    sqrt() = " + (1.0 - cosalpha*cosalpha -
-                             cosbeta*cosbeta -
-                             cosgamma*cosgamma +
-                             2.0*cosalpha*cosbeta*cosgamma));
-        System.out.println("    cosalpha^2 = " + cosalpha*cosalpha);
-        System.out.println("    cosbeta^2 = " +cosbeta*cosbeta);
-        System.out.println("    cosgamma^2 = " +cosgamma*cosgamma);
-        System.out.println("    alp*bet*gam = " + cosalpha*cosbeta*cosgamma);
-        System.out.println("singamma: " + singamma);
         axes[2][0] = clength*cosbeta;   // cx
-        axes[2][1] = clength*(cosalpha- // cy
-             cosbeta*cosgamma)/singamma;
-        axes[2][2] = V/(alength*blength
-             *singamma);                // cz
+        axes[2][1] = clength*(cosalpha-cosbeta*cosgamma)/singamma;
+        axes[2][2] = V/(alength*blength*singamma);                // cz
 
         return axes;
     }
+    
+    /**
+     * Calculates notional coordinates for unit cell axes from cartesian
+     * axes coordinates.
+     *
+     * <p>The array that is returned contains the lengths of a, b and c,
+     * and the angles alpha, beta and gamma (in this order).
+     *
+     * @param a    length of the a axis
+     * @param b    length of the b axis
+     * @param c    length of the c axis
+     * @return     an array of length 6 with a,b,c,alpha,beta and gamma
+     *
+     * @keyword  notional coordinates
+     */
+    public static double[] cartesianToNotional(double[] a, double[] b, double[] c) {
+        double[] notionalCoords = new double[6];
+        notionalCoords[0] = calcAxisLength(a);
+        notionalCoords[1] = calcAxisLength(b);
+        notionalCoords[2] = calcAxisLength(c);
+        notionalCoords[3] = calcAxesAngle(b,c);
+        notionalCoords[4] = calcAxesAngle(a,c);
+        notionalCoords[5] = calcAxesAngle(a,b);
+        return notionalCoords;
+    }
+                               
+    /**
+     * Converts the cell into a P1 cell.
+     * The function assumes that unit cell axes are properly set.
+     *
+     * <p>Recognized space group strings:
+     *   "P1","P 2_1 2_1 2_1"
+     *
+     * <p>This function assumes fractional coordinates, which the
+     * Crystal does not provide by itself. It's broken!
+     *
+     * @return The Crystal clone with P1 space group.
+     *
+     * @deprecated
+     */
+    public static Crystal convertToP1Cell(Crystal crystal) throws CDKException {
+        Crystal result = (Crystal)crystal.clone();
+        if ("P 2_1 2_1 2_1".equals(crystal.getSpaceGroup())) {
+            for (int i =0; i < crystal.getAtomCount(); i++) {
+                Atom atom = crystal.getAtomAt(i);
+                /* symmetry operations:
+
+                identity (skipped) :   x      y      z
+                                        -x+0.5 -y      z+0.5   I
+                                        -x      y+0.5 -z+0.5   II
+                                        x+0.5 -y+0.5 -z        III
+                */
+
+                // do not take into account moving into unit cell
+                Point3d point = atom.getPoint3D();
+
+                if (point != null) {
+                    double[] a = crystal.getA();
+                    double[] b = crystal.getB();
+                    double[] c = crystal.getC();
+                    // point I
+                    Point3d newPoint = new Point3d();
+                    newPoint.x = -1.0*point.x + 0.5*(a[0] + b[0] + c[0]);
+                    newPoint.y = -1.0*point.y;
+                    newPoint.z =      point.z + 0.5*(a[3] + b[3] + c[3]);
+                    Atom syma = (Atom)atom.clone();
+                    syma.setPoint3D(newPoint);
+                    result.addAtom(syma);
+
+                    // point II
+                    newPoint.x = -1.0*point.x + 0.5*(a[0] + b[0] + c[0]);
+                    newPoint.y = -1.0*point.y;
+                    newPoint.z =      point.z + 0.5*(a[3] + b[3] + c[3]);
+                    syma = (Atom)atom.clone();
+                    syma.setPoint3D(newPoint);
+                    result.addAtom(syma);
+
+                    // point III
+                    newPoint.x = -1.0*point.x + 0.5*(a[0] + b[0] + c[0]);
+                    newPoint.y = -1.0*point.y;
+                    newPoint.z =      point.z + 0.5*(a[3] + b[3] + c[3]);
+                    syma = (Atom)atom.clone();
+                    syma.setPoint3D(newPoint);
+                    result.addAtom(syma);
+                } else {
+                    Atom syma = (Atom)atom.clone();
+                    result.addAtom(syma);
+                    syma = (Atom)atom.clone();
+                    result.addAtom(syma);
+                    syma = (Atom)atom.clone();
+                    result.addAtom(syma);
+                }
+            }
+        } else if ("P 2_1 2_1 2_1".equals(crystal.getSpaceGroup())) {
+            // no transformation needed
+        } else {
+            throw new CDKException("This given spacegroup is not supported.");
+        }
+        return result;
+    }
+    
+    /**
+     * Calculates the length of a cell axis.
+     */
+    public static double calcAxisLength(double[] a) {
+        return Math.sqrt(Math.pow(a[0],2.0) + Math.pow(a[1],2.0) + Math.pow(a[2],2.0));
+    };
+
+    /**
+     * Calculates the angle between two axes.
+     */
+    public static double calcAxesAngle(double a[], double b[]) {
+        return Math.toDegrees(Math.acos((a[0]*b[0]+a[1]*b[1]+a[2]*b[2])/
+            (calcAxisLength(a)*calcAxisLength(b))));
+    };
+
 }
 
 
--- src/org/openscience/cdk/test/geometry/CrystalGeometryToolsTest.java
+++ src/org/openscience/cdk/test/geometry/CrystalGeometryToolsTest.java
@@ -24,7 +24,7 @@
 package org.openscience.cdk.test.geometry;
 
 import org.openscience.cdk.*;
-import org.openscience.cdk.tools.*;
+import org.openscience.cdk.geometry.*;
 
 import junit.framework.*;
 
@@ -44,6 +44,147 @@ public class CrystalGeometryToolsTest extends TestCase {
         TestSuite suite = new TestSuite(CrystalGeometryToolsTest.class);
         return suite;
     }
+    
+    public void testNotionalToCartesian() {
+        double[][] cardAxes = CrystalGeometryTools.notionalToCartesian(
+            1.0, 2.0, 3.0, 90.0, 90.0, 90.0
+        );
+        // the a axis
+        assertEquals(1.0, cardAxes[0][0], 0.001);
+        assertEquals(0.0, cardAxes[0][1], 0.001);
+        assertEquals(0.0, cardAxes[0][2], 0.001);
+        // the b axis
+        assertEquals(0.0, cardAxes[1][0], 0.001);
+        assertEquals(2.0, cardAxes[1][1], 0.001);
+        assertEquals(0.0, cardAxes[1][2], 0.001);
+        // the c axis
+        assertEquals(0.0, cardAxes[2][0], 0.001);
+        assertEquals(0.0, cardAxes[2][1], 0.001);
+        assertEquals(3.0, cardAxes[2][2], 0.001);
+    }
 
+    public void testCartesianToNotional() {
+        double[] a = {1.0, 0.0, 0.0};
+        double[] b = {0.0, 2.0, 0.0};
+        double[] c = {0.0, 0.0, 3.0};
+        double[] notionalCoords = CrystalGeometryTools.cartesianToNotional(
+            a,b,c
+        );
+        assertEquals(1.0, notionalCoords[0], 0.001);
+        assertEquals(2.0, notionalCoords[1], 0.001);
+        assertEquals(3.0, notionalCoords[2], 0.001);
+        assertEquals(90.0, notionalCoords[3], 0.001);
+        assertEquals(90.0, notionalCoords[4], 0.001);
+        assertEquals(90.0, notionalCoords[5], 0.001);
+    }
+    
+    public void testFractionalToCartesian() {
+        double[] a = {1.0, 0.0, 0.0};
+        double[] b = {0.0, 2.0, 0.0};
+        double[] c = {0.0, 0.0, 3.0};
+        double[] fractCoords = {0.25, 0.50, 0.75};
+        double[] cartCoords = CrystalGeometryTools.fractionalToCartesian(
+            a, b, c, fractCoords
+        );
+        assertEquals(0.25, cartCoords[0], 0.001);
+        assertEquals(1.0, cartCoords[1], 0.001);
+        assertEquals(2.25, cartCoords[2], 0.001);
+    }
+
+    public void testCartesianToFractional() {
+        double[] a = {1.0, 0.0, 0.0};
+        double[] b = {0.0, 2.0, 0.0};
+        double[] c = {0.0, 0.0, 3.0};
+        double[] cartCoords = {0.25, 1.0, 2.25};
+        double[] fractCoords = CrystalGeometryTools.cartesianToFractional(
+            a, b, c, cartCoords
+        );
+        assertEquals(0.25, fractCoords[0], 0.001);
+        assertEquals(0.50, fractCoords[1], 0.001);
+        assertEquals(0.75, fractCoords[2], 0.001);
+    }
+
+    public void testCalcAxisLength() {
+        double[] a = {1.0, 1.0, 1.0};
+        double length = CrystalGeometryTools.calcAxisLength(a);
+        assertEquals(Math.sqrt(3.0), length, 0.001);
+    }
+
+    public void testCalcAngle() {
+        double[] b = {0.0, 2.0, 0.0};
+        double[] c = {0.0, 0.0, 3.0};
+        double angle = CrystalGeometryTools.calcAxesAngle(b,c);
+        assertEquals(90.0, angle, 0.001);
+    }
+    
+    public void testRoundTripUnitCellCartesianCoordinates() {
+        double[] a = {3.4, 7.6, 5.5};
+        double[] b = {2.8, 4.0, 6.3};
+        double[] c = {1.9, 3.9, 9.1};
+        double[] notionalCoords = CrystalGeometryTools.cartesianToNotional(
+            a,b,c
+        );
+        double[][] cardAxes = CrystalGeometryTools.notionalToCartesian(
+            notionalCoords[0], notionalCoords[1], notionalCoords[2],
+            notionalCoords[3], notionalCoords[4], notionalCoords[5]
+        );
+        // the a axis
+        assertEquals(3.4, cardAxes[0][0], 0.001);
+        assertEquals(7.6, cardAxes[0][1], 0.001);
+        assertEquals(5.5, cardAxes[0][2], 0.001);
+        // the b axis
+        assertEquals(2.8, cardAxes[1][0], 0.001);
+        assertEquals(4.0, cardAxes[1][1], 0.001);
+        assertEquals(6.3, cardAxes[1][2], 0.001);
+        // the c axis
+        assertEquals(1.9, cardAxes[2][0], 0.001);
+        assertEquals(3.9, cardAxes[2][1], 0.001);
+        assertEquals(9.1, cardAxes[2][2], 0.001);
+    }
+    
+    public void testRoundTripUnitCellNotionalCoordinates() {
+        double[][] cardAxes = CrystalGeometryTools.notionalToCartesian(
+            7.6, 3.9, 10.3, 67.0, 91.2, 110.5
+        );
+        double[] a = {cardAxes[0][0], cardAxes[0][1], cardAxes[0][2]};
+        double[] b = {cardAxes[1][0], cardAxes[1][1], cardAxes[1][2]};
+        double[] c = {cardAxes[2][0], cardAxes[2][1], cardAxes[2][2]};
+        double[] notionalCoords = CrystalGeometryTools.cartesianToNotional(
+            a,b,c
+        );
+        assertEquals(7.6, notionalCoords[0], 0.001);
+        assertEquals(3.9, notionalCoords[1], 0.001);
+        assertEquals(10.3, notionalCoords[2], 0.001);
+        assertEquals(67.0, notionalCoords[3], 0.001);
+        assertEquals(91.2, notionalCoords[4], 0.001);
+        assertEquals(110.5, notionalCoords[5], 0.001);
+    }
+    
+    public void testCalcInvertedAxes() {
+        double[] a = {3.4, 7.6, 5.5};
+        double[] b = {2.8, 4.0, 6.3};
+        double[] c = {1.9, 3.9, 9.1};
+        double[][] invertedAxes = CrystalGeometryTools.calcInvertedAxes(
+            a,b,c
+        );
+        double[] a2 = {invertedAxes[0][0], invertedAxes[0][1], invertedAxes[0][2]};
+        double[] b2 = {invertedAxes[1][0], invertedAxes[1][1], invertedAxes[1][2]};
+        double[] c2 = {invertedAxes[2][0], invertedAxes[2][1], invertedAxes[2][2]};
+        double[][] doubleAxes = CrystalGeometryTools.calcInvertedAxes(
+            a2,b2,c2
+        );
+        double[] a3 = {doubleAxes[0][0], doubleAxes[0][1], doubleAxes[0][2]};
+        double[] b3 = {doubleAxes[1][0], doubleAxes[1][1], doubleAxes[1][2]};
+        double[] c3 = {doubleAxes[2][0], doubleAxes[2][1], doubleAxes[2][2]};
+        assertEquals(a[0], a3[0], 0.001);
+        assertEquals(a[1], a3[1], 0.001);
+        assertEquals(a[2], a3[2], 0.001);
+        assertEquals(b[0], b3[0], 0.001);
+        assertEquals(b[1], b3[1], 0.001);
+        assertEquals(b[2], b3[2], 0.001);
+        assertEquals(c[0], c3[0], 0.001);
+        assertEquals(c[1], c3[1], 0.001);
+        assertEquals(c[2], c3[2], 0.001);
+    }
 }
 
