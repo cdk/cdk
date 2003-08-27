fcb5aa85c5e66432f44801ac262a3057766c187b
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-08-27 17:42:04 +0000

    Added two missing manipulator classes. getAllInOneContainer() can now be used on any ChemObject
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1766 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/tools/ChemFileManipulator.java
@@ -0,0 +1,56 @@
+/* $RCSfile$
+ * $Author$ 
+ * $Date$
+ * $Revision$
+ * 
+ * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  */
+package org.openscience.cdk.tools;
+
+import org.openscience.cdk.*;
+import org.openscience.cdk.exception.*;
+
+/**
+ * Class with convenience methods that provide methods from
+ * methods from ChemObjects within the ChemFile.
+ *
+ * @see org.openscience.cdk.AtomContainer#removeAtomAndConnectedElectronContainers(Atom)
+ */
+public class ChemFileManipulator {
+
+    /**
+     * Puts all the Molecules of this container together in one 
+     * AtomCcntainer.
+     *
+     * @return  The AtomContainer with all the Molecules of this container
+     */
+    public static AtomContainer getAllInOneContainer(ChemFile file) {
+        AtomContainer container = new AtomContainer();
+        for (int i=0; i<file.getChemSequenceCount(); i++) {
+            ChemSequence sequence = file.getChemSequence(i);
+            container.add(ChemSequenceManipulator.getAllInOneContainer(sequence));
+        }
+        return container;
+    }
+}
+
