1ed50e09612d78926453530bfdc2f10c6e0ff2ea
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-08-29 11:36:14 +0000

    Cosmetic beautifying of the code.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1773 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/layout/StructureDiagramGeneratorTest.java
+++ src/org/openscience/cdk/test/layout/StructureDiagramGeneratorTest.java
@@ -1,25 +1,26 @@
-/* $RCSfile$
- * $Author$
- * $Date$
- * $Revision$
- * 
- * Copyright (C) 1997-2003  The Chemistry Development Kit (CDK) project
- * 
- * Contact: cdk-devel@lists.sourceforge.net
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 1997-2003  The Chemistry Development Kit (CDK) project
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package org.openscience.cdk.test.layout;
 
@@ -37,21 +38,51 @@ import javax.vecmath.Vector2d;
 import javax.vecmath.Point2d;
 import junit.framework.*;
 
-public class StructureDiagramGeneratorTest extends TestCase {
-    
+/**
+ *  Description of the Class
+ *
+ *@author     steinbeck
+ *@created    August 29, 2003
+ */
+public class StructureDiagramGeneratorTest extends TestCase
+{
+
 	MoleculeListViewer moleculeListViewer = null;
 
-    public StructureDiagramGeneratorTest(String name) {
-        super(name);
-    }
 
-    public void setUp() {}
+	/**
+	 *  Constructor for the StructureDiagramGeneratorTest object
+	 *
+	 *@param  name  Description of the Parameter
+	 */
+	public StructureDiagramGeneratorTest(String name)
+	{
+		super(name);
+	}
 
-    public static Test suite() {
-        return new TestSuite(StructureDiagramGeneratorTest.class);
-    }
-	
-    public void runVisualTests() {
+
+	/**
+	 *  The JUnit setup method
+	 */
+	public void setUp() { }
+
+
+	/**
+	 *  A unit test suite for JUnit
+	 *
+	 *@return    The test suite
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(StructureDiagramGeneratorTest.class);
+	}
+
+
+	/**
+	 *  Description of the Method
+	 */
+	public void runVisualTests()
+	{
 		moleculeListViewer = new MoleculeListViewer();
 		showIt(MoleculeFactory.loadMolecule("data/mdl/reserpine.mol"), "Reserpine");
 		showIt(MoleculeFactory.loadMolecule("data/mdl/four-ring-5x10.mol"), "5x10 condensed four membered rings");
@@ -65,16 +96,27 @@ public class StructureDiagramGeneratorTest extends TestCase {
 		showIt(MoleculeFactory.makeMethylDecaline(), "Methyldecaline");
 		showIt(MoleculeFactory.makeBranchedAliphatic(), "Branched aliphatic");
 		showIt(MoleculeFactory.makeDiamantane(), "Diamantane - A Problem! - Solve it! :-)");
-		showIt(MoleculeFactory.makeEthylCyclohexane(), "Ethylcyclohexane");	
+		showIt(MoleculeFactory.makeEthylCyclohexane(), "Ethylcyclohexane");
 		showIt(MoleculeFactory.makeBicycloRings(), "Bicyclo-[2.2.2]-octane");
 	}
 
-	private boolean showIt(Molecule molecule, String name) {
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  molecule  Description of the Parameter
+	 *@param  name      Description of the Parameter
+	 *@return           Description of the Return Value
+	 */
+	private boolean showIt(Molecule molecule, String name)
+	{
 		MoleculeViewer2D mv = new MoleculeViewer2D();
-		try {
+		try
+		{
 			mv.setAtomContainer(generateCoordinates(molecule));
 			moleculeListViewer.addStructure(mv, name);
-		} catch(Exception exc) {
+		} catch (Exception exc)
+		{
 			System.out.println("*** Exit due to an unexpected error during coordinate generation ***");
 			exc.printStackTrace();
 			return false;
@@ -82,96 +124,201 @@ public class StructureDiagramGeneratorTest extends TestCase {
 		return true;
 	}
 
-    public AtomContainer generateCoordinates(Molecule m) throws Exception {
-        StructureDiagramGenerator sdg = new StructureDiagramGenerator();
-        sdg.setMolecule(m);
-        sdg.generateCoordinates(new Vector2d(0,1));
-        return sdg.getMolecule();
-    }
-    
-	public static void main(String[] args) {
-		try{
-		StructureDiagramGeneratorTest sdg = new StructureDiagramGeneratorTest("StructureDiagramGeneratorTest");
-        //sdg.runVisualTests();
-	sdg.bug736137();
-	}
-	catch(Exception exc)
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  m              Description of the Parameter
+	 *@return                Description of the Return Value
+	 *@exception  Exception  Description of the Exception
+	 */
+	public AtomContainer generateCoordinates(Molecule m) throws Exception
 	{
-		exc.printStackTrace();	
-	}
+		StructureDiagramGenerator sdg = new StructureDiagramGenerator();
+		sdg.setMolecule(m);
+		sdg.generateCoordinates(new Vector2d(0, 1));
+		return sdg.getMolecule();
 	}
 
+
+	/**
+	 *  The main program for the StructureDiagramGeneratorTest class
+	 *
+	 *@param  args  The command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			StructureDiagramGeneratorTest sdg = new StructureDiagramGeneratorTest("StructureDiagramGeneratorTest");
+			//sdg.runVisualTests();
+			sdg.bug736137();
+		} catch (Exception exc)
+		{
+			exc.printStackTrace();
+		}
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@exception  java.lang.Exception  Description of the Exception
+	 */
 	public void bug736137() throws java.lang.Exception
 	{
 		String filename = "data/mdl/bug736137.mol";
 		InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
 		MDLReader reader = new MDLReader(new InputStreamReader(ins));
-		Molecule molecule = (Molecule) reader.read((ChemObject) new Molecule());	
+		Molecule molecule = (Molecule) reader.read((ChemObject) new Molecule());
 		MoleculeViewer2D.display(molecule, true);
-		
+
 	}
-	
-	
-    public void testAlphaPinene() throws Exception {
-        Molecule m = MoleculeFactory.makeAlphaPinene();
-        AtomContainer ac = generateCoordinates(m);
-    }
-    
-    public void testBiphenyl() throws Exception {
-        Molecule m = MoleculeFactory.makeBiphenyl();
-        AtomContainer ac = generateCoordinates(m);
-    }
-    
-    public void test4x3CondensedRings() throws Exception {
-        Molecule m = MoleculeFactory.make4x3CondensedRings();
-        AtomContainer ac = generateCoordinates(m);
-    }
-    
-    public void testPhenylEthylBenzene() throws Exception {
-        Molecule m = MoleculeFactory.makePhenylEthylBenzene();
-        AtomContainer ac = generateCoordinates(m);
-    }
-    
-    public void testSpiroRings() throws Exception {
-        Molecule m = MoleculeFactory.makeSpiroRings();
-        AtomContainer ac = generateCoordinates(m);
-    }
-    
-    public void testMethylDecaline() throws Exception {
-        Molecule m = MoleculeFactory.makeMethylDecaline();
-        AtomContainer ac = generateCoordinates(m);
-    }
-    
-    public void testBranchedAliphatic() throws Exception {
-        Molecule m = MoleculeFactory.makeBranchedAliphatic();
-        AtomContainer ac = generateCoordinates(m);
-    }
-    
-    public void testDiamantane() throws Exception {
-        Molecule m = MoleculeFactory.makeDiamantane();
-        AtomContainer ac = generateCoordinates(m);
-    }
-    
-    public void testEthylCyclohexane() throws Exception {
-        Molecule m = MoleculeFactory.makeEthylCyclohexane();	
-        AtomContainer ac = generateCoordinates(m);
-    }
-    
-    public void testBicycloRings() throws Exception {
-        Molecule m = MoleculeFactory.makeBicycloRings();
-        AtomContainer ac = generateCoordinates(m);
-    }
-    
-	public void testBenzene() throws Exception{
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testAlphaPinene() throws Exception
+	{
+		Molecule m = MoleculeFactory.makeAlphaPinene();
+		AtomContainer ac = generateCoordinates(m);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testBiphenyl() throws Exception
+	{
+		Molecule m = MoleculeFactory.makeBiphenyl();
+		AtomContainer ac = generateCoordinates(m);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void test4x3CondensedRings() throws Exception
+	{
+		Molecule m = MoleculeFactory.make4x3CondensedRings();
+		AtomContainer ac = generateCoordinates(m);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testPhenylEthylBenzene() throws Exception
+	{
+		Molecule m = MoleculeFactory.makePhenylEthylBenzene();
+		AtomContainer ac = generateCoordinates(m);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testSpiroRings() throws Exception
+	{
+		Molecule m = MoleculeFactory.makeSpiroRings();
+		AtomContainer ac = generateCoordinates(m);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testMethylDecaline() throws Exception
+	{
+		Molecule m = MoleculeFactory.makeMethylDecaline();
+		AtomContainer ac = generateCoordinates(m);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testBranchedAliphatic() throws Exception
+	{
+		Molecule m = MoleculeFactory.makeBranchedAliphatic();
+		AtomContainer ac = generateCoordinates(m);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testDiamantane() throws Exception
+	{
+		Molecule m = MoleculeFactory.makeDiamantane();
+		AtomContainer ac = generateCoordinates(m);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testEthylCyclohexane() throws Exception
+	{
+		Molecule m = MoleculeFactory.makeEthylCyclohexane();
+		AtomContainer ac = generateCoordinates(m);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testBicycloRings() throws Exception
+	{
+		Molecule m = MoleculeFactory.makeBicycloRings();
+		AtomContainer ac = generateCoordinates(m);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testBenzene() throws Exception
+	{
 		SmilesParser sp = new SmilesParser();
-        Molecule mol = sp.parseSmiles("c1ccccc1");
-        AtomContainer ac = generateCoordinates(mol);
+		Molecule mol = sp.parseSmiles("c1ccccc1");
+		AtomContainer ac = generateCoordinates(mol);
 	}
-    
-	public void testBug780545() throws Exception{
-        Molecule mol = new Molecule();
-        mol.addAtom(new Atom("C"));
-        AtomContainer ac = generateCoordinates(mol);
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testBug780545() throws Exception
+	{
+		Molecule mol = new Molecule();
+		mol.addAtom(new Atom("C"));
+		AtomContainer ac = generateCoordinates(mol);
 	}
 }
 
