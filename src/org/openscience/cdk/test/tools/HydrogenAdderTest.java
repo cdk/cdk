f2f3abc6070b072555df9fd50da4d9e13d7feb2e
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-09-10 12:38:33 +0000

    Added a test for the multiple structures case
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1849 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/tools/HydrogenAdderTest.java
+++ src/org/openscience/cdk/test/tools/HydrogenAdderTest.java
@@ -92,6 +92,28 @@ public class HydrogenAdderTest extends TestCase {
         assertEquals(4, new MFAnalyser(mol).getAtomCount("H"));
         assertEquals(4, mol.getBondCount(carbon));
     }
+    public void testNaCl() {
+        Molecule mol = new Molecule();
+        Atom cl = new Atom("Cl");
+        cl.setFormalCharge(1);
+        mol.addAtom(cl);
+        Atom na = new Atom("Na");
+        na.setFormalCharge(-1);
+        mol.addAtom(na);
+        
+        try {
+            adder.addExplicitHydrogensToSatisfyValency(mol);
+        } catch (Exception exception) {
+            System.err.println(exception);
+            exception.printStackTrace();
+            fail();
+        }
+        
+        assertEquals(4, mol.getAtomCount());
+        assertEquals(2, new MFAnalyser(mol).getAtomCount("H"));
+        assertEquals(2, mol.getBondCount(cl));
+        assertEquals(0, mol.getBondCount(na));
+    }
     
     public void testAminomethane()
     {
