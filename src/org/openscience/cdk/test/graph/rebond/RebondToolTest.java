5065e8f8fe4b7cb126f8f817a2ec8715e55db0c9
michaelthoward <michaelthoward@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-09-25 15:07:29 +0000

    Changed hydrogen coordinates to bring them within bonding range
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1933 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/graph/rebond/RebondToolTest.java
+++ src/org/openscience/cdk/test/graph/rebond/RebondToolTest.java
@@ -35,39 +35,40 @@ import javax.vecmath.Point3d;
  */
 public class RebondToolTest extends TestCase {
     
-    public RebondToolTest(String name) {
-        super(name);
-    }
+  public RebondToolTest(String name) {
+    super(name);
+  }
     
-    public void setUp() {}
+  public void setUp() {}
 
-    public static Test suite() {
-        return new TestSuite(RebondToolTest.class);
-    }
+  public static Test suite() {
+    return new TestSuite(RebondToolTest.class);
+  }
 
-    public void testRebond() {
-        RebondTool rebonder = new RebondTool(2.0, 0.5, 0.5);
-        Molecule methane = new Molecule();
-        methane.addAtom(new Atom("C", new Point3d(0.0, 0.0, 0.0)));
-        methane.addAtom(new Atom("H", new Point3d(1.0, 1.0, 1.0)));
-        methane.addAtom(new Atom("H", new Point3d(-1.0, -1.0, 1.0)));
-        methane.addAtom(new Atom("H", new Point3d(1.0, -1.0, -1.0)));
-        methane.addAtom(new Atom("H", new Point3d(-1.0, 1.0, -1.0)));
+  public void testRebond() {
+    RebondTool rebonder = new RebondTool(2.0, 0.5, 0.5);
+    Molecule methane = new Molecule();
+    methane.addAtom(new Atom("C", new Point3d(0.0, 0.0, 0.0)));
+    methane.addAtom(new Atom("H", new Point3d(0.6, 0.6, 0.6)));
+    methane.addAtom(new Atom("H", new Point3d(-0.6, -0.6, 0.6)));
+    methane.addAtom(new Atom("H", new Point3d(0.6, -0.6, -0.6)));
+    methane.addAtom(new Atom("H", new Point3d(-0.6, 0.6, -0.6)));
         
-        try {
-            // configure atoms
-            AtomTypeFactory factory = AtomTypeFactory.getInstance("org/openscience/cdk/config/jmol_atomtypes.txt");
-            Atom[] atoms = methane.getAtoms();
-            for (int i=0; i<atoms.length; i++) {
-                factory.configure(atoms[i]);
-            }
-            // rebond
-            rebonder.rebond(methane);
-        } catch (Exception exception) {
-            fail();
-        }
-        
-        assertEquals(5, methane.getAtomCount());
-        assertEquals(4, methane.getBondCount());
+    try {
+      // configure atoms
+      AtomTypeFactory factory = AtomTypeFactory.getInstance("org/openscience/cdk/config/jmol_atomtypes.txt");
+      Atom[] atoms = methane.getAtoms();
+      for (int i=0; i<atoms.length; i++) {
+        factory.configure(atoms[i]);
+      }
+      // rebond
+      rebonder.rebond(methane);
+    } catch (Exception exception) {
+      fail();
+            
     }
+        
+    assertEquals(5, methane.getAtomCount());
+    assertEquals(4, methane.getBondCount());
+  }
 }
