612ea42f124ada307bd5fd57b45620f8862d90a9
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-09-29 14:50:01 +0000

    test case for new OverlapResolver
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@1953 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/test/layout/OverlapResolverTest.java
@@ -0,0 +1,162 @@
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk.test.layout;
+
+import org.openscience.cdk.controller.*;
+import org.openscience.cdk.*;
+import org.openscience.cdk.io.*;
+import org.openscience.cdk.smiles.*;
+import org.openscience.cdk.layout.*;
+import org.openscience.cdk.renderer.*;
+import org.openscience.cdk.tools.*;
+import java.util.*;
+import java.io.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ *  Description of the Class
+ *
+ *@author     steinbeck
+ *@created    September 4, 2003
+ */
+public class OverlapResolverTest extends TestCase
+{
+
+	/**
+	 *  Description of the Field
+	 */
+	public boolean standAlone = false;
+	private LoggingTool logger = null;
+
+
+	/**
+	 *  Constructor for the OverlapResolverTest object
+	 *
+	 *@param  name  Description of the Parameter
+	 */
+	public OverlapResolverTest(String name)
+	{
+		super(name);
+	}
+
+
+	/**
+	 *  The JUnit setup method
+	 */
+	public void setUp()
+	{
+		logger = new org.openscience.cdk.tools.LoggingTool(this.getClass().getName());
+	}
+
+
+	/**
+	 *  A unit test suite for JUnit
+	 *
+	 *@return    The test suite
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(TemplateHandlerTest.class);
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testResolveOverlap1() throws Exception
+	{
+		Molecule molecule = null;
+		try
+		{
+			String filename = "data/overlaptest.cml";
+			InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+			CMLReader reader = new CMLReader(new InputStreamReader(ins));
+			ChemFile chemFile = (ChemFile) reader.read((ChemObject) new ChemFile());
+			ChemSequence[] chemSequence = chemFile.getChemSequences();
+			ChemModel[] chemModels = chemSequence[0].getChemModels();
+			AtomContainer atomContainer = ChemModelManipulator.getAllInOneContainer(chemModels[0]);
+			//MoleculeViewer2D.display(new Molecule(atomContainer), false);
+			//OverlapResolver.resolveOverlap(atomContainer, null);
+			double score = OverlapResolver.getOverlapScore(atomContainer);
+			assertTrue(score > 0);
+		} catch (Exception exc)
+		{
+			fail(exc.toString());
+		}
+
+	}
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testResolveOverlap2() throws Exception
+	{
+		Molecule molecule = null;
+		try
+		{
+			String filename = "data/overlaptest2.cml";
+			InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+			CMLReader reader = new CMLReader(new InputStreamReader(ins));
+			ChemFile chemFile = (ChemFile) reader.read((ChemObject) new ChemFile());
+			ChemSequence[] chemSequence = chemFile.getChemSequences();
+			ChemModel[] chemModels = chemSequence[0].getChemModels();
+			AtomContainer atomContainer = ChemModelManipulator.getAllInOneContainer(chemModels[0]);
+			//MoleculeViewer2D.display(new Molecule(atomContainer), false);
+			double score = OverlapResolver.getOverlapScore(atomContainer);
+			assertTrue(score == 0.0);
+		} catch (Exception exc)
+		{
+			fail(exc.toString());
+		}
+
+	}
+
+	/**
+	 *  The main program for the OverlapResolverTest class
+	 *
+	 *@param  args  The command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			OverlapResolverTest ort = new OverlapResolverTest("OverlapResolverTest");
+			ort.setUp();
+			ort.standAlone = true;
+			ort.testResolveOverlap1();
+			ort.testResolveOverlap2();
+		} catch (Exception exc)
+		{
+			exc.printStackTrace();
+		}
+	}
+}
+
