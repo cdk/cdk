1195bd27dd488b0a274973502638d323ff880b3c
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-10-03 13:29:08 +0000

    Added a method to check if a bond shares an atom with another bond
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2000 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/Bond.java
+++ src/org/openscience/cdk/Bond.java
@@ -1,25 +1,26 @@
-/* $RCSfile$
- * $Author$
- * $Date$
- * $Revision$
- * 
- * Copyright (C) 1997-2003  The Chemistry Development Kit (CDK) project
- * 
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 1997-2003  The Chemistry Development Kit (CDK) project
+ *
  *  Contact: cdk-devel@lists.sourceforge.net
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
 package org.openscience.cdk;
@@ -28,96 +29,112 @@ import java.util.Vector;
 import javax.vecmath.*;
 
 /**
- * Implements the concept of a covalent bond between two atoms.
- * A bond is considered to be a number of electrons connecting 
- * a two of atoms.
+ *  Implements the concept of a covalent bond between two atoms. A bond is
+ *  considered to be a number of electrons connecting a two of atoms.
  *
- * @keyword bond
- * @keyword atom
- * @keyword electron
+ *@author     steinbeck
+ *@created    October 2, 2003
+ *@keyword    bond
+ *@keyword    atom
+ *@keyword    electron
  */
 public class Bond extends ElectronContainer implements java.io.Serializable, Cloneable
 {
-	/** The bond order of this bond. */
+	/**
+	 *  The bond order of this bond.
+	 */
 	protected double order;
 
-    /** Number of atoms contained by this object. */
-    protected int atomCount = 2;
-    
-	/** A list of atoms participating in this bond. */
+	/**
+	 *  Number of atoms contained by this object.
+	 */
+	protected int atomCount = 2;
+
+	/**
+	 *  A list of atoms participating in this bond.
+	 */
 	protected Atom[] atoms;
 
-	/** 
-     * A descriptor the stereochemical orientation of this bond.
-     *
-	 * @see org.openscience.cdk.CDKConstants for predefined values
-	 *      to be used here.
-	 */
-	protected int stereo; 
-	
 	/**
-	 * Constructs an empty bond.
+	 *  A descriptor the stereochemical orientation of this bond.
 	 *
+	 *@see    org.openscience.cdk.CDKConstants for predefined values to be used
+	 *      here.
 	 */
-	public Bond() {
-        this(null, null, 0.0, CDKConstants.STEREO_BOND_NONE);
+	protected int stereo;
+
+
+	/**
+	 *  Constructs an empty bond.
+	 */
+	public Bond()
+	{
+		this(null, null, 0.0, CDKConstants.STEREO_BOND_NONE);
 	}
 
-	/**
-	 * Constructs a bond with a single bond order..
-     *
-     * @param atom1  the first Atom in the bond
-     * @param atom2  the second Atom in the bond
-     */
-    public Bond(Atom atom1, Atom atom2) {
-        this(atom1, atom2, 1.0, CDKConstants.STEREO_BOND_NONE);
-    }
-    
-    /**
-	 * Constructs a bond with a given order.
-     *
-     * @param atom1  the first Atom in the bond
-     * @param atom2  the second Atom in the bond
-     * @param order  the bond order
-     */
-    public Bond(Atom atom1, Atom atom2, double order) {
-        this(atom1, atom2, order, CDKConstants.STEREO_BOND_NONE);
-    }
 
 	/**
-	 * Constructs a bond with a given order and stereo orientation from an array of atoms.
+	 *  Constructs a bond with a single bond order..
 	 *
-     * @param atom1  the first Atom in the bond
-     * @param atom2  the second Atom in the bond
-     * @param order  the bond order
-     * @param stereo a descriptor the stereochemical orientation of this bond
-     */
-    public Bond(Atom atom1, Atom atom2, double order, int stereo) {
-        atoms = new Atom[2];
-        setAtomAt(atom1, 0);
-        setAtomAt(atom2, 1);
-        setOrder(order);
-        setStereo(stereo);
-    }
-
-	/**
-	 * Returns the array of atoms making up this bond.
-	 *
-	 * @return An array of atoms participating in this bond
-     *
-     * @see    #setAtoms
+	 *@param  atom1  the first Atom in the bond
+	 *@param  atom2  the second Atom in the bond
 	 */
-    public Atom[] getAtoms() {
-        Atom[] returnAtoms = new Atom[getAtomCount()];
-        System.arraycopy(this.atoms, 0, returnAtoms, 0, returnAtoms.length);
-        return returnAtoms;
-    }
-	
+	public Bond(Atom atom1, Atom atom2)
+	{
+		this(atom1, atom2, 1.0, CDKConstants.STEREO_BOND_NONE);
+	}
+
+
 	/**
-	 * Prepares and returns a Vector containing all the 
-	 * Atom objects in this bond.
+	 *  Constructs a bond with a given order.
 	 *
-	 * @return A Vector containing all the Atom objects in this AtomContainer    
+	 *@param  atom1  the first Atom in the bond
+	 *@param  atom2  the second Atom in the bond
+	 *@param  order  the bond order
+	 */
+	public Bond(Atom atom1, Atom atom2, double order)
+	{
+		this(atom1, atom2, order, CDKConstants.STEREO_BOND_NONE);
+	}
+
+
+	/**
+	 *  Constructs a bond with a given order and stereo orientation from an array
+	 *  of atoms.
+	 *
+	 *@param  atom1   the first Atom in the bond
+	 *@param  atom2   the second Atom in the bond
+	 *@param  order   the bond order
+	 *@param  stereo  a descriptor the stereochemical orientation of this bond
+	 */
+	public Bond(Atom atom1, Atom atom2, double order, int stereo)
+	{
+		atoms = new Atom[2];
+		setAtomAt(atom1, 0);
+		setAtomAt(atom2, 1);
+		setOrder(order);
+		setStereo(stereo);
+	}
+
+
+	/**
+	 *  Returns the array of atoms making up this bond.
+	 *
+	 *@return    An array of atoms participating in this bond
+	 *@see       #setAtoms
+	 */
+	public Atom[] getAtoms()
+	{
+		Atom[] returnAtoms = new Atom[getAtomCount()];
+		System.arraycopy(this.atoms, 0, returnAtoms, 0, returnAtoms.length);
+		return returnAtoms;
+	}
+
+
+	/**
+	 *  Prepares and returns a Vector containing all the Atom objects in this bond.
+	 *
+	 *@return    A Vector containing all the Atom objects in this AtomContainer
 	 */
 	public Vector getAtomsVector()
 	{
@@ -128,14 +145,13 @@ public class Bond extends ElectronContainer implements java.io.Serializable, Clo
 		}
 		return atomsVector;
 	}
-	
-	
+
+
 	/**
-	 * Sets the array of atoms making up this bond.
+	 *  Sets the array of atoms making up this bond.
 	 *
-	 * @param   atoms An array of atoms that forms this bond
-     *
-     * @see    #getAtoms
+	 *@param  atoms  An array of atoms that forms this bond
+	 *@see           #getAtoms
 	 */
 	public void setAtoms(Atom[] atoms)
 	{
@@ -144,90 +160,88 @@ public class Bond extends ElectronContainer implements java.io.Serializable, Clo
 
 
 	/**
-	 * Returns the number of Atoms in this Bond.
+	 *  Returns the number of Atoms in this Bond.
 	 *
-	 * @return The number of Atoms in this Bond    
+	 *@return    The number of Atoms in this Bond
 	 */
 	public int getAtomCount()
 	{
 		return atomCount;
 	}
 
+
 	/**
-	 * Returns an Atom from this bond.
+	 *  Returns an Atom from this bond.
 	 *
-	 * @param   position  The position in this bond where the atom is
-     * @return            The atom at the specified position
-     *
-     * @see     #setAtomAt
+	 *@param  position  The position in this bond where the atom is
+	 *@return           The atom at the specified position
+	 *@see              #setAtomAt
 	 */
 	public Atom getAtomAt(int position)
 	{
 		return atoms[position];
 	}
 
-    
+
 
 	/**
-	 * Returns the atom connected to the given atom.
+	 *  Returns the atom connected to the given atom.
 	 *
-	 * @param   atom  The atom the bond partner is searched of
-	 * @return     the connected atom or null
+	 *@param  atom  The atom the bond partner is searched of
+	 *@return       the connected atom or null
 	 */
 	public Atom getConnectedAtom(Atom atom)
 	{
 		if (atoms[0] == atom)
 		{
 			return atoms[1];
-		}
-		else if (atoms[1] == atom)
+		} else if (atoms[1] == atom)
 		{
 			return atoms[0];
 		}
 		return null;
-
 	}
 
+
 	/**
-	 * Returns true if the given atom participates in this bond.
+	 *  Returns true if the given atom participates in this bond.
 	 *
-	 * @param   atom  The atom to be tested if it participates in this bond
-	 * @return     true if the atom participates in this bond
+	 *@param  atom  The atom to be tested if it participates in this bond
+	 *@return       true if the atom participates in this bond
 	 */
 	public boolean contains(Atom atom)
 	{
 		if (atoms[0] == atom)
 		{
 			return true;
-		}
-		else if (atoms[1] == atom)
+		} else if (atoms[1] == atom)
 		{
 			return true;
 		}
 		return false;
 	}
-	
+
 
 	/**
-	 * Sets an Atom in this bond.
+	 *  Sets an Atom in this bond.
 	 *
-	 * @param   atom  The atom to be set
-	 * @param   position  The position in this bond where the atom is to be inserted
-     *
-     * @see     #getAtomAt
+	 *@param  atom      The atom to be set
+	 *@param  position  The position in this bond where the atom is to be inserted
+	 *@see              #getAtomAt
 	 */
 	public void setAtomAt(Atom atom, int position)
 	{
 		atoms[position] = atom;
 	}
 
+
 	/**
-	 * Returns the bond order of this bond.
-	 * @see org.openscience.cdk.CDKConstants org.openscience.cdk.CDKConstants for predefined values.
+	 *  Returns the bond order of this bond.
 	 *
-	 * @return The bond order of this bond
-     *
-     * @see     #setOrder
+	 *@return    The bond order of this bond
+	 *@see       org.openscience.cdk.CDKConstants org.openscience.cdk.CDKConstants
+	 *      for predefined values.
+	 *@see       #setOrder
 	 */
 	public double getOrder()
 	{
@@ -236,12 +250,12 @@ public class Bond extends ElectronContainer implements java.io.Serializable, Clo
 
 
 	/**
-	 * Sets the bond order of this bond.
-	 * @see org.openscience.cdk.CDKConstants org.openscience.cdk.CDKConstants for predefined values.
+	 *  Sets the bond order of this bond.
 	 *
-	 * @param   order The bond order to be assigned to this bond
-     *
-     * @see     #getOrder
+	 *@param  order  The bond order to be assigned to this bond
+	 *@see           org.openscience.cdk.CDKConstants
+	 *      org.openscience.cdk.CDKConstants for predefined values.
+	 *@see           #getOrder
 	 */
 	public void setOrder(double order)
 	{
@@ -251,12 +265,11 @@ public class Bond extends ElectronContainer implements java.io.Serializable, Clo
 
 
 	/**
-	 * Returns the stereo descriptor for this bond. 
+	 *  Returns the stereo descriptor for this bond.
 	 *
-	 * @return The stereo descriptor for this bond
-     *
-     * @see     #setStereo
-	 * @see     org.openscience.cdk.CDKConstants for predefined values.
+	 *@return    The stereo descriptor for this bond
+	 *@see       #setStereo
+	 *@see       org.openscience.cdk.CDKConstants for predefined values.
 	 */
 	public int getStereo()
 	{
@@ -265,45 +278,47 @@ public class Bond extends ElectronContainer implements java.io.Serializable, Clo
 
 
 	/**
-	 * Sets the stereo descriptor for this bond.
+	 *  Sets the stereo descriptor for this bond.
 	 *
-	 * @param   stereo The stereo descriptor to be assigned to this bond.
-     *
-     * @see     #getStereo
-	 * @see     org.openscience.cdk.CDKConstants for predefined values.
+	 *@param  stereo  The stereo descriptor to be assigned to this bond.
+	 *@see            #getStereo
+	 *@see            org.openscience.cdk.CDKConstants for predefined values.
 	 */
 	public void setStereo(int stereo)
 	{
 		this.stereo = stereo;
 	}
-	
+
 
 	/**
-	 * Returns the geometric 2D center of the bond.
+	 *  Returns the geometric 2D center of the bond.
 	 *
-	 * @return The geometric 2D center of the bond
+	 *@return    The geometric 2D center of the bond
 	 */
 	public Point2d get2DCenter()
 	{
-		double x = 0, y = 0;
+		double x = 0;
+		double y = 0;
 		for (int f = 0; f < getAtomCount(); f++)
 		{
 			x += getAtomAt(f).getX2D();
 			y += getAtomAt(f).getY2D();
 		}
-		return new Point2d(x / ((double)getAtomCount()), y / ((double)getAtomCount()));
+		return new Point2d(x / ((double) getAtomCount()), y / ((double) getAtomCount()));
 	}
-	
+
 
 
 	/**
-	 * Returns the geometric 3D center of the bond.
+	 *  Returns the geometric 3D center of the bond.
 	 *
-	 * @return The geometric 3D center of the bond    
+	 *@return    The geometric 3D center of the bond
 	 */
 	public Point3d get3DCenter()
 	{
-		double x = 0, y = 0, z = 0;
+		double x = 0;
+		double y = 0;
+		double z = 0;
 		for (int f = 0; f < getAtomCount(); f++)
 		{
 			x += getAtomAt(f).getX3D();
@@ -311,94 +326,123 @@ public class Bond extends ElectronContainer implements java.io.Serializable, Clo
 			z += getAtomAt(f).getZ3D();
 		}
 		return new Point3d(x / getAtomCount(), y / getAtomCount(), z / getAtomCount());
-		
 	}
 
-	
+
 
 	/**
-	 * Returns the geometric length of this bond in 2D space.
+	 *  Returns the geometric length of this bond in 2D space.
 	 *
-	 * @return The geometric length of this bond
+	 *@return    The geometric length of this bond
 	 */
-	public double getLength() {
+	public double getLength()
+	{
 		Point2d p1 = getAtomAt(0).getPoint2D();
 		Point2d p2 = getAtomAt(1).getPoint2D();
 		return p1.distance(p2);
 	}
 
 
-  /**
-   * Compares a bond with this bond.
-   *
-   * @param  object Object of type Bond
-   * @return        Return true, if the bond is equal to this bond
-   */
-  public boolean compare(Object object)
-  {
-    if (object instanceof Bond)
-    {
-      Bond bond = (Bond) object;
-      for(int i=0; i<atoms.length; i++)
-        if (!bond.contains(atoms[i]))
-          return false;
+	/**
+	 *  Compares a bond with this bond.
+	 *
+	 *@param  object  Object of type Bond
+	 *@return         Return true, if the bond is equal to this bond
+	 */
+	public boolean compare(Object object)
+	{
+		if (object instanceof Bond)
+		{
+			Bond bond = (Bond) object;
+			for (int i = 0; i < atoms.length; i++)
+			{
+				if (!bond.contains(atoms[i]))
+				{
+					return false;
+				}
+			}
 
-      // not important ??!!
-      //if (order==bond.order)
-      //  return false;
-      
-      return true;
-    }
-    return false;
-  }
+			// not important ??!!
+			//if (order==bond.order)
+			//  return false;
+
+			return true;
+		}
+		return false;
+	}
 
 
 	/**
-	 * Clones this bond object. Notice that the references to the 
-	 * atom object remain untouched, i.e. you just get a 
-	 * new bond object that points to the same two atoms as the old one.
+	 *  Checks wether a bond is connected to another one, 
+	 *  i. e. if they have one atom in common.
 	 *
-	 * @return  The cloned object   
+	 *@param  bond  The bond which is checked to be connect with this one
+	 *@return       True, if the bonds share an atom, otherwise false
+	 */
+	public boolean isConnectedTo(Bond bond)
+	{
+		for (int f = 0; f < getAtomCount(); f++)
+		{
+			if (bond.contains(getAtomAt(f)))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	/**
+	 *  Clones this bond object. Notice that the references to the atom object
+	 *  remain untouched, i.e. you just get a new bond object that points to the
+	 *  same two atoms as the old one.
+	 *
+	 *@return    The cloned object
 	 */
 	public Object clone()
 	{
 		Bond o = null;
 		try
 		{
-			o = (Bond)super.clone();
-		}
-		catch (Exception e)
+			o = (Bond) super.clone();
+		} catch (Exception e)
 		{
 			e.printStackTrace(System.err);
 		}
 		return o;
 	}
 
+
 	/**
-	 * Returns a one line string representation of this Container.
-     * This method is conform RFC #9.
+	 *  Returns a one line string representation of this Container. This method is
+	 *  conform RFC #9.
 	 *
-	 * @return    The string representation of this Container
+	 *@return    The string representation of this Container
 	 */
-	public String toString() {
+	public String toString()
+	{
 		ElectronContainer ec;
 		StringBuffer s = new StringBuffer();
-        s.append("Bond(");
-        s.append(this.hashCode());
-        s.append(", #O:" + getOrder());
-        s.append(", #S:" + getStereo());
-        // s.append("#L:" + getLength() + ", ");
-        Atom[] atoms = getAtoms();
-        s.append(", #A:" + atoms.length);
-        for (int i=0; i < atoms.length; i++) {
-            if (atoms[i] == null) {
-                s.append(", null");
-            } else {
-                s.append(", " + atoms[i].toString());
-            }
-        }
-        s.append(")");
+		s.append("Bond(");
+		s.append(this.hashCode());
+		s.append(", #O:" + getOrder());
+		s.append(", #S:" + getStereo());
+		// s.append("#L:" + getLength() + ", ");
+		Atom[] atoms = getAtoms();
+		s.append(", #A:" + atoms.length);
+		for (int i = 0; i < atoms.length; i++)
+		{
+			if (atoms[i] == null)
+			{
+				s.append(", null");
+			} else
+			{
+				s.append(", " + atoms[i].toString());
+			}
+		}
+		s.append(")");
 		return s.toString();
 	}
 
 }
+
