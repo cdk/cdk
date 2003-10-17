7bbcd92f228ebe102fcdddab9c6e8193c1e2cf1f
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2003-10-17 14:54:41 +0000

    Added new tests for SSSR search
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2065 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/ringsearch/RingSearchTest.java
+++ src/org/openscience/cdk/test/ringsearch/RingSearchTest.java
@@ -1,25 +1,26 @@
-/* $RCSfile$    
- * $Author$    
- * $Date$    
- * $Revision$
- * 
- * Copyright (C) 1997-2003  The Chemistry Development Kit (CDK) project
- * 
- * Contact: cdk-devel@lists.sourceforge.net
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 1997-2003  The Chemistry Development Kit (CDK) project
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package org.openscience.cdk.test.ringsearch;
 
@@ -28,49 +29,206 @@ import org.openscience.cdk.ringsearch.*;
 import org.openscience.cdk.smiles.*;
 import org.openscience.cdk.tools.*;
 import org.openscience.cdk.templates.*;
+import org.openscience.cdk.io.*;
 import java.util.*;
 import java.io.*;
 import java.net.URL;
 import junit.framework.*;
 
 /**
- * This class tests the SSSRFinder class.
+ *  This class tests the SSSRFinder class.
+ *
+ *@author     steinbeck
+ *@created    October 17, 2003
  */
-public class RingSearchTest extends TestCase {
-    
+public class RingSearchTest extends TestCase
+{
+
+	static boolean standAlone = false;
+	private LoggingTool logger = null;
 	SSSRFinder sssrf;
-	
-	public RingSearchTest(String name) {
+
+
+	/**
+	 *  Constructor for the RingSearchTest object
+	 *
+	 *@param  name  Description of the Parameter
+	 */
+	public RingSearchTest(String name)
+	{
 		super(name);
 	}
 
-	public void setUp() {
+
+	/**
+	 *  The JUnit setup method
+	 */
+	public void setUp()
+	{
+		logger = new org.openscience.cdk.tools.LoggingTool(this.getClass().getName());
 		sssrf = new SSSRFinder();
 	}
 
-	public static Test suite() {
+
+	/**
+	 *  A unit test suite for JUnit
+	 *
+	 *@return    The test suite
+	 */
+	public static Test suite()
+	{
 		return new TestSuite(RingSearchTest.class);
 	}
 
-	public void testAlphaPinene() {
+
+	/**
+	 *  A unit test for JUnit
+	 */
+	public void testAlphaPinene()
+	{
 		Molecule molecule = MoleculeFactory.makeAlphaPinene();
 		RingSet ringSet = sssrf.findSSSR(molecule);
 		assertEquals(2, ringSet.size());
 	}
 
-	public void testBenzene() throws Exception {
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testBenzene() throws Exception
+	{
 		SmilesParser sp = new SmilesParser();
-        Molecule molecule = sp.parseSmiles("c1ccccc1");
-        RingSet ringSet = sssrf.findSSSR(molecule);
-        assertEquals(1, ringSet.size());
+		Molecule molecule = sp.parseSmiles("c1ccccc1");
+		RingSet ringSet = sssrf.findSSSR(molecule);
+		assertEquals(1, ringSet.size());
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 *
+	 *@exception  Exception  Description of the Exception
+	 */
+	public void testBicyclicCompound() throws Exception
+	{
+		SmilesParser sp = new SmilesParser();
+		Molecule molecule = sp.parseSmiles("C1CCC(CCCCC2)C2C1");
+		RingSet ringSet = sssrf.findSSSR(molecule);
+		assertEquals(2, ringSet.size());
+	}
+
+
+
+	/**
+	 *  A unit test for JUnit
+	 */
+	public void testProblem1()
+	{
+		Molecule molecule = null;
+		Ring ring = null;
+		try
+		{
+			String filename = "data/mdl/figueras-test-sep3D.mol";
+			InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+			MDLReader reader = new MDLReader(new InputStreamReader(ins));
+			molecule = (Molecule) reader.read((ChemObject) new Molecule());
+			if (standAlone) System.out.println("Testing " + filename);
+			
+			RingSet ringSet = sssrf.findSSSR(molecule);
+			if (standAlone) System.out.println("Found ring set of size: " + ringSet.size());
+			assertTrue(ringSet.size() == 3);
+			for (int f = 0; f < ringSet.size(); f++)
+			{
+				ring = (Ring) ringSet.elementAt(f);
+				if (standAlone) System.out.println("ring: " + ring.toString(molecule));
+			}
+		} catch (Exception exc)
+		{
+			exc.printStackTrace();
+			fail();
+		}
+	}
+
+
+	/**
+	 *  A unit test for JUnit
+	 */
+	public void testProblem2()
+	{
+		Molecule molecule = null;
+		Ring ring = null;
+		try
+		{
+			String filename = "data/mdl/figueras-test-buried.mol";
+			InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+			MDLReader reader = new MDLReader(new InputStreamReader(ins));
+			molecule = (Molecule) reader.read((ChemObject) new Molecule());
+			if (standAlone) System.out.println("Testing " + filename);
+			
+			RingSet ringSet = sssrf.findSSSR(molecule);
+			if (standAlone) System.out.println("Found ring set of size: " + ringSet.size());
+			assertTrue(ringSet.size() == 10);
+			for (int f = 0; f < ringSet.size(); f++)
+			{
+				ring = (Ring) ringSet.elementAt(f);
+				if (standAlone) System.out.println("ring: " + ring.toString(molecule));
+			}
+		} catch (Exception exc)
+		{
+			exc.printStackTrace();
+			fail();
+		}
+	}
+
+	
+	/**
+	 *  A unit test for JUnit
+	 */
+	public void testProblem3()
+	{
+		Molecule molecule = null;
+		Ring ring = null;
+		try
+		{
+			String filename = "data/mdl/figueras-test-inring.mol";
+			InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+			MDLReader reader = new MDLReader(new InputStreamReader(ins));
+			molecule = (Molecule) reader.read((ChemObject) new Molecule());
+			if (standAlone) System.out.println("Testing " + filename);
+			
+			RingSet ringSet = sssrf.findSSSR(molecule);
+			if (standAlone) System.out.println("Found ring set of size: " + ringSet.size());
+			assertTrue(ringSet.size() == 5);
+			for (int f = 0; f < ringSet.size(); f++)
+			{
+				ring = (Ring) ringSet.elementAt(f);
+				if (standAlone) System.out.println("ring: " + ring.toString(molecule));
+			}
+		} catch (Exception exc)
+		{
+			exc.printStackTrace();
+			fail();
+		}
 	}
 	
-	public void testBicyclicCompound() throws Exception {
-		SmilesParser sp = new SmilesParser();
-        Molecule molecule = sp.parseSmiles("C1CCC(CCCCC2)C2C1");
-        RingSet ringSet = sssrf.findSSSR(molecule);
-        assertEquals(2, ringSet.size());
-	}
 	
+	/**
+	 *  The main program for the RingSearchTest class
+	 *
+	 *@param  args  The command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		RingSearchTest rst = new RingSearchTest("RingSearchTest");
+		standAlone = true;
+		rst.setUp();
+		rst.testProblem1();
+		rst.testProblem2();
+		rst.testProblem3();
+
+	}
+
 }
 
