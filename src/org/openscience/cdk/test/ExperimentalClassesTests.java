60b89d57ad71a3881f884e3791180086447d5794
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-01-12 15:57:38 +0000

    Added test suite for cdk-experimental, and moved Association to experimental
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2277 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/ExperimentalClassesTests.java
+++ src/org/openscience/cdk/test/ExperimentalClassesTests.java
@@ -21,7 +21,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
  */
-
 package org.openscience.cdk.test;
 
 import junit.framework.*;
@@ -30,32 +29,11 @@ import junit.framework.*;
  * TestSuite that runs all the sample tests
  *
  */
-public class CoreClassesTests {
+public class ExperimentalClassesTests {
 
     public static Test suite () {
-        TestSuite suite= new TestSuite("The cdk.core Tests");
-        suite.addTest(AtomContainerTest.suite());
-        suite.addTest(AtomEnumerationTest.suite());
-        suite.addTest(AtomTest.suite());
-        suite.addTest(AtomTypeTest.suite());
-        suite.addTest(BioPolymerTest.suite());
-        suite.addTest(BondTest.suite());
-        suite.addTest(ChemModelTest.suite());
-        suite.addTest(ChemObjectTest.suite());
-        suite.addTest(ChemSequenceTest.suite());
-        suite.addTest(CrystalTest.suite());
-        suite.addTest(ElectronContainerTest.suite());
-        suite.addTest(ElementTest.suite());
-        suite.addTest(IsotopeTest.suite());
-        suite.addTest(LonePairTest.suite());
-        suite.addTest(MoleculeTest.suite());
-        suite.addTest(MonomerTest.suite());
-        suite.addTest(PolymerTest.suite());
-        suite.addTest(PseudoAtomTest.suite());
-        suite.addTest(ReactionTest.suite());
-        suite.addTest(RingTest.suite());
-        suite.addTest(RingSetTest.suite());
-        suite.addTest(SetOfMoleculesTest.suite());
+        TestSuite suite= new TestSuite("The cdk.experimental Tests");
+        suite.addTest(AssociationTest.suite());
         return suite;
     }
 
