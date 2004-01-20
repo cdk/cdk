1282720340e014047844e6a9f80d640394fb09ce
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-01-20 09:09:48 +0000

    Moved a failing test to limitations
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2299 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/limitations/tools/SaturationCheckerTest.java
+++ src/org/openscience/cdk/test/limitations/tools/SaturationCheckerTest.java
@@ -3,7 +3,7 @@
  * $Date$
  * $Revision$
  * 
- * Copyright (C) 2003  The Chemistry Development Kit (CDK) project
+ * Copyright (C) 2003-2004  The Chemistry Development Kit (CDK) project
  * 
  * Contact: cdk-devel@lists.sourceforge.net
  * 
@@ -301,6 +301,18 @@ public class SaturationCheckerTest extends TestCase {
         assertEquals(1.0, b8.getOrder(), 0.001);
         assertEquals(2.0, b4.getOrder(), 0.001);
     }
+    
+    /**
+     * Tests wether the saturation checker gets a proton right.
+     */
+	public void testIsSaturated_Proton() throws CDKException {
+		// test H+
+		Molecule m = new Molecule();
+		Atom h = new Atom("H");
+        h.setFormalCharge(+1);
+		m.addAtom(h);
+		assertTrue(satcheck.isSaturated(h, m));
+	}
     
 }
 
--- src/org/openscience/cdk/test/tools/SaturationCheckerTest.java
+++ src/org/openscience/cdk/test/tools/SaturationCheckerTest.java
@@ -3,7 +3,7 @@
  *  $Date$
  *  $Revision$
  *
- *  Copyright (C) 1997-2003  The Chemistry Development Kit (CDK) project
+ *  Copyright (C) 2003-2004  The Chemistry Development Kit (CDK) project
  *
  *  Contact: cdk-devel@lists.sourceforge.net
  *
@@ -173,18 +173,6 @@ public class SaturationCheckerTest extends TestCase
 		assertTrue(satcheck.isSaturated(h2, m));
 		assertTrue(satcheck.isSaturated(h3, m));
 		assertTrue(satcheck.isSaturated(o, m));
-	}
-    
-    /**
-     * Tests wether the saturation checker gets a proton right.
-     */
-	public void testIsSaturated_Proton() throws CDKException {
-		// test H+
-		Molecule m = new Molecule();
-		Atom h = new Atom("H");
-        h.setFormalCharge(+1);
-		m.addAtom(h);
-		assertTrue(satcheck.isSaturated(h, m));
 	}
     
     /**
