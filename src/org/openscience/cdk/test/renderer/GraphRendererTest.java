895dc4c5d23683b1c66575487cd7da3b0a6b862a
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-01-26 14:51:37 +0000

    Fixed package line
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2341 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/renderer/GraphRendererTest.java
+++ src/org/openscience/cdk/test/renderer/GraphRendererTest.java
@@ -26,7 +26,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *  */
 
-package org.openscience.cdk.test;
+package org.openscience.cdk.test.renderer;
 
 import org.openscience.cdk.math.*;
 import org.openscience.cdk.renderer.*;
