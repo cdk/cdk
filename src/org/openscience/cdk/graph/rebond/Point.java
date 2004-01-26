e883bc0f8f7f227f408c84e996db843c984b20c5
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-01-26 12:15:03 +0000

    Put into separate source file to get it show up in the jar. Actual problem is that the Doclet can not handle more than one class definition in one .java file.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2336 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/graph/rebond/Point.java
@@ -0,0 +1,55 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ *
+ * Copyright (C) 2003  The Jmol Development Team
+ * Copyright (C) 2003-2004  The CDK Project
+ *
+ * Contact: cdk-devel@lists.sf.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ */
+package org.openscience.cdk.graph.rebond;
+
+import java.util.Enumeration;
+
+/**
+ * @author  Miguel Howard
+ * @created 2003-05
+ */
+public class Point implements Bspt.Tuple {
+  double x;
+  double y;
+  double z;
+
+  Point(double x, double y, double z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  public double getDimValue(int dim) {
+    if (dim == 0)
+      return x;
+    if (dim == 1)
+      return y;
+    return z;
+  }
+
+  public String toString() {
+    return "<" + x + "," + y + "," + z + ">";
+  }
+}
