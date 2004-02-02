8b7e070fc03460a14e2705e3b60202b863723a9c
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-02-02 09:40:55 +0000

    Changes the SmilesFromXYZ smiles a little to make things more difficult.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2401 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/smiles/SmilesParserTest.java
+++ src/org/openscience/cdk/test/smiles/SmilesParserTest.java
@@ -95,14 +95,14 @@ public class SmilesParserTest extends TestCase
 		}
 		String[] smiles =
 				{
-		//"C1c2c(c3c(c(O)cnc3)cc2)CC(=O)C1",
-		//"O=C(O3)C1=COC(OC4OC(CO)C(O)C(O)C4O)C2C1C3C=C2COC(C)=O",
-		//"CN1C=NC2=C1C(N(C)C(N2C)=O)=O",
-		//"CN(C)CCC2=CNC1=CC=CC(OP(O)(O)=O)=C12",
-		//"O=C(O)C1C(OC(C3=CC=CC=C3)=O)CC2N(C)C1CC2",
-		//"C1(C2(C)(C))C(C)=CCC2C1",
-		//"C1(C=C(C=C(C=C(C=C(C=CC%35=C%36)C%31=C%35C%32=C%33C%36=C%34)C%22=C%31C%23=C%32C%24=C%25C%33=C%26C%34=CC%27=CC%28=CC=C%29)C%14=C%22C%15=C%23C%16=C%24C%17=C%18C%25=C%19C%26=C%27C%20=C%28C%29=C%21)C6=C%14C7=C%15C8=C%16C9=C%17C%12=C%11C%18=C%10C%19=C%20C%21=CC%10=CC%11=CC(C=C%30)=C%12%13)=C(C6=C(C7=C(C8=C(C9=C%13C%30=C5)C5=C4)C4=C3)C3=C2)C2=CC=C1",
-		//"CC1(C(=C(CC(C1)O)C)C=CC(=CC=CC(=CC=CC=C(C=CC=C(C=CC1=C(CC(CC1(C)C)O)C)C)C)C)C)C",
+		"C1c2c(c3c(c(O)cnc3)cc2)CC(=O)C1",
+		"O=C(O3)C1=COC(OC4OC(CO)C(O)C(O)C4O)C2C1C3C=C2COC(C)=O",
+		"CN1C=NC2=C1C(N(C)C(N2C)=O)=O",
+		"CN(C)CCC2=CNC1=CC=CC(OP(O)(O)=O)=C12",
+		"O=C(O)C1C(OC(C3=CC=CC=C3)=O)CC2N(C)C1CC2",
+		"C1(C2(C)(C))C(C)=CCC2C1",
+		"C1(C=C(C=C(C=C(C=C(C=CC%35=C%36)C%31=C%35C%32=C%33C%36=C%34)C%22=C%31C%23=C%32C%24=C%25C%33=C%26C%34=CC%27=CC%28=CC=C%29)C%14=C%22C%15=C%23C%16=C%24C%17=C%18C%25=C%19C%26=C%27C%20=C%28C%29=C%21)C6=C%14C7=C%15C8=C%16C9=C%17C%12=C%11C%18=C%10C%19=C%20C%21=CC%10=CC%11=CC(C=C%30)=C%12%13)=C(C6=C(C7=C(C8=C(C9=C%13C%30=C5)C5=C4)C4=C3)C3=C2)C2=CC=C1",
+		"CC1(C(=C(CC(C1)O)C)C=CC(=CC=CC(=CC=CC=C(C=CC=C(C=CC1=C(CC(CC1(C)C)O)C)C)C)C)C)C",
 				"NC(C(C)C)C(NC(C(C)O)C(NC(C(C)C)C(NC(CCC(N)=O)C(NC(CC([O-])[O-])C(NCC(NC(CC(N)=O)C(NC(Cc1ccccc1)C(NC(CO)C(NC(Cc2ccccc2)C(NC(CO)C(NC(CC(C)C)C(NC(CCC([O-])[O-])C(NC(CO)C(NC(C(C)C)C(NC(CCCC[N+])C(NC(CCCC[N+])C(NC(CC(C)C)C(NC(CCCC[N+])C(NC(CC([O-])[O-])C(NC(CC(C)C)C(NC(CCC(N)=O)C(NC(CCC([O-])[O-])C(N3CCCC3C(NC(CCC(N)=O)C(NC(CCC([O-])[O-])C(N4CCCC4C(NC(CCCNC([N+])[N+])C(NC(C(C)C)C(NCC(NC(CCCC[N+])C(NC(CC(C)C)C(NC(CCCNC([N+])[N+])C(NC(CC(N)=O)C(NC(Cc5ccccc5)C(NC(C)C(N6CCCC6C(NC(C(C)CC)C(N7CCCC7C(NCC(NC(CCC([O-])[O-])C(N8CCCC8C(NC(C(C)C)C(NC(C(C)C)C(N9CCCC9C(NC(C(C)CC)C(NC(CC(C)C)C(NC%19C[S][S]CC(C(NC(CCCC[N+])C(NC(CCC([O-])[O-])C(N%10CCCC%10C(NC(CC(N)=O)C(NC(C)C(NC(CCC(N)=O)C(NC(CCC([O-])[O-])C(NC(C(C)CC)C(NC(CC(C)C)C(NC(CCC(N)=O)C(NC(CCCNC([N+])[N+])C(NC(CC(C)C)C(NC(CCC([O-])[O-])C(NC(CCC([O-])[O-])C(NC(C(C)CC)C(NC(C)C(NC(CCC([O-])[O-])C(NC(CC([O-])[O-])C(N%11CCCC%11C(NCC(NC(C(C)O)C(NC%14C[S][S]CC%13C(NC(C(C)O)C(NCC(NC(C[S][S]CC(C(NC(C)C(NC(Cc%12ccc(O)cc%12)C(NC(C)C(NC(C)C(N%13)=O)=O)=O)=O)=O)NC(=O)C(C(C)CC)NC(=O)C(CCC([O-])[O-])NC%14=O)C(O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)NC(=O)C(CC(C)C)NC(=O)C%15CCCN%15C(=O)C(CCCC[N+])NC(=O)C(CC(C)C)NC(=O)C(CCC([O-])[O-])NC(=O)C(CCC([O-])[O-])NC(=O)C%16CCCN%16C(=O)C(Cc%17ccccc%17)NC(=O)C(CC(N)=O)NC(=O)C%18CCCN%18C(=O)C(CC(N)=O)NC(=O)C(CO)NC%19=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O)=O"
 				};
 		for (int f = 0; f < smiles.length; f++)
@@ -483,11 +483,12 @@ public class SmilesParserTest extends TestCase
 	 */
 	public void testSMILESFromXYZ() {
 		try {
-			String smiles = "C.C.N.C.C.C.[H].[H].[H].[H].[H].C.C.[H].[H].[H].[H].[H]";
+			String smiles = "C.C.N.[Co].C.C.C.[H].[He].[H].[H].[H].[H].C.C.[H].[H].[H].[H].[H]";
 			SmilesParser sp = new SmilesParser();
 			Molecule mol = sp.parseSmiles(smiles);
-			assertEquals(18, mol.getAtomCount());
+			assertEquals(20, mol.getAtomCount());
 		} catch (Exception e) {
+			e.printStackTrace();
 			fail(e.toString());
 		}
 	}
@@ -548,6 +549,11 @@ public class SmilesParserTest extends TestCase
 	{
 		SmilesParserTest spt = new SmilesParserTest("SmilesParserTest");
 		spt.setStandAlone(true);
+		//sspt.testSmilesParser();
+		//spt.testSFBug630475();
+		//spt.testSFBug585811();
+		//spt.testSFBug593648();
+		spt.testMassNumberReading();
 		spt.testSMILESFromXYZ();
 	}
 }
