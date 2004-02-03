6c28054432d36de477bf6b350c99a528b268530a
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-02-03 10:18:33 +0000

    Added missing import of Hashtable
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2425 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/tools/MFAnalyser.java
+++ src/org/openscience/cdk/tools/MFAnalyser.java
@@ -3,7 +3,7 @@
  * $Date$
  * $Revision$
  * 
- * Copyright (C) 1997-2003  The Chemistry Development Kit (CDK) project
+ * Copyright (C) 1997-2004  The Chemistry Development Kit (CDK) project
  * 
  * Contact: cdk-devel@lists.sourceforge.net
  * 
@@ -29,6 +29,7 @@ package org.openscience.cdk.tools;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
