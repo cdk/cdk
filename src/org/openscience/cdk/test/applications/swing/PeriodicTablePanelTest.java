83b7d3d5de703ab2164eb8c0f62dda31e4f889d7
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-02-10 14:15:18 +0000

    A visual test for the new interactiviy of the PeriodicTablePanel.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2483 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/test/applications/swing/PeriodicTablePanelTest.java
@@ -0,0 +1,103 @@
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 1997-2003  The Chemistry Development Kit (CDK) project
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk.test.applications.swing;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ *  Checks the funcitonality of the PeriodicTablePanelTest
+ *
+ *@author        Steinbeck
+ *@created       February 10, 2004
+ *@cdkPackage    test
+ */
+public class PeriodicTablePanelTest extends TestCase
+{
+
+	AtomTypeFactory atf = null;
+
+
+	/**
+	 *  Constructor for the AtomTypeFactoryTest object
+	 *
+	 *@param  name  Description of the Parameter
+	 */
+	public PeriodicTablePanelTest(String name)
+	{
+		super(name);
+	}
+
+
+	/**
+	 *  The JUnit setup method
+	 */
+	public void setUp()
+	{
+	}
+
+
+	/**
+	 *  A unit test suite for JUnit
+	 *
+	 *@return    The test suite
+	 */
+	public static Test suite()
+	{
+		return new TestSuite(AtomTypeFactoryTest.class);
+	}
+
+
+	/**
+	 *  Description of the Method
+	 */
+	public void runVisual()
+	{
+		PeriodicTablePanel ptp = new PeriodicTablePanel();
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.add(ptp);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+
+	/**
+	 *  The main program for the PeriodicTablePanelTest class
+	 *
+	 *@param  args  The command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		PeriodicTablePanelTest ptpt = new PeriodicTablePanelTest();
+		ptpt.setUp();
+		ptpt.runVisual();
+
+	}
+}
+
