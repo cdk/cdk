2d2705447fb291d1dd33637584866a3fcc61fa9b
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-03-04 14:14:15 +0000

    This is the normalizer
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2641 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/tools/Normalizer.java
@@ -0,0 +1,111 @@
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 1997-2004  The Chemistry Development Kit (CDK) project
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk.tools;
+
+import java.util.*;
+
+import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.smiles.SmilesParser;
+import org.openscience.cdk.exception.*;
+import org.openscience.cdk.isomorphism.*;
+import org.openscience.cdk.isomorphism.mcss.*;
+
+import org.w3c.dom.*;
+
+/**
+ * Adjusts parts of an atomcontainer to the congiguratin of a fragment.
+ *
+ * @author        shk3
+ * @created       March 4, 2004
+ * @cdk.module    standard
+ */
+public class Normalizer {
+
+  /**
+   *  The method takes an xml files like the following:<br>
+   *  &lt;replace-set&gt;<br>
+       &lt;replace&gt;O=N=O&lt;/replace&gt;<br>
+       &lt;replacement&gt;[O-][N+]=O&lt;/replacement&gt;<br>
+   *  &lt;/replace-set&gt;<br>
+   *  All parts in ac which are the same as replace will be changed according to replacement.
+   *  Currently the following changes are done: BondOrder, FormalCharge.
+   *  For detection of fragments like replace, we rely on UniversalIsomorphismTester.
+   *  doc may contain several replace-sets and a replace-set may contain several replace fragments, which will all be normalized according to replacement.
+   *
+   * @param  ac                          The atomcontainer to normalize.
+   * @param  doc                         The configuration file.
+   * @return                             The normalized atomcontainer.
+   * @exception  InvalidSmilesException  doc contains an invalid smiles.
+   */
+  public static AtomContainer normalize(AtomContainer ac, Document doc) throws InvalidSmilesException {
+    NodeList nl = doc.getElementsByTagName("replace-set");
+    SmilesParser sp = new SmilesParser();
+    for (int i = 0; i < nl.getLength(); i++) {
+      Element child = (Element) nl.item(i);
+      NodeList replaces = child.getElementsByTagName("replace");
+      NodeList replacement = child.getElementsByTagName("replacement");
+      String replacementstring = replacement.item(0).getFirstChild().getNodeValue();
+      if (replacementstring.indexOf("\n") > -1 || replacementstring.length() < 1) {
+        replacementstring = replacement.item(0).getFirstChild().getNextSibling().getNodeValue();
+      }
+      AtomContainer replacementStructure = sp.parseSmiles(replacementstring);
+      for (int k = 0; k < replaces.getLength(); k++) {
+        Element replace = (Element) replaces.item(k);
+        String replacestring = replace.getFirstChild().getNodeValue();
+        if (replacestring.indexOf("\n") > -1 || replacestring.length() < 1) {
+          replacestring = replace.getFirstChild().getNextSibling().getNodeValue();
+        }
+        AtomContainer replaceStructure = sp.parseSmiles(replacestring);
+        List l = null;
+        while ((l = UniversalIsomorphismTester.getSubgraphMap(ac, replaceStructure)) != null) {
+          List l2 = UniversalIsomorphismTester.makeAtomsMapOfBondsMap(l, ac, replaceStructure);
+          Iterator bondit = l.iterator();
+          while (bondit.hasNext()) {
+            RMap rmap = (RMap) bondit.next();
+            Bond acbond = ac.getBondAt(rmap.getId1());
+            Bond replacebond = replacementStructure.getBondAt(rmap.getId2());
+            acbond.setOrder(replacebond.getOrder());
+          }
+          Iterator atomit = l2.iterator();
+          while (atomit.hasNext()) {
+            RMap rmap = (RMap) atomit.next();
+            Atom acatom = ac.getAtomAt(rmap.getId1());
+            Atom replaceatom = replacementStructure.getAtomAt(rmap.getId2());
+            acatom.setFormalCharge(replaceatom.getFormalCharge());
+          }
+        }
+      }
+    }
+    return (ac);
+  }
+}
+
