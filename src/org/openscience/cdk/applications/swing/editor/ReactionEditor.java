7a4e6f68e229b120e7e50b6dcba2b8d0e60427e9
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-04-18 13:01:33 +0000

    Added a Swing property editor for Reaction
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2779 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/applications/swing/editor/ReactionEditor.java
@@ -0,0 +1,90 @@
+/* $RCSfile$   
+ * $Author$   
+ * $Date$    
+ * $Revision$
+ * 
+ * Copyright (C) 2001-2004  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ */
+package org.openscience.cdk.applications.swing.editor;
+
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.Reaction;
+import org.openscience.cdk.applications.swing.FieldTablePanel;
+
+/**
+ * @cdk.module applications
+ */
+public class ReactionEditor extends ChemObjectEditor {
+    
+    private final static String SOLVENT = "org.openscience.cdk.Reaction.Solvent";
+    private final static String TEMPERATURE = "org.openscience.cdk.Reaction.Temperature";
+    
+    private JTextField idField;
+    private JComboBox directionField;
+    private JTextField solventField;
+    private JTextField tempField;
+    
+	public ReactionEditor() {
+        super();
+        constructPanel();
+	}
+    
+    private void constructPanel() {
+        idField = new JTextField(40);
+        addField("Reaction ID", idField);
+        // the options given next should match the order in the Reaction class!
+        String[] options = {
+            "", "Forward", "Backward", "Bidirectional"
+        };
+        directionField = new JComboBox(options);
+        addField("Direction", directionField);
+        solventField = new JTextField(40);
+        addField("Solvent", solventField);
+        tempField = new JTextField(10);
+        addField("Temperature", tempField);
+    }
+    
+    public void setChemObject(ChemObject object) {
+        if (object instanceof Reaction) {
+            source = object;     
+            // update table contents
+            Reaction reaction = (Reaction)source;
+            idField.setText(reaction.getID());
+            directionField.setSelectedIndex(reaction.getDirection());
+            solventField.setText((String)reaction.getProperty(SOLVENT));
+            tempField.setText((String)reaction.getProperty(TEMPERATURE));
+        } else {
+            throw new IllegalArgumentException("Argument must be an Reaction");
+        }
+    }
+	
+    public void applyChanges() {
+        Reaction reaction = (Reaction)source;
+        reaction.setID(idField.getText());
+        reaction.setDirection(directionField.getSelectedIndex());
+        reaction.setProperty(SOLVENT, solventField.getText());
+        reaction.setProperty(TEMPERATURE, tempField.getText());
+    }
+}
+
+
