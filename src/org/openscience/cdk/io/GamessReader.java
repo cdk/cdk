80d708bb7b8a3d317d27edd8344fdd0f03efeb5a
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-04-19 10:47:43 +0000

    Fixed import
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2784 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/io/GamessReader.java
+++ src/org/openscience/cdk/io/GamessReader.java
@@ -38,6 +38,7 @@ import org.openscience.cdk.ChemModel;
 import org.openscience.cdk.ChemObject;
 import org.openscience.cdk.ChemSequence;
 import org.openscience.cdk.Molecule;
+import org.openscience.cdk.PhysicalConstants;
 import org.openscience.cdk.SetOfMolecules;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.io.DefaultChemObjectReader;
