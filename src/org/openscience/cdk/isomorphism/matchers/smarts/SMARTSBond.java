81a452688bd9f2cba5cc59b957344f3c829b341a
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-04-22 14:55:02 +0000

    More work on SMARTS bonds: now also parser ':' and '#'
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2794 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/isomorphism/matchers/smarts/SMARTSBond.java
@@ -0,0 +1,48 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ * 
+ * Copyright (C) 2004  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk.isomorphism.matchers.smarts;
+
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.isomorphism.matchers.QueryAtom;
+import org.openscience.cdk.isomorphism.matchers.QueryBond;
+
+/**
+ * This is just a simple proof of concept, and far from a functional SMARTSAtom.
+ *
+ * @cdk.module experimental
+ */
+public class SMARTSBond extends Bond implements QueryBond {
+    
+    public SMARTSBond() {}
+    
+    public SMARTSBond(QueryAtom atom1, QueryAtom atom2, double order) {
+        super((Atom)atom1, (Atom)atom2, order);
+    }
+
+	public boolean matches(Bond bond) {
+        return false;
+    };
+}
+
