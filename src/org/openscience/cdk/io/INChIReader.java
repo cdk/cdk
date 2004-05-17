a074c5657e47a8a42ef7be0ed205b0377f57e977
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-05-17 15:20:56 +0000

    Added IUPAC/NIST Chemical Identifier reader
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2852 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/io/INChIReader.java
@@ -0,0 +1,201 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2002-2004  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk.io;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.openscience.cdk.ChemFile;
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.io.inchi.INChIHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+/**
+ * Reads the content of a IUPAC/NIST Chemical Identifier (INChI) document. See
+ * <a href="http://www.nist.gov/public_affairs/update/upd20020610.htm#International">this
+ * press release</a>. Recently a new INChI format was introduced an files generated
+ * with the latest INChI generator cannot be parsed with this class. This class
+ * needs to be updated.
+ *
+ * <P>The elements that are read are given in the INChIHandler class.
+ *
+ * <p>Reference: <a href="http://cdk.sf.net/biblio.html#HEL01">HEL01</a>.
+ *
+ * @cdk.module experimental
+ *
+ * @author     Egon Willighagen <egonw@sci.kun.nl>
+ * @cdk.created    25 September 2002
+ *
+ * @cdk.keyword file format, INChI
+ * @cdk.keyword chemical identifier
+ * @cdk.require java1.4
+ *
+ * @see     org.openscience.cdk.io.ichi.INChIHandler
+ */
+public class INChIReader extends DefaultChemObjectReader {
+
+    private XMLReader parser;
+    private Reader input;
+
+    private org.openscience.cdk.tools.LoggingTool logger;
+
+    /**
+     * Construct a INChI reader from a Reader object.
+     *
+     * @param input the Reader with the content
+     */
+    public INChIReader(Reader input) {
+        this.init();
+        this.input = input;
+    }
+
+    public INChIReader() {
+        this(new StringReader(""));
+    }
+    
+    public String getFormatName() {
+        return "IUPAC Chemical Identifier";
+    }
+    
+    public void setReader(Reader input) throws CDKException {
+        this.input = input;
+    }
+
+    public boolean matches(int lineNumber, String line) {
+        if (line.indexOf("<identifier") != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Initializes this reader.
+     */
+    private void init() {
+        logger = new org.openscience.cdk.tools.LoggingTool(this.getClass().getName());
+        boolean success = false;
+        // If JAXP is prefered (comes with Sun JVM 1.4.0 and higher)
+        if (!success) {
+            try {
+                javax.xml.parsers.SAXParserFactory spf = javax.xml.parsers.SAXParserFactory.newInstance();
+                spf.setNamespaceAware(true);
+                javax.xml.parsers.SAXParser saxParser = spf.newSAXParser();
+                parser = saxParser.getXMLReader();
+                logger.info("Using JAXP/SAX XML parser.");
+                success = true;
+            } catch (Exception e) {
+                logger.warn("Could not instantiate JAXP/SAX XML reader!");
+                logger.debug(e);
+            }
+        }
+        // Aelfred is first alternative.
+        if (!success) {
+            try {
+                parser = (XMLReader)this.getClass().getClassLoader().
+                        loadClass("gnu.xml.aelfred2.XmlReader").
+                        newInstance();
+                logger.info("Using Aelfred2 XML parser.");
+                success = true;
+            } catch (Exception e) {
+                logger.warn("Could not instantiate Aelfred2 XML reader!");
+                logger.debug(e);
+            }
+        }
+        // Xerces is second alternative
+        if (!success) {
+            try {
+                parser = (XMLReader)this.getClass().getClassLoader().
+                        loadClass("org.apache.xerces.parsers.SAXParser").
+                        newInstance();
+                logger.info("Using Xerces XML parser.");
+                success = true;
+            } catch (Exception e) {
+                logger.warn("Could not instantiate Xerces XML reader!");
+                logger.debug(e);
+            }
+        }
+        if (!success) {
+            logger.error("Could not instantiate any XML parser!");
+        }
+    }
+
+    /**
+     * Reads a ChemObject of type object from input.
+     * Supported types are: ChemFile.
+     *
+     * @param  object type of requested ChemObject
+     * @return the content in a ChemFile object
+     */
+    public ChemObject read(ChemObject object) throws CDKException {
+      if (object instanceof ChemFile) {
+        return (ChemObject)readChemFile();
+      } else {
+        throw new CDKException("Only supported is reading of ChemFile objects.");
+      }
+    }
+
+    // private functions
+
+    /**
+     * Reads a ChemFile object from input.
+     *
+     * @return ChemFile with the content read from the input
+     */
+    private ChemFile readChemFile() {
+        ChemFile cf = null;
+        try {
+            parser.setFeature("http://xml.org/sax/features/validation", false);
+            logger.info("Deactivated validation");
+        } catch (SAXException e) {
+            logger.warn("Cannot deactivate validation.");
+        }
+        INChIHandler handler = new INChIHandler();
+        parser.setContentHandler(handler);
+        try {
+            parser.parse(new InputSource(input));
+            cf = handler.getChemFile();
+        } catch (IOException e) {
+            logger.error("IOException: ", e.getMessage());
+            logger.debug(e);
+        } catch (SAXException saxe) {
+            logger.error("SAXException: ", saxe.getClass().getName());
+            logger.debug(saxe);
+        }
+        return cf;
+    }
+
+    public void close() throws IOException {
+        input.close();
+    }
+}
+
---
+++ src/org/openscience/cdk/io/inchi/INChIHandler.java
@@ -0,0 +1,251 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2002-2004  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: cdk-devel@lists.sf.net
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ */
+package org.openscience.cdk.io.inchi;
+
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.CDKConstants;
+import org.openscience.cdk.ChemFile;
+import org.openscience.cdk.ChemModel;
+import org.openscience.cdk.ChemSequence;
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.SetOfMolecules;
+import org.openscience.cdk.tools.LoggingTool;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * SAX2 implementation for INChI XML fragment parsing.
+ *
+ * <p>The supported elements are: identifier, formula and
+ * connections. All other elements are not parsed (at this moment).
+ * This parser is written based on the INChI files in data/ichi
+ * for version 1.1Beta.
+ *
+ * <p>The returned ChemFile contains a ChemSequence in
+ * which the ChemModel represents the molecule.
+ *
+ * @cdk.module experimental
+ *
+ * @see org.openscience.cdk.io.INChIReader
+ *
+ * @cdk.require java1.4
+ */
+public class INChIHandler extends DefaultHandler {
+
+    private LoggingTool logger;
+
+    private ChemFile chemFile;
+    private ChemSequence chemSequence;
+    private ChemModel chemModel;
+    private SetOfMolecules setOfMolecules;
+    private Molecule tautomer;
+
+    /** Used to store all chars between two tags */
+    private String currentChars;
+
+    /**
+     * Constructor for the IChIHandler.
+     **/
+    public INChIHandler() {
+        logger = new LoggingTool(this);
+    }
+
+    public void doctypeDecl(String name, String publicId, String systemId)
+        throws Exception {
+        logger.info("DocType root element: " + name);
+        logger.info("DocType root PUBLIC: " + publicId);
+        logger.info("DocType root SYSTEM: " + systemId);
+    }
+
+    public void startDocument() {
+        chemFile = new ChemFile();
+        chemSequence = new ChemSequence();
+        chemModel = new ChemModel();
+        setOfMolecules = new SetOfMolecules();
+    }
+
+    public void endDocument() {
+        chemFile.addChemSequence(chemSequence);
+    }
+
+    public void endElement(String uri, String local, String raw) {
+        logger.debug("end element: ", raw);
+        if ("identifier".equals(local)) {
+            if (tautomer != null) {
+                // ok, add tautomer
+                setOfMolecules.addMolecule(tautomer);
+                chemModel.setSetOfMolecules(setOfMolecules);
+                chemSequence.addChemModel(chemModel);
+            }
+        } else if ("formula".equals(local)) {
+            if (tautomer != null) {
+                logger.info("Parsing <formula> chars: ", currentChars);
+                analyseAtomsEncoding(currentChars);
+            } else {
+                logger.warn("Cannot set atom info for empty tautomer");
+            }
+        } else if ("connections".equals(local)) {
+            if (tautomer != null) {
+                logger.info("Parsing <connections> chars: ", currentChars);
+                analyseBondsEncoding(currentChars, -1);
+            } else {
+                logger.warn("Cannot set dbond info for empty tautomer");
+            }
+        } else {
+            // skip all other elements
+        }
+    }
+
+    /**
+     * Implementation of the startElement() procedure overwriting the
+     * DefaultHandler interface.
+     *
+     * @param uri       the Universal Resource Identifier
+     * @param local     the local name (without namespace part)
+     * @param raw       the complete element name (with namespace part)
+     * @param atts      the attributes of this element
+     */
+    public void startElement(String uri, String local, 
+                             String raw, Attributes atts) {
+        currentChars = "";
+        logger.debug("startElement: ", raw);
+        logger.debug("uri: ", uri);
+        logger.debug("local: ", local);
+        logger.debug("raw: ", raw);
+        if ("INChI".equals(local)) {
+            // check version
+            for (int i = 0; i < atts.getLength(); i++) {
+                if (atts.getQName(i).equals("version"))
+                    logger.info("INChI version: ", atts.getValue(i));
+            }
+        } else if ("structure".equals(local)) {
+            tautomer = new Molecule();
+        } else {
+            // skip all other elements
+        }
+    }
+
+    /**
+     * Implementation of the characters() procedure overwriting the
+     * DefaultHandler interface.
+     *
+     * @param ch        characters to handle
+     */
+    public void characters(char ch[], int start, int length) {
+        logger.debug("character data");
+        currentChars += new String(ch, start, length);
+    }
+
+    public ChemFile getChemFile() {
+        return chemFile;
+    }
+
+    // private methods
+
+    private void analyseAtomsEncoding(String atomsEncoding) {
+        logger.debug("Parsing atom data: ", atomsEncoding);
+
+        Atom atomToAdd = null;
+        Pattern pattern = Pattern.compile("([A-Z][a-z]?)(\\d+)?(.*)");
+        String remainder = atomsEncoding;
+        while (remainder.length() > 0) {
+            logger.debug("Remaining: ", remainder);
+            Matcher matcher = pattern.matcher(remainder);
+            if (matcher.matches()) {
+                String symbol = matcher.group(1);
+                logger.debug("Atom symbol: ", symbol);
+                if (symbol.equals("H")) {
+                    // don't add explicit hydrogens
+                } else {
+                    String occurenceStr = matcher.group(2);
+                    int occurence = 1;
+                    if (occurenceStr != null) {
+                        occurence = Integer.parseInt(occurenceStr);
+                    }
+                    logger.debug("  occurence: ", occurence);
+                    for (int i=1; i<=occurence; i++) {
+                        tautomer.addAtom(new Atom(symbol));
+                    }
+                }
+                remainder = matcher.group(3);
+                if (remainder == null) remainder = "";
+                logger.debug("  Remaining: ", remainder);
+            } else {
+                logger.error("No match found!");
+                remainder = "";
+            }
+            logger.debug("NO atoms: ", tautomer.getAtomCount());
+        }
+        return;
+    }
+
+    /**
+     * @param source the atom to build the path upon. If -1, then start new path
+     */
+    private void analyseBondsEncoding(String bondsEncoding, int source){
+        logger.debug("Parsing bond data: ", bondsEncoding);
+
+        int atoms = tautomer.getAtomCount();
+
+        Bond bondToAdd = null;
+        /* Fixme: treatment of branching is too limited! */
+        Pattern pattern = Pattern.compile("^(\\d+)(\\(.*\\))?-?(.*)");
+        String remainder = bondsEncoding;
+        while (remainder.length() > 0) {
+            logger.debug("Bond part: ", remainder);
+            Matcher matcher = pattern.matcher(remainder);
+            if (matcher.matches()) {
+                String targetStr = matcher.group(1);
+                int target = Integer.parseInt(targetStr);
+                logger.debug("Source atom: ", source);
+                logger.debug("Target atom: ", targetStr);
+                Atom targetAtom = tautomer.getAtomAt(target-1);
+                if (source != -1) {
+                    Atom sourceAtom = tautomer.getAtomAt(source-1);
+                    bondToAdd = new Bond(sourceAtom, targetAtom, 1.0);
+                    tautomer.addBond(bondToAdd);
+                }
+                String branch = matcher.group(2);
+                logger.debug("  branch: ", branch);
+                if (branch != null) {
+                    analyseBondsEncoding(branch.substring(1,branch.length()-1), target); // make branch from target
+                }
+                source = target;
+                remainder = matcher.group(3);
+                logger.debug("  remainder: ", remainder);
+            } else {
+                logger.error("Could not get next bond info part");
+                return;
+            }
+        }
+        return;
+    }
+
+}
