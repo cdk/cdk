217cd3a95b1c23f103f769bcc57fe44178a4f1b3
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-06-11 09:44:11 +0000

    Added doclet that creates a docbook page listing the jar dependencies of the source files, as taken from JavaDoc
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@2953 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ doc/javadoc/source/MakeJarDependencyDoclet.java
@@ -0,0 +1,136 @@
+import com.sun.javadoc.*;
+import java.util.*;
+import java.io.*;
+
+/**
+ * This class makes an overview of which classes depend on which jar.
+ */
+public class MakeJarDependencyDoclet {
+
+    private final String webUrl = "http://cdk.sf.net/";
+
+    private final String javaDocBuildDependsTag = "cdk.builddepends";
+    private final String javaDocDependsTag = "cdk.depends";
+    private Hashtable jarBuildDependencies;
+    private Hashtable jarDependencies;
+
+    public MakeJarDependencyDoclet() {
+        jarBuildDependencies = new Hashtable();
+        jarDependencies = new Hashtable();
+    }
+
+    private String toAPIPath(String className) {
+        StringBuffer sb = new StringBuffer();
+        className = className;
+        for (int i=0; i<className.length(); i++) {
+            if (className.charAt(i) == '.') {
+                sb.append('/');
+            } else {
+                sb.append(className.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public void process(RootDoc root) throws IOException {
+        processPackages(root.specifiedPackages());
+
+        // the build dependencies
+        PrintWriter out = new PrintWriter((Writer)new FileWriter("builddepends.xml"));
+        out.println("<variablelist><title>Build Dependencies</title>");
+        Enumeration keys = jarBuildDependencies.keys();
+        while (keys.hasMoreElements()) {
+            String key = (String)keys.nextElement();
+            out.println("  <varlistentry>");
+            out.println("    <term>" + key + "</term>");
+            Vector jarClasses = (Vector)jarBuildDependencies.get(key);
+            Enumeration classes = jarClasses.elements();
+            out.println("    <listitem><para>");
+            while (classes.hasMoreElements()) {
+                String packageClass = (String)classes.nextElement();
+                out.println("      <ulink url=\"" + webUrl + "api/" +
+                    toAPIPath(packageClass) + "\">" + packageClass + "</ulink>");
+            }
+            out.println("    </para></listitem>");
+            out.println("  </varlistentry>");
+        }
+        out.println("</variablelist>");
+        out.flush(); out.close();
+        // the runtime dependencies
+        out = new PrintWriter((Writer)new FileWriter("depends.xml"));
+        out.println("<variablelist><title>Run-time Dependencies</title>");
+        keys = jarDependencies.keys();
+        while (keys.hasMoreElements()) {
+            String key = (String)keys.nextElement();
+            out.println("  <varlistentry>");
+            out.println("    <term>" + key + "</term>");
+            Vector jarClasses = (Vector)jarDependencies.get(key);
+            Enumeration classes = jarClasses.elements();
+            out.println("    <listitem><para>");
+            while (classes.hasMoreElements()) {
+                String packageClass = (String)classes.nextElement();
+                out.println("      <ulink url=\"" + webUrl + "api/" +
+                    toAPIPath(packageClass) + "\">" + packageClass + "</ulink>");
+            }
+            out.println("    </para></listitem>");
+            out.println("  </varlistentry>");
+        }
+        out.println("</variablelist>");
+        out.flush(); out.close();
+    }
+
+    private void processPackages(PackageDoc[] pkgs) throws IOException {
+        for (int i=0; i < pkgs.length; i++) {
+            processClasses(pkgs[i].allClasses());
+        }
+    }
+
+    private void addClass(Hashtable table, String packageClass, String cdkPackageName) {
+        Vector packageClasses = (Vector)table.get(cdkPackageName);
+        if (packageClasses == null) {
+            packageClasses = new Vector();
+            table.put(cdkPackageName, packageClasses);
+        }
+        packageClasses.addElement(packageClass);
+    }
+    
+    private void processClass(ClassDoc classDoc) throws IOException {
+        if (classDoc == null) return;
+        
+        String className = classDoc.qualifiedName();
+        // first deal with build dependencies
+        Tag[] tags = classDoc.tags(javaDocBuildDependsTag);
+        if (tags.length > 0) {
+            for (int i=0; i<tags.length; i++) {
+                addClass(jarBuildDependencies, className, tags[i].text());
+            }
+        }
+        // then deal with runtime dependencies
+        tags = classDoc.tags(javaDocDependsTag);
+        if (tags.length > 0) {
+            for (int i=0; i<tags.length; i++) {
+                addClass(jarDependencies, className, tags[i].text());
+            }
+        }
+    }
+    
+    private void processClasses(ClassDoc[] classes) throws IOException {
+        for (int i=0; i<classes.length; i++) {
+            ClassDoc doc = classes[i];
+            processClass(doc);
+        }
+    }
+
+    public static boolean start(RootDoc root) {
+        try {
+            MakeJarDependencyDoclet doclet = new MakeJarDependencyDoclet();
+            doclet.process(root);
+            return true;
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
