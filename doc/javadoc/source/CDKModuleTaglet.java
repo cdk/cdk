c7a0856e2553dde0d89284323b1591ac51d6f14f
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-07-31 14:36:29 +0000

    Added missing licenses and JavaDoc
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@3047 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- doc/javadoc/source/CDKModuleTaglet.java
+++ doc/javadoc/source/CDKModuleTaglet.java
@@ -1,7 +1,33 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2004  The Chemistry Development Kit (CDK) project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 import com.sun.tools.doclets.Taglet;
 import com.sun.javadoc.*;
 import java.util.Map;
 
+/**
+ * Taglet that expands @cdk.module tag into a weblink to the CDK
+ * webpage.
+ */
 public class CDKModuleTaglet implements Taglet {
     
     private static final String NAME = "cdk.module";
