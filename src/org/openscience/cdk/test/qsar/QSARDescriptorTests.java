3b6827a84537691a09bb3ea16fd6d0bcc7a51a5f
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-11-22 15:44:25 +0000

    Added test class for AtomCountDescriptor and wrapper for all tests
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@3253 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/test/qsar/QSARDescriptorTests.java
@@ -0,0 +1,70 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2004  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk.test.qsar;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.openscience.cdk.applications.swing.MoleculeListViewer;
+import org.openscience.cdk.test.applications.APIVersionTesterTest;
+import org.openscience.cdk.test.aromaticity.HueckelAromaticityDetectorTest;
+import org.openscience.cdk.test.config.ConfigTests;
+import org.openscience.cdk.test.fingerprint.FingerprinterTest;
+import org.openscience.cdk.test.geometry.CrystalGeometryToolsTest;
+import org.openscience.cdk.test.geometry.GeometryToolsTest;
+import org.openscience.cdk.test.graph.invariant.MorganNumberToolsTest;
+import org.openscience.cdk.test.graph.rebond.RebondToolTest;
+import org.openscience.cdk.test.index.CASNumberTest;
+import org.openscience.cdk.test.io.IOTests;
+import org.openscience.cdk.test.isomorphism.IsomorphismTests;
+import org.openscience.cdk.test.layout.HydrogenPlacerTest;
+import org.openscience.cdk.test.layout.StructureDiagramGeneratorTest;
+import org.openscience.cdk.test.layout.OverlapResolverTest;
+import org.openscience.cdk.test.layout.TemplateHandlerTest;
+import org.openscience.cdk.test.reaction.ReactionBalancerTest;
+import org.openscience.cdk.test.ringsearch.AllRingsFinderTest;
+import org.openscience.cdk.test.ringsearch.RingSearchTest;
+import org.openscience.cdk.test.smiles.SmilesGeneratorTest;
+import org.openscience.cdk.test.smiles.SmilesParserTest;
+import org.openscience.cdk.test.structgen.RandomStructureGeneratorTest;
+import org.openscience.cdk.test.tools.ToolsTests;
+import org.openscience.cdk.test.charges.GasteigerMarsiliPartialChargesTest;
+import org.openscience.cdk.test.qsar.*;
+
+/**
+ * TestSuite that runs all QSAR tests.
+ *
+ * @cdk.module test
+ */
+ public class QSARDescriptorTests {
+    
+    static MoleculeListViewer moleculeListViewer = null;
+    
+    public static Test suite() {
+        TestSuite suite = new TestSuite("All QSAR Descriptor Tests");
+        
+        suite.addTest(AtomCountDescriptorTest.suite());
+        return suite;
+    }
+    
+}
