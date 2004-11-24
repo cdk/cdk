843679d9ad8e37f5d81671539d4379861a815a01
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-11-24 18:40:12 +0000

    First version of an Exception class derived from CDKException.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@3280 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/qsar/DescriptorException.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (C) 1997-2004  The Chemistry Development Kit (CDK) project
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk.qsar;
+
+import org.openscience.cdk.exception.CDKException;
+/**
+ * Exception that is thrown by descriptor routines when a problem has occured
+ *
+ */
+public class DescriptorException extends CDKException {
+
+    /**
+     * Constructs a new DescriptorException with the given message.
+     *
+     * @param message for the constructed exception
+     */
+    public DescriptorException(String message) {
+        super( message );
+    }
+
+    /**
+     *  Constructs a new DescriptorException with from the supplied
+     *  descriptor name and associated message
+     *
+     *  @param name Name of the descriptor raising the exception
+     *  @param message The message associated with the exception
+     */
+    public DescriptorException(String name, String message) {
+        super( name + ": " + message );
+    }
+        
+}
+
