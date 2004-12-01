1055f76396f44bf3ffe8cf843280a97f905f70e9
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-12-01 20:37:42 +0000

    Added two methods (isOdd/isEven) and tests for all methods
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@3327 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/test/math/MathToolsTest.java
@@ -0,0 +1,69 @@
+/* $RCSfile$
+ * $Author$    
+ * $Date$    
+ * $Revision$
+ * 
+ * Copyright (C) 2003-2004  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ * 
+ */
+package org.openscience.cdk.test.math;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.openscience.cdk.math.MathTools;
+
+/**
+ * @cdk.module test
+ */
+public class MathToolsTest extends TestCase {
+
+    public MathToolsTest(String name) {
+        super(name);
+    }
+
+    public void setUp() {}
+
+    public static Test suite() {
+        return new TestSuite(MathToolsTest.class);
+    }
+    
+    public void testMax() {
+        int[] ints = { 1, 2, 3, 4, 5};
+        assertEquals(5, MathTools.max(ints));
+    }
+    
+    public void testMin() {
+        int[] ints = { 1, 2, 3, 4, 5};
+        assertEquals(1, MathTools.min(ints));
+    }
+    
+    public void testEven() {
+        assertTrue(MathTools.isEven(2));
+        assertTrue(MathTools.isEven(208));
+    }
+    
+    public void testOdd() {
+        assertTrue(MathTools.isOdd(1));
+        assertTrue(MathTools.isOdd(3));
+        assertTrue(MathTools.isOdd(209));
+    }
+    
+}
