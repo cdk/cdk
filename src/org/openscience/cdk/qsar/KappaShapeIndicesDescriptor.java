b535019ac7018c678284337a05f0448509acb614
mfe4 <mfe4@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-12-03 14:55:15 +0000

    added 3 kappa shape indeces descriptors from Kier and Hall, and test class
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@3373 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/qsar/KappaShapeIndicesDescriptor.java
@@ -0,0 +1,211 @@
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 2004  The Chemistry Development Kit (CDK) project
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk.qsar;
+
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.tools.*;
+import org.openscience.cdk.exception.CDKException;
+import java.util.Map;
+import java.util.Hashtable;
+import java.util.ArrayList;
+
+/**
+ *  Kier and Hall kappa molecular shape indices compare the molecular graph with minimal and maximal molecular graphs;
+ *  a description is given at: http://www.chemcomp.com/Journal_of_CCG/Features/descr.htm#KH :
+ *  "they are intended to capture different aspects of molecular shape. 
+ *  In the following description, n denotes the number of atoms in the hydrogen suppressed graph, 
+ *  m is the number of bonds in the hydrogen suppressed graph. Also, let p2 denote the number of paths of length 2 
+ *  and let p3 denote the number of paths of length 3". 
+ *  Values kier1, kier2 and kier3 are returned as arrayList of doubles.
+ *
+ * @author      mfe4
+ * @cdk.created 2004-11-03
+ * @cdk.module  qsar
+ * @cdk.set     qsar-descriptors
+ */
+
+public class KappaShapeIndicesDescriptor implements Descriptor {
+
+	/**
+	 *  Constructor for the KappaShapeIndicesDescriptor object
+	 */
+	public KappaShapeIndicesDescriptor() { }
+
+
+	/**
+	 *  Gets the specification attribute of the
+	 *  KappaShapeIndicesDescriptor object
+	 *
+	 *@return    The specification value
+	 */
+	public DescriptorSpecification getSpecification() {
+        return new DescriptorSpecification(
+            "http://qsar.sourceforge.net/dicts/qsar-descriptors:kierValues",
+		    this.getClass().getName(),
+		    "$Id$",
+            "The Chemistry Development Kit");
+	}
+
+
+	/**
+	 *  Sets the parameters attribute of the
+	 *  KappaShapeIndicesDescriptor object
+	 *
+	 *@param  params            The new parameters value
+	 *@exception  CDKException  Description of the Exception
+	 */
+	public void setParameters(Object[] params) throws CDKException {
+		// no parameters for this descriptor
+	}
+
+
+	/**
+	 *  Gets the parameters attribute of the
+	 *  KappaShapeIndicesDescriptor object
+	 *
+	 *@return    The parameters value
+	 */
+	public Object[] getParameters() {
+		// no parameters to return
+		return (null);
+	}
+
+
+	/**
+	 *  calculates the kier shape indices for an atom container
+	 *
+	 *@param  ac                AtomContainer
+	 *@return                   kier1, kier2 and kier3 are returned as arrayList of doubles
+	 *@exception  CDKException  Possible Exceptions
+	 */
+	public Object calculate(AtomContainer ac) throws CDKException {
+
+		Atom[] atoms = ac.getAtoms();
+		Atom[] firstAtomNeighboors = null;
+		Atom[] secondAtomNeighboors = null;
+		Atom[] thirdAtomNeighboors = null;
+		ArrayList kierValues = new ArrayList(3);
+		double bond1 = 0;
+		double bond2 = 0;
+		double bond3 = 0;
+		double kier1 = 0;
+		double kier2 = 0;
+		double kier3 = 0;
+		double atomsCount = atoms.length;
+		ArrayList singlePaths = new ArrayList();
+		ArrayList doublePaths = new ArrayList();
+		ArrayList triplePaths = new ArrayList();
+		double[] sorterFirst = new double[2];
+		double[] sorterSecond = new double[3];
+		String tmpbond2 = "";
+		String tmpbond3 = "";
+		
+		for (int a1 = 0; a1 < atoms.length; a1 ++) {
+			bond1 = 0;
+			firstAtomNeighboors = ac.getConnectedAtoms(atoms[a1]);
+			for (int a2 = 0; a2 < firstAtomNeighboors.length; a2 ++) {
+				bond1 = ac.getBondNumber( atoms[a1], firstAtomNeighboors[a2] );
+				if(!singlePaths.contains(new Double(bond1))) {
+					singlePaths.add(new Double(bond1));
+					java.util.Collections.sort(singlePaths);
+				}
+				secondAtomNeighboors = ac.getConnectedAtoms(firstAtomNeighboors[a2]);
+				for (int a3 = 0; a3 < secondAtomNeighboors.length; a3 ++) {
+					bond2 = ac.getBondNumber( firstAtomNeighboors[a2], secondAtomNeighboors[a3] );
+					if(!singlePaths.contains(new Double(bond2))) {
+						singlePaths.add(new Double(bond2));
+					}
+					sorterFirst[0] = bond1;
+					sorterFirst[1] = bond2;
+					java.util.Arrays.sort(sorterFirst);
+					
+					tmpbond2 = sorterFirst[0] + "+" + sorterFirst[1];
+					
+					if(!doublePaths.contains(new String(tmpbond2)) && (bond1 != bond2)) {
+						doublePaths.add(new String(tmpbond2));
+					}
+					thirdAtomNeighboors = ac.getConnectedAtoms(secondAtomNeighboors[a3]);
+					for (int a4 = 0; a4 < thirdAtomNeighboors.length; a4 ++) {
+						bond3 = ac.getBondNumber( secondAtomNeighboors[a3], thirdAtomNeighboors[a4] );
+						if(!singlePaths.contains(new Double(bond3))) {
+							singlePaths.add(new Double(bond3));
+						}
+						sorterSecond[0] = bond1;
+						sorterSecond[1] = bond2;
+						sorterSecond[2] = bond3;
+						java.util.Arrays.sort(sorterSecond);
+						
+						tmpbond3 = sorterSecond[0] + "+" + sorterSecond[1] + "+" + sorterSecond[2];
+						if(!triplePaths.contains(new String(tmpbond3))) {
+							if((bond1 != bond2) && (bond1 != bond3) && (bond2 != bond3)) {
+								triplePaths.add(new String(tmpbond3));
+							}
+						}
+					}
+				}
+			}
+		}
+
+		kier1 = ( ( (atomsCount) * ( (atomsCount - 1) * (atomsCount - 1) ) ) / ( singlePaths.size() * singlePaths.size() ) );
+		kier2 = ( ( (atomsCount - 1) * ( (atomsCount - 2) * (atomsCount - 2) ) ) / ( doublePaths.size() * doublePaths.size() ) );
+		if(atomsCount % 2 != 0) {
+			kier3 = ( ( (atomsCount - 1) * ( (atomsCount - 3) * (atomsCount - 3) ) ) / ( triplePaths.size() * triplePaths.size() ) );
+		}
+		else {
+			kier3 = ( ( (atomsCount - 3) * ( (atomsCount - 2) * (atomsCount - 2) ) ) / ( triplePaths.size() * triplePaths.size() ) );
+		}
+		kierValues.add(new Double(kier1));
+		kierValues.add(new Double(kier2));
+		kierValues.add(new Double(kier3));		
+		return new ArrayList(kierValues);
+	}
+
+
+	/**
+	 *  Gets the parameterNames attribute of the
+	 *  KappaShapeIndicesDescriptor object
+	 *
+	 *@return    The parameterNames value
+	 */
+	public String[] getParameterNames() {
+		// no param names to return
+		return (null);
+	}
+
+
+
+	/**
+	 *  Gets the parameterType attribute of the
+	 *  KappaShapeIndicesDescriptor object
+	 *
+	 *@param  name  Description of the Parameter
+	 *@return       The parameterType value
+	 */
+	public Object getParameterType(String name) {
+		return (null);
+	}
+}
+
---
+++ src/org/openscience/cdk/test/qsar/KappaShapeIndicesDescriptorTest.java
@@ -0,0 +1,66 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ * 
+ * Copyright (C) 2004  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ */
+package org.openscience.cdk.test.qsar;
+
+import org.openscience.cdk.qsar.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
+import org.openscience.cdk.smiles.SmilesParser;
+import org.openscience.cdk.exception.CDKException;
+import java.util.ArrayList;
+import java.io.*;
+
+/**
+ * TestSuite that runs all QSAR tests.
+ *
+ * @cdk.module test
+ */
+
+ public class KappaShapeIndicesDescriptorTest extends TestCase {
+	
+	public  KappaShapeIndicesDescriptorTest() {}
+    
+	public static Test suite() {
+		return new TestSuite(KappaShapeIndicesDescriptorTest.class);
+	}
+    
+	public void testKappaShapeIndicesDescriptor() throws ClassNotFoundException, CDKException, java.lang.Exception {
+		double [] testResult={ 5,2.25, 14};
+		Descriptor descriptor = new KappaShapeIndicesDescriptor();
+		SmilesParser sp = new SmilesParser();
+		AtomContainer mol = sp.parseSmiles("O=C(O)CC");
+		AtomContainerManipulator acm = new AtomContainerManipulator();
+		acm.removeHydrogens(mol);
+		ArrayList retval = (ArrayList)descriptor.calculate(mol);
+		// position 0 =  kier1
+		// positions 1 = kier2
+		// THIS IS OK: assertEquals(testResult[1], ((Double)retval.get(1)).doubleValue(), 0.0001);
+		// THIS IS OK: assertEquals(testResult[0], ((Double)retval.get(0)).doubleValue(), 0.0001);
+		assertEquals(testResult[2], ((Double)retval.get(2)).doubleValue(), 0.0001);
+	}
+}
+
