d13af8258c72fed466514ec42e2e637d1d0c3630
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-12-06 23:10:41 +0000

    Forgot to update the package line
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@3398 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/graph/ConnectivityCheckerTest.java
+++ src/org/openscience/cdk/test/graph/ConnectivityCheckerTest.java
@@ -22,7 +22,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
-package org.openscience.cdk.test.tools;
+package org.openscience.cdk.test.graph;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
