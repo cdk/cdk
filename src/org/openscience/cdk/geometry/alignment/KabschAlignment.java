d30ac51f1dcc2b89fbf10101fe5e553ec582c6c5
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2004-12-11 22:28:31 +0000

    Implementation of the Kabsch algorithm to align two sets of coordinates to minimize RMSD between them
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@3419 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/geometry/alignment/KabschAlignment.java
@@ -0,0 +1,414 @@
+package org.openscience.cdk.geometry.alignment;
+
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.config.IsotopeFactory;
+
+import javax.vecmath.Point3d;
+import java.util.Vector;
+import java.lang.Math;
+
+import Jama.Matrix;
+import Jama.EigenvalueDecomposition;
+
+/**
+ * Aligns two structures to minimize the RMSD using the Kabsch algorithm.
+ *
+ * This class is an implementation of the Kabsch algorithm ({@cdk.cite KAB76}, {@cdk.cite KAB78})
+ * and evaluates the optimal rotation matrix (U) to minimize the RMSD between the two structures.
+ * Since the algorithm assumes that the number of points are the same in the two structures
+ * it is the job of the caller to pass the proper number of atoms from the two structures. Constructors
+ * which take whole <code>AtomContainer</code>'s are provided but they should have the same number
+ * of atoms.
+ * The algorithm allows for the use of atom weightings and by default all points are given a weight of 1.0
+ * <p>
+ * Example usage can be:
+ * <pre>
+ *       AtomContainer ac1, ac2;
+ *
+ *       try {
+ *           KabschAlignment sa = new KabschAlignment(ac1.getAtoms(),ac2.getAtoms());
+ *           sa.align();
+ *           System.out.println(sa.getRMSD());
+ *       } catch (CDKException e){}
+ * </pre>
+ *
+ * @author Rajarshi Guha
+ * @cdk.created 2004-12-11
+ * @cdk.builddepends Jama-1.0.1.jar
+ * @cdk.depends Jama-1.0.1.jar
+ */ 
+public class KabschAlignment {
+
+    private double[][] U;
+    private double rmsd = -1.0;
+    private Point3d[] p1,p2,rp; // rp are the rotated coordinates
+    private double[] wts;
+    private int npoint;
+    private Point3d cm1, cm2;
+    private double[] atwt1, atwt2;
+    
+    private Point3d[] getPoint3dArray(Atom[] a) {
+        Point3d[] p = new Point3d[ a.length ];
+        for (int i = 0; i < a.length; i++) {
+            p[i] = new Point3d( a[i].getPoint3d() );
+        }
+        return(p);
+    }
+
+    private double[] getAtomicMasses(Atom[] a) {
+        double[] am = new double[a.length];
+        IsotopeFactory factory = null;
+        try {
+            factory = IsotopeFactory.getInstance();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            am[i] = factory.getMajorIsotope( a[i].getSymbol() ).getExactMass();
+        }
+        return(am);
+    }
+
+    private Point3d getCenterOfMass(Point3d[] p, double[] atwt) {
+        double x = 0.;
+        double y = 0.;
+        double z = 0.;
+        double totalmass = 0.;
+        for (int i = 0; i  < p.length; i++) {
+            x += atwt[i]*p[i].x;
+            y += atwt[i]*p[i].y;
+            z += atwt[i]*p[i].z;
+            totalmass += atwt[i];
+        }
+        return( new Point3d(x/totalmass, y/totalmass, z/totalmass) );
+    }
+        
+    private Point3d getCenterOfGeometry(Point3d[] p) {
+        double x = 0.;
+        double y = 0.;
+        double z = 0.;
+        for (int i = 0; i  < p.length; i++) {
+            x += p[i].x;
+            y += p[i].y;
+            z += p[i].z;
+        }
+        return( new Point3d(x/(double)p.length, y/(double)p.length, z/(double)p.length) );
+    }
+
+
+    /**
+     * Sets up variables for the alignment algorithm.
+     *
+     * The algorithm allows for atom weighting and the default is 1.0 for all
+     * atoms.
+     *
+     * @param al1 An array of Atom objects
+     * @param al2 An array of Atom objects. This array will have its coordinates rotated
+     *            so that the RMDS is minimzed to the coordinates of the first array
+     * @exception CDKException Thrown if the number of Atom's are not the same in the two arrays
+     */            
+    public KabschAlignment(Atom[] al1, Atom[] al2) throws CDKException {
+        if (al1.length != al2.length) {
+            throw new CDKException("The Atom[]'s being aligned must have the same numebr of atoms");
+        }
+        this.npoint = al1.length;
+        this.p1 = getPoint3dArray(al1);
+        this.p2 = getPoint3dArray(al2);
+        this.wts = new double[this.npoint];
+
+        this.atwt1 = getAtomicMasses(al1);
+        this.atwt2 = getAtomicMasses(al2);
+
+        for (int i = 0; i < this.npoint; i++) this.wts[i] = 1.0;
+    }
+    /**
+     * Sets up variables for the alignment algorithm
+     *
+     * @param al1 An array of Atom objects
+     * @param al2 An array of Atom objects. This array will have its coordinates rotated
+     *            so that the RMDS is minimzed to the coordinates of the first array
+     * @param wts A vector atom weights.           
+     * @exception CDKException Thrown if the number of Atom's are not the same in the two arrays or
+     *                         length of the weight vector is not the same as the Atom arrays
+     */            
+    public KabschAlignment(Atom[] al1, Atom[] al2, double[] wts) throws CDKException {
+        if (al1.length != al2.length) {
+            throw new CDKException("The Atom[]'s being aligned must have the same number of atoms");
+        }
+        if (al1.length != wts.length) {
+            throw new CDKException("Number of weights must equal number of atoms");
+        }
+        this.npoint = al1.length;
+        this.p1 = getPoint3dArray(al1);
+        this.p2 = getPoint3dArray(al2);
+        this.wts = new double[this.npoint];
+        for (int i = 0; i < this.npoint; i++) this.wts[i] = wts[i];
+
+        this.atwt1 = getAtomicMasses(al1);
+        this.atwt2 = getAtomicMasses(al2);
+    }
+
+    /**
+     * Sets up variables for the alignment algorithm
+     *
+     * The algorithm allows for atom weighting and the default is 1.0 for all
+     * atoms.
+     *
+     * @param ac1 An AtomContainer 
+     * @param ac2 An AtomContainer. This AtomContainer will have its coordinates rotated
+     *            so that the RMDS is minimzed to the coordinates of the first one
+     * @exception CDKException Thrown if the number of atom's are not the same in the two AtomContainer's
+     */            
+    public KabschAlignment(AtomContainer ac1, AtomContainer ac2) throws CDKException {
+        if (ac1.getAtomCount() != ac2.getAtomCount()) {
+            throw new CDKException("The AtomContainer's being aligned must have the same number of atoms");
+        }
+        this.npoint = ac1.getAtomCount();
+        this.p1 = getPoint3dArray(ac1.getAtoms());
+        this.p2 = getPoint3dArray(ac2.getAtoms());
+        this.wts = new double[npoint];
+        for (int i = 0; i < npoint; i++) this.wts[i] = 1.0;
+
+        this.atwt1 = getAtomicMasses(ac1.getAtoms());
+        this.atwt2 = getAtomicMasses(ac2.getAtoms());
+    }
+
+    /**
+     * Sets up variables for the alignment algorithm
+     *
+     * @param ac1 An AtomContainer 
+     * @param ac2 An AtomContainer. This AtomContainer will have its coordinates rotated
+     *            so that the RMDS is minimzed to the coordinates of the first one
+     * @param wts A vector atom weights.           
+     * @exception CDKException Thrown if the number of atom's are not the same in the two AtomContainer's or
+     *                         length of the weight vector is not the same as number of atoms.
+     */            
+    public KabschAlignment(AtomContainer ac1, AtomContainer ac2, double[] wts) throws CDKException {
+        if (ac1.getAtomCount() != ac2.getAtomCount()) {
+            throw new CDKException("The AtomContainer's being aligned must have the same number of atoms");
+        }
+        if (ac1.getAtomCount() != wts.length) {
+            throw new CDKException("Number of weights must equal number of atoms");
+        }
+        this.npoint = ac1.getAtomCount();
+        this.p1 = getPoint3dArray(ac1.getAtoms());
+        this.p2 = getPoint3dArray(ac2.getAtoms());
+        this.wts = new double[npoint];
+        for (int i = 0; i < npoint; i++) this.wts[i] = wts[i];
+
+        this.atwt1 = getAtomicMasses(ac1.getAtoms());
+        this.atwt2 = getAtomicMasses(ac2.getAtoms());
+    }
+
+    public void align() {
+        
+        Matrix tmp;
+
+       // get center of gravity and translate both to 0,0,0 
+        this.cm1 = new Point3d();
+        this.cm2 = new Point3d();
+
+        this.cm1 = getCenterOfMass(p1, atwt1);
+        this.cm2 = getCenterOfMass(p2, atwt2);
+        /*
+        this.cm1 = getCenterOfGeometry(p1);
+        this.cm2 = getCenterOfGeometry(p2);
+        */
+
+        // move the points 
+        for (int i = 0; i < this.npoint; i++) {
+            p1[i].x = p1[i].x - this.cm1.x;
+            p1[i].y = p1[i].y - this.cm1.y;
+            p1[i].z = p1[i].z - this.cm1.z;
+
+            p2[i].x = p2[i].x - this.cm2.x;
+            p2[i].y = p2[i].y - this.cm2.y;
+            p2[i].z = p2[i].z - this.cm2.z;
+        }
+        
+        // get the R matrix 
+        double[][] R = new double[3][3];
+        for (int i = 0; i < this.npoint; i++) {
+            wts[i] = 1.0;
+        }
+        for (int i = 0; i < this.npoint; i++) {
+            R[0][0] += p1[i].x * p2[i].x * wts[i];
+            R[0][1] += p1[i].x * p2[i].y * wts[i];
+            R[0][2] += p1[i].x * p2[i].z * wts[i];
+
+            R[1][0] += p1[i].y * p2[i].x * wts[i];
+            R[1][1] += p1[i].y * p2[i].y * wts[i];
+            R[1][2] += p1[i].y * p2[i].z * wts[i];
+
+            R[2][0] += p1[i].z * p2[i].x * wts[i];
+            R[2][1] += p1[i].z * p2[i].y * wts[i];
+            R[2][2] += p1[i].x * p2[i].z * wts[i];
+        }
+
+
+        // now get the RtR (=R'R) matrix 
+        double[][] RtR = new double[3][3];
+        Matrix jamaR = new Matrix(R);
+        //System.out.println("det(R) = "+jamaR.det());
+        tmp = jamaR.transpose().times( jamaR );
+        RtR = tmp.getArray();
+
+  
+        // get eigenvalues of RtR (a's)
+        Matrix jamaRtR = new Matrix(RtR);
+        EigenvalueDecomposition ed = jamaRtR.eig();
+        double[] mu = ed.getRealEigenvalues();
+        double[][] a = ed.getV().getArray();
+
+ 
+        // Jama returns the eigenvalues in increasing order so
+        // swap the eigenvalues and vectors
+        double tmp2 = mu[2];
+        mu[2] = mu[0];
+        mu[0] = tmp2;
+        for (int i = 0; i < 3; i++) {
+            double tmp3 = a[i][2];
+            a[i][2] = a[i][0];
+            a[i][0] = tmp3;
+        }
+
+
+        // make sure that the a3 = a1 x a2
+        double[] tmpa = new double[3];
+        tmpa[0] = (a[1][0]*a[2][1]) - (a[1][1]*a[2][0]);
+        tmpa[1] = (a[0][1]*a[2][0]) - (a[0][0]*a[2][1]);
+        tmpa[2] = (a[0][0]*a[1][1]) - (a[0][1]*a[1][0]);
+        a[0][2] = tmpa[0];
+        a[1][2] = tmpa[1];
+        a[2][2] = tmpa[2];
+
+
+        // lets work out the b vectors
+        double[][] b = new double[3][3];
+
+        /*
+        for(int i = 0; i < 3; i++){
+            b[0][i] = (R[0][0] * a[0][i]+R[0][1] * a[1][i]+R[0][2] * a[2][i]) / Math.sqrt(mu[i]);
+            b[1][i] = (R[1][0] * a[0][i]+R[1][1] * a[1][i]+R[1][2] * a[2][i]) / Math.sqrt(mu[i]);
+            b[2][i] = (R[2][0] * a[0][i]+R[2][1] * a[1][i]+R[2][2] * a[2][i]) / Math.sqrt(mu[i]);
+        }
+        */
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    b[i][j] += R[i][k] * a[k][j];
+                }
+            }
+        }
+        
+        // normalize and set b3 = b1 x b2
+        double norm1 = 0.;
+        double norm2 = 0.;
+        for (int i = 0; i < 3; i++) {
+            norm1 += b[i][0]*b[i][0];
+            norm2 += b[i][1]*b[i][1];
+        }
+        norm1 = Math.sqrt(norm1);
+        norm2 = Math.sqrt(norm2);
+        for (int i = 0; i < 3; i++) {
+            b[i][0] /= norm1;
+            b[i][1] /= norm2;
+        }
+        b[0][2] = (b[1][0]*b[2][1]) - (b[1][1]*b[2][0]);
+        b[1][2] = (b[0][1]*b[2][0]) - (b[0][0]*b[2][1]);
+        b[2][2] = (b[0][0]*b[1][1]) - (b[0][1]*b[1][0]);
+
+        // get the rotation matrix
+        double[][] tU = new double[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    tU[i][j] += b[i][k]*a[j][k];
+                }
+            }
+        }
+
+        // take the transpose
+        double[][] U = new double[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                U[i][j] = tU[j][i];
+            }
+        }
+
+
+        // now eval the RMS error
+        // first, rotate the second set of points and ...
+        this.rp = new Point3d[ this.npoint ];
+        for (int i = 0; i < this.npoint; i++) {
+            this.rp[i] = new Point3d(
+                    U[0][0]*p2[i].x + U[0][1]*p2[i].y + U[0][2]*p2[i].z,
+                    U[1][0]*p2[i].x + U[1][1]*p2[i].y + U[1][2]*p2[i].z,
+                    U[2][0]*p2[i].x + U[2][1]*p2[i].y + U[2][2]*p2[i].z
+                    );
+        }
+
+        // ... then eval rms
+        double rms = 0.;
+        for (int i = 0; i < this.npoint; i++) {
+            rms += (p1[i].x-this.rp[i].x)*(p1[i].x-this.rp[i].x) +
+                (p1[i].y-this.rp[i].y)*(p1[i].y-this.rp[i].y) +
+                (p1[i].z-this.rp[i].z)*(p1[i].z-this.rp[i].z);
+        }
+        this.rmsd = Math.sqrt(rms / this.npoint);
+    }
+
+    /**
+     * Returns the RMSD from the alignment.
+     *
+     * If align() has not been called the return value is -1.0
+     *
+     * @return The RMSD for this alignment
+     */
+    public double getRMSD() {
+        return(this.rmsd);
+    }
+
+    /**
+     * Returns the rotation matrix (u).
+     *
+     * @return A double[][] representing the rotation matrix
+     */
+    public double[][] getRotationMatrix() {
+        return(this.U);
+    }
+
+    /**
+     * Rotates the AtomContainer coordinates by the rotation matrix.
+     *
+     * In general if you align a subset of atoms in a AtomContainer
+     * this function can be applied to the whole AtomContainer to rotate all
+     * atoms. This should be called with the second AtomContainer (or Atom[])
+     * that was passed to the constructor.
+     *
+     * Note that the AtomContainer coordinates also get translated such that the
+     * center of mass is at the origin.
+     *
+     * @param ac The AtomContainer whose coordinates are to be rotated
+     */
+    public void rotateAtomContainer(AtomContainer ac) throws CDKException {
+        Point3d[] p = getPoint3dArray( ac.getAtoms() );
+        for (int i = 0; i < ac.getAtomCount(); i++) {
+            // translate the the origin we have calculated
+            p[i].x = p[i].x = this.cm2.x;
+            p[i].y = p[i].y = this.cm2.y;
+            p[i].z = p[i].z = this.cm2.z;
+
+            // do the actual rotation
+            ac.getAtomAt(i).setX3d( U[0][0]*p[i].x + U[0][1]*p[i].y + U[0][2]*p[i].z );
+            ac.getAtomAt(i).setY3d( U[0][0]*p[i].x + U[0][1]*p[i].y + U[0][2]*p[i].z );
+            ac.getAtomAt(i).setZ3d( U[0][0]*p[i].x + U[0][1]*p[i].y + U[0][2]*p[i].z );
+        }
+    }
+
+ }
+
