81d77fb825453e809f1886b3c0cb7258ce0c503e
labarta <labarta@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-01-18 10:57:31 +0000

    forcefield: new version of GeometricMinimizer and ForceField classes
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@3652 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/modeling/forcefield/ForceField.java
+++ src/org/openscience/cdk/modeling/forcefield/ForceField.java
@@ -7,57 +7,104 @@ import javax.vecmath.*;
 import org.openscience.cdk.*;
 
 /**
- *  To get parameters from the molecule to the potential function
- *
+ *  To work with the coordinates of the molecule, like get the 3d coordinates of the atoms or 
+ *  calculate the distance between two atoms.
  *@author     vlabarta
  *
  */
 public class ForceField {
-	Vector point0ToShow = new Vector();
 	
 	/**
 	 *  Constructor for the ForceField object
 	 */
-	public ForceField() {
-	}
+	public ForceField() {}
 	
 	
 	/**
-	 *  Read the coordinates of atoms in the molecule from its atom container
+	 *  Read the 3d coordinates of atoms in a molecule from its atom container
 	 *
-	 *@param  molecule  Molecule to be optimized
-	 *@return           Atoms coordinates r=(x1,y1,z1,...,xn,yn,zn).
+	 *@param  molecule  molecule store in an AtomContainer 
+	 *@return           GVector with 3N coordinates (N: atom numbers)
 	 */
-	public GVector readAtomsCoordinates(AtomContainer molecule) {
-		//	input: molecule like AtomContainer object,
-		//	output: GVector with 3N coordinates (N: atom numbers)
+	public GVector getCoordinatesVector(AtomContainer molecule) {
 		
-	//	System.out.println("molecule to optimize: " + molecule.toString());
+		System.out.println("molecule: " + molecule.toString());
 		System.out.println("Atoms number = " + molecule.getAtomCount());
 		GVector point0 = new GVector(3 * (molecule.getAtomCount()));
-	//	System.out.println("point0 = " + point0);
+		System.out.println("point0 = " + point0);
+
 		Atom thisAtom = new Atom();
+		int j = 0;
 		
 		for (int i = 0; i < molecule.getAtomCount(); i++) {
 			thisAtom = molecule.getAtomAt(i);
-		//	System.out.println("i = " + i);
-		//	System.out.println("thisAtom = " + thisAtom);
-		//	System.out.println("point0ToShow = " + point0ToShow);
-		//	System.out.println("thisAtom.getPoint3d() = " + thisAtom.getPoint3d());
-			point0ToShow.add(i, thisAtom.getPoint3d());
-			//	point0ToShow is a Vector with 3D atom coordinates
-		//	System.out.println("point0ToShow = " + point0ToShow);
+			System.out.println("thisAtom = " + thisAtom);
+			System.out.println("thisAtom.getPoint3d() = " + thisAtom.getPoint3d());
 			
-			int j = 3 * i;
+			j = 3 * i;
 			point0.setElement(j, thisAtom.getX3d());
-			//	point0 is a GVector with 3N coordinates of the molecule
 			point0.setElement(j + 1, thisAtom.getY3d());
 			point0.setElement(j + 2, thisAtom.getZ3d());
 		}
 		
-	//	System.out.println("Starting coordinates : " + point0ToShow);
-	//	System.out.println("Starting coordinates : " + point0);
+		System.out.println("Atoms coordinates vector: " + point0);
 		
 		return point0;
 	}
+	
+	/**
+	 *  Read the coordinates vector of atoms in a molecule from its atom container
+	 *
+	 *@param  molecule  molecule store in an AtomContainer 
+	 *@return           GVector with 3N coordinates (N: atom numbers)
+	 */
+	public Vector getPoint3dCoordinates(AtomContainer molecule) {
+		
+		System.out.println("molecule: " + molecule.toString());
+		System.out.println("Atoms number = " + molecule.getAtomCount());
+		Vector point3dCoordinates = new Vector();
+
+		Atom thisAtom = new Atom();
+		
+		for (int i = 0; i < molecule.getAtomCount(); i++) {
+			thisAtom = molecule.getAtomAt(i);
+			System.out.println("thisAtom = " + thisAtom);
+			System.out.println("thisAtom.getPoint3d() = " + thisAtom.getPoint3d());
+			
+			point3dCoordinates.add( new Point3d(thisAtom.getPoint3d()) );
+			//Point3d ia = (Point3d)point3dCoordinates.get(i);
+			//System.out.println(i + "a = " + ia);
+		}
+		
+		System.out.println("Atoms 3d coordinates : " + point3dCoordinates);
+		
+		return point3dCoordinates;
+	}
+	
+	public double get3dDistanceBetweenTwoAtoms(Vector atoms3dCoordinates, int atomNum1, int atomNum2) {
+		
+		Point3d atom13dCoord = (Point3d)atoms3dCoordinates.get(atomNum1);
+		Point3d atom23dCoord = (Point3d)atoms3dCoordinates.get(atomNum2);
+		
+		double atomsDistance = 0;
+		atomsDistance = atom13dCoord.distance(atom23dCoord);
+		System.out.println("atomsDistance = " + atomsDistance);
+		
+		return atomsDistance;
+	}
+	public double get3dDistanceBetweenTwoAtomOfTwoDiffMolecules(GVector atomsCoordinatesVector1, GVector atomsCoordinatesVector2, int atomNumM1, int atomNumM2) {
+		
+		double atomsDistance = 0;
+		double difference = 0;
+		for (int j = 0; j < 3; j++) {
+			difference = atomsCoordinatesVector2.getElement(atomNumM2*3+j) - atomsCoordinatesVector2.getElement(atomNumM1*3+j);
+			difference = Math.pow(difference, 2);
+			atomsDistance = atomsDistance + difference;
+		}
+		atomsDistance = Math.sqrt(atomsDistance);
+		System.out.println("atomsDistance = " + atomsDistance);
+		return atomsDistance;
+	}
 }
+
+
--- src/org/openscience/cdk/modeling/forcefield/GeometricMinimizer.java
+++ src/org/openscience/cdk/modeling/forcefield/GeometricMinimizer.java
@@ -7,21 +7,17 @@ import javax.vecmath.*;
 import org.openscience.cdk.*;
 
 /**
- *  Call the minimization methods and check results
+ *  Call the minimization methods. Check the convergence.
  *
  *@author     vlabarta
  *
  */
 public class GeometricMinimizer {
 
-	GVector initial3dCoordinates = new GVector(3);
 	GVector kCoordinates = new GVector(3);
-//	Vector kCoordinatesToShow = new Vector();
 	GVector kplus1Coordinates = new GVector(3);
-//	Vector kplus1CoordinatesToShow = new Vector();
 	GVector gradient = new GVector(3);
 	GVector minimumCoordinates = new GVector(3);
-//	Vector minimumCoordinatesToShow = new Vector();
 	int iterationNumber = 0;
 	boolean convergence = false;
 	double B11 = 0;
@@ -45,16 +41,6 @@ public class GeometricMinimizer {
 
 
 	/**
-	 *  Gets the initial3dCoordinates attribute of the GeometricMinimizer object
-	 *
-	 *@return    The initial3dCoordinates value
-	 */
-	public GVector getInitial3dCoordinates() {
-		return initial3dCoordinates;
-	}
-
-
-	/**
 	 *  Gets the minimumCoordinates attribute of the GeometricMinimizer object
 	 *
 	 *@return    The minimumCoordinates value
@@ -65,88 +51,46 @@ public class GeometricMinimizer {
 
 
 	/**
-	 *  Set the initial coordinates of the molecule from the Atom container 
-	 */
-	public void setInitial3dCoordinates() {
-
-		AtomContainer testMolecule = new AtomContainer();
-
-		//	Pending: Load actual AtomContainer
-		//	Read Molecule
-		// Creation of artificial molecule: AtomNumbers: 1 ("C"), coordinates (9,9,0).
-
-		Point3d atomCoordinate = new Point3d(9, 9, 0);
-		Atom exampleAtom = new Atom("C");
-		exampleAtom.setPoint3d(atomCoordinate);
-		testMolecule.addAtom(exampleAtom);
-
-		System.out.println("");
-		System.out.println("Test Molecule :  " + testMolecule);
-
-		//	Read atoms coordinates
-
-		initial3dCoordinates.setSize(3 * (testMolecule.getAtomCount()));
-		ForceField forceFieldObject = new ForceField();
-		initial3dCoordinates.set(forceFieldObject.readAtomsCoordinates(testMolecule));
-		//	System.out.println("initial3dCoordinates: " + initial3dCoordinates);
-		System.out.println("");
-		System.out.println("Initial coordinates: " + forceFieldObject.point0ToShow);
-		//	To check initialPoint size
-
-		return;
-	}
-
-
-	/**
 	 *  To check convergence
 	 */
 	public void checkConvergence() {
 
 		RMS = 0;
 		RMS = gradient.dot(gradient);
-		//System.out.println("RMS = gradient.dot(gradient) = " + RMS);
 		RMS = RMS / dimension;
-		//System.out.println("RMS = gradient.dot(gradient) / gradient.getSize() = " + RMS);
 		RMS = Math.sqrt(RMS);
-		//System.out.println("RMS = sqrt( gradient.dot(gradient) / gradient.getSize()) = " + RMS);
-		System.out.println("RMS = " + RMS);
+		////ln("");		
+		//System.out.print("RMS = " + RMS);
 		if (RMS < 0.001) {
 			convergence = true;
-			System.out.println("RMS convergence");
+			//System.out.println("RMS convergence");
 		}
-
 
 		RMSD = 0;
 		for (int i = 0; i < dimension; i++) {
-			//System.out.println("i = " + i);
 			d = 0;
 			for (int j = 0; j < 3; j++) {
 				difference = kplus1Coordinates.getElement(i + j) - kCoordinates.getElement(i + j);
 				difference = Math.pow(difference, 2);
 				d = d + difference;
 			}
-			//System.out.println("d = " + d);
 			d = Math.sqrt(d);
-			//System.out.println("d = " + d);
 			i = i + 2;
 			RMSD = RMSD + Math.pow(d, 2);
-			//System.out.println("RMSD = " + RMSD);
 		}
-		//System.out.println("RMSD = " + RMSD);
 		RMSD = RMSD / dimension;
-		//System.out.println("RMSD = " + RMSD);
 		RMSD = Math.sqrt(RMSD);
-		System.out.println("RMSD = " + RMSD);
+		//System.out.print("RMSD = " + RMSD);
 
 		if (RMSD < 0.001) {
 			convergence = true;
-			System.out.println("RMSD convergence");
+			//System.out.println("RMSD convergence");
 		}
+
 		
 		if (iterationNumber == 1) {
 			B11 = kplus1Coordinates.norm() / kCoordinates.norm();
 		}
-		
 		else {
 			if (iterationNumber == 2) {
 				B12 = kplus1Coordinates.norm() / kCoordinates.norm();
@@ -157,24 +101,25 @@ public class GeometricMinimizer {
 			}
 		}
 
-		System.out.println("B11 = " + B11);
-		System.out.println("B12 = " + B12);
+		//System.out.println("B11 = " + B11);
+		//System.out.println("B12 = " + B12);
 
 		if (iterationNumber>1) {
 			B = B12 - B11;
 			B = Math.abs(B);
-			System.out.println("B = " + B);
+			//System.out.print("B = " + B);
 			if (B == 0) {
 				convergence = true;
-				System.out.println("Superlinear convergence");
+				//System.out.println("Superlinear convergence");
 			}
 			else {
 				if (B < 0.000001) {
 					convergence = true;
-					System.out.println("Linear convergence");
+					//System.out.println("Linear convergence");
 				}
+
 			}
-		}	
+		}
 
 		if (iterationNumber == 1) {
 			B21 = kplus1Coordinates.norm() / (Math.pow(kCoordinates.norm(), 2));
@@ -187,32 +132,29 @@ public class GeometricMinimizer {
 			else {
 				B21 = B22;
 				temp = kplus1Coordinates.norm();
-				System.out.println("numerator = " + temp);
 				temp = kCoordinates.norm();
-				System.out.println("denominator = " + temp);
 				temp = Math.pow(kCoordinates.norm(), 2);
-				System.out.println("denominator = " + temp);
 				B22 = kplus1Coordinates.norm() / (Math.pow(kCoordinates.norm(), 2));
 			}
 		}
 
-		System.out.println("B21 = " + B21);
-		System.out.println("B22 = " + B22);
+		//System.out.println("B21 = " + B21);
+		//System.out.println("B22 = " + B22);
 
 		if (iterationNumber>1) {
 			B = B22 - B21;
 			B = Math.abs(B);
-			System.out.println("B = " + B);
+			//System.out.print("B = " + B);
 			if (B < 0.0000001) {
 				convergence = true;
-				System.out.println("Quadratic convergence");
+				//System.out.println("Quadratic convergence");
 			}
 		}
 		return;
 	}
 
 
-	public void setConvergence(){
+	public void setConvergenceParameters(){
 		return;
 	}
 
@@ -224,61 +166,70 @@ public class GeometricMinimizer {
 	 * @param  NRMaximumIteration  Maximum number of iteration for Newton-Raphson method
 	 * @param  forceField		The potential function to be used
 	 */
-	public void energyOptimization(int SDMaximumIteration, int CGMaximumIteration, int NRMaximumIteration, PotentialFunction forceField) {
+	public void energyMinimization(GVector initialCoordinates, int SDMaximumIteration, int CGMaximumIteration, int NRMaximumIteration, PotentialFunction forceField) {
 		
-		//	System.out.println("initial3dCoordinates : " + initial3dCoordinates);
-		dimension = initial3dCoordinates.getSize();
-		System.out.println("dimension : " + dimension);
-		kCoordinates.setSize(initial3dCoordinates.getSize());
-		kCoordinates.set(initial3dCoordinates);
-		System.out.println("Coordinates at iteration 1: X1 = " + kCoordinates);
-		gradient.setSize(initial3dCoordinates.getSize());
+		dimension = initialCoordinates.getSize();
+		kCoordinates.setSize(initialCoordinates.getSize());
+		kCoordinates.set(initialCoordinates);
+		//System.out.println("Coordinates at iteration 1: X1 = " + kCoordinates);
+		gradient.setSize(initialCoordinates.getSize());
 		gradient.set(forceField.gradientInPoint(kCoordinates));
-		System.out.println("gradient: g1 = " + gradient);
-		/*
-		 *  kCoordinatesToShow.setSize(point0ToShow.size());
-		 *  for (int i=0;i<kCoordinatesToShow.size();i++) {
-		 *  kCoordinatesToShow.setElementAt(point0ToShow.elementAt(i),i);
-		 *  }
-		 *  System.out.println("kCoordinatesToShow : " + kCoordinatesToShow);
-		 */
+		//System.out.println("gradient at iteration 1 : g1 = " + gradient);
 		kplus1Coordinates.setSize(kCoordinates.getSize());
+		minimumCoordinates.setSize(kCoordinates.getSize());
 		minimumCoordinates.set(kCoordinates);
-		//	kplus1CoordinatesToShow.setSize(kplus1CoordinatesToShow.size());
+		int iterationNumberBefore = -1;
+		LineSearch ls = new LineSearch();
 		
 		System.out.println("");
 		System.out.println("FORCEFIELDTESTS steepestDescentTest");
 		
 		SteepestDescentsMethod sdm = new SteepestDescentsMethod(kCoordinates);
-		LineSearch ls = new LineSearch(kCoordinates);
-		
+				
 		while ((iterationNumber < SDMaximumIteration) & (convergence == false)) {
 			
 			iterationNumber += 1;
+			iterationNumberBefore += 1;
+			System.out.println("");
 			System.out.println("");
 			System.out.println("SD Iteration number: " + iterationNumber);
 			
 			if (iterationNumber != 1) {
 				kCoordinates.set(kplus1Coordinates);
 			} 			
-			sdm.setSk(gradient);
-			ls.bracketingTheMinimum(kCoordinates, sdm.sk, forceField);
+			//System.out.println("Search direction: ");
+			sdm.setSk(gradient, iterationNumber);
+			//System.out.println("");
+			//System.out.println("Start the line search: ");
+			ls.bracketingTheMinimum(kCoordinates, sdm.sk, forceField, iterationNumberBefore);
+			kplus1Coordinates.set(sdm.sk);
+			kplus1Coordinates.scale(ls.arbitraryStepSize);
+			kplus1Coordinates.add(kCoordinates);
+			System.out.print("x" + iterationNumber + " = " + kplus1Coordinates + "	");
+			System.out.println("f(x" + iterationNumber + ") = " + forceField.functionInPoint(kplus1Coordinates));
+			//System.out.println("Parabolic interpolation: ");
+			ls.parabolicInterpolation();
+			kplus1Coordinates.set(sdm.sk);
+			kplus1Coordinates.scale(ls.parabolMinimumLambda);
+			kplus1Coordinates.add(kCoordinates);
+			System.out.print("x" + iterationNumber + " = " + kplus1Coordinates + "	");
+			System.out.println("f(x" + iterationNumber + ") = " + forceField.functionInPoint(kplus1Coordinates));
 			//kplus1Coordinates.set(sdm.setNewCoordinates(kCoordinates, sdm.getArbitraryStepSize()));
-			kplus1Coordinates.set(ls.kplus1Point2);
 
 			gradient.set(forceField.gradientInPoint(kplus1Coordinates));
 			checkConvergence();
 			System.out.println("convergence: " + convergence);
-			System.out.println("f(xk) = " + forceField.functionInPoint(kCoordinates));
-			System.out.println("f(xk+1) = " + forceField.functionInPoint(kplus1Coordinates));
+			System.out.println("");
+			System.out.println("f(x" + iterationNumberBefore + ") = " + forceField.functionInPoint(kCoordinates));
+			System.out.println("f(x" + iterationNumber + ") = " + forceField.functionInPoint(kplus1Coordinates));
 
 		}
 		minimumCoordinates.set(kplus1Coordinates);
 		System.out.println("The SD minimum energy is at: " + minimumCoordinates);
 		
-		
 		convergence = false;
 		iterationNumber = 0;
+		iterationNumberBefore = -1;
 				
 		System.out.println("");
 		System.out.println("FORCEFIELDTESTS ConjugatedGradientTest");
@@ -289,30 +240,39 @@ public class GeometricMinimizer {
 		while ((iterationNumber < CGMaximumIteration) & (convergence == false)) {
 			
 			iterationNumber += 1;
+			iterationNumberBefore += 1;
+			System.out.println("");
 			System.out.println("");
 			System.out.println("CG Iteration number: " + iterationNumber);
 			
 			if (iterationNumber != 1) {
-				cgm.setmicrok(kCoordinates, kplus1Coordinates, forceField);
+				cgm.setµk(kCoordinates, kplus1Coordinates, forceField);
 				kCoordinates.set(kplus1Coordinates);
 			}
+			//System.out.println("Search direction: ");
 			cgm.setvk(gradient, iterationNumber);
-			ls.bracketingTheMinimum(kCoordinates, cgm.vk, forceField);
-			kplus1Coordinates.set(ls.kplus1Point2);
-			
-/*			if (iterationNumber == 1) {
-				kplus1Coordinates.setElement(0,4);
-				kplus1Coordinates.setElement(1,-1);
-				kplus1Coordinates.setElement(2,0);
-				System.out.println("Xk+1 coordinates : " + kplus1Coordinates);
-			}
-*/			
+			//System.out.println("");
+			//System.out.println("Start the line search: ");
+			ls.bracketingTheMinimum(kCoordinates, cgm.vk, forceField, iterationNumberBefore);
+			kplus1Coordinates.set(cgm.vk);
+			kplus1Coordinates.scale(ls.arbitraryStepSize);
+			kplus1Coordinates.add(kCoordinates);
+			System.out.print("x" + iterationNumber + " = " + kplus1Coordinates + "	");
+			System.out.println("f(x" + iterationNumber + ") = " + forceField.functionInPoint(kplus1Coordinates));
+			System.out.println("Parabolic interpolation: ");
+			ls.parabolicInterpolation();
+			kplus1Coordinates.set(cgm.vk);
+			kplus1Coordinates.scale(ls.parabolMinimumLambda);
+			kplus1Coordinates.add(kCoordinates);
+			System.out.print("x" + iterationNumber + " = " + kplus1Coordinates + "	");
+			System.out.println("f(x" + iterationNumber + ") = " + forceField.functionInPoint(kplus1Coordinates));
 			
 			gradient.set(forceField.gradientInPoint(kplus1Coordinates));
 			checkConvergence();
 			System.out.println("convergence: " + convergence);
-			System.out.println("f(xk) = " + forceField.functionInPoint(kCoordinates));
-			System.out.println("f(xk+1) = " + forceField.functionInPoint(kplus1Coordinates));
+			System.out.println("");
+			System.out.println("f(x" + iterationNumberBefore + ") = " + forceField.functionInPoint(kCoordinates));
+			System.out.println("f(x" + iterationNumber + ") = " + forceField.functionInPoint(kplus1Coordinates));
 		 }
 		 
 		 minimumCoordinates.set(kplus1Coordinates);
