bccc341df41f32cc7ebe9aa8950e056b7331fda2
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-02-24 11:00:45 +0000

    Added class that can be used instead of TestCase to ensure that log4j is configured.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@3794 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/test/CDKTestCase.java
@@ -0,0 +1,45 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ * 
+ * Copyright (C) 2005  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ */
+package org.openscience.cdk.test;
+
+import junit.framework.TestCase;
+import org.openscience.cdk.tools.LoggingTool;
+
+/**
+ * Super class for <b>all</b> CDK TestCase implementations that ensures that
+ * the LoggingTool is configured.
+ *
+ * @cdk.module test
+ */
+public class CDKTestCase extends TestCase {
+
+    static {
+        LoggingTool.configureLog4j();
+    }
+    
+    public CDKTestCase(String name) {
+        super(name);
+    }
+    
+}
