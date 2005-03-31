3472a0fcbc31b418b5cd40db90811fb1ddc5fa8c
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-03-31 05:29:37 +0000

    Some minor cleanup of the alignment code and added another test using Atom[] to the JUnit test for Kabsch alignment
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4004 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/geometry/alignment/KabschAlignment.java
+++ src/org/openscience/cdk/geometry/alignment/KabschAlignment.java
@@ -309,14 +309,12 @@ public class KabschAlignment {
         }
 
         // make sure that the a3 = a1 x a2
-        double[] tmpa = new double[3];
         a[0][2] = (a[1][0]*a[2][1]) - (a[1][1]*a[2][0]);
         a[1][2] = (a[0][1]*a[2][0]) - (a[0][0]*a[2][1]);
         a[2][2] = (a[0][0]*a[1][1]) - (a[0][1]*a[1][0]);
 
         // lets work out the b vectors
         double[][] b = new double[3][3];
-
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 for (int k = 0; k < 3; k++) {
--- src/org/openscience/cdk/test/geometry/alignment/KabschAlignmentTest.java
+++ src/org/openscience/cdk/test/geometry/alignment/KabschAlignmentTest.java
@@ -56,9 +56,49 @@ public class KabschAlignmentTest extends CDKTestCase {
         KabschAlignment ka = new KabschAlignment(ac,ac);
         ka.align();
         double rmsd = ka.getRMSD();
-
         assertTrue(1e-8 > rmsd);
         assertNotNull(ka.getRotationMatrix());
+
+        double[][] p1 = {
+            {16.754  ,20.462  ,45.049  },
+            {19.609  ,18.145  ,46.011  },
+            {17.101  ,17.256  ,48.707  },
+            {13.963  ,18.314  ,46.820  },
+            {14.151  ,15.343  ,44.482  },
+            {14.959  ,12.459  ,46.880  },
+            {11.987  ,13.842  ,48.862  },
+            {9.586  ,12.770  ,46.123  },
+            {11.006   ,9.245  ,46.116  },
+            {10.755   ,9.090  ,49.885  }
+        };
+        double[][] p2 = {
+            {70.246 ,317.510 ,188.263  },
+            {73.457 ,317.369 ,190.340  },
+            {71.257 ,318.976 ,193.018  },
+            {68.053 ,317.543 ,191.651  },
+            {68.786 ,313.954 ,192.637  },
+            {70.248 ,314.486 ,196.151  },
+            {67.115 ,316.584 ,196.561  },
+            {64.806 ,313.610 ,196.423  },
+            {66.804 ,311.735 ,199.035  },
+            {66.863 ,314.832 ,201.113  }
+        };
+        Atom[] a1 = new Atom[10];
+        Atom[] a2 = new Atom[10];
+        for (int i = 0; i < 10; i++) {
+            a1[i] = new Atom("C");
+            a1[i].setX3d( p1[i][0] );
+            a1[i].setY3d( p1[i][1] );
+            a1[i].setZ3d( p1[i][2] );
+            a2[i] = new Atom("C");
+            a2[i].setX3d( p2[i][0] );
+            a2[i].setY3d( p2[i][1] );
+            a2[i].setZ3d( p2[i][2] );
+        }
+        ka = new KabschAlignment(a1,a2);
+        ka.align();
+        rmsd = ka.getRMSD();
+        assertEquals(0.13479726, rmsd, 0.00000001);
     }
     
 }
