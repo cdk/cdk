42556a6640225f2936e6f104313e2a1f9f924f94
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-04-22 13:14:04 +0000

    First addition of applet dir files, tagged with new module tag
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4170 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/applications/jchempaint/applet/AppletCanvas.java
@@ -0,0 +1,131 @@
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 2002-2005  The Jmol Development Team
+ *
+ *  Contact: jmol-developers@lists.sf.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.jchempaint.applet;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.vecmath.Point2d;
+
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.geometry.GeometryTools;
+import org.openscience.cdk.renderer.Renderer2D;
+import org.openscience.cdk.tools.manipulator.ChemModelManipulator;
+import org.openscience.jchempaint.JChemPaintModel;
+
+/**
+ * The canvas for a JChempaint applet
+ *
+ * @cdk.module jchempaint
+ * @author     steinbeck
+ * @created    22. April 2005
+ */
+public class AppletCanvas extends Canvas
+{
+
+	private Renderer2D renderer;
+	private JChemPaintModel model;
+
+
+	/**
+	 *  Sets the renderer attribute of the AppletCanvas object
+	 *
+	 *@param  renderer  The new renderer value
+	 */
+	public void setRenderer(Renderer2D renderer)
+	{
+		this.renderer = renderer;
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  g  Description of the Parameter
+	 */
+	public void update(Graphics g)
+	{
+		paint(g);
+	}
+
+
+	/**
+	 *  Sets the jChemPaintModel attribute of the AppletCanvas object
+	 *
+	 *@param  model  The new jChemPaintModel value
+	 */
+	public void setJChemPaintModel(JChemPaintModel model)
+	{
+		this.model = model;
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  g  Description of the Parameter
+	 */
+	public void paint(Graphics g)
+	{
+		renderer.getRenderer2DModel().setBackgroundDimension(getSize());
+		AtomContainer container = null;
+		if (model != null)
+		{
+			container = ChemModelManipulator.getAllInOneContainer(model.getChemModel());
+		} else
+		{
+			Atom carbon = new Atom("C");
+			carbon.setHydrogenCount(4);
+			carbon.setPoint2d(new Point2d(1.0, 1.0));
+			container.addAtom(carbon);
+		}
+		/*
+		 *  this code ensures that the molecule ends up somewhere in the model
+		 *  of the view screen
+		 */
+		GeometryTools.translateAllPositive(container);
+		double scaleFactor = GeometryTools.getScaleFactor(container, 40.0);
+		GeometryTools.scaleMolecule(container, scaleFactor);
+		GeometryTools.center(container, getSize());
+
+		setBackground(Color.white);
+		if (model != null)
+		{
+			renderer.paintChemModel(model.getChemModel(), (Graphics2D) g);
+		} else
+		{
+			renderer.paintMolecule(container, (Graphics2D) g);
+		}
+	}
+
+}
+
