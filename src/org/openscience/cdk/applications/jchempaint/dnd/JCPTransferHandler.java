9bcc7486a6494690cc3ecf45f9bf7d24fad82211
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-04-22 13:31:51 +0000

    Addition of directory DnD.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4175 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/applications/jchempaint/dnd/JCPTransferHandler.java
@@ -0,0 +1,122 @@
+/*  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 2004-2005  The JChemPaint project
+ *
+ *  Contact: jchempaint-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk.applications.jchempaint.dnd;
+
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.datatransfer.*;
+import java.awt.dnd.*;
+import javax.swing.*;
+
+import org.openscience.cdk.tools.LoggingTool;
+
+/**
+ * Created after http://java.sun.com/docs/books/tutorial/uiswing/misc/example-1dot4/ArrayListTransferHandler.java
+ *
+ * @cdk.module jchempaint
+ * @cdk.creator Egon Willighagen
+ */
+public class JCPTransferHandler extends TransferHandler {
+    
+    private DataFlavor localFlavor = DataFlavor.plainTextFlavor;
+
+	private LoggingTool logger;
+    private String handlerFor;
+    
+    public JCPTransferHandler(String handlerFor) {
+        logger = new LoggingTool(this);
+        this.handlerFor = handlerFor;
+        logger.debug("Instantiated new transfer handler for:", handlerFor);
+    }
+
+    public boolean importData(JComponent c, Transferable t) {
+        logger.debug(handlerFor, "Importing data into: ", c.getClass().getName());
+        if (!canImport(c, t.getTransferDataFlavors())) {
+            logger.debug("Cannot import data...");
+            return false;
+        }
+
+        return false;
+    }
+
+    protected void exportDone(JComponent c, Transferable data, int action) {
+        // nothing to be done right now
+    }
+
+    public boolean canImport(JComponent c, DataFlavor[] flavors) {
+        logger.debug(handlerFor, "Offered formats:");
+        for (int i=0; i<flavors.length; i++) {
+            logger.debug("  ", flavors[i].getMimeType());
+        }
+        return true;
+    }
+
+    protected Transferable createTransferable(JComponent c) {
+        logger.debug(handlerFor, "Creating transferable...");
+        if (false) {
+            // return new JCPTransferable(some content);
+        }
+        return null;
+    }
+
+    public int getSourceActions(JComponent c) {
+        logger.debug(handlerFor, "Getting source action...");
+        return COPY_OR_MOVE;
+    }
+
+    public class JCPTransferable implements Transferable {
+        
+        ArrayList data;
+
+        public JCPTransferable(ArrayList alist) {
+            data = alist;
+        }
+
+        public Object getTransferData(DataFlavor flavor)
+                                 throws UnsupportedFlavorException {
+            if (!isDataFlavorSupported(flavor)) {
+                throw new UnsupportedFlavorException(flavor);
+            }
+            return data;
+        }
+
+        public DataFlavor[] getTransferDataFlavors() {
+            return new DataFlavor[] { localFlavor };
+        }
+
+        public boolean isDataFlavorSupported(DataFlavor flavor) {
+            if (localFlavor.equals(flavor)) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
