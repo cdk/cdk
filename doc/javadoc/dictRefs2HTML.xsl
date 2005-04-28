4bcd6d617c05cbfa3c2e754b4d549ed53dd32248
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-04-28 14:03:17 +0000

    Added Taglet for creating Plone pages
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4287 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ doc/javadoc/dictRefs2HTML.xsl
@@ -0,0 +1,59 @@
+<?xml version="1.0"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns="http://www.w3.org/1999/xhtml"
+                version="1.0">
+
+  <!-- $Author$
+       $Date$
+       $Revision$ -->
+
+  <xsl:output method="xml" indent="yes"
+    omit-xml-declaration="no" encoding="utf-8"/>
+
+  <xsl:template match="*">
+    <xsl:apply-templates/>
+  </xsl:template>
+
+  <xsl:template match="text()">
+    <xsl:value-of select="."/>
+  </xsl:template>
+
+  <xsl:template match="/">
+<div><div>
+<h2>Dictionary References</h2>
+</div></div>
+
+<div><div>
+<h3>Blue Obelisk Chemoinformatics Dictionary</h3>
+</div></div>
+<table>
+<xsl:for-each select="//dictRef[./dictionary='blue-obelisk']">
+  <xsl:sort select="."/>
+  <xsl:apply-templates select="."/>
+</xsl:for-each>
+</table>
+
+<div><div>
+<h3>QSAR.sf.net Descriptors Dictionary</h3>
+</div></div>
+<table>
+<xsl:for-each select="//dictRef[./dictionary='qsar-descriptors']">
+  <xsl:sort select="."/>
+  <xsl:apply-templates select="."/>
+</xsl:for-each>
+</table>
+  </xsl:template>
+
+  <xsl:template match="dictRef">
+<tr>
+  <td><xsl:value-of select="./dictionary"/><xsl:text>:</xsl:text><xsl:value-of select="./entry"/></td>
+  <td>
+   <xsl:element name="a">
+     <xsl:attribute name="href">http://cdk.sf.net/<xsl:value-of select="./api"/></xsl:attribute>
+     <xsl:value-of select="./class"/>
+   </xsl:element>
+  </td>
+</tr>
+  </xsl:template>
+
+</xsl:stylesheet>
---
+++ doc/javadoc/source/MakeDictionaryIndexDoclet.java
@@ -0,0 +1,134 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2004  The Chemistry Development Kit (CDK) project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+import com.sun.javadoc.*;
+import java.util.*;
+import java.io.*;
+
+/**
+ * This class makes an overview of which classes refer to which entry in 
+ * which dictionary.
+ */
+public class MakeDictionaryIndexDoclet {
+
+    private final String webUrl = "http://cdk.sf.net/";
+
+    private final String javaDocDictRefTag = "cdk.dictref";
+    private Hashtable jarDependencies;
+
+    public MakeDictionaryIndexDoclet() {
+        jarDependencies = new Hashtable();
+    }
+
+    private String toAPIPath(String className) {
+        StringBuffer sb = new StringBuffer();
+        className = className;
+        for (int i=0; i<className.length(); i++) {
+            if (className.charAt(i) == '.') {
+                sb.append('/');
+            } else {
+                sb.append(className.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public void process(RootDoc root) throws IOException {
+        processPackages(root.specifiedPackages());
+
+        // the dictionary references
+        PrintWriter out = new PrintWriter((Writer)new FileWriter("cdk.dictref.xml"));
+        out.println("<dictRefs>");
+        Enumeration keys = jarDependencies.keys();
+        while (keys.hasMoreElements()) {
+            String key = (String)keys.nextElement();
+            StringTokenizer tokenizer = new StringTokenizer(key, ":");
+            if (tokenizer.countTokens() != 2) {
+                out.println("  <!-- unparsable dictref: " + key + " -->");
+            } else {
+                String dictCode = tokenizer.nextToken();
+                String entry = tokenizer.nextToken();
+                Vector jarClasses = (Vector)jarDependencies.get(key);
+                Enumeration classes = jarClasses.elements();
+                while (classes.hasMoreElements()) {
+                    out.println("  <dictRef>");
+                    out.println("    <dictionary>" + dictCode + "</dictionary>");
+                    out.println("    <entry>" + entry + "</entry>");
+                    String packageClass = (String)classes.nextElement();
+                    out.println("    <class>" + packageClass.substring(16) + "</class>");
+                    out.println("    <api>api/" + toAPIPath(packageClass) + ".html</api>");
+                    out.println("  </dictRef>");
+                }
+            }
+        }
+        out.println("</dictRefs>");
+        out.flush(); out.close();
+    }
+
+    private void processPackages(PackageDoc[] pkgs) throws IOException {
+        for (int i=0; i < pkgs.length; i++) {
+            processClasses(pkgs[i].allClasses());
+        }
+    }
+
+    private void addClass(Hashtable table, String packageClass, String cdkPackageName) {
+        Vector packageClasses = (Vector)table.get(cdkPackageName);
+        if (packageClasses == null) {
+            packageClasses = new Vector();
+            table.put(cdkPackageName, packageClasses);
+        }
+        packageClasses.addElement(packageClass);
+    }
+    
+    private void processClass(ClassDoc classDoc) throws IOException {
+        if (classDoc == null) return;
+        
+        String className = classDoc.qualifiedName();
+        // first deal with build dependencies
+        Tag[] tags = classDoc.tags(javaDocDictRefTag);
+        if (tags.length > 0) {
+            for (int i=0; i<tags.length; i++) {
+                addClass(jarDependencies, className, tags[i].text());
+            }
+        }
+    }
+    
+    private void processClasses(ClassDoc[] classes) throws IOException {
+        for (int i=0; i<classes.length; i++) {
+            ClassDoc doc = classes[i];
+            processClass(doc);
+        }
+    }
+
+    public static boolean start(RootDoc root) {
+        try {
+            MakeDictionaryIndexDoclet doclet = new MakeDictionaryIndexDoclet();
+            doclet.process(root);
+            return true;
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
