3ab33e149d43ca556847a85e1395b5ef04606561
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-05-06 10:00:34 +0000

    Also introduces an AtomContainerBondPermutor and added a test in SmilesGeneratorTest to see if bond permutation leads to same canonical SMILES
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4371 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/graph/AtomContainerBondPermutor.java
@@ -0,0 +1,79 @@
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 1997-2005  The Chemistry Development Kit (CDK) project
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk.graph;
+
+import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.graph.AtomContainerPermutor;;
+
+
+/**
+ *
+ *@author         steinbeck
+ *@created        4. Mai 2005
+ *@cdk.created    2002-02-24
+ *@cdk.keyword    permutation
+ */
+public class AtomContainerBondPermutor extends AtomContainerPermutor
+{
+	
+	public AtomContainerBondPermutor(AtomContainer ac)
+	{
+		setAtomContainer(ac);
+		N = atomContainer.getBondCount();
+		initBookkeeping();
+		initObjectArray();
+	}
+	
+	public void initObjectArray()
+	{
+		Bond[] bonds = atomContainer.getBonds();
+		objects = new Object[atomContainer.getBondCount()];
+		for (int f = 0; f < N; f++)
+		{
+			objects[f] = bonds[f];	
+		}
+	}
+	
+	AtomContainer makeResult()
+	{
+		Bond[] bonds = new Bond[objects.length];
+		for (int f = 0; f < objects.length; f++)
+		{
+			bonds[f] = ((Bond)objects[f]);	
+		}
+		AtomContainer ac = new AtomContainer(atomContainer);
+		ac.setElectronContainers(bonds);
+		return (AtomContainer)ac.clone();
+	}
+	
+}
+
