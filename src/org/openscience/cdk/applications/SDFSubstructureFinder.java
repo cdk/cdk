4a5d965763bc340a81fd8267d80b0552de77d6d3
steinbeck <steinbeck@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-05-08 11:53:31 +0000

    Added a command line application for searching a given structure in all the structures of an SDF file
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4387 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/applications/SDFSubstructureFinder.java
@@ -0,0 +1,119 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2003-2005  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk.applications;
+
+import java.io.File;
+import java.io.FileReader;
+
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.CDKConstants;
+import org.openscience.cdk.io.ChemObjectReader;
+import org.openscience.cdk.io.ReaderFactory;
+import org.openscience.cdk.io.iterator.IteratingMDLReader;
+import org.openscience.cdk.isomorphism.matchers.QueryAtomContainer;
+import org.openscience.cdk.isomorphism.UniversalIsomorphismTester;
+import org.openscience.cdk.smiles.smarts.SMARTSParser;
+import org.openscience.cdk.tools.LoggingTool;
+import org.openscience.cdk.smiles.SmilesGenerator;
+
+/**
+ * Command line utility that matches the given SMARTS against the given set of 
+ * files.
+ *
+ * @cdk.module  experimental
+ *
+ * @author      Egon Willighagen
+ * @cdk.created 2003-08-14
+ * @cdk.require java1.4+
+ * @cdk.require ant1.6
+ */
+public class SDFSubstructureFinder {
+
+	/*
+	 *  This is a command line application            *
+	 *  Do not convert these System.out/err.println() *
+	 *  to logger statements
+	 */
+	
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("syntax: SubstructureFinder <queryfile> <file1.sdf> <file2.sdf> ...");
+            System.exit(0);
+        }
+
+        // to make sure the CDK LoggingTool is configured
+        LoggingTool logger = new LoggingTool();
+        LoggingTool.configureLog4j();
+        logger.dumpSystemProperties();
+	String ifilename = args[0];
+	Molecule substructure = null;
+	SmilesGenerator smilesGenerator = new SmilesGenerator();
+       try {
+	    File input = new File(ifilename);
+	    if (!input.isDirectory()) {
+		ChemObjectReader reader = new ReaderFactory().createReader(new FileReader(input));
+		if (reader.accepts(new Molecule())) {
+		   substructure = (Molecule)reader.read(new Molecule());
+		}
+	    }
+	} catch (Exception exception) {
+	    System.err.println(ifilename + ": error=");
+	    exception.printStackTrace();
+	}
+ 
+        if (substructure != null) {
+		Molecule molecule;
+            // loop over all files
+            for (int i=1; i<args.length; i++) {
+                ifilename = args[i];
+                try {
+                    File input = new File(ifilename);
+                    if (!input.isDirectory()) {
+                        IteratingMDLReader reader = new IteratingMDLReader(new FileReader(input));
+                        while (reader.hasNext()) {
+                            molecule = (Molecule)reader.next();
+                            if (molecule != null) {
+				//System.out.print(".");
+                                boolean matches = UniversalIsomorphismTester.isSubgraph(molecule, substructure);
+                                if (matches) {
+                                    System.out.println("Query matches molecule with title: " + molecule.getProperty(CDKConstants.TITLE));
+				    System.out.println("Hit molecule's remark: " + molecule.getProperty(CDKConstants.REMARK));
+				    System.out.println("Hit molecule's SMILES is: " + smilesGenerator.createSMILES(molecule));
+				    
+                                }
+                            }
+                        }
+                    }
+                } catch (Exception exception) {
+                    System.err.println(ifilename + ": error=");
+                    exception.printStackTrace();
+                }
+            }
+        } else {
+            System.err.println("Too few arguments."); 
+            System.exit(-1);
+        }
+    }
+
+}
