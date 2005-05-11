377085f1e2199dbb31c11488597a676c117d29ce
tohel <tohel@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-05-11 08:44:58 +0000

    removed double entry in PopUpMenu integrated BondEditor in PopUpMenu preferences
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4413 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/applications/jchempaint/action/EditChemObjectPropsAction.java
+++ src/org/openscience/cdk/applications/jchempaint/action/EditChemObjectPropsAction.java
@@ -1,29 +1,30 @@
-/* $RCSfile$
- * $Author$
- * $Date$
- * $Revision$
+/*
+ *  $RCSfile$
+ *  $Author$
+ *  $Date$
+ *  $Revision$
  *
- * Copyright (C) 2003-2005  The JChemPaint project
+ *  Copyright (C) 2003-2005  The JChemPaint project
  *
- * Contact: jchempaint-devel@lists.sf.net
+ *  Contact: jchempaint-devel@lists.sf.net
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- * All we ask is that proper credit is given for our work, which includes
- * - but is not limited to - adding the above copyright notice to the beginning
- * of your source code files, and to any copyright notice that you may distribute
- * with programs based on this work.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package org.openscience.cdk.applications.jchempaint.action;
 
@@ -33,45 +34,58 @@ import org.openscience.cdk.Atom;
 import org.openscience.cdk.ChemObject;
 import org.openscience.cdk.PseudoAtom;
 import org.openscience.cdk.Reaction;
+import org.openscience.cdk.Bond;
 import org.openscience.cdk.applications.swing.editor.AtomEditor;
 import org.openscience.cdk.applications.swing.editor.ChemObjectEditor;
 import org.openscience.cdk.applications.swing.editor.PseudoAtomEditor;
 import org.openscience.cdk.applications.swing.editor.ReactionEditor;
+import org.openscience.cdk.applications.swing.editor.BondEditor;
 
 import org.openscience.cdk.applications.jchempaint.JChemPaintModel;
 import org.openscience.cdk.applications.jchempaint.dialogs.ChemObjectPropertyDialog;
 
-
 /**
  * Action for triggering an edit of a ChemObject
  *
- * @cdk.module jchempaint
- * @author E.L. Willighagen <elw38@cam.ac.uk>
+ * @author        E.L. Willighagen <elw38@cam.ac.uk>
+ * @created       2. Mai 2005
+ * @cdk.module    jchempaint
  */
 public class EditChemObjectPropsAction extends JCPAction {
 
-    public void actionPerformed(ActionEvent event) {
-        if (jcpPanel.getJChemPaintModel() != null) {
-            JChemPaintModel jcpmodel = jcpPanel.getJChemPaintModel();
-            ChemObject object = getSource(event);
-            logger.debug("Showing object properties for: ", object);
-            ChemObjectEditor editor = null;
-            if (object instanceof PseudoAtom) {
-                editor = new PseudoAtomEditor();
-            } else if (object instanceof Atom) {
-                editor = new AtomEditor();
-            } else if (object instanceof Reaction) {
-                editor = new ReactionEditor();
-            }
+	/**
+	 *  Description of the Method
+	 *
+	 * @param  event  Description of the Parameter
+	 */
+	public void actionPerformed(ActionEvent event) {
+		if (jcpPanel.getJChemPaintModel() != null) {
+			JChemPaintModel jcpmodel = jcpPanel.getJChemPaintModel();
+			ChemObject object = getSource(event);
+			logger.debug("Showing object properties for: ", object);
+			ChemObjectEditor editor = null;
+			if (object instanceof PseudoAtom) {
+				editor = new PseudoAtomEditor();
+			}
+			else if (object instanceof Atom) {
+				editor = new AtomEditor();
+			}
+			else if (object instanceof Reaction) {
+				editor = new ReactionEditor();
+			}
+			else if (object instanceof Bond) {
+				editor = new BondEditor();
+			}
+			
+			if (editor != null) {
+				editor.setChemObject(object);
+				ChemObjectPropertyDialog frame =
+						new ChemObjectPropertyDialog(jcpmodel, editor);
+				frame.pack();
+				frame.show();
+			}
+		}
+	}
 
-            if (editor != null) {
-                editor.setChemObject(object);
-                ChemObjectPropertyDialog frame =
-                    new ChemObjectPropertyDialog(jcpmodel, editor);
-                frame.pack();
-                frame.show();
-            }
-        }
-    }
-    
 }
+
