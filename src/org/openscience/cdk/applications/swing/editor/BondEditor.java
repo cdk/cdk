92a5abe1ab2fed3fcfa0228325faa983d24a4487
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-05-11 08:32:29 +0000

    Added simple bond editor
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4412 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/applications/swing/editor/BondEditor.java
@@ -0,0 +1,75 @@
+/* $RCSfile$   
+ * $Author$   
+ * $Date$    
+ * $Revision$
+ * 
+ * Copyright (C) 2001-2005  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. 
+ */
+package org.openscience.cdk.applications.swing.editor;
+
+import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.JOptionPane;
+import javax.swing.SpinnerNumberModel;
+
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.applications.swing.FieldTablePanel;
+
+/**
+ * @cdk.module applications
+ * @cdk.require swing
+ */
+public class BondEditor extends ChemObjectEditor {
+    
+    JTextField orderField;
+    
+    public BondEditor() {
+        super();
+        constructPanel();
+    }
+    
+    private void constructPanel() {
+        orderField = new JTextField(20);
+        addField("Order", orderField);
+    }
+    
+    public void setChemObject(ChemObject object) {
+        if (object instanceof Bond) {
+            source = object;
+            // update table contents
+            Bond bond = (Bond)source;
+            orderField.setText("" + bond.getOrder());
+        } else {
+            throw new IllegalArgumentException("Argument must be an Bond");
+        }
+    }
+	
+    public void applyChanges() {
+        Bond bond = (Bond)source;
+        try {
+            double newOrder = Double.parseDouble(orderField.getText());
+            bond.setOrder(newOrder);
+        } catch (NumberFormatException exception) {
+            JOptionPane.showMessageDialog(null, "The entered bond order is not a double: " + orderField.getText());
+        }
+    }
+}
+
+
