503d2d1bcc04f5eb6de69faf38007c8fda86d56b
dirk49 <dirk49@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-06-05 22:46:23 +0000

    initial
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4477 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/applications/swing/JExternalFrame.java
@@ -0,0 +1,100 @@
+/*
+ *  Copyright (C) 2002-2005  The Jmol Development Team
+ *
+ *  Contact: jmol-developers@lists.sf.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk.applications.swing;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Point;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * @author dirk49
+ *
+ * This class allows to transfer an embedded or applet viewer or editor panel  
+ * to an external frame. This frame can be resized.
+ */
+
+public class JExternalFrame extends JFrame {
+
+	private Component theComponent = null;
+	private Container theParent = null;
+	private JPanel dummyPanel = null;
+	private Dimension embeddedSize = null;
+	private Dimension oldFrameSize = null;
+	private Point oldFrameLocation = null;
+	
+	/**
+	 * @return Returns the dummyPanel.
+	 */
+	private JPanel getDummyPanel() {
+		if (dummyPanel == null)
+			dummyPanel = new JPanel();
+		return dummyPanel;
+	}
+	
+	/**
+	 * @param comp Component that is transfered to the external frame
+	 */
+	public void show(Component comp) {
+		theComponent = comp;
+		if (comp == null)
+			return;
+		theParent = comp.getParent();
+		if (theParent == null)
+			return;
+		embeddedSize = theComponent.getSize(embeddedSize);
+		theParent.remove(theComponent);
+		theParent.add(getDummyPanel());
+		theParent.repaint();
+		if (oldFrameSize == null) {
+			this.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+			getContentPane().setLayout(new BorderLayout());
+		}
+		super.show();
+		if (oldFrameSize == null) {
+			int deltaW = this.getWidth() - getContentPane().getWidth();
+			int deltaH = this.getHeight() - getContentPane().getHeight();
+			this.setSize(embeddedSize.width + deltaW, embeddedSize.height + deltaH);
+		}
+		getContentPane().add(theComponent, BorderLayout.CENTER);
+		this.repaint();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.Window#dispose()
+	 */
+	public void dispose() {
+		theParent.remove(getDummyPanel());
+		oldFrameSize = this.getSize(oldFrameSize);
+		oldFrameLocation = this.getLocation(oldFrameLocation);
+		this.getContentPane().remove(theComponent);
+		theComponent.setSize(embeddedSize);
+		theParent.add(theComponent, BorderLayout.CENTER);
+		super.dispose();
+		theParent.repaint();
+	}
+}
