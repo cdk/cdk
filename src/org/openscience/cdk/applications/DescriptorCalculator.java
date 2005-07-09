8095d8f4658c34c5ee287c609aa906091f3c3fe4
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-07-09 18:07:01 +0000

    Added the option and corresponding code to dump descriptor values in plaintext format
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4586 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/applications/DescriptorCalculator.java
+++ src/org/openscience/cdk/applications/DescriptorCalculator.java
@@ -21,7 +21,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package org.openscience.cdk.applications;
+//package org.openscience.cdk.applications;
 
 import java.io.*;
 import java.util.*;
@@ -33,8 +33,13 @@ import org.openscience.cdk.SetOfMolecules;
 import org.openscience.cdk.io.*;
 import org.openscience.cdk.io.iterator.IteratingMDLReader;
 import org.openscience.cdk.io.listener.PropertiesListener;
-import org.openscience.cdk.qsar.DescriptorEngine;
 import org.openscience.cdk.tools.LoggingTool;
+
+import org.openscience.cdk.qsar.DescriptorEngine;
+import org.openscience.cdk.qsar.DescriptorSpecification;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.result.*;
+
 
 /**
  * Command line utility that calculates QSAR descriptor values.
@@ -50,7 +55,10 @@ public class DescriptorCalculator {
 
     private LoggingTool logger;
     
+    private static boolean firstTime;
     private boolean inputIsSMILES;
+    private String outputFormat = null;
+    private String suffix = null;
     private String[] descTypes = null;
     private PropertiesListener propsListener;
     private DescriptorEngine engine;
@@ -61,6 +69,9 @@ public class DescriptorCalculator {
         logger.dumpSystemProperties();
 
         inputIsSMILES = false;
+        outputFormat = "cml";
+        suffix = ".cml";
+        firstTime = true;
         
         Properties props = new Properties();
         props.setProperty("CMLIDs", "false");
@@ -100,12 +111,58 @@ public class DescriptorCalculator {
     }
 
     private void printCMLMolecule(Writer writer, Molecule molecule) throws Exception {
+        logger.info("Writing output in CML format");
         StringWriter stringWriter = new StringWriter();
         CMLWriter cmlWriter = new CMLWriter(stringWriter);
         cmlWriter.addChemObjectIOListener(propsListener);
         cmlWriter.write(molecule);
         cmlWriter.close();
         writer.write(stringWriter.toString());
+        writer.flush();
+    }
+    private void printTXTMolecule(Writer writer, Molecule molecule) throws Exception {
+        logger.info("Writing output in TXT format");
+        String headerLine = "";
+        StringWriter stringWriter = new StringWriter();
+        List specList = engine.getDescriptorSpecifications();
+
+        for (Iterator it = specList.iterator(); it.hasNext();) {
+            DescriptorSpecification spec = (DescriptorSpecification)it.next();
+            String title = spec.getImplementationTitle();
+
+            // the title contains the full class path. We just need
+            // the last component
+            String[] comps = title.split("\\.");
+            title = comps[ comps.length-1 ];
+
+            DescriptorValue value = (DescriptorValue)molecule.getProperty(spec);
+            DescriptorResult result = value.getValue();
+            if (result instanceof DoubleResult) {
+                stringWriter.write(((DoubleResult)result).doubleValue()+" ");
+                if (firstTime) headerLine = headerLine + title + " ";
+            } else if (result instanceof IntegerResult) {
+                stringWriter.write(((IntegerResult)result).intValue()+" ");
+                if (firstTime) headerLine = headerLine + title + " ";
+            } else if (result instanceof BooleanResult) {
+                stringWriter.write(((BooleanResult)result).booleanValue()+" ");
+                if (firstTime) headerLine = headerLine + title + " ";
+            } else if (result instanceof DoubleArrayResult) {
+                for (int i = 0; i < ((DoubleArrayResult)result).size(); i++) {
+                    stringWriter.write(((DoubleArrayResult)result).get(i)+" ");
+                    if (firstTime) headerLine = headerLine + title + "." + i + " ";
+                }
+            } else if (result instanceof IntegerArrayResult) {
+                for (int i = 0; i < ((IntegerArrayResult)result).size(); i++) {
+                    stringWriter.write(((IntegerArrayResult)result).get(i)+" ");
+                    if (firstTime) headerLine = headerLine + title + "." + i + " ";
+                }
+            }
+        }
+        if (firstTime) {
+            writer.write(headerLine+"\n");
+            firstTime = false;
+        }
+        writer.write(stringWriter.toString()+"\n");
         writer.flush();
     }
     
@@ -119,7 +176,8 @@ public class DescriptorCalculator {
             engineError = true;
         }
         if (!engineError) {
-            printCMLMolecule(writer, molecule);
+            if (outputFormat.equals("cml")) printCMLMolecule(writer, molecule);
+            else printTXTMolecule(writer,molecule);
             if (!inputIsSMILES) System.out.print(".");
         } else {
             if (!inputIsSMILES) System.out.print("x");
@@ -132,9 +190,10 @@ public class DescriptorCalculator {
             if (inputIsSMILES) {
                 writer = new OutputStreamWriter(System.out);
             } else {
-                writer = new FileWriter(new File(toProcess + ".cml"));
+                writer = new FileWriter(new File(toProcess + suffix));
             }
-            printCMLHeader(writer);
+
+            if (outputFormat.equals("cml")) printCMLHeader(writer);
             
             if (inputIsSMILES) {
                 SMILESReader reader = new SMILESReader(
@@ -155,7 +214,7 @@ public class DescriptorCalculator {
                 }
             }
             
-            printCMLFooter(writer);
+            if (outputFormat.equals("cml")) printCMLFooter(writer);
             if (!inputIsSMILES) System.out.println("\n");
         } catch (FileNotFoundException exception) {
             logger.debug(exception);
@@ -191,6 +250,9 @@ public class DescriptorCalculator {
         options.addOption("t","type",true,
                 "specify which types of descriptor to calculate (comma seperated list). "+
                 "Possible values are: constitutional, molecular, topological, geometrical, electronic");
+        options.addOption("o","output",true,
+                "Format in which to output descriptors. Options are 'cml' or'txt' to"+
+                "indicate CML output or comma seperated text");
  
         CommandLine line = null;
         try {
@@ -206,6 +268,15 @@ public class DescriptorCalculator {
         if (line.hasOption("t") || line.hasOption("type")) {
             String optvalue = line.getOptionValue("t");
             descTypes = optvalue.split(",");
+        }
+        if (line.hasOption("o") || line.hasOption("output")) {
+            String optvalue = line.getOptionValue("o");
+            if (!optvalue.equals("cml") && !optvalue.equals("txt")) {
+                System.out.println("Invalid output format");
+                printHelp(options);
+            }
+            outputFormat = optvalue;
+            if (outputFormat.equals("txt")) suffix = ".txt";
         }
     
         String[] filesToConvert = line.getArgs();
@@ -223,6 +294,7 @@ public class DescriptorCalculator {
         System.out.println();
         System.out.println(" OUTPUT FORMATS:");
         System.out.println("  cml    Chemical Markup Language (the default)");
+        System.out.println("  txt    Space seperated text");
         
         System.exit(0);
     }
