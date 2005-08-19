84cfd9c09c76b32d4741c7ddca34b58f9f3dcc0d
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-08-19 08:47:35 +0000

    Added helper method for building amino acid polymers.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4736 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/tools/manipulator/AminoAcidManipulator.java
@@ -0,0 +1,70 @@
+/* $RCSfile$
+ * $Author$ 
+ * $Date$
+ * $Revision$
+ * 
+ * Copyright (C) 2003-2005  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  */
+package org.openscience.cdk.tools.manipulator;
+
+import org.openscience.cdk.AminoAcid;
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.CDKConstants;
+import org.openscience.cdk.exception.CDKException;
+
+/**
+ * Class with convenience methods that provide methods to manipulate
+ * AtomContainer's. For example:
+ * <pre>
+ * AtomContainerManipulator.replaceAtomByAtom(container, atom1, atom2);
+ * </pre>
+ * will replace the Atom in the AtomContainer, but in all the ElectronContainer's
+ * it participates too.
+ *
+ * @cdk.module standard
+ *
+ * @author  Egon Willighagen
+ * @cdk.created 2003-08-07
+ */
+public class AminoAcidManipulator {
+
+	public static void removeAcidicOxygen(AminoAcid acid) throws CDKException {
+		if (acid.getCTerminus() == null) 
+			throw new CDKException("Cannot remove oxygen: C-terminus is not defined!");
+		
+		Bond[] bonds = acid.getConnectedBonds(acid.getCTerminus());
+		// ok, look for the oxygen which is singly bonded
+		for (int i=0; i<bonds.length; i++) {
+			if (bonds[i].getOrder() == CDKConstants.BONDORDER_SINGLE) {
+				Atom[] atoms = bonds[i].getAtoms();
+				if (atoms[i].getSymbol().equals("O")) {
+					// yes, we found a singly bonded oxygen!
+					acid.removeAtomAndConnectedElectronContainers(atoms[i]);
+				}
+			}
+		}
+	}
+
+}
+
