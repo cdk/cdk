8068288e8e4b90e0d0f0a8fcfa5bb23bae33593b
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-08-24 15:11:37 +0000

    Added Bond interface, and clean up code of other interfaces a bit.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4781 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/interfaces/Bond.java
@@ -0,0 +1,169 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 1997-2005  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.openscience.cdk.interfaces;
+
+import javax.vecmath.Point2d;
+import javax.vecmath.Point3d;
+
+/**
+ * Implements the concept of a covalent bond between two or more atoms. A bond is
+ * considered to be a number of electrons connecting two ore more atoms.
+ *
+ * @cdk.module data
+ *
+ * @author      egonw
+ * @cdk.created 2005-08-24
+ * @cdk.keyword bond
+ * @cdk.keyword atom
+ * @cdk.keyword electron
+ */
+public interface Bond extends ElectronContainer {
+
+	/**
+	 * Returns the array of atoms making up this bond.
+	 *
+	 * @return    An array of atoms participating in this bond
+	 * @see       #setAtoms
+	 */
+	public org.openscience.cdk.Atom[] getAtoms();
+
+	/**
+	 * Sets the array of atoms making up this bond.
+	 *
+	 * @param  atoms  An array of atoms that forms this bond
+	 * @see           #getAtoms
+	 */
+	public void setAtoms(Atom[] atoms);
+
+	/**
+	 * Returns the number of Atoms in this Bond.
+	 *
+	 * @return    The number of Atoms in this Bond
+	 */
+	public int getAtomCount();
+
+	/**
+	 * Returns an Atom from this bond.
+	 *
+	 * @param  position  The position in this bond where the atom is
+	 * @return           The atom at the specified position
+	 * @see              #setAtomAt
+	 */
+	public org.openscience.cdk.Atom getAtomAt(int position);
+
+
+	/**
+	 * Returns the atom connected to the given atom.
+	 *
+	 * @param  atom  The atom the bond partner is searched of
+	 * @return       the connected atom or null
+	 */
+	public org.openscience.cdk.Atom getConnectedAtom(Atom atom);
+
+	/**
+	 * Returns true if the given atom participates in this bond.
+	 *
+	 * @param  atom  The atom to be tested if it participates in this bond
+	 * @return       true if the atom participates in this bond
+	 */
+	public boolean contains(Atom atom);
+
+	/**
+	 * Sets an Atom in this bond.
+	 *
+	 * @param  atom      The atom to be set
+	 * @param  position  The position in this bond where the atom is to be inserted
+	 * @see              #getAtomAt
+	 */
+	public void setAtomAt(Atom atom, int position);
+
+	/**
+	 * Returns the bond order of this bond.
+	 *
+	 * @return The bond order of this bond
+	 * @see    org.openscience.cdk.CDKConstants org.openscience.cdk.CDKConstants
+	 *         for predefined values.
+	 * @see    #setOrder
+	 */
+	public double getOrder();
+
+	/**
+	 * Sets the bond order of this bond.
+	 *
+	 * @param  order The bond order to be assigned to this bond
+	 * @see          org.openscience.cdk.CDKConstants for predefined values.
+	 * @see          #getOrder
+	 */
+	public void setOrder(double order);
+
+	/**
+	 * Returns the stereo descriptor for this bond.
+	 *
+	 * @return    The stereo descriptor for this bond
+	 * @see       #setStereo
+	 * @see       org.openscience.cdk.CDKConstants for predefined values.
+	 */
+	public int getStereo();
+
+	/**
+	 * Sets the stereo descriptor for this bond.
+	 *
+	 * @param  stereo  The stereo descriptor to be assigned to this bond.
+	 * @see            #getStereo
+	 * @see            org.openscience.cdk.CDKConstants for predefined values.
+	 */
+	public void setStereo(int stereo);
+
+	/**
+	 * Returns the geometric 2D center of the bond.
+	 *
+	 * @return    The geometric 2D center of the bond
+	 */
+	public Point2d get2DCenter();
+
+	/**
+	 * Returns the geometric 3D center of the bond.
+	 *
+	 * @return    The geometric 3D center of the bond
+	 */
+	public Point3d get3DCenter();
+	
+	/**
+	 * Compares a bond with this bond.
+	 *
+	 * @param  object  Object of type Bond
+	 * @return         Return true, if the bond is equal to this bond
+	 */
+	public boolean compare(Object object);
+
+	/**
+	 * Checks wether a bond is connected to another one.
+	 * This can only be true if the bonds have an Atom in common.
+	 *
+	 * @param  bond  The bond which is checked to be connect with this one
+	 * @return       True, if the bonds share an atom, otherwise false
+	 */
+	public boolean isConnectedTo(Bond bond);
+}
+
--- src/org/openscience/cdk/interfaces/Isotope.java
+++ src/org/openscience/cdk/interfaces/Isotope.java
@@ -11,10 +11,6 @@
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2.1
  * of the License, or (at your option) any later version.
- * All I ask is that proper credit is given for my work, which includes
- * - but is not limited to - adding the above copyright notice to the beginning
- * of your source code files, and to any copyright notice that you may distribute
- * with programs based on this work.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -24,7 +20,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
  */
 package org.openscience.cdk.interfaces;
 
