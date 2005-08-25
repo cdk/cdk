14ae1256eebf3e7052ea3a7da2a540383b205a8d
tohel <tohel@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-08-25 12:48:34 +0000

    fixed the Point2d.clone() problem in FlipAction, too
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4793 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/applications/jchempaint/action/FlipAction.java
+++ src/org/openscience/cdk/applications/jchempaint/action/FlipAction.java
@@ -61,7 +61,7 @@ public class FlipAction extends JCPAction {
             Atom[] atoms = toflip.getAtoms();
             for (int i=0; i<atoms.length; i++) {
                 Point2d atom = atoms[i].getPoint2d();
-                Point2d oldCoord = (Point2d) atom.clone();;
+                Point2d oldCoord = new Point2d(atom.x, atom.y);
                 if (horiz) {
                     atom.y = 2.0*center.y - atom.y;
                 } else {
