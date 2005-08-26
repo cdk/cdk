54bd19754fb2031eda6b1d96e5862e86b986c282
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-08-26 12:59:36 +0000

    Synched aaBondInfo() with templates in CML, and removed the acidic oxygen, restoring the original behaviour of these templates, fixing the PDBReader bug caused by this change.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@4798 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/templates/AminoAcids.java
+++ src/org/openscience/cdk/templates/AminoAcids.java
@@ -33,6 +33,7 @@ import org.openscience.cdk.Bond;
 import org.openscience.cdk.ChemFile;
 import org.openscience.cdk.dict.DictRef;
 import org.openscience.cdk.io.CMLReader;
+import org.openscience.cdk.tools.manipulator.AminoAcidManipulator;
 import org.openscience.cdk.tools.manipulator.ChemFileManipulator;
 import org.openscience.cdk.tools.LoggingTool;
 
@@ -55,161 +56,33 @@ public class AminoAcids {
      * @return info
      */
     public static int[][] aaBondInfo()	{
+
+    	if (aminoAcids == null) {
+            createAAs();
+        }
         
-        int[][] info = {
-                {0, 0, 1, 1},	/*GLYCIN*/  
-                {1, 1, 2, 1},
-                {2, 2, 3, 2},
-                {3, 0, 1, 1},	/*ALANINE*/	
-                {4, 1, 2, 1},
-                {5, 2, 3, 2},
-                {6, 1, 4, 1},
-                {7, 0, 1, 1},	/*VALINE*/
-                {8, 1, 2, 1},
-                {9, 2, 3, 2},
-                {10, 1, 4, 1},
-                {11, 4, 5, 1},
-                {12, 4, 6, 1},
-                {13, 0, 1, 1},	/*LEUCINE*/
-                {14, 1, 2, 1},
-                {15, 2, 3, 2},
-                {16, 1, 4, 1},
-                {17, 4, 5, 1},
-                {18, 5, 6, 1},
-                {19, 5, 7, 1},
-                {20, 0, 1, 1},	/*ISOLEUCINE*/
-                {21, 1, 2, 1},
-                {22, 2, 3, 2},
-                {23, 1, 4, 1},
-                {24, 4, 5, 1},	
-                {25, 4, 6, 1},
-                {26, 5, 7, 1},
-                {27, 0, 1, 1},	/*SERINE*/
-                {28, 1, 2, 1},
-                {29, 2, 3, 2},
-                {30, 1, 4, 1},
-                {31, 4, 5, 1},
-                {32, 0, 1, 1},	/*THREONINE*/
-                {33, 1, 2, 1},
-                {34, 2, 3, 2},
-                {35, 1, 4, 1},
-                {36, 4, 5, 1},
-                {37, 4, 6, 1},
-                {38, 0, 1, 1},	/*CYSTEINE*/
-                {39, 1, 2, 1},
-                {40, 2, 3, 2},
-                {41, 1, 4, 1},
-                {42, 4, 5, 1},
-                {43, 0, 1, 1},	/*METHIONINE*/
-                {44, 1, 2, 1},
-                {45, 2, 3, 2},
-                {46, 1, 4, 1},
-                {47, 4, 5, 1},
-                {48, 5, 6, 1},
-                {49, 6, 7, 1},
-                {50, 0, 1, 1},	/*ASPARTIC ACID*/	
-                {51, 1, 2, 1},
-                {52, 2, 3, 2},
-                {53, 1, 4, 1},
-                {54, 4, 5, 1},
-                {55, 5, 6, 2},
-                {56, 5, 7, 1},
-                {57, 0, 1, 1},	/*ASPARAGINE*/
-                {58, 1, 2, 1},
-                {59, 2, 3, 2},
-                {60, 1, 4, 1},
-                {61, 4, 5, 1},
-                {62, 5, 6, 2},
-                {63, 5, 7, 1},
-                {64, 0, 1, 1},	/*GLUTAMIC ACID*/
-                {65, 1, 2, 1},
-                {66, 2, 3, 2},
-                {67, 1, 4, 1},
-                {68, 4, 5, 1},
-                {69, 5, 6, 1},
-                {70, 6, 7, 2},
-                {71, 6, 8, 1},
-                {72, 0, 1, 1},	/*GLUTAMINE*/
-                {73, 1, 2, 1},
-                {74, 2, 3, 2},
-                {75, 1, 4, 1},
-                {76, 4, 5, 1},
-                {77, 5, 6, 1},
-                {78, 6, 7, 2},
-                {79, 6, 8, 1},
-                {80, 0, 1, 1},	/*ARGININE*/
-                {81, 1, 2, 1},
-                {82, 2, 3, 2},
-                {83, 1, 4, 1},
-                {84, 4, 5, 1},
-                {85, 5, 6, 1},
-                {86, 6, 7, 1},
-                {87, 7, 8, 1},
-                {88, 8, 9, 2},
-                {89, 8, 10, 1},
-                {90, 0, 1, 1},	/*LYSINE*/
-                {91, 1, 2, 1},
-                {92, 2, 3, 2},
-                {93, 1, 4, 1},
-                {94, 4, 5, 1},
-                {95, 5, 6, 1},
-                {96, 6, 7, 1},
-                {97, 7, 8, 1},
-                {98, 0, 1, 1},	/*HISTIDINE*/
-                {99, 1, 2, 1},
-                {100, 2, 3, 2},
-                {101, 1, 4, 1},
-                {102, 4, 5, 1},
-                {103, 5, 6, 1},
-                {104, 5, 7, 2},
-                {105, 6, 8, 2},
-                {106, 7, 9, 1},
-                {107, 8, 9, 2},
-                {108, 0, 1, 1},	/*PHENYLALANINE*/
-                {109, 1, 2, 1},
-                {110, 2, 3, 2},
-                {111, 1, 4, 1},
-                {112, 4, 5, 1},
-                {113, 5, 6, 1},
-                {114, 5, 7, 2},
-                {115, 6, 8, 2},
-                {116, 7, 9, 1},
-                {117, 8, 10, 1},
-                {118, 9, 10, 2},
-                {119, 0, 1, 1},	/*TYROSINE*/
-                {120, 1, 2, 1},
-                {121, 2, 3, 2},
-                {122, 1, 4, 1},
-                {123, 4, 5, 1},
-                {124, 5, 6, 1},
-                {125, 5, 7, 2},
-                {126, 6, 8, 2},
-                {127, 7, 9, 1},
-                {128, 8, 10, 1},
-                {129, 9, 10, 2},
-                {130, 10, 11, 1},
-                {131, 0, 1, 1},	/*TRYPTOPHANE*/
-                {132, 1, 2, 1},
-                {133, 2, 3, 2},
-                {134, 1, 4, 1},
-                {135, 4, 5, 1},
-                {136, 5, 6, 2},
-                {137, 5, 7, 1},
-                {138, 6, 8, 1},
-                {139, 8, 9, 1},
-                {140, 7, 9, 2},
-                {141, 7, 10, 1},
-                {142, 9, 11, 1},
-                {143, 10, 12, 2},
-                {144, 11, 13, 2},
-                {145, 12, 13, 1},
-                {146, 0, 1, 1},	/*PROLINE*/
-                {147, 1, 2, 1},
-                {148, 2, 3, 2},
-                {149, 1, 4, 1},
-                {150, 4, 5, 1},
-                {151, 5, 6, 1},
-                {152, 0, 6, 1}};
+        int[][] info = new int[153][4];
+        
+        int counter = 0;
+        int total = 0;
+        for (int aa=0; aa<aminoAcids.length; aa++) {
+        	AminoAcid acid = aminoAcids[aa];
+        	Bond[] bonds = acid.getBonds();
+        	logger.debug("#bonds for ", acid.getProperty(RESIDUE_NAME).toString(), " = " + bonds.length);
+        	total += bonds.length;
+        	logger.debug("total #bonds: ", total);
+        	for (int bCounter=0; bCounter<bonds.length; bCounter++) {
+        		info[counter][0] = counter;
+        		info[counter][1] = acid.getAtomNumber(bonds[bCounter].getAtomAt(0));
+        		info[counter][2] = acid.getAtomNumber(bonds[bCounter].getAtomAt(1));
+        		info[counter][3] = (int)bonds[bCounter].getOrder();
+        		counter++;
+        	}
+        }
+        
+        if (counter>152) {
+        	logger.error("Error while creating AA info! Bond count is too small: ", counter);
+        }
         
         return info;
     }
@@ -284,6 +157,7 @@ public class AminoAcids {
         		for (int bondCount=0; bondCount<bonds.length; bondCount++) {
         			aminoAcid.addBond(bonds[bondCount]);
         		}
+        		AminoAcidManipulator.removeAcidicOxygen(aminoAcid);
                 aminoAcid.setProperty(NO_ATOMS, "" + aminoAcid.getAtomCount());
                 aminoAcid.setProperty(NO_BONDS, "" + aminoAcid.getBondCount());
                 if (i < aminoAcids.length) {
