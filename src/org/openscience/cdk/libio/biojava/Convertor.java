7e71c16ca5fff053345af31263a912940ceadab1
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-10-12 11:48:46 +0000

    Added dummy class for CDK <-> BioJava conversion.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@5029 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/libio/biojava/Convertor.java
@@ -0,0 +1,50 @@
+/* $RCSfile$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *
+ * Copyright (C) 2002-2005  The Chemistry Development Kit (CDK) project
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+package org.openscience.cdk.libio.biojava;
+
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.graph.matrix.ConnectionMatrix;
+
+/**
+ * Abstract class that provides convertor procedures to
+ * convert CDK classes to <a href="http://www.biojava.org/">BioJava</a>
+ * classes and visa versa.
+ *
+ * @cdk.module libio-biojava
+ *
+ * @cdk.keyword    BioJava
+ * @cdk.keyword    class convertor
+ */
+public class Convertor {
+
+	// TODO: implement convertor methods
+	
+}
