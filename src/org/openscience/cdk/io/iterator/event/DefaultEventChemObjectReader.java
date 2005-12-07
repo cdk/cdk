16db17db5857ff62ff38320446e0657fa4bfa1c9
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-12-07 11:13:47 +0000

    The ChemObjectIO interfaces deprecates accepts(ChemObject) in favor of the new accepts(Class). Implements RFE #1261434.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@5277 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/io/iterator/event/DefaultEventChemObjectReader.java
+++ src/org/openscience/cdk/io/iterator/event/DefaultEventChemObjectReader.java
@@ -58,6 +58,9 @@ public abstract class DefaultEventChemObjectReader implements EventChemObjectRea
     }
 
     public boolean accepts(ChemObject object) {
+    	return accepts(object.getClass());
+    }
+    public boolean accepts(Class objectClass) {
         // leave it up the read(ChemObject) to decide by default
         return true;
     }
