114a0df60ed08f81cbc5176ee383d5adcfe548dc
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-12-15 10:23:20 +0000

    More test classes for the new debug classes.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@5377 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/debug/DebugAtomTest.java
+++ src/org/openscience/cdk/test/debug/DebugAtomTest.java
@@ -1,4 +1,4 @@
-/* $RCSfile$    
+/* $RCSfile$
  * $Author$    
  * $Date$    
  * $Revision$
@@ -20,34 +20,34 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
+ * 
  */
 package org.openscience.cdk.test.debug;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.openscience.cdk.test.exception.CDKExceptionTest;
-import org.openscience.cdk.test.exception.NoSuchAtomExceptionTest;
+import org.openscience.cdk.debug.DebugChemObjectBuilder;
+import org.openscience.cdk.test.AtomContainerTest;
+import org.openscience.cdk.test.AtomTest;
 
 /**
- * TestSuite that runs all the sample tests.
+ * Checks the funcitonality of the AtomContainer.
  *
  * @cdk.module test
  */
-public class DebugDataClassesTests {
+public class DebugAtomTest extends AtomTest {
 
-    public static Test suite () {
-        TestSuite suite= new TestSuite("The cdk.debug Tests");
-        
-        suite.addTest(DebugCoverageTest.suite());
-        
-        suite.addTest(DebugAtomContainerTest.suite());
-        suite.addTest(DebugAtomTest.suite());
-        suite.addTest(DebugAtomTypeTest.suite());
-        suite.addTest(DebugChemObjectTest.suite());
-        suite.addTest(DebugElementTest.suite());
-        suite.addTest(DebugIsotopeTest.suite());
-        return suite;
+    public DebugAtomTest(String name) {
+        super(name);
+    }
+
+    public void setUp() {
+    	super.builder = DebugChemObjectBuilder.getInstance();
+    }
+
+    public static Test suite() {
+        return new TestSuite(DebugAtomTest.class);
     }
 
 }
