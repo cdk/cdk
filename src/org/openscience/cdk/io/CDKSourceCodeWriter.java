d8d61cf3a71ecaedeeb9aa08781a901c9a721893
miguelrojasch <miguelrojasch@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-12-29 23:35:35 +0000

    elimination of unnecessary import
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@5496 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/io/CDKSourceCodeWriter.java
+++ src/org/openscience/cdk/io/CDKSourceCodeWriter.java
@@ -27,11 +27,8 @@
  */
 package org.openscience.cdk.io;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.StringWriter;
