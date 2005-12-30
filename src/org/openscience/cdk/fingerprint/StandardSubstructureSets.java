126922d2071e7cf278efa262ad15e8cca914757b
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2005-12-30 14:09:20 +0000

    Added a factory of substructure 'standards'. No real open/published subset there yet, but that will come in 2006.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@5503 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/fingerprint/StandardSubstructureSets.java
@@ -0,0 +1,43 @@
+package org.openscience.cdk.fingerprint;
+
+import org.openscience.cdk.interfaces.SetOfAtomContainers;
+import org.openscience.cdk.smiles.SmilesParser;
+
+/**
+ * Default sets of atom containers aimed for use with the substructure
+ * 
+ * @author egonw
+ */
+public class StandardSubstructureSets {
+
+	private static SetOfAtomContainers functionalGroupSubstructureSet = null;
+	
+	/**
+	 * @return A set of the functional groups.
+	 */
+	public static SetOfAtomContainers getFunctionalGroupSubstructureSet() throws Exception {
+		if (functionalGroupSubstructureSet == null) {
+			functionalGroupSubstructureSet = new org.openscience.cdk.SetOfAtomContainers();
+			
+			SmilesParser parser = new SmilesParser();
+			String[] groups = {
+				"O=CO", // carboxilyc acid 
+				"[H]N[H]", // amine
+				"O[H]", // hydroxide
+				"COC", // alkoxyalkane
+				"O=C[H]", // aldehyde
+				"O=S(=O)O", // sulfonate
+				"O=P(=O)O", // phosphate
+				"C1=CC=CC=C1" // phenyl
+		    };
+			for (int i=0; i<groups.length; i++) {
+			    functionalGroupSubstructureSet.addAtomContainer(
+			    	parser.parseSmiles(groups[i])
+			    );
+			}
+		}
+		
+		return functionalGroupSubstructureSet;
+	}
+	
+}
---
+++ src/org/openscience/cdk/test/fingerprint/StandardSubstructureSetsTest.java
@@ -0,0 +1,73 @@
+/*
+ * $RCSfile$    
+ * $Author$    
+ * $Date$    
+ * $Revision$
+ * 
+ * Copyright (C) 1997-2005  The Chemistry Development Kit (CKD) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All I ask is that proper credit is given for my work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
+ * 
+ */
+
+package org.openscience.cdk.test.fingerprint;
+
+import java.util.BitSet;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.openscience.cdk.fingerprint.IFingerprinter;
+import org.openscience.cdk.fingerprint.StandardSubstructureSets;
+import org.openscience.cdk.fingerprint.SubstructureFingerprinter;
+import org.openscience.cdk.interfaces.Molecule;
+import org.openscience.cdk.interfaces.SetOfAtomContainers;
+import org.openscience.cdk.templates.MoleculeFactory;
+import org.openscience.cdk.test.CDKTestCase;
+
+/**
+ * @cdk.module test
+ */
+public class StandardSubstructureSetsTest extends CDKTestCase {
+	
+	public StandardSubstructureSetsTest(String name) {
+		super(name);
+	}
+	
+	public static Test suite() {
+		return new TestSuite(StandardSubstructureSetsTest.class);
+	}
+
+	public void testGetFunctionalGroupSubstructureSet() {
+		SetOfAtomContainers set = null;
+		try {
+			set = StandardSubstructureSets.getFunctionalGroupSubstructureSet();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+			fail(exception.getMessage());
+		}
+		
+		assertNotNull(set);
+		assertEquals(8, set.getAtomContainerCount());
+	}
+	
+}
+
