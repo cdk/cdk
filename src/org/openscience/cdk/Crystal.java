a4e502f5fcfb580ddb851ed887549130b2770966
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-04-11 18:37:50 +0000

    Fixed URL: removed newline.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@5915 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/Crystal.java
+++ src/org/openscience/cdk/Crystal.java
@@ -47,8 +47,7 @@ public class Crystal extends AtomContainer implements java.io.Serializable, org.
      *
      * This value must only be changed if and only if the new version
      * of this class is imcompatible with the old version. See Sun docs
-     * for <a href=http://java.sun.com/products/jdk/1.1/docs/guide
-     * /serialization/spec/version.doc.html>details</a>.
+     * for <a href=http://java.sun.com/products/jdk/1.1/docs/guide/serialization/spec/version.doc.html>details</a>.
 	 */
 	private static final long serialVersionUID = 5919649450390509278L;
 
