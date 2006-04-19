1f54c0c8693f47db91f8dbb1b15d26eac6f1c97e
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-04-19 12:41:37 +0000

    Fixed URL.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6008 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/Polymer.java
+++ src/org/openscience/cdk/Polymer.java
@@ -47,8 +47,7 @@ public class Polymer extends Molecule implements java.io.Serializable, org.opens
      *
      * This value must only be changed if and only if the new version
      * of this class is imcompatible with the old version. See Sun docs
-     * for <a href=http://java.sun.com/products/jdk/1.1/docs/guide
-     * /serialization/spec/version.doc.html>details</a>.
+     * for <a href=http://java.sun.com/products/jdk/1.1/docs/guide/serialization/spec/version.doc.html>details</a>.
 	 */
 	private static final long serialVersionUID = -2596790658835319339L;
 
