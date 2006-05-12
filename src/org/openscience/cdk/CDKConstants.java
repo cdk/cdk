0c4d04ebaa238d16b12b875706009f3b929bc5d6
miguelrojasch <miguelrojasch@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-05-12 09:27:18 +0000

    new static integer flag "ISCONJUGATED" with the number 5
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6244 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/CDKConstants.java
+++ src/org/openscience/cdk/CDKConstants.java
@@ -41,164 +41,163 @@ package org.openscience.cdk;
  * @cdk.keyword stereochemistry
  */
 public class CDKConstants {
+	
 
-        /** A bond of degree 1.0. */
-        public final static double BONDORDER_SINGLE = 1.0;
+	/** A bond of degree 1.0. */
+	public final static double BONDORDER_SINGLE = 1.0;
 
-        /** A bond of degree 1.5. To indicate aromaticity, the flag ISAROMATIC should be used instead.
-         *  This constant is used for specific file formats only, and should generally not be used. */
-        public final static double BONDORDER_AROMATIC = 1.5;
+	/** A bond of degree 1.5. To indicate aromaticity, the flag ISAROMATIC should be used instead.
+	 *  This constant is used for specific file formats only, and should generally not be used. */
+    public final static double BONDORDER_AROMATIC = 1.5;
 
-        /** A bond of degree 2.0. */
-        public final static double BONDORDER_DOUBLE = 2.0;
+    /** A bond of degree 2.0. */
+    public final static double BONDORDER_DOUBLE = 2.0;
 
-        /** A bond of degree 3.0. */
-        public final static double BONDORDER_TRIPLE = 3.0;
+    /** A bond of degree 3.0. */
+    public final static double BONDORDER_TRIPLE = 3.0;
 
-        /** A bonds which end is above the drawing plane. */
-        public final static int STEREO_BOND_UP = 1;
-        /** A bonds which start is above the drawing plane. */
-        public final static int STEREO_BOND_UP_INV = 2;
+    /** A bonds which end is above the drawing plane. */
+    public final static int STEREO_BOND_UP = 1;
+    /** A bonds which start is above the drawing plane. */
+    public final static int STEREO_BOND_UP_INV = 2;
 
-        /** A bonds for which the stereochemistry is undefined. */
-        public final static int STEREO_BOND_UNDEFINED = 4;
+    /** A bonds for which the stereochemistry is undefined. */
+    public final static int STEREO_BOND_UNDEFINED = 4;
 
-        /** A bonds for which there is no stereochemistry. */
-        public final static int STEREO_BOND_NONE = 0;
+    /** A bonds for which there is no stereochemistry. */
+    public final static int STEREO_BOND_NONE = 0;
 
-        /** A bonds which end is below the drawing plane.
-         *  The bond is draw from the first to the second bond atom.
-         */
-        public final static int STEREO_BOND_DOWN = -1;
-        /** A bonds which end is below the drawing plane.
-         *  The bond is draw from the second to the first bond atom.
-         */
-        public final static int STEREO_BOND_DOWN_INV = -2;
+    /** A bonds which end is below the drawing plane.
+     *  The bond is draw from the first to the second bond atom.
+     */
+    public final static int STEREO_BOND_DOWN = -1;
+    /** A bonds which end is below the drawing plane.
+     *  The bond is draw from the second to the first bond atom.
+     */
+    public final static int STEREO_BOND_DOWN_INV = -2;
 
-        /** A positive atom parity. */
-        public final static int STEREO_ATOM_PARITY_PLUS = 1;
-        /** A negative atom parity. */
-        public final static int STEREO_ATOM_PARITY_MINUS = -1;
-        /** A undefined atom parity. */
-        public final static int STEREO_ATOM_PARITY_UNDEFINED = 0;
+    /** A positive atom parity. */
+    public final static int STEREO_ATOM_PARITY_PLUS = 1;
+    /** A negative atom parity. */
+    public final static int STEREO_ATOM_PARITY_MINUS = -1;
+    /** A undefined atom parity. */
+    public final static int STEREO_ATOM_PARITY_UNDEFINED = 0;
 
-        /** A undefined hybridization. */
-        public final static int HYBRIDIZATION_UNSET = 0;
-        /** A geometry of neighboring atoms when an s orbital is hybridized
-         *  with one p orbital. */
-        public final static int HYBRIDIZATION_SP1 = 1;
-        /** A geometry of neighboring atoms when an s orbital is hybridized
-         *  with two p orbitals. */
-        public final static int HYBRIDIZATION_SP2 = 2;
-        /** A geometry of neighboring atoms when an s orbital is hybridized
-         *  with three p orbitals. */
-        public final static int HYBRIDIZATION_SP3 = 3;
-	/** A geometry of neighboring atoms when an s orbital is hybridized
-         *  with three p orbitals with one d orbital. */
-        public final static int HYBRIDIZATION_SP3D1 = 4;
-	/** A geometry of neighboring atoms when an s orbital is hybridized
-         *  with three p orbitals with two d orbitals. */
-        public final static int HYBRIDIZATION_SP3D2 = 5;
-	/** A geometry of neighboring atoms when an s orbital is hybridized
-         *  with three p orbitals with three d orbitals. */
-        public final static int HYBRIDIZATION_SP3D3 = 6;
-	/** A geometry of neighboring atoms when an s orbital is hybridized
-         *  with three p orbitals with four d orbitals. */
-        public final static int HYBRIDIZATION_SP3D4 = 7;
-	/** A geometry of neighboring atoms when an s orbital is hybridized
-         *  with three p orbitals with five d orbitals. */
-        public final static int HYBRIDIZATION_SP3D5 = 8;
-        /** 
-         * Carbon NMR shift contant for use as a key in the
-         * IChemObject.physicalProperties hashtable.
-         * @see org.openscience.cdk.ChemObject
-         */
-        public final static String NMRSHIFT_CARBON = "carbon nmr shift";
+    /** A undefined hybridization. */
+    public final static int HYBRIDIZATION_UNSET = 0;
+    /** A geometry of neighboring atoms when an s orbital is hybridized
+     *  with one p orbital. */
+    public final static int HYBRIDIZATION_SP1 = 1;
+    /** A geometry of neighboring atoms when an s orbital is hybridized
+     *  with two p orbitals. */
+    public final static int HYBRIDIZATION_SP2 = 2;
+    /** A geometry of neighboring atoms when an s orbital is hybridized
+     *  with three p orbitals. */
+    public final static int HYBRIDIZATION_SP3 = 3;
+    /** A geometry of neighboring atoms when an s orbital is hybridized
+     *  with three p orbitals with one d orbital. */
+    public final static int HYBRIDIZATION_SP3D1 = 4;
+    /** A geometry of neighboring atoms when an s orbital is hybridized
+     *  with three p orbitals with two d orbitals. */
+    public final static int HYBRIDIZATION_SP3D2 = 5;
+    /** A geometry of neighboring atoms when an s orbital is hybridized
+     *  with three p orbitals with three d orbitals. */
+    public final static int HYBRIDIZATION_SP3D3 = 6;
+    /** A geometry of neighboring atoms when an s orbital is hybridized
+     *  with three p orbitals with four d orbitals. */
+    public final static int HYBRIDIZATION_SP3D4 = 7;
+    /** A geometry of neighboring atoms when an s orbital is hybridized
+     *  with three p orbitals with five d orbitals. */
+    public final static int HYBRIDIZATION_SP3D5 = 8;
+    /** 
+     * Carbon NMR shift contant for use as a key in the
+     * IChemObject.physicalProperties hashtable.
+     * @see org.openscience.cdk.ChemObject
+     */
+    public final static String NMRSHIFT_CARBON = "carbon nmr shift";
+    /** Hydrogen NMR shift contant for use as a key in the
+     * IChemObject.physicalProperties hashtable.
+     * @see org.openscience.cdk.ChemObject
+     */
+    public final static String NMRSHIFT_HYDROGEN = "hydrogen nmr shift";
+    /** Nitrogen NMR shift contant for use as a key in the
+     * IChemObject.physicalProperties hashtable.
+     * @see org.openscience.cdk.ChemObject
+     */
+    public final static String NMRSHIFT_NITROGEN = "nitrogen nmr shift";
 
-        /** Hydrogen NMR shift contant for use as a key in the
-          * IChemObject.physicalProperties hashtable.
-          * @see org.openscience.cdk.ChemObject
-          */
-        public final static String NMRSHIFT_HYDROGEN = "hydrogen nmr shift";
+    /** Phosphorus NMR shift contant for use as a key in the
+     * IChemObject.physicalProperties hashtable.
+     * @see org.openscience.cdk.ChemObject
+     */
+    public final static String NMRSHIFT_PHOSPORUS = "phosphorus nmr shift";
 
-        /** Nitrogen NMR shift contant for use as a key in the
-          * IChemObject.physicalProperties hashtable.
-          * @see org.openscience.cdk.ChemObject
-          */
-        public final static String NMRSHIFT_NITROGEN = "nitrogen nmr shift";
+    /** Fluorine NMR shift contant for use as a key in the
+     * IChemObject.physicalProperties hashtable.
+     * @see org.openscience.cdk.ChemObject
+     */
+    public final static String NMRSHIFT_FLUORINE = "fluorine nmr shift";
 
-        /** Phosphorus NMR shift contant for use as a key in the
-          * IChemObject.physicalProperties hashtable.
-          * @see org.openscience.cdk.ChemObject
-          */
-        public final static String NMRSHIFT_PHOSPORUS = "phosphorus nmr shift";
-
-        /** Fluorine NMR shift contant for use as a key in the
-          * IChemObject.physicalProperties hashtable.
-          * @see org.openscience.cdk.ChemObject
-          */
-        public final static String NMRSHIFT_FLUORINE = "fluorine nmr shift";
-
-        /** Deuterium NMR shift contant for use as a key in the
-          * IChemObject.physicalProperties hashtable.
-          * @see org.openscience.cdk.ChemObject
-          */
-        public final static String NMRSHIFT_DEUTERIUM = "deuterium nmr shift";
+    /** Deuterium NMR shift contant for use as a key in the
+     * IChemObject.physicalProperties hashtable.
+     * @see org.openscience.cdk.ChemObject
+     */
+    public final static String NMRSHIFT_DEUTERIUM = "deuterium nmr shift";
 
 
-        /****************************************
-         * Some predefined flags - keep the     *
-         * numbers below 50 free for other      *
-         * purposes                             *
-         ****************************************/
+    /****************************************
+     * Some predefined flags - keep the     *
+     * numbers below 50 free for other      *
+     * purposes                             *
+     ****************************************/
 
-        /** Flag that is set if the chemobject is placed (somewhere).
-         */
-        public final static int ISPLACED = 0;
-        /** Flag that is set when the chemobject is part of a ring.
-         */
-        public final static int ISINRING = 1;
-        /** Flag that is set if a chemobject is part of an alipahtic chain.
-         */
-        public final static int ISALIPHATIC = 2;
-        /** Flag is set if chemobject has been visited.
-         */
-        public final static int VISITED = 3; // Use in tree searches
-        /** Flag is set if chemobject is part of an aromatic system. */
-        public final static int ISAROMATIC = 4;
-        /** Flag is set if a chemobject is mapped to another chemobject.
-         *  It is used for example in subgraph isomorphism search.
-         */
-        public final static int MAPPED = 5;
+    /** Flag that is set if the chemobject is placed (somewhere).
+     */
+    public final static int ISPLACED = 0;
+    /** Flag that is set when the chemobject is part of a ring.
+     */
+    public final static int ISINRING = 1;
+    /** Flag that is set if a chemobject is part of an alipahtic chain.
+     */
+    public final static int ISALIPHATIC = 2;
+    /** Flag is set if chemobject has been visited.
+     */
+    public final static int VISITED = 3; // Use in tree searches
+    /** Flag is set if chemobject is part of an aromatic system. */
+    public final static int ISAROMATIC = 4;
+    /** Flag is set if chemobject is part of a conjugated system. */
+    public final static int ISCONJUGATED = 5;
+    /** Flag is set if a chemobject is mapped to another chemobject.
+     *  It is used for example in subgraph isomorphism search.
+     */
+    public final static int MAPPED = 6;
 
-        /** Set to true if the atom is an hydrogen bond donor. */
-        public final static int IS_HYDROGENBOND_DONOR = 6;
-        /** Set to true if the atom is an hydrogen bond acceptor. */
-        public final static int IS_HYDROGENBOND_ACCEPTOR = 7;
+    /** Set to true if the atom is an hydrogen bond donor. */
+    public final static int IS_HYDROGENBOND_DONOR = 7;
+    /** Set to true if the atom is an hydrogen bond acceptor. */
+    public final static int IS_HYDROGENBOND_ACCEPTOR = 8;
         
-        /** Flag is set if a chemobject has reactive center.
-         *  It is used for example in reaction.
-         */
-        public static final int REACTIVE_CENTER = 8;
+    /** Flag is set if a chemobject has reactive center.
+     *  It is used for example in reaction.
+     */
+    public static final int REACTIVE_CENTER = 9;
     /**
      * Maximum flags array index.
      */
-    public final static int MAX_FLAG_INDEX = 8;
-
+    public final static int MAX_FLAG_INDEX = 9;
     /**
      * Flag used for JUnit testing the pointer functionality.
      */
     public final static int DUMMY_POINTER = 1;
-
     /**
      * Maximum pointers array index.
      */
     public final static int MAX_POINTER_INDEX = 1;
     
     /****************************************
-    * Some predefined property names for    *
-    * ChemObjects                           *
-    ****************************************/
+     * Some predefined property names for    *
+     * ChemObjects                           *
+     ****************************************/
 
     /** The title for a IChemObject. */
     public static final String TITLE = "Title";
