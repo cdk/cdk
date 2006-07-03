ef956c7a9fccf01de864babca586c6bc49e868da
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-07-03 16:54:15 +0000

    added the python version of the bug analysis tool and removed the perl version
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6571 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ tools/analyzeBugs.py
@@ -0,0 +1,184 @@
+#!/usr/bin/python
+
+# Rajarshi Guha <rajarshi.guha@gmail.com>
+# 07/02/2006
+
+import string, re, urllib, os, sys
+       
+def getBugPage(pageNum, which = 'sf'):
+    filename = 'page%d.html' % (pageNum)
+    offset = (pageNum - 1) * 50
+    if which == 'sf':
+        url = 'http://sourceforge.net/tracker/index.php?func=browse&group_id=20024&atid=120024&set=custom&_assigned_to=0&_status=1&_category=100&_group=100&order=artifact_id&sort=DESC&offset=%d' % (offset)
+        msg = 'Getting SF bugs %d to %d ...' % (offset, offset+49)
+    else:
+        url = 'http://sourceforge.net/tracker/index.php?func=browse&group_id=20024&atid=120024&set=custom&_assigned_to=0&_status=100&_category=100&_group=100&order=artifact_id&sort=DESC&offset=%d' % (offset)
+        msg = 'Getting all bugs %d to %d ...' % (offset, offset+49)
+        
+    print msg
+    data = urllib.urlopen(url)
+    return data.read()
+
+
+
+def analyzeBugs(outputFile, cdkSrcDir):
+
+    openBugs = {}
+    allBugs = {}
+
+    pageCount = 0
+    while True:
+        pageCount += 1
+        data = getBugPage(pageCount, 'sf')
+        bugre = re.compile('func=detail&amp;aid=(?P<aid>[0-9]*)&amp;group_id=(?P<gid>[0-9]*)&amp;atid=(?P<tid>[0-9]*)')
+        bugs = re.findall(bugre, data)
+
+        for bug in bugs:
+            openBugs[bug[0]] = 'open'
+        if data.find('Next 50') == -1: break
+
+    # now do for all the bugs
+    pageCount = 0
+    while True:
+        pageCount += 1
+        data = getBugPage(pageCount, 'all')
+        bugre = re.compile('func=detail&amp;aid=(?P<aid>[0-9]*)&amp;group_id=(?P<gid>[0-9]*)&amp;atid=(?P<tid>[0-9]*)')
+        bugs = re.findall(bugre, data)
+
+        for bug in bugs:
+            allBugs[bug[0]] = 'open'
+        if data.find('Next 50') == -1: break    
+
+
+    # now do the analysis
+
+    testedBugs = {}
+    markedBugs = {}
+
+    # find all Java files
+    javaFiles = []
+    dirname = os.path.join(cdkSrcDir, 'org')
+    
+    for root, dirs, files in os.walk(dirname):
+        for filename in files:
+            if filename.endswith('.java'):
+                javaFiles.append( os.path.join(root, filename) )
+
+
+    for filename in javaFiles:
+        data = open(filename, 'r').readlines()
+        data = ''.join(data)
+        bugre = re.compile('cdk\.bug\s+(?P<id>\d+)')
+        results = re.findall(bugre, data)
+
+        if filename.find( os.path.join('cdk', 'test') ) != -1:
+            for result in results:
+                testedBugs[result] = 'tested'
+        else:
+            for result in results:
+                markedBugs[result] = filename
+
+    print '\n'
+
+    print 'Found bugs on SF = %d' % (len(allBugs.keys()))
+    print ' of which %d are open\n' % (len(openBugs.keys()))
+    print 'Found %d marked bugs' % (len(markedBugs.keys()))
+    print 'Found %d tested bugs' % (len(testedBugs.keys()))
+    print '\n'
+
+    # generate page
+    page = """
+    <html>
+    <head>
+    <title>
+    CDK Bug Analysis
+    </title>
+    </head>
+    <body>
+    <center><h2>Mark up of Open Bugs in JavaDoc</h2></center>
+    Every open bug is supposed to be marked with @cdk.bug in the JavaDoc off the buggy class and/or method.
+    <h3>Open bugs not marked in source</h3>
+    <ul>
+    """
+
+    keys = openBugs.keys()
+    keys.sort()
+    count  = 0
+    for key in keys:
+        if key in markedBugs.keys():
+            pass # it was marked, so OK
+        else:
+            page = page + """
+            <li>Open bug <a href=\"http://sourceforge.net/tracker/index.php?func=detail&aid=%s&group_id=20024&atid=120024\">#%s</a> is <b><i>not</i></b> marked in the source code with @cdk.bug!</li>\n
+            """ % (key, key)
+            count += 1
+    page += "</ul>\n"
+
+    print 'Open bugs that are not marked with @cdk.bug =  %d' % (count)
+
+    page += "<h3>Marked bugs that are now fixed</h3>\n<ul>\n"
+    keys = markedBugs.keys()
+    keys.sort()
+    count  = 0
+    for key in keys:
+        if key in openBugs.keys():
+            pass # all is fine
+        elif key in allBugs.keys():
+            page += """
+            <li>Marked bug <a href=\"http://sourceforge.net/tracker/index.php?func=detail&aid=%s&group_id=20024&atid=120024\">#%s</a> in %s is fixed!</li>\n
+            """ % (key, key, markedBugs[key])
+            count += 1
+        else:
+            page += """
+            <li>Marked bug #%s is not reported within the CDK project.</li>
+            """ % (key)
+    page += "</ul>\n"
+
+    print "Marked bugs that are now fixed = %d" % (count)
+
+    page += """
+    <h3>JUnit tests for reported bugs</h3>
+    Every reported bug should be tested by a JUnit test. Reported bugs with JUnit tests:
+    <ul>
+    """
+
+    keys = allBugs.keys()
+    keys.sort()
+    count = 0
+    for key in keys:
+        if key in testedBugs.keys():
+            pass
+        else:
+            page += """
+            <li>Bug <a href=\"http://sourceforge.net/tracker/index.php?func=detail&aid=%s&group_id=20024&atid=120024\">#%s</a> is not JUnit tested!</li>
+            """ % (key,key)
+            count += 1
+    page += """
+    </ul>
+    """
+
+    print "Bugs that are not JUnit tested = %d\n" % (count)
+            
+    
+
+    page = page + """
+    </body>
+    </html>
+    """
+    f = open(outputFile,'w')
+    f.write(page)
+    f.close()
+
+if __name__ == '__main__':
+
+    outputFile = 'bugAnalysis.html'
+    srcdir = 'cdk/src/'
+
+    analyzeBugs(outputFile, srcdir)
+
+            
+
+
+                                  
+    
+    
