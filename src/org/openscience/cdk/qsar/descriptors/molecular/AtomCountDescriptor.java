dc217559ec54383c12de77c683d0296cf120ce1a
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-07-05 18:48:59 +0000

    Moved a molecular descriptor to the correct package
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6599 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/qsar/descriptors/molecular/AtomCountDescriptor.java
+++ src/org/openscience/cdk/qsar/descriptors/molecular/AtomCountDescriptor.java
@@ -22,7 +22,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-package org.openscience.cdk.qsar.descriptors.atomic;
+package org.openscience.cdk.qsar.descriptors.molecular;
 
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtomContainer;
--- src/org/openscience/cdk/test/qsar/descriptors/molecular/AtomCountDescriptorTest.java
+++ src/org/openscience/cdk/test/qsar/descriptors/molecular/AtomCountDescriptorTest.java
@@ -21,14 +21,14 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
  */
-package org.openscience.cdk.test.qsar.descriptors.atomic;
+package org.openscience.cdk.test.qsar.descriptors.molecular;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.openscience.cdk.AtomContainer;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.qsar.IMolecularDescriptor;
-import org.openscience.cdk.qsar.descriptors.atomic.AtomCountDescriptor;
+import org.openscience.cdk.qsar.descriptors.molecular.AtomCountDescriptor;
 import org.openscience.cdk.qsar.result.IntegerResult;
 import org.openscience.cdk.smiles.SmilesParser;
 import org.openscience.cdk.test.CDKTestCase;
