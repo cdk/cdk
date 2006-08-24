239f8bbda8aa706a34c2de65381c1903e679d0b5
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-08-24 16:07:26 +0000

    Updated Javadocs
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6809 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/qsar/descriptors/molecular/TaeAminoAcidDescriptor.java
+++ src/org/openscience/cdk/qsar/descriptors/molecular/TaeAminoAcidDescriptor.java
@@ -27,6 +27,56 @@ import java.util.Iterator;
  * <p/>
  * The class expects that it will be supplied an object which implements the {@link IBioPolymer}. Thus ordinary
  * AtomContainer objects  will result in an exception.
+ * <p/>
+ * The descriptors are returned in the following order (see
+ * <a href="http://www.chem.rpi.edu/chemweb/recondoc/TAE.doc">here</a>
+ * for a detailed description of the individual descriptors):
+ * <pre>
+ * Energy Population VOLTAE SurfArea
+ * SIDel.Rho.N Del.Rho.NMin Del.Rho.NMax Del.Rho.NIA Del.Rho.NA1
+ * Del.Rho.NA2 Del.Rho.NA3 Del.Rho.NA4 Del.Rho.NA5 Del.Rho.NA6
+ * Del.Rho.NA7 Del.Rho.NA8 Del.Rho.NA9 Del.Rho.NA10 SIDel.K.N
+ * Del.K.Min Del.K.Max Del.K.IA Del.K.NA1 Del.K.NA2
+ * Del.K.NA3 Del.K.NA4 Del.K.NA5 Del.K.NA6 Del.K.NA7
+ * Del.K.NA8 Del.K.NA9 Del.K.NA10 SIK SIKMin
+ * SIKMax SIKIA SIKA1 SIKA2 SIKA3
+ * SIKA4 SIKA5 SIKA6 SIKA7 SIKA8
+ * SIKA9 SIKA10 SIDel.G.N Del.G.NMin Del.G.NMax
+ * Del.G.NIA Del.G.NA1 Del.G.NA2 Del.G.NA3 Del.G.NA4
+ * Del.G.NA5 Del.G.NA6 Del.G.NA7 Del.G.NA8 Del.G.NA9
+ * Del.G.NA10 SIG SIGMin SIGMax SIGIA
+ * SIGA1 SIGA2 SIGA3 SIGA4 SIGA5
+ * SIGA6 SIGA7 SIGA8 SIGA9 SIGA10
+ * SIEP SIEPMin SIEPMax SIEPIA SIEPA1
+ * SIEPA2 SIEPA3 SIEPA4 SIEPA5 SIEPA6
+ * SIEPA7 SIEPA8 SIEPA9 SIEPA10 EP1
+ * EP2 EP3 EP4 EP5 EP6
+ * EP7 EP8 EP9 EP10 PIPMin
+ * PIPMax PIPAvg PIP1 PIP2 PIP3
+ * PIP4 PIP5 PIP6 PIP7 PIP8
+ * PIP9 PIP10 PIP11 PIP12 PIP13
+ * PIP14 PIP15 PIP16 PIP17 PIP18
+ * PIP19 PIP20 Fuk FukMin FukMax
+ * Fuk1 Fuk2 Fuk3 Fuk4 Fuk5
+ * Fuk6 Fuk7 Fuk8 Fuk9 Fuk10
+ * Lapl LaplMin LaplMax Lapl1 Lapl2
+ * Lapl3 Lapl4 Lapl5 Lapl6 Lapl7
+ * Lapl8 Lapl9 Lapl10
+ * </pre>
+ * <p/>
+ * <p>This descriptor uses these parameters:
+ * <table border="1">
+ * <tr>
+ * <td>Name</td>
+ * <td>Default</td>
+ * <td>Description</td>
+ * </tr>
+ * <tr>
+ * <td></td>
+ * <td></td>
+ * <td>no parameters</td>
+ * </tr>
+ * </table>
  *
  * @author Rajarshi Guha
  * @cdk.created 2006-08-23
