7db84324e8293aaa09f455d65acb312fe6f2d25a
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-08-30 09:48:07 +0000

    Set up an interface for atom-pair descriptors.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6821 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/qsar/IAtomPairDescriptor.java
@@ -0,0 +1,50 @@
+/* $Revision: 5855 $ $Author: egonw $ $Date: 2006-03-29 10:27:08 +0200 (Wed, 29 Mar 2006) $
+ * 
+ * Copyright (C) 2006  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openscience.cdk.qsar;
+
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+/**
+ * Classes that implement this interface are QSAR descriptor calculators.
+ *
+ * @cdk.module qsar
+ */
+public interface IAtomPairDescriptor extends IDescriptor {
+    
+    /** 
+     * Calculates the descriptor value for the given IAtom.
+     *
+     * @param  atom         The first {@link IAtom} of the pair for which this
+     * 						descriptor should be calculated
+     * @param  atom2        The second {@link IAtom} of the pair for which this
+     * 						descriptor should be calculated
+     * @param  container TODO
+     * @return              An object of {@link DescriptorValue} that contain the 
+     *                      calculated value as well as specification details
+     * @throws CDKException if an error occurs during calculation. See 
+     *                      documentation for individual descriptors
+     */
+    public DescriptorValue calculate(IAtom atom, IAtom atom2, IAtomContainer container) throws CDKException;
+    
+}
+
