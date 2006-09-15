ab16338b5a5ed4be0ccde5745ac8a0e189ece9c8
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-09-15 12:44:55 +0000

    Fixed @cdk.module line.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6870 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/qsar/descriptors/atomic/PartialTChargeMMFF94Descriptor.java
+++ src/org/openscience/cdk/qsar/descriptors/atomic/PartialTChargeMMFF94Descriptor.java
@@ -1,26 +1,22 @@
-/*
- *  $RCSfile$
- *  $Author: egonw $
- *  $Date: 2006-03-29 10:27:08 +0200 (Wed, 29 Mar 2006) $
- *  $Revision: 5855 $
+/* $Revision: 5855 $ $Author: egonw $ $Date: 2006-03-29 10:27:08 +0200 (Wed, 29 Mar 2006) $
+ * 
+ * Copyright (C) 2006  The Chemistry Development Kit (CDK) project
  *
- *  Copyright (C) 2004-2006  The Chemistry Development Kit (CDK) project
+ * Contact: cdk-devel@lists.sourceforge.net
  *
- *  Contact: cdk-devel@lists.sourceforge.net
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2.1
- *  of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 package org.openscience.cdk.qsar.descriptors.atomic;
 
@@ -54,7 +50,6 @@ import org.openscience.cdk.qsar.result.DoubleResult;
  * @author      Miguel Rojas
  * @cdk.created 2006-04-11
  * @cdk.module  builder3d
- * @cdk.module  qsar
  * @cdk.set     qsar-descriptors
  * @cdk.dictref qsar-descriptors:partialTChargeMMFF94
  * @see MMFF94PartialCharges
