b8803ab2ea39ec5a8a8cad5a90ad80c765773f4d
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-09-17 23:49:26 +0000

    Fixed PMD warnings
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6879 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/io/HINWriter.java
+++ src/org/openscience/cdk/io/HINWriter.java
@@ -23,77 +23,68 @@
  */
 package org.openscience.cdk.io;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.StringWriter;
-import java.io.Writer;
-
-import javax.vecmath.Point3d;
-
 import org.openscience.cdk.CDKConstants;
 import org.openscience.cdk.exception.CDKException;
-import org.openscience.cdk.interfaces.IAtom;
-import org.openscience.cdk.interfaces.IBond;
-import org.openscience.cdk.interfaces.IChemObject;
-import org.openscience.cdk.interfaces.IMolecule;
-import org.openscience.cdk.interfaces.IMoleculeSet;
+import org.openscience.cdk.interfaces.*;
 import org.openscience.cdk.io.formats.HINFormat;
 import org.openscience.cdk.io.formats.IResourceFormat;
+import org.openscience.cdk.tools.LoggingTool;
+
+import javax.vecmath.Point3d;
+import java.io.*;
 //import org.openscience.cdk.tools.LoggingTool;
 
 /**
  * Writer that outputs in the HIN format.
  *
+ * @author Rajarshi Guha <rajarshi@presidency.com>
  * @cdk.module io
- *
- * @author  Rajarshi Guha <rajarshi@presidency.com>
  * @cdk.created 2004-01-27
  */
 public class HINWriter extends DefaultChemObjectWriter {
 
-	static BufferedWriter writer;
-    //private LoggingTool logger; 
-    
+    static BufferedWriter writer;
+
     /**
      * Constructor.
+     *
      * @param out the stream to write the HIN file to.
      */
     public HINWriter(Writer out) {
-    	//logger = new LoggingTool(this);
-    	try {
-    		if (out instanceof BufferedWriter) {
-                writer = (BufferedWriter)out;
+        LoggingTool logger = new LoggingTool(this);
+        try {
+            if (out instanceof BufferedWriter) {
+                writer = (BufferedWriter) out;
             } else {
                 writer = new BufferedWriter(out);
             }
         } catch (Exception exc) {
+            logger.debug(exc.toString());
         }
     }
 
     public HINWriter(OutputStream output) {
         this(new OutputStreamWriter(output));
     }
-    
+
     public HINWriter() {
         this(new StringWriter());
     }
-    
+
     public IResourceFormat getFormat() {
         return HINFormat.getInstance();
     }
-    
+
     public void setWriter(Writer out) throws CDKException {
-    	if (out instanceof BufferedWriter) {
-            writer = (BufferedWriter)out;
+        if (out instanceof BufferedWriter) {
+            writer = (BufferedWriter) out;
         } else {
             writer = new BufferedWriter(out);
         }
     }
 
     public void setWriter(OutputStream output) throws CDKException {
-    	setWriter(new OutputStreamWriter(output));
+        setWriter(new OutputStreamWriter(output));
     }
 
     /**
@@ -103,27 +94,27 @@ public class HINWriter extends DefaultChemObjectWriter {
         writer.close();
     }
 
-	public boolean accepts(Class classObject) {
-		Class[] interfaces = classObject.getInterfaces();
-		for (int i=0; i<interfaces.length; i++) {
-			if (IMolecule.class.equals(interfaces[i])) return true;
-			if (IMoleculeSet.class.equals(interfaces[i])) return true;
-		}
-		return false;
-	}
+    public boolean accepts(Class classObject) {
+        Class[] interfaces = classObject.getInterfaces();
+        for (int i = 0; i < interfaces.length; i++) {
+            if (IMolecule.class.equals(interfaces[i])) return true;
+            if (IMoleculeSet.class.equals(interfaces[i])) return true;
+        }
+        return false;
+    }
 
     public void write(IChemObject object) throws CDKException {
         if (object instanceof IMolecule) {
             try {
                 IMoleculeSet som = object.getBuilder().newMoleculeSet();
-                som.addMolecule((IMolecule)object);
+                som.addMolecule((IMolecule) object);
                 writeMolecule(som);
-            } catch(Exception ex) {
+            } catch (Exception ex) {
                 throw new CDKException("Error while writing HIN file: " + ex.getMessage(), ex);
             }
         } else if (object instanceof IMoleculeSet) {
             try {
-                writeMolecule((IMoleculeSet)object);
+                writeMolecule((IMoleculeSet) object);
             } catch (IOException ex) {
                 //
             }
@@ -136,26 +127,27 @@ public class HINWriter extends DefaultChemObjectWriter {
      * writes all the molecules supplied in a SetOfMolecules class to
      * a single HIN file. You can also supply a single Molecule object
      * as well
-     * @param mol the Molecule to write
+     *
+     * @param som the set of molecules to write
      */
     private void writeMolecule(IMoleculeSet som) throws IOException {
 
         //int na = 0;
         //String info = "";
-        String sym = "";
-        double chrg = 0.0;
+        String sym;
+        double chrg;
         //boolean writecharge = true;
 
         for (int molnum = 0; molnum < som.getMoleculeCount(); molnum++) {
 
-        	IMolecule mol = som.getMolecule(molnum);
+            IMolecule mol = som.getMolecule(molnum);
 
             try {
 
                 int natom = mol.getAtomCount();
                 int nbond = mol.getBondCount();
 
-                String molname = "mol " + (molnum+1) + " " + (String)mol.getProperty(CDKConstants.TITLE);
+                String molname = "mol " + (molnum + 1) + " " + mol.getProperty(CDKConstants.TITLE);
 
                 writer.write(molname, 0, molname.length());
                 writer.newLine();
@@ -173,40 +165,40 @@ public class HINWriter extends DefaultChemObjectWriter {
                     chrg = a.getCharge();
                     Point3d p3 = a.getPoint3d();
 
-                    line = line + new Integer(i+1).toString() + " - " + sym + " ** - " + 
-                        new Double(chrg).toString() + " " +
-                        new Double(p3.x).toString() + " " +
-                        new Double(p3.y).toString() + " " +
-                        new Double(p3.z).toString() + " " ;
+                    line = line + Integer.toString(i + 1) + " - " + sym + " ** - " +
+                            Double.toString(chrg) + " " +
+                            Double.toString(p3.x) + " " +
+                            Double.toString(p3.y) + " " +
+                            Double.toString(p3.z) + " ";
 
                     String buf = "";
                     int ncon = 0;
                     for (int j = 0; j < nbond; j++) {
-                    	IBond b = bonds[j];
+                        IBond b = bonds[j];
                         if (b.contains(a)) {
                             // current atom is in the bond so lets get the connected atom
-                        	IAtom ca = b.getConnectedAtom(a);
+                            IAtom ca = b.getConnectedAtom(a);
                             double bo = b.getOrder();
-                            int serial = -1;
+                            int serial;
                             String bt = "";
 
                             // get the serial no for this atom
                             serial = mol.getAtomNumber(ca);
 
-                            if (bo == 1) bt = new String("s");
-                            else if (bo == 2) bt = new String("d");
-                            else if (bo == 3) bt = new String("t");
-                            else if (bo == 1.5) bt = new String("a");
-                            buf = buf + new Integer(serial+1).toString() + " " + bt + " ";
+                            if (bo == 1) bt = "s";
+                            else if (bo == 2) bt = "d";
+                            else if (bo == 3) bt = "t";
+                            else if (bo == 1.5) bt = "a";
+                            buf = buf + Integer.toString(serial + 1) + " " + bt + " ";
                             ncon++;
                         }
                     }
-                    line = line + " " + new Integer(ncon).toString() + " " + buf;
+                    line = line + " " + Integer.toString(ncon) + " " + buf;
                     writer.write(line, 0, line.length());
                     writer.newLine();
                 }
-                String buf = "endmol " + (molnum+1);
-                writer.write(buf,0,buf.length());
+                String buf = "endmol " + (molnum + 1);
+                writer.write(buf, 0, buf.length());
                 writer.newLine();
             } catch (IOException e) {
                 throw e;
