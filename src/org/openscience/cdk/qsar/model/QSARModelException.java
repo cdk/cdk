7b9629862f37ac6d74a31cba94e37ec0d624121d
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-09-18 14:56:40 +0000

    Added copyright notices
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6905 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/qsar/model/QSARModelException.java
+++ src/org/openscience/cdk/qsar/model/QSARModelException.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2004-2006  The Chemistry Development Kit (CDK) project
+ *  Copyright (C) 2004-2006  Rajarshi Guha <rajarshi@users.sourceforge.net>
  *
  *  Contact: cdk-devel@lists.sourceforge.net
  *
