f266d52268de400982f5dc8c361b083ecfcdf1de
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-09-19 02:13:22 +0000

    Updated copyright and fixed some Javadocs
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6928 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/qsar/descriptors/molecular/MDEDescriptor.java
+++ src/org/openscience/cdk/qsar/descriptors/molecular/MDEDescriptor.java
@@ -1,3 +1,22 @@
+/*
+*  Copyright (C) 2004-2006  Rajarshi Guha <rajarshi@users.sourceforge.net>
+*
+*  Contact: cdk-devel@lists.sourceforge.net
+*
+*  This program is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public License
+*  as published by the Free Software Foundation; either version 2.1
+*  of the License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 package org.openscience.cdk.qsar.descriptors.molecular;
 
 import org.openscience.cdk.exception.CDKException;
@@ -19,10 +38,7 @@ import java.util.Vector;
  * in addition it calculates variants where O and N are considered (as found in the DEDGE routine
  * from ADAPT).
  * <p/>
- * <b>Validated (against Liu et al. and ADAPT)</b>
- * <p/>
- * <hr>
- * The variants are described below:
+ * * The variants are described below:
  * <center>
  * <table border=1>
  * <p/>
