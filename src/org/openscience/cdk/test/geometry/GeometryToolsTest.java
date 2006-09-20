d41265a6adf631e7c645da09c6e49b6769c61e8c
chhoppe <chhoppe@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-09-20 09:26:57 +0000

    add a test for RMSD calculation
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@6960 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/geometry/GeometryToolsTest.java
+++ src/org/openscience/cdk/test/geometry/GeometryToolsTest.java
@@ -23,6 +23,10 @@
  */
 package org.openscience.cdk.test.geometry;
 
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.vecmath.Point2d;
 
 import junit.framework.Test;
@@ -30,7 +34,10 @@ import junit.framework.TestSuite;
 
 import org.openscience.cdk.Atom;
 import org.openscience.cdk.Bond;
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.geometry.GeometryTools;
+import org.openscience.cdk.io.MDLReader;
 import org.openscience.cdk.test.CDKTestCase;
 
 /**
@@ -68,6 +75,51 @@ public class GeometryToolsTest extends CDKTestCase {
         
         assertEquals(1.0, GeometryTools.getLength2D(bond), 0.001);
     }
+    
+    public void testMapAtomsOfAlignedStructures(){
+    	 
+    	String filenameMolOne = "data/mdl/murckoTest6_3d_2.mol";
+		String filenameMolTwo = "data/mdl/murckoTest6_3d.mol";
+    	//String filenameMolTwo = "data/mdl/murckoTest6_3d_2.mol";
+	    InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filenameMolOne);
+	    Molecule molOne=null;
+	    Molecule molTwo=null;
+	    Map mappedAtoms=new HashMap();
+	    try {
+	    	MDLReader reader = new MDLReader(ins);
+	        molOne = (Molecule)reader.read(new Molecule());
+	    } catch (Exception exception) {
+	        System.out.println("Error: Cannot read in mol1 due to "+exception.toString());
+	    }
+		
+	    ins = this.getClass().getClassLoader().getResourceAsStream(filenameMolTwo);
+	    try {
+	    	MDLReader reader = new MDLReader(ins);
+	        molTwo = (Molecule)reader.read(new Molecule());
+	    } catch (Exception exception) {
+	        System.out.println("Error: Cannot read in mol2 due to "+exception.toString());
+	    }
+	   
+	    try {
+			mappedAtoms=GeometryTools.mapAtomsOfAlignedStructures(molOne, molTwo, mappedAtoms);
+			//System.out.println("mappedAtoms:"+mappedAtoms.toString());
+			//System.out.println("***** ANGLE VARIATIONS *****");
+			double AngleRMSD=GeometryTools.getAngleRMSD(molOne,molTwo,mappedAtoms);
+			//System.out.println("The Angle RMSD between the first and the second structure is :"+AngleRMSD);
+			//System.out.println("***** ALL ATOMS RMSD *****");
+			assertEquals(0.2, AngleRMSD, 0.1);
+			double AllRMSD=GeometryTools.getAllAtomRMSD(molOne,molTwo,mappedAtoms,true);
+			//System.out.println("The RMSD between the first and the second structure is :"+AllRMSD);
+			assertEquals(0.242, AllRMSD, 0.001);
+			//System.out.println("***** BOND LENGTH RMSD *****");
+			double BondLengthRMSD=GeometryTools.getBondLengthRMSD(molOne,molTwo,mappedAtoms,true);
+			//System.out.println("The Bond length RMSD between the first and the second structure is :"+BondLengthRMSD);
+			assertEquals(0.2, BondLengthRMSD, 0.1);
+	    } catch (CDKException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	    }
+   }
     
 }
 
