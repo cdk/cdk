3ce6f03b37f0a9aee6d602487911899efae6c616
miguelrojasch <miguelrojasch@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-10-18 15:05:27 +0000

    new descriptor: IPMolecularDescriptor. it calculates the 1,2 and .. energy ionization. Based in machine learning.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7131 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/qsar/descriptors/molecular/IPMolecularDescriptor.java
@@ -0,0 +1,169 @@
+/*
+ *  $RCSfile$
+ *  $Author: kaihartmann $
+ *  $Date: 2006-09-22 17:26:48 +0200 (Fri, 22 Sep 2006) $
+ *  $Revision: 7032 $
+ *
+ *  Copyright (C) 2004-2006  Miguel Rojas <miguel.rojas@uni-koeln.de>
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openscience.cdk.qsar.descriptors.molecular;
+
+import java.util.Iterator;
+
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.qsar.DescriptorSpecification;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.IMolecularDescriptor;
+import org.openscience.cdk.qsar.descriptors.atomic.IPAtomicDescriptor;
+import org.openscience.cdk.qsar.descriptors.bond.IPBondDescriptor;
+import org.openscience.cdk.qsar.model.weka.J48WModel;
+import org.openscience.cdk.qsar.result.DoubleArrayResult;
+import org.openscience.cdk.qsar.result.DoubleResult;
+
+/**
+ *  
+ *  This class returns the ionization potential of an atomContainer. It 
+ *  search 1, 2 and so.. ionization potential. It is
+ *  based in learning machine (in this case J48 see J48WModel) 
+ *  from experimental values. Up to now is
+ *  only possible for atomContainers which contain; see IPAtomicDescriptor and
+ *  IPBondDescriptor.
+ *
+ * <p>This descriptor uses these parameters:
+ * <table border="1">
+ *   <tr>
+ *     <td>Name</td>
+ *     <td>Default</td>
+ *     <td>Description</td>
+ *   </tr>
+ *   <tr>
+ *     <td></td>
+ *     <td></td>
+ *     <td>no parameters</td>
+ *   </tr>
+ * </table>
+ *
+ * @author         Miguel Rojas
+ * @cdk.created    2006-05-26
+ * @cdk.module     qsar
+ * @cdk.set        qsar-descriptors
+ * @cdk.dictref qsar-descriptors:ionizationPotential
+ * @cdk.depends weka.jar
+ * @cdk.builddepends weka.jar
+ * @see J48WModel
+ * @see IPAtomicDescriptor
+ * @see IPBondDescriptor
+ */
+public class IPMolecularDescriptor implements IMolecularDescriptor {
+
+    /**
+     *  Constructor for the IPMolecularDescriptor object
+     */
+    public IPMolecularDescriptor() { }
+
+
+    /**
+     *  Gets the specification attribute of the IPMolecularDescriptor object
+     *
+     *@return    The specification value
+     */
+    public DescriptorSpecification getSpecification() {
+        return new DescriptorSpecification(
+            "http://www.blueobelisk.org/ontologies/chemoinformatics-algorithms/#chi1",
+            this.getClass().getName(),
+            "$Id: ConnectivityOrderOneDescriptor.java 7032 2006-09-22 15:26:48Z kaihartmann $",
+            "The Chemistry Development Kit");
+    }
+
+
+    /**
+     *  Sets the parameters attribute of the IPMolecularDescriptor object
+     *
+     *@param  params            The new parameters value
+     *@exception  CDKException  Description of the Exception
+     */
+    public void setParameters(Object[] params) throws CDKException {
+        // no parameters for this descriptor
+    }
+
+
+    /**
+     *  Gets the parameters attribute of the IPMolecularDescriptor object
+     *
+     *@return    The parameters value
+     */
+    public Object[] getParameters() {
+        return (null);
+        // no parameters to return
+    }
+
+
+    /**
+     *  Description of the Method
+     *
+     *@param  atomContainer                AtomContainer
+     *@return                   The 1,2 .. ionization energy
+     *@exception  CDKException  Possible Exceptions
+     */
+    public DescriptorValue calculate(IAtomContainer atomContainer) throws CDKException {
+    	DoubleArrayResult dar = new DoubleArrayResult();
+    	IPAtomicDescriptor descriptorA = new IPAtomicDescriptor();
+    	Iterator itA = atomContainer.atoms();
+    	while(itA.hasNext()){
+    		IAtom atom = (IAtom) itA.next();
+    		double result= ((DoubleResult)descriptorA.calculate(atom,atomContainer).getValue()).doubleValue();
+    		if(result != -1.0)
+    			dar.add(result);
+            
+    	}
+    	IPBondDescriptor descriptorB = new IPBondDescriptor();
+    	for(int i = 0; i < atomContainer.getBondCount(); i++){
+    		double result= ((DoubleResult)descriptorB.calculate(atomContainer.getBond(i),atomContainer).getValue()).doubleValue();
+    		if(result != -1.0)
+    			dar.add(result);
+    	}
+    	return new DescriptorValue(getSpecification(), getParameterNames(), getParameters(), dar);
+    }
+
+
+    /**
+     *  Gets the parameterNames attribute of the IPMolecularDescriptor object
+     *
+     *@return    The parameterNames value
+     */
+    public String[] getParameterNames() {
+        // no param names to return
+        return (null);
+    }
+
+
+
+    /**
+     *  Gets the parameterType attribute of the IPMolecularDescriptor object
+     *
+     *@param  name  Description of the Parameter
+     *@return       The parameterType value
+     */
+    public Object getParameterType(String name) {
+        return (null);
+    }
+}
+
---
+++ src/org/openscience/cdk/test/qsar/descriptors/molecular/IPMolecularDescriptorTest.java
@@ -0,0 +1,127 @@
+/* $RCSfile$
+ * $Author: miguelrojasch $
+ * $Date: 2006-05-12 10:58:22 +0200 (Fr, 12 Mai 2006) $
+ * $Revision: 1.0
+ $
+ * 
+ *  Copyright (C) 2004-2006  Miguel Rojas <miguel.rojas@uni-koeln.de>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
+ */
+package org.openscience.cdk.test.qsar.descriptors.molecular;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.qsar.descriptors.molecular.IPMolecularDescriptor;
+import org.openscience.cdk.qsar.result.DoubleArrayResult;
+import org.openscience.cdk.qsar.result.DoubleResult;
+import org.openscience.cdk.smiles.SmilesParser;
+import org.openscience.cdk.test.CDKTestCase;
+import org.openscience.cdk.tools.HydrogenAdder;
+import org.openscience.cdk.tools.LonePairElectronChecker;
+
+/**
+ * TestSuite that runs all QSAR tests.
+ *
+ * @cdk.module test-qsar
+ */
+
+public class IPMolecularDescriptorTest extends CDKTestCase {
+	IPMolecularDescriptor descriptor;
+	private SmilesParser sp;
+	/**
+	 *  Constructor for the IPMolecularDescriptorTest object
+	 *
+	 */
+    public  IPMolecularDescriptorTest() {
+    	descriptor = new IPMolecularDescriptor();
+    	sp = new SmilesParser();
+    	
+    }
+    /**
+	 *  A unit test suite for JUnit
+	 *
+	 *@return    The test suite
+	 */
+    public static Test suite() {
+        return new TestSuite(IPMolecularDescriptorTest.class);
+    }
+
+    /**
+	 *  A unit test for JUnit with C-Cl
+	 */
+    public void testIPDescriptor_1() throws ClassNotFoundException, CDKException, java.lang.Exception{
+        
+		Molecule mol = sp.parseSmiles("C-Cl");
+
+		HydrogenAdder hAdder = new HydrogenAdder();
+		hAdder.addExplicitHydrogensToSatisfyValency(mol);
+		
+		LonePairElectronChecker lpcheck = new LonePairElectronChecker();
+		lpcheck.newSaturate(mol);
+		
+		DoubleArrayResult dar = ((DoubleArrayResult)descriptor.calculate(mol).getValue());
+        double resultAccordingNIST = 11.26; 
+//        System.out.println(resultAccordingNIST+"="+dar.get(0));
+
+        assertEquals(1, dar.size());
+        assertEquals(resultAccordingNIST, dar.get(0), 0.02);
+    }
+    /**
+	 *  A unit test for JUnit with COCCCC=O
+	 */
+    public void testIPDescriptor_2() throws ClassNotFoundException, CDKException, java.lang.Exception{
+        
+		Molecule mol = sp.parseSmiles("COCCCC=O");
+
+		HydrogenAdder hAdder = new HydrogenAdder();
+		hAdder.addExplicitHydrogensToSatisfyValency(mol);
+		
+		LonePairElectronChecker lpcheck = new LonePairElectronChecker();
+		lpcheck.newSaturate(mol);
+		
+		DoubleArrayResult dar = ((DoubleArrayResult)descriptor.calculate(mol).getValue());
+        
+        double resultAccordingNIST = 9.37; 
+        assertEquals(2, dar.size());
+        assertEquals(resultAccordingNIST, dar.get(0), 0.3);
+    }
+    /**
+	 *  A unit test for JUnit with C=CCC(=O)CC
+	 */
+    public void testIPDescriptor_3() throws ClassNotFoundException, CDKException, java.lang.Exception{
+        
+		Molecule mol = sp.parseSmiles("C=CCC(=O)CC");
+		
+		HydrogenAdder hAdder = new HydrogenAdder();
+		hAdder.addExplicitHydrogensToSatisfyValency(mol);
+		
+		LonePairElectronChecker lpcheck = new LonePairElectronChecker();
+		lpcheck.newSaturate(mol);
+		
+		DoubleArrayResult dar = ((DoubleArrayResult)descriptor.calculate(mol).getValue());
+
+      System.out.println(dar.get(0)+", "+dar.get(1));
+        double resultAccordingNIST = 9.37; 
+        assertEquals(2, dar.size());
+        assertEquals(resultAccordingNIST, dar.get(0), 1.4);
+    }
+
+}
