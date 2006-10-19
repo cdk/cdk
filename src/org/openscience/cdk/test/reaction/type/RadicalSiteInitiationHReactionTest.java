1fa68269a4a98738a0e5f7442a2c6ffbf0df9380
miguelrojasch <miguelrojasch@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-10-19 12:17:22 +0000

    added new Reaction RadicalSiteInitiation for Hydrogen
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7134 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/test/reaction/type/RadicalSiteInitiationHReactionTest.java
@@ -0,0 +1,123 @@
+/* $RCSfile$
+ * $Author: miguelrojasch $
+ * $Date: 2006-05-11 14:25:07 +0200 (Do, 11 Mai 2006) $
+ * $Revision: 6221 $
+ *
+ *  Copyright (C) 2004-2006  Miguel Rojas <miguel.rojas@uni-koeln.de>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
+ */
+package org.openscience.cdk.test.reaction.type;
+
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.SingleElectron;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.interfaces.IMoleculeSet;
+import org.openscience.cdk.interfaces.IReactionSet;
+import org.openscience.cdk.isomorphism.UniversalIsomorphismTester;
+import org.openscience.cdk.isomorphism.matchers.QueryAtomContainer;
+import org.openscience.cdk.isomorphism.matchers.QueryAtomContainerCreator;
+import org.openscience.cdk.reaction.IReactionProcess;
+import org.openscience.cdk.reaction.type.RadicalSiteInitiationHReaction;
+import org.openscience.cdk.smiles.SmilesParser;
+import org.openscience.cdk.test.CDKTestCase;
+import org.openscience.cdk.tools.HydrogenAdder;
+import org.openscience.cdk.tools.manipulator.ReactionManipulator;
+
+/**
+ * TestSuite that runs a test for the RadicalSiteInitiationHReactionTest.
+ * Generalized Reaction: [A*]-B-H => A=B + [H*].
+ *
+ * @cdk.module test-reaction
+ */
+public class RadicalSiteInitiationHReactionTest extends CDKTestCase {
+	
+	private IReactionProcess type;
+	/**
+	 * Constructror of the RadicalSiteInitiationHReactionTest object
+	 *
+	 */
+	public  RadicalSiteInitiationHReactionTest() {
+		type  = new RadicalSiteInitiationHReaction();
+	}
+    
+	public static Test suite() {
+		return new TestSuite(RadicalSiteInitiationHReactionTest.class);
+	}
+	/**
+	 * A unit test suite for JUnit. Reaction: [C*]([H])([H])C([H])([H])[H] => C=C +[H*]
+	 * Automatic sarch of the centre active.
+	 *
+	 * @return    The test suite
+	 */
+	public void testAutomaticSearchCentreActiveExample1() throws ClassNotFoundException, CDKException, java.lang.Exception {
+        
+		/*[C*]-C-C*/
+		Molecule molecule = (new SmilesParser()).parseSmiles("[C+]([H])([H])C([H])([H])[H]");
+        IAtom atom =  molecule.getAtom(0);
+        molecule.addElectronContainer(new SingleElectron(atom));
+        atom.setFormalCharge(0);
+		IMoleculeSet setOfReactants = DefaultChemObjectBuilder.getInstance().newMoleculeSet();
+		setOfReactants.addMolecule(molecule);
+		
+        Object[] params = {Boolean.FALSE};
+        type.setParameters(params);
+        IReactionSet setOfReactions = type.initiate(setOfReactants, null);
+        
+        Assert.assertEquals(1, setOfReactions.getReactionCount());
+        Assert.assertEquals(2, setOfReactions.getReaction(0).getProductCount());
+
+        
+        IMolecule product1 = setOfReactions.getReaction(0).getProducts().getMolecule(0);
+		
+        /*C=C*/
+        Molecule molecule2 = (new SmilesParser()).parseSmiles("C([H])([H])=C([H])[H]");
+        QueryAtomContainer qAC = QueryAtomContainerCreator.createSymbolAndChargeQueryContainer(product1);
+		Assert.assertTrue(UniversalIsomorphismTester.isIsomorph(molecule2,qAC));
+		
+		IMolecule product2 = setOfReactions.getReaction(0).getProducts().getMolecule(1);
+		
+        /*[H*]*/
+		molecule2 = (Molecule) molecule2.getBuilder().newMolecule();
+		molecule2.addAtom(new Atom("H"));
+        molecule2.addElectronContainer(new SingleElectron(molecule2.getAtom(0)));
+        		
+        qAC = QueryAtomContainerCreator.createSymbolAndChargeQueryContainer(product2);
+		Assert.assertTrue(UniversalIsomorphismTester.isIsomorph(molecule2,qAC));
+
+		Assert.assertEquals(4,setOfReactions.getReaction(0).getMappingCount());
+		IAtom mappedProductA1 = (IAtom)ReactionManipulator.getMappedChemObject(setOfReactions.getReaction(0), molecule.getAtom(4));
+		assertEquals(mappedProductA1, product2.getAtom(0));
+        IBond mappedProductB1 = (IBond)ReactionManipulator.getMappedChemObject(setOfReactions.getReaction(0), molecule.getBond(2));
+        assertEquals(mappedProductB1, product1.getBond(2));
+        IAtom mappedProductA2 = (IAtom)ReactionManipulator.getMappedChemObject(setOfReactions.getReaction(0), molecule.getAtom(0));
+        assertEquals(mappedProductA2, product1.getAtom(0));
+        IAtom mappedProductA3 = (IAtom)ReactionManipulator.getMappedChemObject(setOfReactions.getReaction(0), molecule.getAtom(3));
+        assertEquals(mappedProductA3, product1.getAtom(3));
+       
+	}
+}
