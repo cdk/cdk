7e7011b60fc3424ca5960208c700ed10e7c2dffe
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-10-24 13:44:35 +0000

    Put into a qsar-weka package. Licensed as GPL, as it really depends on the GPL weka library.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7162 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/qsar/model/weka/LinearRegressionWModel.java
+++ src/org/openscience/cdk/qsar/model/weka/LinearRegressionWModel.java
@@ -1,26 +1,22 @@
-/*
- *  $RCSfile$
- *  $Author: egonw $
- *  $Date: 2006-03-29 10:27:08 +0200 (Wed, 29 Mar 2006) $
- *  $Revision: 5855 $
+/* $Revision: 6228 $ $Author: egonw $ $Date: 2006-05-11 18:34:42 +0200 (Thu, 11 May 2006) $
  *
- *  Copyright (C) 2004-2006  Miguel Rojas <miguel.rojas@uni-koeln.de>
+ * Copyright (C) 2006  Miguel Rojas <miguel.rojas@uni-koeln.de>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
  *
- *  Contact: cdk-devel@lists.sourceforge.net
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2.1
- *  of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 package org.openscience.cdk.qsar.model.weka;
 
@@ -78,9 +74,10 @@ import weka.classifiers.functions.LinearRegression;
  * <p>-C: Do no try to eleminate colinear attributes</p>
  * <p>-R num: The ridge parameter(default 1.0e-8)</p>
  * 
- * @author Miguel Rojas
+ * @author      Miguel Rojas
  * @cdk.require weka.jar
- * @cdk.module qsar
+ * @cdk.license GPL
+ * @cdk.module  qsar-weka
  * @see Weka
  * 
  * @cdk.keyword regression, linear
