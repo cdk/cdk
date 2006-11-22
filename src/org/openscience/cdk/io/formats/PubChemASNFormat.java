9e9f3c1933dca3ab065fcac57bc5a49179bf506f
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-11-22 22:49:35 +0000

    Fixed copyright.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7340 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/io/formats/PubChemASNFormat.java
+++ src/org/openscience/cdk/io/formats/PubChemASNFormat.java
@@ -1,9 +1,6 @@
-/* $RCSfile$
- * $Author: egonw $
- * $Date: 2006-03-29 10:27:08 +0200 (Mi, 29 Mrz 2006) $
- * $Revision: 5855 $
+/* $Revision: 5855 $ $Author: egonw $$Date: 2006-03-29 10:27:08 +0200 (Mi, 29 Mrz 2006) $
  *
- * Copyright (C) 2004-2006  The Chemistry Development Kit (CDK) project
+ * Copyright (C) 2006  Egon Willighagen <egonw@users.sf.net>
  *
  * Contact: cdk-devel@lists.sourceforge.net
  *
