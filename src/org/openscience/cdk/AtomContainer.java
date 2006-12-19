8828b067c3d6a724d51e3934b34af54176e5ba23
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2006-12-19 13:58:31 +0000

    Fixed copyright lines to state the original author
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7507 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/AtomContainer.java
+++ src/org/openscience/cdk/AtomContainer.java
@@ -3,7 +3,7 @@
  *  $Date$
  *  $Revision$
  *
- *  Copyright (C) 1997-2006  The Chemistry Development Kit (CDK) project
+ *  Copyright (C) 1997-2006  Christoph Steinbeck
  *
  *  Contact: cdk-devel@lists.sourceforge.net
  *
