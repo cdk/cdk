b64711f0a99eef568b0ded8b5cb42777d9c79856
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-01-10 08:42:16 +0000

    Added missing source file header.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7679 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/smiles/DeduceBondSystemToolTest.java
+++ src/org/openscience/cdk/test/smiles/DeduceBondSystemToolTest.java
@@ -1,3 +1,23 @@
+/* $Revision: 7635 $ $Author: egonw $ $Date: 2007-01-04 18:32:54 +0100 (Thu, 04 Jan 2007) $
+ *
+ * Copyright (C) 2006-2007  Rajarshi Guha <rajarshi@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.openscience.cdk.test.smiles;
 
 import junit.framework.Test;
@@ -14,14 +34,12 @@ import org.openscience.cdk.smiles.SmilesParser;
 import org.openscience.cdk.test.CDKTestCase;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
-
 /**
  *
  * @author         Rajarshi Guha
  * @cdk.created    2006-09-18
  * @cdk.module     test-smiles
  */
-
 public class DeduceBondSystemToolTest extends CDKTestCase {
 
     public DeduceBondSystemToolTest(String name) {
