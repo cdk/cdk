97a8b6c03bc439352b7417e6e2f7ab91441ff1ff
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-01-13 19:37:41 +0000

    Fixed copyright line.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7715 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/math/FortranFormat.java
+++ src/org/openscience/cdk/math/FortranFormat.java
@@ -1,9 +1,6 @@
-/* $RCSfile$
- * $Author$
- * $Date$
- * $Revision$
+/* $Revision$ $Author$ $Date$
  *
- * Copyright (C) 1998-2007  The Chemistry Development Kit (CDK) project
+ * Copyright (C) 1998-2007  Dan Gezelter
  *
  * Contact: cdk-devel@lists.sourceforge.net
  *
@@ -24,7 +21,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
  */
 package org.openscience.cdk.math;
 
