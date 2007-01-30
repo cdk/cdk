b96b36ec0a81d1a32d1f67a802ae61e3d6040446
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-01-30 21:09:12 +0000

    started a test for BondTools
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7781 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/geometry/BondToolsTest.java
+++ src/org/openscience/cdk/test/geometry/BondToolsTest.java
@@ -20,16 +20,23 @@
  */
 package org.openscience.cdk.test.geometry;
 
+import java.io.InputStream;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.openscience.cdk.ChemFile;
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.geometry.BondTools;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.io.MDLV2000Reader;
 import org.openscience.cdk.test.CDKTestCase;
 
 /**
  * @cdk.module test-standard
  */
 public class BondToolsTest extends CDKTestCase {
-    
+
     public BondToolsTest(String name) {
         super(name);
     }
@@ -37,7 +44,71 @@ public class BondToolsTest extends CDKTestCase {
 	public static Test suite() {
 		return new TestSuite(BondToolsTest.class);
 	}
+	
+	
+	public void testIsValidDoubleBondConfiguration_IAtomContainer_IBond(){
+		try{
+			String filename = "data/mdl/testdoublebondconfig.mol";
+		    InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+		    MDLV2000Reader reader = new MDLV2000Reader(ins);
+	        ChemFile chemFile = (ChemFile)reader.read((ChemObject)new ChemFile());
+	        IMolecule mol=chemFile.getChemSequence(0).getChemModel(0).getMoleculeSet().getMolecule(0);
+	        assertTrue(BondTools.isValidDoubleBondConfiguration(mol, mol.getBond(0)));
+	        assertFalse(BondTools.isValidDoubleBondConfiguration(mol, mol.getBond(1)));
+	        assertFalse(BondTools.isValidDoubleBondConfiguration(mol, mol.getBond(2)));
+	        assertFalse(BondTools.isValidDoubleBondConfiguration(mol, mol.getBond(3)));
+	        assertFalse(BondTools.isValidDoubleBondConfiguration(mol, mol.getBond(4)));
+	        assertFalse(BondTools.isValidDoubleBondConfiguration(mol, mol.getBond(5)));
+	        assertFalse(BondTools.isValidDoubleBondConfiguration(mol, mol.getBond(6)));
+	        assertFalse(BondTools.isValidDoubleBondConfiguration(mol, mol.getBond(7)));
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			fail(exc.getMessage());
+		}		
+	}
 
+	public void testIsCisTrans_IAtom_IAtom_IAtom_IAtom_IAtomContainer(){
+		try{
+			String filename = "data/mdl/testdoublebondconfig.mol";
+		    InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+		    MDLV2000Reader reader = new MDLV2000Reader(ins);
+	        ChemFile chemFile = (ChemFile)reader.read((ChemObject)new ChemFile());
+	        IMolecule mol=chemFile.getChemSequence(0).getChemModel(0).getMoleculeSet().getMolecule(0);
+	        assertFalse(BondTools.isCisTrans(mol.getAtom(2),mol.getAtom(0),mol.getAtom(1),mol.getAtom(4),mol));
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			fail(exc.getMessage());
+		}		
+	}
+
+	public void testIsLeft_IAtom_IAtom_IAtom(){
+		try{
+			String filename = "data/mdl/testdoublebondconfig.mol";
+		    InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+		    MDLV2000Reader reader = new MDLV2000Reader(ins);
+	        ChemFile chemFile = (ChemFile)reader.read((ChemObject)new ChemFile());
+	        IMolecule mol=chemFile.getChemSequence(0).getChemModel(0).getMoleculeSet().getMolecule(0);
+	        assertFalse(BondTools.isLeft(mol.getAtom(1),mol.getAtom(0),mol.getAtom(2)));
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			fail(exc.getMessage());
+		}		
+	}
+
+	public void testGiveAngleBothMethods_IAtom_IAtom_IAtom_boolean(){
+		try{
+			String filename = "data/mdl/testdoublebondconfig.mol";
+		    InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+		    MDLV2000Reader reader = new MDLV2000Reader(ins);
+	        ChemFile chemFile = (ChemFile)reader.read((ChemObject)new ChemFile());
+	        IMolecule mol=chemFile.getChemSequence(0).getChemModel(0).getMoleculeSet().getMolecule(0);
+	        assertEquals(2.0943946986086157,BondTools.giveAngleBothMethods(mol.getAtom(0),mol.getAtom(2),mol.getAtom(3),true),0.2);
+	        assertEquals(2.0943946986086157,BondTools.giveAngleBothMethods(mol.getAtom(0),mol.getAtom(2),mol.getAtom(3),false),0.2);
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			fail(exc.getMessage());
+		}		
+	}
 }
 
 
