28fd90b89466e71fced59213efafdc8dc275972c
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-01-31 15:00:39 +0000

    Started the test for geometrytools
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7793 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/geometry/GeometryToolsInternalCoordinatesTest.java
+++ src/org/openscience/cdk/test/geometry/GeometryToolsInternalCoordinatesTest.java
@@ -20,15 +20,24 @@
  */
 package org.openscience.cdk.test.geometry;
 
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.vecmath.Point2d;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.Molecule;
 import org.openscience.cdk.config.Elements;
+import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.geometry.GeometryToolsInternalCoordinates;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.io.MDLReader;
 import org.openscience.cdk.nonotify.NNAtom;
 import org.openscience.cdk.nonotify.NNAtomContainer;
 import org.openscience.cdk.test.CDKTestCase;
@@ -56,6 +65,57 @@ public class GeometryToolsInternalCoordinatesTest extends CDKTestCase {
 		assertTrue(0 <= atom.getPoint2d().y);
 	}
 	
+    public void testGetLength2D_IBond() {
+        Atom o = new Atom("O", new Point2d(0.0, 0.0));
+        Atom c = new Atom("C", new Point2d(1.0, 0.0));
+        Bond bond = new Bond(c,o);
+        
+        assertEquals(1.0, GeometryToolsInternalCoordinates.getLength2D(bond), 0.001);
+    }
+    public void testMapAtomsOfAlignedStructures(){
+   	 
+    	String filenameMolOne = "data/mdl/murckoTest6_3d_2.mol";
+		String filenameMolTwo = "data/mdl/murckoTest6_3d.mol";
+    	//String filenameMolTwo = "data/mdl/murckoTest6_3d_2.mol";
+	    InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filenameMolOne);
+	    Molecule molOne=null;
+	    Molecule molTwo=null;
+	    Map mappedAtoms=new HashMap();
+	    try {
+	    	MDLReader reader = new MDLReader(ins);
+	        molOne = (Molecule)reader.read(new Molecule());
+	    } catch (Exception exception) {
+	        System.out.println("Error: Cannot read in mol1 due to "+exception.toString());
+	    }
+		
+	    ins = this.getClass().getClassLoader().getResourceAsStream(filenameMolTwo);
+	    try {
+	    	MDLReader reader = new MDLReader(ins);
+	        molTwo = (Molecule)reader.read(new Molecule());
+	    } catch (Exception exception) {
+	        System.out.println("Error: Cannot read in mol2 due to "+exception.toString());
+	    }
+	   
+	    try {
+			mappedAtoms=GeometryToolsInternalCoordinates.mapAtomsOfAlignedStructures(molOne, molTwo, mappedAtoms);
+			//logger.debug("mappedAtoms:"+mappedAtoms.toString());
+			//logger.debug("***** ANGLE VARIATIONS *****");
+			double AngleRMSD=GeometryToolsInternalCoordinates.getAngleRMSD(molOne,molTwo,mappedAtoms);
+			//logger.debug("The Angle RMSD between the first and the second structure is :"+AngleRMSD);
+			//logger.debug("***** ALL ATOMS RMSD *****");
+			assertEquals(0.2, AngleRMSD, 0.1);
+			double AllRMSD=GeometryToolsInternalCoordinates.getAllAtomRMSD(molOne,molTwo,mappedAtoms,true);
+			//logger.debug("The RMSD between the first and the second structure is :"+AllRMSD);
+			assertEquals(0.242, AllRMSD, 0.001);
+			//logger.debug("***** BOND LENGTH RMSD *****");
+			double BondLengthRMSD=GeometryToolsInternalCoordinates.getBondLengthRMSD(molOne,molTwo,mappedAtoms,true);
+			//logger.debug("The Bond length RMSD between the first and the second structure is :"+BondLengthRMSD);
+			assertEquals(0.2, BondLengthRMSD, 0.1);
+	    } catch (CDKException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	    }
+   }
 }
 
 
--- src/org/openscience/cdk/test/geometry/GeometryToolsTest.java
+++ src/org/openscience/cdk/test/geometry/GeometryToolsTest.java
@@ -25,6 +25,7 @@ package org.openscience.cdk.test.geometry;
 
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import javax.vecmath.Point2d;
@@ -34,10 +35,17 @@ import junit.framework.TestSuite;
 
 import org.openscience.cdk.Atom;
 import org.openscience.cdk.Bond;
+import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.Molecule;
+import org.openscience.cdk.config.Elements;
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.geometry.GeometryTools;
 import org.openscience.cdk.geometry.GeometryToolsInternalCoordinates;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.io.MDLReader;
+import org.openscience.cdk.nonotify.NNAtom;
+import org.openscience.cdk.nonotify.NNAtomContainer;
 import org.openscience.cdk.test.CDKTestCase;
 
 /**
@@ -68,58 +76,38 @@ public class GeometryToolsTest extends CDKTestCase {
         return suite;
     }
     
+	public void testTranslateAllPositive_IAtomContainer() {
+		IAtomContainer container = new NNAtomContainer();
+		IAtom atom = new NNAtom(Elements.CARBON);
+		atom.setPoint2d(new Point2d(-3, -2));
+		container.addAtom(atom);
+		HashMap map=this.makeCoordsMap(container);
+		GeometryTools.translateAllPositive(container,map);
+		assertTrue(0 <= ((Point2d)map.get(atom)).x);
+		assertTrue(0 <= ((Point2d)map.get(atom)).y);
+	}
+	
     public void testGetLength2D_IBond() {
         Atom o = new Atom("O", new Point2d(0.0, 0.0));
         Atom c = new Atom("C", new Point2d(1.0, 0.0));
         Bond bond = new Bond(c,o);
-        
-        assertEquals(1.0, GeometryToolsInternalCoordinates.getLength2D(bond), 0.001);
+        IAtomContainer container=DefaultChemObjectBuilder.getInstance().newAtomContainer();
+        container.addAtom(o);
+        container.addAtom(c);
+        container.addBond(bond);
+        HashMap map=this.makeCoordsMap(container);
+        assertEquals(1.0, GeometryTools.getLength2D(bond,map), 0.001);
     }
+
     
-    public void testMapAtomsOfAlignedStructures(){
-    	 
-    	String filenameMolOne = "data/mdl/murckoTest6_3d_2.mol";
-		String filenameMolTwo = "data/mdl/murckoTest6_3d.mol";
-    	//String filenameMolTwo = "data/mdl/murckoTest6_3d_2.mol";
-	    InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filenameMolOne);
-	    Molecule molOne=null;
-	    Molecule molTwo=null;
-	    Map mappedAtoms=new HashMap();
-	    try {
-	    	MDLReader reader = new MDLReader(ins);
-	        molOne = (Molecule)reader.read(new Molecule());
-	    } catch (Exception exception) {
-	        System.out.println("Error: Cannot read in mol1 due to "+exception.toString());
-	    }
-		
-	    ins = this.getClass().getClassLoader().getResourceAsStream(filenameMolTwo);
-	    try {
-	    	MDLReader reader = new MDLReader(ins);
-	        molTwo = (Molecule)reader.read(new Molecule());
-	    } catch (Exception exception) {
-	        System.out.println("Error: Cannot read in mol2 due to "+exception.toString());
-	    }
-	   
-	    try {
-			mappedAtoms=GeometryToolsInternalCoordinates.mapAtomsOfAlignedStructures(molOne, molTwo, mappedAtoms);
-			//logger.debug("mappedAtoms:"+mappedAtoms.toString());
-			//logger.debug("***** ANGLE VARIATIONS *****");
-			double AngleRMSD=GeometryToolsInternalCoordinates.getAngleRMSD(molOne,molTwo,mappedAtoms);
-			//logger.debug("The Angle RMSD between the first and the second structure is :"+AngleRMSD);
-			//logger.debug("***** ALL ATOMS RMSD *****");
-			assertEquals(0.2, AngleRMSD, 0.1);
-			double AllRMSD=GeometryToolsInternalCoordinates.getAllAtomRMSD(molOne,molTwo,mappedAtoms,true);
-			//logger.debug("The RMSD between the first and the second structure is :"+AllRMSD);
-			assertEquals(0.242, AllRMSD, 0.001);
-			//logger.debug("***** BOND LENGTH RMSD *****");
-			double BondLengthRMSD=GeometryToolsInternalCoordinates.getBondLengthRMSD(molOne,molTwo,mappedAtoms,true);
-			//logger.debug("The Bond length RMSD between the first and the second structure is :"+BondLengthRMSD);
-			assertEquals(0.2, BondLengthRMSD, 0.1);
-	    } catch (CDKException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-	    }
-   }
-    
+    private HashMap makeCoordsMap(IAtomContainer container){
+    	HashMap map=new HashMap();
+    	Iterator it=container.atoms();
+    	while(it.hasNext()){
+    		IAtom atom=(IAtom)it.next();
+    		map.put(atom, atom.getPoint2d());
+    	}
+    	return map;
+    }
 }
 
