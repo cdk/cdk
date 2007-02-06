2db078523bd1f54eb1b0366f1fceefd7b9f5bc01
f_marighetti <f_marighetti@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-02-06 12:41:27 +0000

    New class made for generation of a Topological Matrix
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7872 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/graph/matrix/TopologicalMatrix.java
@@ -0,0 +1,32 @@
+package org.openscience.cdk.graph.matrix;
+
+import org.openscience.cdk.graph.PathTools;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.graph.matrix.AdjacencyMatrix;
+
+/**
+ * Calculator for a topological matrix representation of this AtomContainer. An
+ * topological matrix is a matrix of quare NxN matrix, where N is the number of
+ * atoms in the AtomContainer. The element i,j of the matrix is the distance between
+ * two atoms in a molecule.
+ * 
+ * @author federico
+ *
+ */
+
+public class TopologicalMatrix implements IGraphMatrix {
+	
+	/**
+	 * Returns the topological matrix for the given AtomContainer.
+	 *
+     * @param  container The AtomContainer for which the matrix is calculated
+	 * @return           A topological matrix representating this AtomContainer
+	 */
+	
+	public static int[][] getMatrix(IAtomContainer container) {
+			int[][]conMat = AdjacencyMatrix.getMatrix(container);
+			int[][]TopolDistance = PathTools.computeFloydAPSP(conMat);
+		
+	return TopolDistance;
+		}
+	}
---
+++ src/org/openscience/cdk/test/graph/matrix/TopologicalMatrixTest.java
@@ -0,0 +1,47 @@
+package org.openscience.cdk.test.graph.matrix;
+
+import java.io.InputStream;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.graph.matrix.TopologicalMatrix;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.io.MDLReader;
+import org.openscience.cdk.test.CDKTestCase;
+
+/**
+ * @cdk.module test-standard
+ */
+
+public class TopologicalMatrixTest extends CDKTestCase {
+
+	public TopologicalMatrixTest(String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		return new TestSuite(TopologicalMatrixTest.class);
+	}
+
+	public void testTopologicalMatrix_IAtomContainer() throws Exception {
+		String filename = "data/mdl/clorobenzene.mol";
+		InputStream ins = this.getClass().getClassLoader().getResourceAsStream(
+				filename);
+		MDLReader reader = new MDLReader(ins);
+		IMolecule container = (Molecule) reader
+				.read((ChemObject) new Molecule());
+		int[][] matrix = TopologicalMatrix.getMatrix(container);
+		assertEquals(12, matrix.length);
+		for (int i = 0; i < matrix.length; i++) {
+
+			System.out.println("");
+
+			for (int j = 0; j < matrix.length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+		}
+	}
+}
