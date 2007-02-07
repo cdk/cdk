35fd78c356847f9f6b58c0c5f0177f5512c772cb
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-02-07 19:49:20 +0000

    Removed inaccurate test. The substructure has a single bond where is the bond is aromatic in the target structure.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7881 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/fingerprint/FingerprinterTest.java
+++ src/org/openscience/cdk/test/fingerprint/FingerprinterTest.java
@@ -24,9 +24,7 @@
  */
 package org.openscience.cdk.test.fingerprint;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.math.BigInteger;
 import java.util.BitSet;
 
@@ -37,13 +35,8 @@ import org.openscience.cdk.Atom;
 import org.openscience.cdk.ChemObject;
 import org.openscience.cdk.Molecule;
 import org.openscience.cdk.applications.swing.MoleculeViewer2D;
-import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.fingerprint.Fingerprinter;
-import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.io.IChemObjectReader;
 import org.openscience.cdk.io.MDLReader;
-import org.openscience.cdk.io.MDLV2000Reader;
-import org.openscience.cdk.isomorphism.UniversalIsomorphismTester;
 import org.openscience.cdk.templates.MoleculeFactory;
 import org.openscience.cdk.test.CDKTestCase;
 import org.openscience.cdk.tools.LoggingTool;
@@ -307,24 +300,6 @@ public class FingerprinterTest extends CDKTestCase
 		BitSet bs1 = fingerprinter.getFingerprint(frag1);
 		assertTrue(Fingerprinter.isSubset(bs, bs1));
 	}
-	
-    /* ethanolamine */
-	private static final String ethanolamine = "\n\n\n  4  3  0     0  0  0  0  0  0  1 V2000\n    2.5187   -0.3500    0.0000 N   0  0  0  0  0  0  0  0  0  0\n    0.0938   -0.3500    0.0000 C   0  0  0  0  0  0  0  0  0  0\n    1.3062    0.3500    0.0000 C   0  0  0  0  0  0  0  0  0  0\n   -1.1187    0.3500    0.0000 O   0  0  0  0  0  0  0  0  0  0\n  2  3  1  0  0  0  0\n  2  4  1  0  0  0  0\n  1  3  1  0  0  0  0\nM  END\n";
-
-    /* 2,4-diamino-5-hydroxypyrimidin-dihydrochlorid */
-	private static final String molecule_test_2 = "\n\n\n 13 11  0     0  0  0  0  0  0  1 V2000\n   -0.5145   -1.0500    0.0000 C   0  0  0  0  0  0  0  0  0  0\n   -1.7269   -1.7500    0.0000 N   0  0  0  0  0  0  0  0  0  0\n   -2.9393   -1.0500    0.0000 C   0  0  0  0  0  0  0  0  0  0\n   -2.9393    0.3500    0.0000 C   0  0  0  0  0  0  0  0  0  0\n   -1.7269    1.0500    0.0000 C   0  0  0  0  0  0  0  0  0  0\n   -0.5145    0.3500    0.0000 N   0  0  0  0  0  0  0  0  0  0\n   -4.1518    1.0500    0.0000 O   0  0  0  0  0  0  0  0  0  0\n   -4.1518   -1.7500    0.0000 N   0  0  0  0  0  0  0  0  0  0\n    0.6980   -1.7500    0.0000 N   0  0  0  0  0  0  0  0  0  0\n   -4.1518    2.4500    0.0000 H   0  0  0  0  0  1  0  0  0  0\n   -5.3642    3.1500    0.0000 Cl  0  0  0  0  0  0  0  0  0  0\n   -4.1518   -3.1500    0.0000 H   0  0  0  0  0  1  0  0  0  0\n   -5.3642   -3.8500    0.0000 Cl  0  0  0  0  0  0  0  0  0  0\n  1  2  1  0  0  0  0\n  2  3  2  0  0  0  0\n  3  4  1  0  0  0  0\n  4  5  2  0  0  0  0\n  5  6  1  0  0  0  0\n  1  6  2  0  0  0  0\n  4  7  1  0  0  0  0\n  3  8  1  0  0  0  0\n  1  9  1  0  0  0  0\n 10 11  1  0  0  0  0\n 12 13  1  0  0  0  0\nM  END\n";
-
-    private static Molecule createMolecule(String molecule) throws IOException, CDKException {
-    	Molecule structure = null;
-    	if (molecule != null) {
-    		IChemObjectReader reader = new MDLV2000Reader(new StringReader(molecule));
-    		assertNotNull("Could not create reader", reader);
-    		if (reader.accepts(Molecule.class)) {
-    			structure = (Molecule) reader.read(new Molecule());
-    		}
-    	}
-    	return structure;
-    }
 	
 	public static Molecule makeFragment1()
 	{
