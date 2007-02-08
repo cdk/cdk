292a5fa6bdf53adc863433705f3d278aaff5d501
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-02-08 21:56:19 +0000

    Added running of HOSECode tests, which was not tested in the past. Not all tests run.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7920 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/MextraTests.java
+++ src/org/openscience/cdk/test/MextraTests.java
@@ -54,6 +54,7 @@ import org.openscience.cdk.test.layout.StructureDiagramGeneratorTest;
 import org.openscience.cdk.test.layout.TemplateHandlerTest;
 import org.openscience.cdk.test.libio.openbabel.OpenBabelConvertTest;
 import org.openscience.cdk.test.similarity.TanimotoTest;
+import org.openscience.cdk.test.tools.HOSECodeTest;
 
 /**
  * TestSuite that runs all the sample tests.
@@ -103,7 +104,8 @@ public class MextraTests {
         // from cdk.test.math
         // from cdk.test.similarity
         suite.addTest(TanimotoTest.suite());
-
+        suite.addTest(HOSECodeTest.suite());
+        
         // Below are the tests that are not always possible to execute, because
         // the class might not be compiled (depeding on Ant and Java VM versions).
 
