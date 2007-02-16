5e749fbf32a8594ab31d02e4b10f18c269df4849
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-02-16 14:56:39 +0000

    Added a few missing test methods.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@7962 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/math/RandomNumbersToolTest.java
+++ src/org/openscience/cdk/test/math/RandomNumbersToolTest.java
@@ -53,6 +53,58 @@ public class RandomNumbersToolTest extends CDKTestCase {
 		assertTrue(random == 0 || random == 1);
 	}
 	
+	public void testRandomBoolean() {
+		boolean random = RandomNumbersTool.randomBoolean();
+		assertTrue(random == true || random == false);
+	}
+	
+	public void testRandomLong() {
+		long random = RandomNumbersTool.randomLong();
+		assertTrue(random >= 0l);
+		assertTrue(random <= 1l);
+	}
+	
+	public void testRandomLong_Long_Long() {
+		long lower_limit = 2l;
+		long upper_limit = 4l;
+		long random = RandomNumbersTool.randomLong(lower_limit, upper_limit);
+		assertTrue(random >= lower_limit);
+		assertTrue(random <= upper_limit);
+	}
+	
+	public void testRandomDouble() {
+		double random = RandomNumbersTool.randomDouble();
+		assertTrue(random >= 0.0);
+		assertTrue(random <= 1.0);
+	}
+	
+	public void testRandomDouble_double_double() {
+		double lower_limit = 2.0;
+		double upper_limit = 4.0;
+		double random = RandomNumbersTool.randomDouble(lower_limit, upper_limit);
+		assertTrue(random >= lower_limit);
+		assertTrue(random <= upper_limit);
+	}
+	
+	public void testRandomFloat() {
+		float random = RandomNumbersTool.randomFloat();
+		assertTrue(random >= 0.0);
+		assertTrue(random <= 1.0);
+	}
+	
+	public void testRandomFloat_Float_Float() {
+		float lower_limit = (float)2.0;
+		float upper_limit = (float)4.0;
+		float random = RandomNumbersTool.randomFloat(lower_limit, upper_limit);
+		assertTrue(random >= lower_limit);
+		assertTrue(random <= upper_limit);
+	}
+	
+	public void testRandomBit() {
+		int random = RandomNumbersTool.randomBit();
+		assertTrue(random == 0 || random == 1);
+	}
+	
 	public void testRandomInt_int_int() {
 		int random = RandomNumbersTool.randomInt(0,5);
 		assertTrue(random == 0 || random == 1 || random == 2 ||
