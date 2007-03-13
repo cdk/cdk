5ea6c000aad230afc329afadcdb7cb6257197362
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-03-13 18:11:17 +0000

    Implemented the 13 missing test methods.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8093 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/smiles/InvPairTest.java
+++ src/org/openscience/cdk/test/smiles/InvPairTest.java
@@ -23,6 +23,10 @@ package org.openscience.cdk.test.smiles;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.config.Elements;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.smiles.InvPair;
 import org.openscience.cdk.test.CDKTestCase;
 
 /**
@@ -43,4 +47,104 @@ public class InvPairTest extends CDKTestCase {
         return new TestSuite(InvPairTest.class);
     }
 
+    public void testInvPair() {
+    	InvPair pair = new InvPair();
+    	assertNotNull(pair);
+    }
+    
+    public void testInvPair_long_IAtom() {
+    	IAtom atom = new Atom(Elements.CARBON);
+    	InvPair pair = new InvPair(5l, atom);
+    	assertNotNull(pair);
+    	assertEquals(5l, pair.getCurr());
+    	assertEquals(atom, pair.getAtom());
+    }
+    
+    public void testEquals_Object() {
+    	IAtom atom = new Atom(Elements.CARBON);
+    	InvPair pair = new InvPair(5l, atom);
+    	assertTrue(pair.equals(pair));
+    	assertFalse(pair.equals("NotSame"));
+    	assertFalse(pair.equals(new InvPair()));
+    }
+    
+    public void testToString() {
+    	IAtom atom = new Atom(Elements.CARBON);
+    	InvPair pair = new InvPair(5l, atom);
+    	assertNotNull(pair.toString());
+    	assertTrue(pair.toString().length() > 0);
+    }
+    
+    public void testSetAtom_IAtom() {
+    	IAtom atom = new Atom(Elements.CARBON);
+    	InvPair pair = new InvPair();
+    	assertNotSame(atom, pair.getAtom());
+    	pair.setAtom(atom);
+    	assertEquals(atom, pair.getAtom());
+    }
+    
+    public void testGetAtom() {
+    	InvPair pair = new InvPair();
+    	assertNull(pair.getAtom());
+    	pair.setAtom(new Atom(Elements.CARBON));
+    	assertNotNull(pair.getAtom());
+    }
+    
+    public void testGetPrime() {
+    	IAtom atom = new Atom(Elements.CARBON);
+    	InvPair pair = new InvPair(5l, atom);
+    	pair.setPrime();
+    	int prime = pair.getPrime();
+    	pair.setPrime();
+    	assertEquals("The prime should not change when curr is not changed",
+    		prime, pair.getPrime());
+    	pair.setCurr(6l);
+    	assertNotSame(new Integer(prime), new Integer(pair.getPrime()));
+    }
+    
+    public void testSetPrime() {
+    	InvPair pair = new InvPair();
+    	try {
+    		pair.setPrime();
+    		fail("should have failed with an ArrayIndexOutOfBounds exception");
+    	} catch (Exception e) {
+			// OK, is apparently expected to happen
+		}
+    }
+    
+    public void testComit() {
+    	IAtom atom = new Atom(Elements.CARBON);
+    	InvPair pair = new InvPair(5l, atom);
+    	pair.comit();
+    	assertNotNull(atom.getProperty(InvPair.CANONICAL_LABEL));
+    	assertEquals(new Long(5l), (Long)atom.getProperty(InvPair.CANONICAL_LABEL));
+    }
+    
+    public void testSetCurr_long() {
+    	IAtom atom = new Atom(Elements.CARBON);
+    	InvPair pair = new InvPair(5l, atom);
+    	assertEquals(5l, pair.getCurr());
+    	pair.setCurr(4l);
+    	assertEquals(4l, pair.getCurr());
+    }
+
+    public void testGetCurr() {
+    	IAtom atom = new Atom(Elements.CARBON);
+    	InvPair pair = new InvPair(5l, atom);
+    	assertEquals(5l, pair.getCurr());
+    }
+
+    public void testSetLast_long() {
+    	IAtom atom = new Atom(Elements.CARBON);
+    	InvPair pair = new InvPair(5l, atom);
+    	assertEquals(0l, pair.getLast());
+    	pair.setLast(4l);
+    	assertEquals(4l, pair.getLast());
+    }
+
+    public void testGetLast() {
+    	IAtom atom = new Atom(Elements.CARBON);
+    	InvPair pair = new InvPair(5l, atom);
+    	assertEquals(0l, pair.getLast());
+    }
 }
