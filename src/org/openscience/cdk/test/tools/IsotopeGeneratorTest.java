352a742da2d10f0f4d661284cdfc8651b7293191
miguelrojasch <miguelrojasch@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-04-01 17:07:48 +0000

    Class which generates all combinatorial chemical isotopes given a structure. Its corresponding tests
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8159 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/test/tools/IsotopeGeneratorTest.java
@@ -0,0 +1,167 @@
+/* $RCSfile$
+ * $Author: miguelrojasch $
+ * $Date: 2006-05-11 14:25:07 +0200 (Do, 11 Mai 2006) $
+ * $Revision: 6221 $
+ *
+ *  Copyright (C) 2004-2007  Miguel Rojas <miguel.rojas@uni-koeln.de>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
+ */
+package org.openscience.cdk.test.tools;
+
+import java.util.Iterator;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.Isotope;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomContainerSet;
+import org.openscience.cdk.test.CDKTestCase;
+import org.openscience.cdk.tools.IsotopeGenerator;
+
+/**
+ * @cdk.module  test-extra
+ *
+ * @author         Miguel Rojas
+ * @cdk.created    2007-03-01
+ */
+public class IsotopeGeneratorTest extends CDKTestCase
+{
+
+	IsotopeGenerator isotopeG = null;
+
+	public IsotopeGeneratorTest(String name){
+		super(name);
+	}
+
+    /**
+     *  The JUnit setup method
+     */
+    public void setUp() throws Exception {
+    	isotopeG = new IsotopeGenerator();
+    }
+
+	/**
+	 * A unit test suite for JUnit
+	 *
+	 * @return    The test suite
+	 */
+    public static Test suite() {
+        TestSuite suite = new TestSuite(IsotopeGeneratorTest.class);
+        return suite;
+	}
+
+    /**
+	 * A unit test for JUnit: Isotopes of the Bromine.
+	 *
+	 * @return    Description of the Return Value
+	 */
+	public void testCalculateIsotopesBromine() throws CDKException {
+		IAtomContainer m = new AtomContainer();
+		Atom b1 = new Atom("Br");
+		Atom b2 = new Atom("Br");
+		m.addAtom(b1);
+		m.addAtom(b2);
+		m.addBond(new Bond(b1, b2));
+		
+		IAtomContainerSet containerSet = (IAtomContainerSet)isotopeG.getIsotopes(m);
+		
+		assertEquals(4, containerSet.getAtomContainerCount());
+		
+		double mm = Math.round((getMass(containerSet.getAtomContainer(0))));
+		assertEquals(158.0, mm);
+		mm = Math.round((getMass(containerSet.getAtomContainer(1))));
+		assertEquals(160.0, mm);
+		mm = Math.round((getMass(containerSet.getAtomContainer(2))));
+		assertEquals(160.0, mm);
+		mm = Math.round((getMass(containerSet.getAtomContainer(3))));
+		assertEquals(162.0, mm);
+	}
+	/**
+	 * A unit test for JUnit: Isotopes of the Chloromethylidyne.
+	 *
+	 * @return    Description of the Return Value
+	 */
+	public void testCalculateIsotopesChloromethylidyne() throws CDKException {
+		IAtomContainer m = new AtomContainer();
+		Atom b1 = new Atom("C");
+		Atom b2 = new Atom("Cl");
+		m.addAtom(b1);
+		m.addAtom(b2);
+		m.addBond(new Bond(b1, b2));
+		
+		IAtomContainerSet containerSet = (IAtomContainerSet)isotopeG.getIsotopes(m);
+		
+		assertEquals(2, containerSet.getAtomContainerCount());
+		
+		double mm = Math.round((getMass(containerSet.getAtomContainer(0))));
+		assertEquals(47.0, mm);
+		mm = Math.round((getMass(containerSet.getAtomContainer(1))));
+		assertEquals(49.0, mm);
+	}
+	/**
+	 * A unit test for JUnit: Isotopes of the Chloromethylidyne with a 
+	 * restriction for those istopes which their abundance is higher than 50%.
+	 *
+	 * @return    Description of the Return Value
+	 */
+	public void testCalculateIsotopesChloromethylidyne2() throws CDKException {
+		IAtomContainer m = new AtomContainer();
+		Atom b1 = new Atom("C");
+		Atom b2 = new Atom("Cl");
+		m.addAtom(b1);
+		m.addAtom(b2);
+		m.addBond(new Bond(b1, b2));
+
+		IsotopeGenerator isotopeG2 = new IsotopeGenerator(50.0);
+		IAtomContainerSet containerSet = (IAtomContainerSet)isotopeG2.getIsotopes(m);
+		
+		assertEquals(1, containerSet.getAtomContainerCount());
+		
+		double mm = Math.round((getMass(containerSet.getAtomContainer(0))));
+		assertEquals(47.0, mm);
+	}
+	/**
+	 * returns the exact mass for a given molecular formula, using mass already set
+	 * for each element.
+	 * 
+	 * @param  ac The IAtomContiner
+	 * @return The mass value
+	 */
+	private float getMass(IAtomContainer ac){
+		float mass = 0;
+		Isotope h = new Isotope(1, "H", 1.0, 1.0);
+		Iterator it = ac.atoms();
+		while(it.hasNext()){
+			IAtom atom = (IAtom) it.next();
+			mass += atom.getExactMass();
+
+			mass += atom.getHydrogenCount() * h.exactMass;
+		}
+		return mass;
+	}
+
+	
+    
+}
+
---
+++ src/org/openscience/cdk/tools/IsotopeGenerator.java
@@ -0,0 +1,198 @@
+/* $RCSfile$
+ * $Author: miguelrojasch $
+ * $Date: 2006-05-11 14:25:07 +0200 (Do, 11 Mai 2006) $
+ * $Revision: 6221 $
+ *
+ *  Copyright (C) 2004-2007  Miguel Rojas <miguel.rojas@uni-koeln.de>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
+ */
+package org.openscience.cdk.tools;
+
+import java.io.IOException;
+import java.io.OptionalDataException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.AtomContainerSet;
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.Isotope;
+import org.openscience.cdk.config.IsotopeFactory;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomContainerSet;
+import org.openscience.cdk.interfaces.IIsotope;
+
+
+/**
+* Generates all Combinatorial chemical isotopes given a structure..
+* 
+* @author         Miguel Rojas
+* @cdk.created    2007-03-01
+*
+* @cdk.keyword    isotope
+ */
+public class IsotopeGenerator implements java.io.Serializable{
+
+	private LoggingTool logger;
+	private IsotopeFactory isotopeFactory;
+	private static final long serialVersionUID = -5513399059175488001L;
+
+	/** Minimun abundance of the isotopo to be added in the combinatorial search.*/
+	private double minAbundance = 10.0;
+	/**
+	 *  Constructor for the IsotopeGenerator
+	 */
+	public IsotopeGenerator(){
+		this(10.0);
+	}
+	/**
+	 * Constructor for the IsotopeGenerator
+	 * 
+	 * @param minAbundance Minimun abundance of the isotopo to be added 
+	 * 						in the combinatorial search
+	 */
+	public IsotopeGenerator(double minAb){
+		minAbundance = minAb;
+        logger = new LoggingTool(this);
+        logger.info("Generating all Isotope structures with IsotopeGenerator");
+
+		try {
+			isotopeFactory = IsotopeFactory.getInstance(new ChemObject().getBuilder());
+		} catch (OptionalDataException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * get all combinatorial chemical isotopes given a structure. 
+	 * 
+	 * @param container
+	 * @return
+	 */
+	public IAtomContainerSet getIsotopes(IAtomContainer container){
+		IAtomContainerSet containerSet = new AtomContainerSet();
+        /** all isotopes found*/
+		ArrayList isotopicAtoms = new ArrayList();
+		/** Atoms with isotopes*/
+		ArrayList atomWithIsotopes = new ArrayList();
+        /** value of the number of combinations*/
+        int nC = 1;
+        
+        /*search atoms which have more than one isotope and they have the minimum abundance*/
+		Iterator itA = container.atoms();
+        while(itA.hasNext()){
+        	IAtom atom = (IAtom)itA.next();
+        	
+        	IIsotope[] isotopes = isotopeFactory.getIsotopes(atom.getSymbol());
+        	int count = 0;
+    		if(isotopes.length > 1)
+        	for (int i = 0; i < isotopes.length; i++)
+				if (isotopes[i].getNaturalAbundance() > minAbundance )
+					count++;
+        		
+			
+        	if(count > 1){
+        		for (int i = 0; i < isotopes.length; i++){
+    				if (isotopes[i].getNaturalAbundance() > minAbundance ){
+    					isotopicAtoms.add(isotopes[i]);
+            		}
+    			}
+        		atomWithIsotopes.add(atom);
+            	nC = nC*count;
+        	}else{
+        		isotopeFactory.configure(atom);
+        	}
+        }
+
+        if(isotopicAtoms.size() != 0)
+        	containerSet = mixer(container, isotopicAtoms, atomWithIsotopes, nC);
+        else
+        	containerSet.addAtomContainer(container);
+        
+		return containerSet;
+	}
+	/**
+	 * combine all possible isotopos
+	 * 
+	 * @param atomContainer    IAtomContainer to analyze
+	 * @param isotopicAtoms    An arrayList containing all isotopes
+	 * @param atomWithIsotopes An arrayList containing atoms which have isotopes
+	 * @param nc               Number of combinations
+	 * 
+	 * @return The IAtomContainerSet
+	 */
+	private IAtomContainerSet mixer(IAtomContainer atomContainer, ArrayList isotopicAtoms, ArrayList atomWithIsotopes, int nC){
+		 IAtomContainerSet containerSet = new AtomContainerSet();
+	    
+		int[][] ordreComb = new int[nC][atomWithIsotopes.size()];
+		IAtom[][] atomsComb = new Atom[nC][atomWithIsotopes.size()];
+
+		int column[] = new int[atomWithIsotopes.size()];
+		for (int j = 0; j < atomWithIsotopes.size(); j++)
+		{
+			column[j] = 1;
+		}
+
+		// create a matrix with the necessary order
+		for (int i = 0; i < nC; i++){
+			// add the combnation
+			for (int j = 0; j < atomWithIsotopes.size(); j++)
+				ordreComb[i][j] = column[j];
+
+			for (int j = 0; j < ordreComb[0].length; j++)
+				atomsComb[i][j] = (IAtom) atomWithIsotopes.get(j);
+			
+			column[atomWithIsotopes.size() - 1]++;
+			
+			// control of the end of each column
+			for (int k = atomWithIsotopes.size() - 1; k >= 0; k--){
+				if (column[k] > 2){
+					column[k] = 1;
+					if(k-1 >= 0)
+						column[k - 1]++;
+				}
+			}
+			
+		}
+		
+		/*set the correct isotope for each structure*/
+		for (int i = 0; i < nC; i++){
+			try {
+				IAtomContainer containerClon  = (IAtomContainer) atomContainer.clone();
+				
+					for (int j = 0; j < ordreComb[i].length; j++){
+						
+						int posAtom = atomContainer.getAtomNumber((IAtom) atomsComb[i][j]);
+						int or = ordreComb[i][j]-1;
+						double mass = ((Isotope)isotopicAtoms.get(or)).getExactMass();
+						containerClon.getAtom(posAtom).setExactMass(mass);
+						mass = containerClon.getAtom(posAtom).getExactMass();
+						
+					}
+					containerSet.addAtomContainer(containerClon);
+			} catch (CloneNotSupportedException e) {
+				e.printStackTrace();
+			}
+
+
+		}
+		return containerSet;
+	}
+}
