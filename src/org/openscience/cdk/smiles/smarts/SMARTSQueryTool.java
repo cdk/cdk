a0efe1d824eef7a46f0f0ffc4d91d0deb44cd535
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-04-09 00:01:12 +0000

    Added copyright notice
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8177 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/smiles/smarts/SMARTSQueryTool.java
+++ src/org/openscience/cdk/smiles/smarts/SMARTSQueryTool.java
@@ -1,3 +1,27 @@
+/*  $RCSfile$
+ *  $Author: rajarshi $
+ *  $Date: 2007-04-08 14:46:29 -0500 (Thu, 01 Feb 2007) $
+ *  $Revision: 7844 $
+ *
+ *  Copyright (C) 2004-2007  Rajarshi Guha <rajarshi@users.sourceforge.net>
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package org.openscience.cdk.smiles.smarts;
 
 import org.openscience.cdk.CDKConstants;
@@ -26,7 +50,7 @@ import java.util.List;
  * SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
  * IAtomContainer atomContainer = sp.parseSmiles("CC(=O)OC(=O)C");
  * SMARTSQueryTool querytool = new SMARTSQueryTool("O=CO");
- *
+ * <p/>
  * boolean status = querytool.matches(atomContainer);
  * if (status) {
  *    int nmatch = querytool.countMatches();
@@ -44,7 +68,7 @@ import java.util.List;
  * @cdk.keyword substructure search
  */
 public class SMARTSQueryTool {
-     private LoggingTool logger;
+    private LoggingTool logger;
 
     private String smarts;
     private IAtomContainer atomContainer = null;
