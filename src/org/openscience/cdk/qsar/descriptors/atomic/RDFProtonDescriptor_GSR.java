237d236813f4c46b51658f2e103a2379eddb15d0
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-04-15 08:24:47 +0000

    Added missing headers + bug management
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8196 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/qsar/descriptors/atomic/RDFProtonDescriptor_GSR.java
+++ src/org/openscience/cdk/qsar/descriptors/atomic/RDFProtonDescriptor_GSR.java
@@ -1,3 +1,24 @@
+/* $Revision: 7895 $ $Author: egonw $ $Date: 2007-02-07 23:20:44 +0100 (Wed, 07 Feb 2007) $
+ *
+ * Copyright (C) 2004-2007  Matteo Floris <mfe4@users.sf.net>
+ * Copyright (C) 2006-2007  Federico
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.openscience.cdk.qsar.descriptors.atomic;
 
 import org.openscience.cdk.AtomContainerSet;
@@ -42,15 +63,13 @@ import java.util.List;
  * </tr>
  * </table>
  *
- * @author Federico
+ * @author      Federico
  * @cdk.created 2006-12-11
- * @cdk.module qsar
- * @cdk.set qsar-descriptors
+ * @cdk.module  qsar
+ * @cdk.set     qsar-descriptors
  * @cdk.dictref qsar-descriptors:rdfProtonCalculatedValues
- * @cdk.bug xxxxxx
+ * @cdk.bug     1632419
  */
-
-
 public class RDFProtonDescriptor_GSR implements IAtomicDescriptor {
 
     private boolean checkAromaticity = false;
