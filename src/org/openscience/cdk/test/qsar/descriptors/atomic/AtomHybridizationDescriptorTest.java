ef9f0a74cfb629fa9803a7382c9b9edbfdefc0e9
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-04-15 18:01:57 +0000

    Added new hybridization atom types to fix the errors reported in bug 1701073 . Also updated the tests for the AtomHybridizationDescriptor class
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8199 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/qsar/descriptors/atomic/AtomHybridizationDescriptorTest.java
+++ src/org/openscience/cdk/test/qsar/descriptors/atomic/AtomHybridizationDescriptorTest.java
@@ -25,10 +25,10 @@ package org.openscience.cdk.test.qsar.descriptors.atomic;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-
 import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.Molecule;
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.qsar.IAtomicDescriptor;
 import org.openscience.cdk.qsar.descriptors.atomic.AtomHybridizationDescriptor;
@@ -36,6 +36,9 @@ import org.openscience.cdk.qsar.result.IntegerResult;
 import org.openscience.cdk.smiles.SmilesParser;
 import org.openscience.cdk.test.CDKTestCase;
 import org.openscience.cdk.tools.HydrogenAdder;
+
+import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * TestSuite that runs all QSAR tests.
@@ -45,18 +48,58 @@ import org.openscience.cdk.tools.HydrogenAdder;
 
 public class AtomHybridizationDescriptorTest extends CDKTestCase {
 
-    public  AtomHybridizationDescriptorTest() {}
+    public AtomHybridizationDescriptorTest() {
+    }
 
     public static Test suite() {
         return new TestSuite(AtomHybridizationDescriptorTest.class);
     }
 
     public void testAtomHybridizationDescriptorTest() throws ClassNotFoundException, CDKException, java.lang.Exception {
-        IAtomicDescriptor descriptor  = new AtomHybridizationDescriptor();
+        IAtomicDescriptor descriptor = new AtomHybridizationDescriptor();
         SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
         IAtomContainer mol = sp.parseSmiles("C#CC=C"); //
         HydrogenAdder hAdder = new HydrogenAdder();
-        hAdder.addExplicitHydrogensToSatisfyValency((Molecule)mol);
-        assertEquals(1, ((IntegerResult)descriptor.calculate(mol.getAtom(0), mol).getValue()).intValue());
+        hAdder.addExplicitHydrogensToSatisfyValency((Molecule) mol);
+        assertEquals(1, ((IntegerResult) descriptor.calculate(mol.getAtom(0), mol).getValue()).intValue());
+    }
+
+    public void testBug1701073() throws CDKException, IOException, ClassNotFoundException {
+
+        String[] smiles = new String[]
+                {
+                        "C1CCCC=2[C]1(C(=O)NN2)C",
+                        "C1CCCC=2[C]1(C(=O)NN2)O",
+                        "C[Si](C)(C)[CH](Br)CC(F)(Br)F",
+                        "c1(ccc(cc1)O)C#N",
+                        "CCN(CC)C#CC#CC(=O)OC",
+                        "C(#CN1CCCCC1)[Sn](C)(C)C",
+                        "c1([As+](c2ccccc2)(c2ccccc2)C)ccccc1.[I-]",
+                        "c1(noc(n1)CCC(=O)N(CC)CC)c1ccc(cc1)C",
+                        "c1c(c(ccc1)O)/C=N/CCCC",
+                        "c1(ccc(cc1)C#Cc1ccc(cc1)C#C)OC"
+                };
+
+        IAtomicDescriptor descriptor = new AtomHybridizationDescriptor();
+        SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+        IAtomContainer mol;
+        Iterator atoms;
+        HydrogenAdder hAdder = new HydrogenAdder();
+
+        for (int i = 0; i < smiles.length; i++) {
+            mol = sp.parseSmiles(smiles[i]);
+            hAdder.addExplicitHydrogensToSatisfyValency(mol);
+            atoms = mol.atoms();
+            while (atoms.hasNext()) {
+                IAtom atom = (IAtom) atoms.next();
+                try {
+                    int htype = ((IntegerResult) descriptor.calculate(atom, mol).getValue()).intValue();
+                } catch (Exception e) {
+                    fail(smiles[i] + " failed\n" + e.toString());
+                }
+            }
+        }
+
+
     }
 }
