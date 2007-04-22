215b60b8ec71236e834fe57028b0b4e8514f9402
sushil_ronghe <sushil_ronghe@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-04-22 14:49:30 +0000

    added constant for total connection
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8239 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/CDKConstants.java
+++ src/org/openscience/cdk/CDKConstants.java
@@ -1,26 +1,32 @@
-/* $RCSfile$
- * $Author$    
- * $Date$    
- * $Revision$
+/* $
+ *  
+ *  $RCSfile: $
+ *  $Author$
+ *  $Date$
+ *  $Revision$
  *
- * Copyright (C) 1997-2007  Christoph Steinbeck <steinbeck@users.sf.net>
- * 
- * Contact: cdk-devel@lists.sourceforge.net
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
- * 
+ *  Copyright (C) 1997-2007   Christoph Steinbeck <steinbeck@users.sf.net>
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All I ask is that proper credit is given for my work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
 package org.openscience.cdk;
 
@@ -277,6 +283,12 @@ public class CDKConstants {
      * the given atom.
      */
     public static final String RING_CONNECTIONS = "RingConnections";
+    
+     /*
+     *This property indicate how many bond are present on the atom.
+     *
+     */
+    public static final String TOTAL_CONNECTIONS = "TotalConnections";
 
     /** The Isotropic Shielding, usually calculated by
       * a quantum chemistry program like Gaussian.
