c629c0d77a63d11e807e592442adfbd7e3d8684f
sushil_ronghe <sushil_ronghe@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-04-26 21:33:09 +0000

    New class to check the "["
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8271 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/isomorphism/matchers/smarts/BracketAtom.java
@@ -0,0 +1,48 @@
+
+/*
+ *  $RCSfile$
+ *  $Author: Sushil Ronghe $
+ *  $Date: 2007-04-12  $
+ *  $Revision: 6631 $
+ *
+ *  Copyright (C) 2002-2006  The Chemistry Development Kit (CDK) project
+ *
+ *  Contact: cdk-devel@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All I ask is that proper credit is given for my work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+package org.openscience.cdk.isomorphism.matchers.smarts;
+import org.openscience.cdk.interfaces.IAtom;
+      
+/**
+ *
+ * @author niper
+ */
+public class BracketAtom extends SMARTSAtom{
+    
+    /** Creates a new instance of BracketAtom */
+    public BracketAtom(){
+    }
+    public String toString(){
+        return ("BracketAtom()");
+    }
+    
+    
+}
