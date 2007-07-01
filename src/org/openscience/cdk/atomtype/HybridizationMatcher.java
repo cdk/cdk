1f61b9d23516074882fbe589ede7d9c06d70b1ec
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-07-01 22:20:26 +0000

    Updated hybridization based atom type matcher to check whether an atom has a specified hyb state and if not, evaluate the hybridization state and set it on the atom and return the matching type
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8481 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/atomtype/HybridizationMatcher.java
+++ src/org/openscience/cdk/atomtype/HybridizationMatcher.java
@@ -19,52 +19,56 @@
  */
 package org.openscience.cdk.atomtype;
 
+import org.openscience.cdk.CDKConstants;
 import org.openscience.cdk.config.AtomTypeFactory;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IAtomType;
+import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.tools.LoggingTool;
+
+import java.util.List;
 
 /**
  * AtomTypeMatcher that finds an AtomType by matching the Atom's element symbol,
  * formal charge and hybridization state.
- *
- * <p>This class uses the <b>cdk/config/data/hybridization_atomtypes.xml</b> 
- * list. If there is not an atom type defined for the tested atom, then null 
+ * <p/>
+ * <p>This class uses the <b>cdk/config/data/hybridization_atomtypes.xml</b>
+ * list. If there is not an atom type defined for the tested atom, then null
  * is returned.
  *
- * @author         egonw
- * @cdk.created    2005-04-15
- * @cdk.module     core
+ * @author egonw
+ * @cdk.created 2005-04-15
+ * @cdk.module core
  */
 public class HybridizationMatcher implements IAtomTypeMatcher {
 
-	private static AtomTypeFactory factory = null;
+    private static AtomTypeFactory factory = null;
     private LoggingTool logger;
-    
-	/**
-	 * Constructor for the HybridizationMatcher object.
-	 */
-	public HybridizationMatcher() {
-		logger = new LoggingTool(this);
-	}
+
+    /**
+     * Constructor for the HybridizationMatcher object.
+     */
+    public HybridizationMatcher() {
+        logger = new LoggingTool(this);
+    }
 
 
-	/**
-	 * Finds the AtomType matching the Atom's element symbol, formal charge and 
+    /**
+     * Finds the AtomType matching the Atom's element symbol, formal charge and
      * hybridization state.
-	 *
-	 * @param  atomContainer  AtomContainer
-	 * @param  atom            the target atom
-	 * @exception CDKException Exception thrown if something goed wrong
-	 * @return                 the matching AtomType
-	 */
-	public IAtomType findMatchingAtomType(IAtomContainer atomContainer, IAtom atom) throws CDKException {
+     *
+     * @param atomContainer AtomContainer
+     * @param atom          the target atom
+     * @return the matching AtomType
+     * @throws CDKException Exception thrown if something goed wrong
+     */
+    public IAtomType findMatchingAtomType(IAtomContainer atomContainer, IAtom atom) throws CDKException {
         if (factory == null) {
             try {
                 factory = AtomTypeFactory.getInstance("org/openscience/cdk/config/data/hybridization_atomtypes.xml",
-                          atom.getBuilder());
+                        atom.getBuilder());
             } catch (Exception ex1) {
                 logger.error(ex1.getMessage());
                 logger.debug(ex1);
@@ -75,20 +79,59 @@ public class HybridizationMatcher implements IAtomTypeMatcher {
         IAtomType[] types = factory.getAtomTypes(atom.getSymbol());
         for (IAtomType type : types) {
             logger.debug("   ... matching atom ", atom, " vs ", type);
-            int charge = atom.getFormalCharge();
-            if (charge == type.getFormalCharge()) {
-                logger.debug("     formal charge matches...");
+
+            // if formal charge doesn't match, no use carrying on
+            int formalCharge = atom.getFormalCharge();
+            if (formalCharge != type.getFormalCharge()) {
+                logger.debug("      formal charge does not match.");
+                continue;
+            }
+
+            // lets see if we can get by with the stored hybridization
+            if (atom.getHybridization() != CDKConstants.UNSET) {
                 if (atom.getHybridization() == type.getHybridization()) {
                     logger.debug("     hybridization is OK... We have a match!");
                     return type;
+                } else {
+                    logger.debug("     hybridization does not match");
+                    continue;
                 }
+            }
+
+            // we'll have to evaluate the hyb state
+            logger.debug("      Evaluating hybridization state");
+            List<IBond> connectedBonds = atomContainer.getConnectedBondsList(atom);
+            double maxBondOrder = -1;            
+            for (IBond bond : connectedBonds) {
+                if (bond.getOrder() > maxBondOrder) maxBondOrder = bond.getOrder();
+            }
+
+            // in case the atom has all implicit hydrogens ad no explicit bonds,
+            // then we can return a max bond order of 1
+            if (maxBondOrder == -1 && atom.getHydrogenCount() != CDKConstants.UNSET) {
+                maxBondOrder = 1.0;
+            }
+
+            Integer hybridizationState = -1;
+
+            // TODO we should take into account sp3d an sp3d2 states
+            if (maxBondOrder == 1.0) hybridizationState = CDKConstants.HYBRIDIZATION_SP3;
+            else if (maxBondOrder == 2.0 || maxBondOrder == 1.5) hybridizationState = CDKConstants.HYBRIDIZATION_SP2;
+            else if (maxBondOrder == 3.0) hybridizationState = CDKConstants.HYBRIDIZATION_SP1;
+
+            // we should check max bond order as well, since double bonded
+            // carbons have the same state as aromatic carbons
+            if (hybridizationState == type.getHybridization() && maxBondOrder == type.getMaxBondOrder()) {
+                logger.debug("     hybridization matches. Setting the state");
+                // set the state on the atom
+                atom.setHybridization(hybridizationState);
+                return type;
             } else {
-                logger.debug("     formal charge does NOT match...");
+                logger.debug("     hybridization state does not match.");
             }
         }
-        logger.debug("    No Match");
-        
+
         return null;
-	}
+    }
 }
 
