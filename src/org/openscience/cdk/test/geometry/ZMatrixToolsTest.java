a35a8a2ad2afb09e3f941da373fb2ad3539044c4
speleo3 <speleo3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-07-06 11:01:35 +0000

    new test for zmatrixToCartesian (see bug #1653028)
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8519 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/geometry/ZMatrixToolsTest.java
+++ src/org/openscience/cdk/test/geometry/ZMatrixToolsTest.java
@@ -20,9 +20,12 @@
  */
 package org.openscience.cdk.test.geometry;
 
+import javax.vecmath.Point3d;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.openscience.cdk.geometry.ZMatrixTools;
 import org.openscience.cdk.test.CDKTestCase;
 
 /**
@@ -36,6 +39,21 @@ public class ZMatrixToolsTest extends CDKTestCase {
     
 	public static Test suite() {
 		return new TestSuite(ZMatrixToolsTest.class);
+	}
+
+	public void testZmatrixToCartesian_arraydouble_arrayint_arraydouble_arrayint_arraydouble_arrayint() {
+		// acetaldehyde example from http://www.shodor.org/chemviz/zmatrices/babelex.html
+		int[]  first_atoms = { 0, 0,    0,    0,    3,    3,     3   };
+		double[] distances = { 0, 1.2,  1.1,  1.5,  1.1,  1.1,   1.1 };
+		int[] second_atoms = { 0, 0,    1,    1,    0,    0,     0   };
+		double[]    angles = { 0, 0,  120,  120,  110,  110,   110   };
+		int[]  third_atoms = { 0, 0,    0,    2,    1,    1,     1   };
+		double[] dihedrals = { 0, 0,    0,  180,    0,  120,  -120   };
+		Point3d points[] = ZMatrixTools.zmatrixToCartesian(distances,
+				first_atoms, angles, second_atoms, dihedrals, third_atoms);
+		assertEquals(-0.5500, points[2].x, 0.0001);
+		assertEquals(-1.3664, points[5].y, 0.0001);
+		assertEquals(-0.8952, points[6].z, 0.0001);
 	}
 
 }
