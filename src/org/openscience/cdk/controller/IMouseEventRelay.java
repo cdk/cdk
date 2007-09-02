d09914043e20f831a187fd536005a815081499f8
nielsout <nielsout@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-09-02 09:47:47 +0000

    new Interface for both SWING and SWT mouse events
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8826 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/controller/IMouseEventRelay.java
@@ -0,0 +1,32 @@
+/* $Author: nielsout $
+ * $Date: 2007-09-02 11:46:10 +0100 (su, 02 sep 2007) $
+ * $Revision:  $
+ * 
+ * Copyright (C) 1997-2007  The Chemistry Development Kit (CDK) project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net or nout@science.uva.nl
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All I ask is that proper credit is given for my work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+ package org.openscience.cdk.controller;
+
+public class IMouseEventRelay {
+
+}
