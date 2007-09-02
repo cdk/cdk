609f86dff7a369c7acab600b910a89c01df5b0de
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-09-02 15:01:24 +0000

    Added an interface to extract model info from the Controller2DHub: this ensures that the ControllerModule can only make abstract calls on the model. This is basically what Tashiro and I were talking about earlier in the chat channel. Though the API will likely be fairly large, it does make a better separation between controller and model
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@8839 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/controller/IChemModelRelay.java
@@ -0,0 +1,38 @@
+/* $Revision: 7636 $ $Author: nielsout $ $Date: 2007-09-02 11:46:10 +0100 (su, 02 sep 2007) $
+ * 
+ * Copyright (C) 2007  Egon Willighagen <egonw@users.lists.sf>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All I ask is that proper credit is given for my work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openscience.cdk.controller;
+
+import javax.vecmath.Point2d;
+
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IBond;
+
+public interface IChemModelRelay {
+
+	public abstract IAtom getClosestAtom(Point2d worldCoord);
+
+	public abstract IBond getClosestBond(Point2d worldCoord);
+
+}
