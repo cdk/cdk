b9149d5ef97ec3da0ba6fc86836d6196da7be890
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-10-03 16:55:23 +0000

    Updated to call the new test routines for the pcore util methods
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@9007 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/MpcoreTests.java
+++ src/org/openscience/cdk/test/MpcoreTests.java
@@ -4,6 +4,7 @@ import junit.framework.JUnit4TestAdapter;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.openscience.cdk.test.pharmacophore.PharmacophoreMatcherTest;
+import org.openscience.cdk.test.pharmacophore.PharmacophoreUtilityTest;
 
 /**
  * TestSuite that runs all the sample tests for pharmacophore classes.
@@ -15,6 +16,7 @@ public class MpcoreTests {
     public static Test suite() {
         TestSuite suite = new TestSuite("The cdk.pcore Tests");
         suite.addTest(new JUnit4TestAdapter(PharmacophoreMatcherTest.class));
+        suite.addTest(new JUnit4TestAdapter(PharmacophoreUtilityTest.class));
         return suite;
     }
 
