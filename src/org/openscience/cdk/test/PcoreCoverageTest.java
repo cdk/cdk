a9105bc46aec6699d4bbe90f8b330124ed564773
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-10-25 18:21:48 +0000

    Added a version of the CoverageTest class that utilizes annotations. Added a subclass that exercises it for the pcore module
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@9215 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/org/openscience/cdk/test/PcoreCoverageTest.java
@@ -0,0 +1,56 @@
+/* $Revision: 7635 $ $Author: egonw $ $Date: 2007-01-04 18:32:54 +0100 (Thu, 04 Jan 2007) $
+ * 
+ * Copyright (C) 2004-2007  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
+ */
+package org.openscience.cdk.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * TestSuite that uses tests wether all public methods in the standard
+ * module are tested. Unlike Emma, it does not test that all code is
+ * tested, just all methods.
+ *
+ * @cdk.module test-standard
+ */
+public class PcoreCoverageTest extends CoverageAnnotationTest {
+
+    private final static String CLASS_LIST = "pcore.javafiles";
+
+    public PcoreCoverageTest(String name) {
+        super(name);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        super.loadClassList(CLASS_LIST);
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTestSuite(PcoreCoverageTest.class);
+        return suite;
+    }
+
+    public void testCoverage() {
+        assertTrue(super.runCoverageTest());
+    }
+
+}
