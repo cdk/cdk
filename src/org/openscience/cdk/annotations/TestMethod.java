82292d655b15b364c10b451f184cf0e126f764e6
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-10-25 19:01:31 +0000

    Added missing module declaration, and missing license info.
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@9221 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/annotations/TestMethod.java
+++ src/org/openscience/cdk/annotations/TestMethod.java
@@ -1,3 +1,27 @@
+/* $Revision: 6707 $ $Author: egonw $ $Date: 2006-07-30 16:38:18 -0400 (Sun, 30 Jul 2006) $
+ * 
+ * Copyright (C) 2007  Rajarshi Guha
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.openscience.cdk.annotations;
 
 import java.lang.annotation.Documented;
@@ -7,9 +31,9 @@ import java.lang.annotation.Documented;
  *
  * @cdk.author Rajarshi Guha
  * @cdk.svnrev $Revision: 9162 $
+ * @cdk.module core
  */
 @Documented
-
 public @interface TestMethod {
     String value();
 }
