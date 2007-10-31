7e66f3f9f4f048617a82c4a7f5df534eeab4b010
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-10-31 22:44:50 +0000

    Removed exponentialFloat since float does not provide accuracy to satisfy a reasonable (2 decimal places) test. Use the double version if required
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@9305 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/math/RandomNumbersTool.java
+++ src/org/openscience/cdk/math/RandomNumbersTool.java
@@ -33,224 +33,210 @@ import java.util.Random;
  * Class supplying useful methods to generate random numbers.
  * This class isn't supposed to be instantiated. You should use it by calling
  * its static methods.
- * 
- * @cdk.module  standard
- * @cdk.svnrev  $Revision$
+ *
+ * @cdk.module standard
+ * @cdk.svnrev $Revision$
  */
 public class RandomNumbersTool extends Random {
-    
+
     private static final long serialVersionUID = -8238833473383641882L;
-    
+
     private static java.util.Random random;
     private static long randomSeed;
 
     static {
-        randomSeed=System.currentTimeMillis();
-        random=new java.util.Random(randomSeed);
+        randomSeed = System.currentTimeMillis();
+        random = new java.util.Random(randomSeed);
     }
 
     /**
      * Sets the base generator to be used by this class.
-     * <p>
+     * <p/>
+     *
      * @param base_random a <code>java.util.Random</code> subclass.
      */
-    public static void setRandom(java.util.Random base_random)
-    {
-        random=base_random;
+    public static void setRandom(java.util.Random base_random) {
+        random = base_random;
     }
 
     /**
      * Sets the seed of this random number generator using a single
      * <code>long</code> seed.
      *
-     * @param new_seed  the seed to be used by the random number generator.
+     * @param new_seed the seed to be used by the random number generator.
      */
-    public static void setRandomSeed(long new_seed)
-    {
-        randomSeed=new_seed;
+    public static void setRandomSeed(long new_seed) {
+        randomSeed = new_seed;
         random.setSeed(randomSeed);
     }
 
     /**
      * Returns the seed being used by this random number generator.
-     * <p>
+     * <p/>
+     *
      * @return the <code>long</code> seed.
      */
-    public static long getRandomSeed()
-    {
+    public static long getRandomSeed() {
         return randomSeed;
     }
 
     /**
      * Generates a random integer between <code>0</code> and <code>1</code>.
-     * <p>
+     * <p/>
+     *
      * @return a random integer between <code>0</code> and <code>1</code>.
      */
-    public static int randomInt()
-    {
-        return randomInt(0,1);
+    public static int randomInt() {
+        return randomInt(0, 1);
     }
 
     /**
      * Generates a random integer between the specified values.
-     * <p>
+     * <p/>
+     *
      * @param lo the lower bound for the generated integer.
      * @param hi the upper bound for the generated integer.
      * @return a random integer between <code>lo</code> and <code>hi</code>.
      */
-    public static int randomInt(int lo,int hi)
-    {
-        return (Math.abs(random.nextInt())%(hi-lo+1))+lo;
+    public static int randomInt(int lo, int hi) {
+        return (Math.abs(random.nextInt()) % (hi - lo + 1)) + lo;
     }
 
     /**
      * Generates a random long between <code>0</code> and <code>1</code>.
-     * <p>
+     * <p/>
+     *
      * @return a random long between <code>0</code> and <code>1</code>.
      */
-    public static long randomLong()
-    {
-        return randomLong(0,1);
+    public static long randomLong() {
+        return randomLong(0, 1);
     }
 
     /**
      * Generates a random long between the specified values.
-     * <p>
+     * <p/>
+     *
      * @param lo the lower bound for the generated long.
      * @param hi the upper bound for the generated long.
      * @return a random long between <code>lo</code> and <code>hi</code>.
      */
-    public static long randomLong(long lo,long hi)
-    {
-        return (Math.abs(random.nextLong())%(hi-lo+1))+lo;
+    public static long randomLong(long lo, long hi) {
+        return (Math.abs(random.nextLong()) % (hi - lo + 1)) + lo;
     }
 
     /**
      * Generates a random float between <code>0</code> and <code>1</code>.
-     * <p>
+     * <p/>
+     *
      * @return a random float between <code>0</code> and <code>1</code>.
      */
-    public static float randomFloat()
-    {
+    public static float randomFloat() {
         return random.nextFloat();
     }
 
     /**
      * Generates a random float between the specified values.
-     * <p>
+     * <p/>
+     *
      * @param lo the lower bound for the generated float.
      * @param hi the upper bound for the generated float.
      * @return a random float between <code>lo</code> and <code>hi</code>.
      */
-    public static float randomFloat(float lo,float hi)
-    {
-        return (hi-lo)*random.nextFloat()+lo;
+    public static float randomFloat(float lo, float hi) {
+        return (hi - lo) * random.nextFloat() + lo;
     }
 
     /**
      * Generates a random double between <code>0</code> and <code>1</code>.
-     * <p>
+     * <p/>
+     *
      * @return a random double between <code>0</code> and <code>1</code>.
      */
-    public static double randomDouble()
-    {
+    public static double randomDouble() {
         return random.nextDouble();
     }
 
     /**
      * Generates a random double between the specified values.
-     * <p>
+     * <p/>
+     *
      * @param lo the lower bound for the generated double.
      * @param hi the upper bound for the generated double.
      * @return a random double between <code>lo</code> and <code>hi</code>.
      */
-    public static double randomDouble(double lo,double hi)
-    {
-        return (hi-lo)*random.nextDouble()+lo;
+    public static double randomDouble(double lo, double hi) {
+        return (hi - lo) * random.nextDouble() + lo;
     }
 
     /**
      * Generates a random boolean.
-     * <p>
+     * <p/>
+     *
      * @return a random boolean.
      */
-    public static boolean randomBoolean()
-    {
-        return (randomInt()==1);
+    public static boolean randomBoolean() {
+        return (randomInt() == 1);
     }
 
     /**
      * Generates a random bit: either <code>0</code> or <code>1</code>.
-     * <p>
+     * <p/>
+     *
      * @return a random bit.
      */
-    public static int randomBit()
-    {
+    public static int randomBit() {
         return randomInt();
     }
 
     /**
      * Returns a boolean value based on a biased coin toss.
-     * <p>
+     * <p/>
+     *
      * @param p the probability of success.
      * @return <code>true</code> if a success was found; <code>false</code>
-     * otherwise.
+     *         otherwise.
      */
-    public static boolean flipCoin(double p)
-    {
-        return (randomDouble()<p ? true : false);
+    public static boolean flipCoin(double p) {
+        return (randomDouble() < p ? true : false);
     }
 
     /**
      * Generates a random float from a Gaussian distribution with the specified
      * deviation.
-     * <p>
+     * <p/>
+     *
      * @param dev the desired deviation.
      * @return a random float from a Gaussian distribution with deviation
-     * <code>dev</code>.
+     *         <code>dev</code>.
      */
-    public static float gaussianFloat(float dev)
-    {
-        return (float)random.nextGaussian()*dev;
+    public static float gaussianFloat(float dev) {
+        return (float) random.nextGaussian() * dev;
     }
 
     /**
      * Generates a random double from a Gaussian distribution with the specified
      * deviation.
-     * <p>
+     * <p/>
+     *
      * @param dev the desired deviation.
      * @return a random double from a Gaussian distribution with deviation
-     * <code>dev</code>.
+     *         <code>dev</code>.
      */
-    public static double gaussianDouble(double dev)
-    {
-        return random.nextGaussian()*dev;
-    }
-
-    /**
-     * Generates a random float from an Exponential distribution with the specified
-     * mean value.
-     * <p>
-     * @param mean the desired mean value.
-     * @return a random float from an Exponential distribution with mean value
-     * <code>mean</code>.
-     */
-    public static float exponentialFloat(float mean)
-    {
-        return (float)(-mean*Math.log(randomDouble()));
+    public static double gaussianDouble(double dev) {
+        return random.nextGaussian() * dev;
     }
 
     /**
      * Generates a random double from an Exponential distribution with the specified
      * mean value.
-     * <p>
+     * <p/>
+     *
      * @param mean the desired mean value.
      * @return a random double from an Exponential distribution with mean value
-     * <code>mean</code>.
+     *         <code>mean</code>.
      */
-    public static double exponentialDouble(double mean)
-    {
-        return -mean*Math.log(randomDouble());
+    public static double exponentialDouble(double mean) {
+        return -mean * Math.log(randomDouble());
     }
 }
 
