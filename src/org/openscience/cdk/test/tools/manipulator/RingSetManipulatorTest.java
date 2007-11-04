1b19beafd97f00eb4507a1e453476ee301afc479
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-11-04 21:18:04 +0000

    Added missing license+copyright info
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@9361 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/tools/manipulator/RingSetManipulatorTest.java
+++ src/org/openscience/cdk/test/tools/manipulator/RingSetManipulatorTest.java
@@ -1,3 +1,27 @@
+/* $Revision: 8973 $ $Author: egonw $ $Date: 2007-09-26 13:47:29 +0200 (Wed, 26 Sep 2007) $
+ * 
+ * Copyright (C) 2006-2007  The Chemistry Development Kit Project
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.openscience.cdk.test.tools.manipulator;
 
 import java.util.List;
