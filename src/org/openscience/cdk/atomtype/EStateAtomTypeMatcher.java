6d1037c0f3fcddf872c865790fcf118b08e30168
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-11-20 16:30:46 +0000

    Update the AlogP descriptor with the code provided by Nina and Nick, which now correctly evaluates the descriptor. Also updated the EState atom type matcher to correctly match the required atom types
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@9470 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/atomtype/EStateAtomTypeMatcher.java
+++ src/org/openscience/cdk/atomtype/EStateAtomTypeMatcher.java
@@ -25,47 +25,56 @@ package org.openscience.cdk.atomtype;
 
 import org.openscience.cdk.CDKConstants;
 import org.openscience.cdk.interfaces.*;
-import org.openscience.cdk.ringsearch.AllRingsFinder;
 
 /**
  * Determines the EState atom types.
- * 
- * @author     Todd Martin
+ *
+ * @author Todd Martin
+ * @author nick
  * @cdk.module standard
- * @cdk.svnrev  $Revision: 9162 $
+ * @cdk.svnrev $Revision: 9162 $
  */
+
+	
+
 public class EStateAtomTypeMatcher  implements IAtomTypeMatcher {
 
-	public IAtomType findMatchingAtomType(IAtomContainer atomContainer, IAtom atom) {
+	IRingSet ringSet = null;
+
+	public void setRingSet(IRingSet rs)
+	{
+		ringSet = rs;
+	}
+
+	public IAtomType findMatchingAtomType(IAtomContainer atomContainer, IAtom atom)
+	{
+
 		IAtomType atomType = null;
-		
 		try {
-			String fragment;
-			
-			AllRingsFinder arf = new AllRingsFinder();
-			IRingSet rs = arf.findAllRings(atomContainer);
-			
+			String fragment = "";
+			int NumHAtoms = 0;
 			int NumSingleBonds2 = 0;
 			int NumDoubleBonds2 = 0;
 			int NumTripleBonds2 = 0;
 			int NumAromaticBonds2 = 0;
 			int NumAromaticBondsTotal2 = 0;
-			
+
 			String element = atom.getSymbol();
-			
-			java.util.List<IAtom> attachedAtoms = atomContainer.getConnectedAtomsList(atom);
-			
+
+			java.util.List attachedAtoms = atomContainer.getConnectedAtomsList(atom);
+
 			for (int j = 0; j <= attachedAtoms.size() - 1; j++) {
-				IAtom attached = attachedAtoms.get(j);
+				IAtom attached = (IAtom)attachedAtoms.get(j);
 				IBond b = atomContainer.getBond(atom, attached);
-				
+				if(attached.getSymbol().equals("H"))
+					NumHAtoms++;
+
 				if (atom.getFlag(CDKConstants.ISAROMATIC)
-						&& attached
-						                 .getFlag(CDKConstants.ISAROMATIC)) {
-					
+						&& attached.getFlag(CDKConstants.ISAROMATIC)) {
+
 					boolean SameRing = inSameAromaticRing(atomContainer, atom,
-							attached, rs);
-					
+							attached, ringSet);
+
 					if (SameRing) {
 						NumAromaticBonds2++;
 						if (element.equals("N")) {
@@ -82,9 +91,9 @@ public class EStateAtomTypeMatcher  implements IAtomTypeMatcher {
 						if (b.getOrder() == 3)
 							NumTripleBonds2++;
 					}
-					
+
 				} else {
-					
+
 					if (b.getOrder() == 1)
 						NumSingleBonds2++;
 					if (b.getOrder() == 2)
@@ -93,81 +102,78 @@ public class EStateAtomTypeMatcher  implements IAtomTypeMatcher {
 						NumTripleBonds2++;
 				}
 			}
-			
+			NumSingleBonds2 = NumSingleBonds2 - NumHAtoms;
+
 			// assign frag here
 			fragment = "S";
-			
+
 			for (int j = 0; j <= NumTripleBonds2 - 1; j++) {
 				fragment += "t";
 			}
-			
+
 			for (int j = 0; j <= NumDoubleBonds2 - 1; j++) {
 				fragment += "d";
 			}
-			
+
 			for (int j = 0; j <= NumSingleBonds2 - 1; j++) {
 				fragment += "s";
 			}
-			
+
 			for (int j = 0; j <= NumAromaticBonds2 - 1; j++) {
 				fragment += "a";
 			}
-			
+
 			fragment += element;
-			
+
 			if (atom.getFormalCharge() == 1) {
 				fragment += "p";
 			} else if (atom.getFormalCharge() == -1) {
 				fragment += "m";
 			}
-			
-			fragment += "*";// flag it as unassigned fragment
-			
-			// for now set #H = 0 TODO- add routine to figure out #Hs for
-			// possible valence states
-			atom.setHydrogenCount(0);
+
+			if (NumHAtoms == 1)
+				fragment += "H";
+			else
+				if (NumHAtoms > 1)
+					fragment += ("H" + NumHAtoms);
+
+			//atom.setHydrogenCount(0);
 
 			atomType = atom.getBuilder().newAtomType(fragment, atom.getSymbol());
 			atomType.setFormalCharge(atom.getFormalCharge());
 			if (atom.getFlag(CDKConstants.ISAROMATIC))
 				atomType.setFlag(CDKConstants.ISAROMATIC, true);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return atomType;
 	}
 
 	public static boolean inSameAromaticRing(IAtomContainer m, IAtom atom1,
-			IAtom atom2, IRingSet rs) {
-		
-		for (int i = 0; i <= rs.getAtomContainerCount() - 1; i++) {
+			IAtom atom2, IRingSet rs)
+	{
+		for (int i = 0; i <= rs.getAtomContainerCount() - 1; i++)
+		{
 			IRing r = (IRing) rs.getAtomContainer(i);
-			
-			if (!r.getFlag(CDKConstants.ISAROMATIC))
-				continue;
-			
-			// ArrayList al=new ArrayList();
-			
-			boolean HaveOne = false;
-			boolean HaveTwo = false;
-			
-			for (int j = 0; j <= r.getAtomCount() - 1; j++) {
-				if (m.getAtomNumber(r.getAtom(j)) == m.getAtomNumber(atom1))
-					HaveOne = true;
-				if (m.getAtomNumber(r.getAtom(j)) == m.getAtomNumber(atom2))
-					HaveTwo = true;
+			if (r.contains(atom1) && r.contains(atom2))
+			{
+				if (isAromaticRing(r))
+					return(true);
 			}
-			
-			if (HaveOne && HaveTwo) {
-				return true;
-			}
-			
-		} // end ring for loop
-		
+		}
 		return false;
 	}
-	
-	
+
+
+	static boolean  isAromaticRing(IRing ring)
+	{
+		for (int i = 0; i < ring.getAtomCount(); i++)
+			if(!ring.getAtom(i).getFlag(CDKConstants.ISAROMATIC))
+				return(false);
+
+		return(true);
+	}
+
 }
