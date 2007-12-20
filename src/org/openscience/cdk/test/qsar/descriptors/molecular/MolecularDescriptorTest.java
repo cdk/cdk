9ec188c060307a9378583a12ee198fa35e2374f5
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2007-12-20 10:19:57 +0000

    More testing: are the parameters properly given and consistent
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@9645 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/qsar/descriptors/molecular/MolecularDescriptorTest.java
+++ src/org/openscience/cdk/test/qsar/descriptors/molecular/MolecularDescriptorTest.java
@@ -26,6 +26,7 @@ import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.qsar.DescriptorValue;
 import org.openscience.cdk.qsar.IMolecularDescriptor;
 import org.openscience.cdk.test.CDKTestCase;
@@ -63,23 +64,12 @@ public abstract class MolecularDescriptorTest extends CDKTestCase {
 	}
 	
 	/**
-	 * Checks if the output is consistent.
+	 * Checks if the given labels are consistent.
 	 * 
 	 * @throws Exception Passed on from calculate.
 	 */
-    public void testDescriptorSanity() throws Exception {
-        IAtomContainer mol = DefaultChemObjectBuilder.getInstance().newMolecule();
-        IAtom c1 = DefaultChemObjectBuilder.getInstance().newAtom("O");
-        c1.setPoint3d(new Point3d(0.0, 0.0, 0.0));
-        IAtom h1 = DefaultChemObjectBuilder.getInstance().newAtom("H");
-        h1.setPoint3d(new Point3d(1.0, 0.0, 0.0));
-        IAtom h2 = DefaultChemObjectBuilder.getInstance().newAtom("H");
-        h2.setPoint3d(new Point3d(-1.0, 0.0, 0.0));
-        mol.addAtom(c1);
-        mol.addAtom(h1);
-        mol.addAtom(h2);
-        mol.addBond(0,1,IBond.Order.SINGLE);
-        mol.addBond(0,2,IBond.Order.SINGLE);
+    public void testLabels() throws Exception {
+        IAtomContainer mol = someoneBringMeSomeWater();
         
         DescriptorValue v = descriptor.calculate(mol);
         assertNotNull(v);
@@ -111,4 +101,96 @@ public abstract class MolecularDescriptorTest extends CDKTestCase {
         );
     }
 
+	/**
+	 * Checks if the parameterization is consistent.
+	 * 
+	 * @throws Exception 
+	 */
+    public void testGetParameterNames() throws Exception {
+        String[] paramNames = descriptor.getParameterNames();
+//        FIXME: the next would be nice, but not currently agreed-upon policy
+//        assertNotNull(
+//        	"The method getParameterNames() must return a non-null value, possible a zero length String[] array",
+//        	paramNames
+//        );
+//        FIXME: so instead:
+        if (paramNames == null) paramNames = new String[0];
+        for (int i=0; i<paramNames.length; i++) {
+        	assertNotNull(
+        		"A parameter name must not be null.",
+        		paramNames[i]
+        	);
+        	assertNotSame(
+            	"A parameter name String must not be empty.",
+            	0, paramNames[i].length()
+            );
+        }
+    }
+    
+    public void testGetParameters() {
+        Object[] params = descriptor.getParameters();
+//      FIXME: the next would be nice, but not currently agreed-upon policy
+//      assertNotNull(
+//      	"The method getParameters() must return a non-null value, possible a zero length Object[] array",
+//      	paramNames
+//      );
+//      FIXME: so instead:
+        if (params == null) params = new Object[0];
+        for (int i=0; i<params.length; i++) {
+        	assertNotNull(
+        		"A parameter default must not be null.",
+        		params[i]
+        	);
+        }
+    }
+    
+    public void testGetParameterType() {
+        String[] paramNames = descriptor.getParameterNames();
+//      FIXME: see testGetParameterNames() comment on the same line 
+        if (paramNames == null) paramNames = new String[0];
+        Object[] params = descriptor.getParameters();
+//      FIXME: see testGetParameters() comment on the same line
+        if (params == null) params = new Object[0];
+
+        for (int i=0; i<paramNames.length; i++) {
+        	Object type = descriptor.getParameterType(paramNames[i]);
+        	assertEquals(
+        		"The getParameterType(String) return type is not consistent " +
+        		"with the getParameters() types for parameter " + i,
+        		type.getClass().getName(),
+        		params[i].getClass().getName()
+        	);
+        }
+    }
+    
+    public void testParameterConsistency() {
+        String[] paramNames = descriptor.getParameterNames();
+//      FIXME: see testGetParameterNames() comment on the same line 
+        if (paramNames == null) paramNames = new String[0];
+        Object[] params = descriptor.getParameters();
+//      FIXME: see testGetParameters() comment on the same line
+        if (params == null) params = new Object[0];
+        
+        assertEquals(
+        	"The number of returned parameter names must equate the number of returned parameters",
+        	paramNames.length, params.length
+        );
+    }
+
+    private IMolecule someoneBringMeSomeWater() {
+        IMolecule mol = DefaultChemObjectBuilder.getInstance().newMolecule();
+        IAtom c1 = DefaultChemObjectBuilder.getInstance().newAtom("O");
+        c1.setPoint3d(new Point3d(0.0, 0.0, 0.0));
+        IAtom h1 = DefaultChemObjectBuilder.getInstance().newAtom("H");
+        h1.setPoint3d(new Point3d(1.0, 0.0, 0.0));
+        IAtom h2 = DefaultChemObjectBuilder.getInstance().newAtom("H");
+        h2.setPoint3d(new Point3d(-1.0, 0.0, 0.0));
+        mol.addAtom(c1);
+        mol.addAtom(h1);
+        mol.addAtom(h2);
+        mol.addBond(0,1,IBond.Order.SINGLE);
+        mol.addBond(0,2,IBond.Order.SINGLE);
+        return mol;
+    }
+    
 }
