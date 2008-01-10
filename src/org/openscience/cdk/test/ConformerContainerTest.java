4311bb0abbf8df6bf6b859fc6900c47fe148c60b
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-01-10 18:39:05 +0000

    Updated some set up code, to better replicate real world usage. Also rather than have a one-time setup, converted it to per test method - this avoids one test changing the test data (confs and base) and causing a subsequent test to fail since it does not expect the test data to change.
    Also fixed some of the tests, which were not really checking the proper thing - related to the fact thet get() for any integer X, always returns the same IAtomContainer - just the 3D coords change for different values of X
    
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@9882 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/org/openscience/cdk/test/ConformerContainerTest.java
+++ src/org/openscience/cdk/test/ConformerContainerTest.java
@@ -1,12 +1,7 @@
 package org.openscience.cdk.test;
 
-import java.util.Iterator;
-import java.util.Random;
-
-import javax.vecmath.Point3d;
-
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.openscience.cdk.CDKConstants;
 import org.openscience.cdk.ConformerContainer;
@@ -15,13 +10,17 @@ import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
 
+import javax.vecmath.Point3d;
+import java.util.Iterator;
+import java.util.Random;
+
 
 /**
  * @cdk.module test-data
  */
 public class ConformerContainerTest extends NewCDKTestCase {
-    private static IAtomContainer base;
-    private static IAtomContainer[] confs;
+    private IAtomContainer base;
+    private IAtomContainer[] confs;
 
     private static int natom = 10;
     private static int nconfs = 20;
@@ -31,7 +30,7 @@ public class ConformerContainerTest extends NewCDKTestCase {
     private static IAtomContainer getBaseAtomContainer(int natom, String title) {
         IAtomContainer container = DefaultChemObjectBuilder.getInstance().newAtomContainer();
         container.setProperty(CDKConstants.TITLE, title);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < natom; i++) {
             Point3d coord = new Point3d();
             coord.x = rnd.nextDouble();
             coord.y = rnd.nextDouble();
@@ -53,22 +52,20 @@ public class ConformerContainerTest extends NewCDKTestCase {
     private static IAtomContainer[] getConformers(IAtomContainer base, int nconf) throws CloneNotSupportedException {
         IAtomContainer[] ret = new IAtomContainer[nconf];
         for (int i = 0; i < nconf; i++) {
-            IAtomContainer aClone = (IAtomContainer) base.clone();
-            aClone.setProperty(CDKConstants.TITLE, base.getProperty(CDKConstants.TITLE));
-            for (int j = 0; j < aClone.getAtomCount(); j++) {
-                Point3d p = aClone.getAtom(j).getPoint3d();
+            for (int j = 0; j < base.getAtomCount(); j++) {
+                Point3d p = base.getAtom(j).getPoint3d();
                 p.x = rnd.nextDouble();
                 p.y = rnd.nextDouble();
                 p.z = rnd.nextDouble();
-                aClone.getAtom(j).setPoint3d(p);
+                base.getAtom(j).setPoint3d(p);
             }
-            ret[i] = aClone;
+            ret[i] = (IAtomContainer) base.clone();
         }
         return ret;
     }
 
-    @BeforeClass
-    public static void setUp() throws CloneNotSupportedException {
+    @Before
+    public void setUp() throws CloneNotSupportedException {
         base = getBaseAtomContainer(natom, "myMolecule");
         confs = getConformers(base, nconfs);
     }
@@ -184,61 +181,74 @@ public class ConformerContainerTest extends NewCDKTestCase {
     @Test public void testAdd_int_IAtomContainer() {
     	ConformerContainer container = new ConformerContainer(confs);
     	container.add(5, confs[5]);
-    };
+    }
+
     @Test public void testAdd_int_Object() {
     	ConformerContainer container = new ConformerContainer(confs);
     	container.add(5, confs[5]);
-    };
+    }
+
     @Test public void testAdd_Object() {
         ConformerContainer container = new ConformerContainer();
         Assert.assertNotNull(container);
         for (IAtomContainer conf : confs) container.add(conf);
         Assert.assertEquals(nconfs, container.size());
-    };
-    @Test public void testIndexOf_Object() {
+    }
+
+    @Test
+    public void testIndexOf_Object() {
         ConformerContainer container = new ConformerContainer(confs);
         Assert.assertNotNull(container);
+
         int counter = 0;
         for (IAtomContainer conf : confs) {
-        	Assert.assertEquals(counter, container.indexOf(conf));
-        	counter++;
+            Assert.assertEquals(counter, container.indexOf(conf));
+            counter++;
         }
-    };
+    }
+
     @Test public void testClear() {
     	ConformerContainer container = new ConformerContainer(confs);
     	Assert.assertEquals(nconfs, container.size());
     	container.clear();
     	Assert.assertEquals(0, container.size());
-    };
+    }
+
     @Test public void testSize() {
     	ConformerContainer container = new ConformerContainer(confs);
     	Assert.assertEquals(nconfs, container.size());
-    };
+    }
+
     @Test public void testLastIndexOf_Object() {
     	ConformerContainer container = new ConformerContainer(confs);
     	Assert.assertEquals(nconfs, container.size());
     	Assert.assertEquals(3, container.lastIndexOf(container.get(3)));
-    };
+    }
+
     @Test public void testContains_Object() {
     	ConformerContainer container = new ConformerContainer(confs);
     	Assert.assertEquals(nconfs, container.size());
     	Assert.assertTrue(container.contains(container.get(3)));
-    };
+    }
+
     @Test public void testAddAll_Collection() {
     	ConformerContainer container = new ConformerContainer(confs);
     	Assert.assertEquals(nconfs, container.size());
     	Assert.assertTrue(container.contains(container.get(3)));
-    };
+    }
+
     @Test(expected=UnsupportedOperationException.class)
     public void testAddAll_int_Collection() {
     	ConformerContainer container = new ConformerContainer(confs);
     	container.addAll(5, null);
-    };
+    }
+
     @Test(expected=UnsupportedOperationException.class) 
     public void testToArray_arrayObject() {
     	ConformerContainer container = new ConformerContainer(confs);
     	container.toArray(new IAtomContainer[]{});
-    };
+    }
+
     @Test public void testRemove_Object() {
     	ConformerContainer cContainer = new ConformerContainer(confs);
     	Assert.assertEquals(nconfs, cContainer.size());
@@ -247,28 +257,44 @@ public class ConformerContainerTest extends NewCDKTestCase {
     	cContainer.remove(container);
     	Assert.assertEquals(nconfs-1, cContainer.size());
     	Assert.assertFalse(cContainer.contains(container));
-    };
+    }
+
     @Test public void testSet_int_IAtomContainer() {
     	ConformerContainer container = new ConformerContainer(confs);
     	int location = 5;
-        Assert.assertNotSame(container.get(location+1), container.get(location));
         container.set(location, container.get(location+1));
-        Assert.assertEquals(container.get(location+1), container.get(location));
-    };
+        Assert.assertEquals(location, container.indexOf(container.get(location+1)));
+    }
+
+    /* note that assertNotSame checks whether object A *refers* to the same as object B
+     since a ConformerContainer.get always returns the sme IAtomContainer object (just
+     changing the 3D coords for a given conformer) the following will always fail
+
+     Assert.assertNotSame(container.get(location+1), container.get(location));
+
+     Sinmilarly: Assert.assertEquals(container.get(location+1), container.get(location));
+     since this will check whether the references of X & Y are the same. Since get() returns
+     the same atom container (with the coords chanegd), there is no difference in the reference
+     returned. Thus this test is always the same.
+
+     Better to check set position X to X+1 and then find the first occurence of the object
+     originally at X+1 - it should now be X
+     */
     @Test public void testSet_int_Object() {
     	ConformerContainer container = new ConformerContainer(confs);
     	int location = 5;
-        Assert.assertNotSame(container.get(location+1), container.get(location));
         container.set(location, container.get(location+1));
-        Assert.assertEquals(container.get(location+1), container.get(location));
-    };
+        Assert.assertEquals(location, container.indexOf(container.get(location+1)));
+    }
+
     @Test(expected=UnsupportedOperationException.class)
     public void testContainsAll_Collection() {
         ConformerContainer container = new ConformerContainer(confs);
         Assert.assertNotNull(container);
         Assert.assertEquals(nconfs, container.size());        
         Assert.assertTrue(container.containsAll(container));
-    };
+    }
+
     @Test(expected=UnsupportedOperationException.class) 
     public void testRemoveAll_Collection() {
         ConformerContainer container = new ConformerContainer(confs);
@@ -277,31 +303,36 @@ public class ConformerContainerTest extends NewCDKTestCase {
         
         container.removeAll(container);
         Assert.assertEquals(0, container.size());
-    };       
+    }
+
     @Test(expected=UnsupportedOperationException.class)
     public void testRetainAll_Collection() {
     	ConformerContainer container = new ConformerContainer(base);
         container.retainAll(null);
-    };
+    }
+
     @Test(expected=UnsupportedOperationException.class)
     public void testSubList_int_int() {
     	ConformerContainer container = new ConformerContainer(base);
         container.subList(3, 4);
-    };
+    }
+
     @Test(expected=UnsupportedOperationException.class)
     public void testListIterator() {
         ConformerContainer container = new ConformerContainer(base);
         container.listIterator();
-    };
+    }
+
     @Test(expected=UnsupportedOperationException.class)
     public void testListIterator_int() {
     	ConformerContainer container = new ConformerContainer(base);
         container.listIterator(1);
-    };
+    }
+
     @Test public void testConformerContainer_IAtomContainer() {
         ConformerContainer container = new ConformerContainer(base);
         Assert.assertNotNull(container);
         Assert.assertEquals(1, container.size());
-    };
+    }
     
 }
