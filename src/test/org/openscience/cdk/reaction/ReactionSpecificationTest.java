79e97e45d6280c24d74ad17276c821e4294c63ff
miguelrojasch <miguelrojasch@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-03-05 11:07:54 +0000

    Applied patch 10310 from the miguelrojasch/reaction branch: 'New test for ReactionSpecificationTest'
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/trunk/cdk@10331 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/test/org/openscience/cdk/reaction/ReactionSpecificationTest.java
@@ -0,0 +1,78 @@
+/* $Revision: 7691 $ $Author: egonw $ $Date: 2007-01-11 12:47:48 +0100 (Thu, 11 Jan 2007) $
+ * 
+ * Copyright (C) 2008  Miguel Rojas <miguelrojasch@users.sf.net>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
+ */
+package org.openscience.cdk.reaction;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openscience.cdk.NewCDKTestCase;
+
+/**
+ * @cdk.module test-reaction
+ */
+public class ReactionSpecificationTest extends NewCDKTestCase {
+    
+    public ReactionSpecificationTest() {
+        super();
+    }
+
+	private final static String REAC_REF = "bla"; 
+	private final static String REAC_IMPL_TITLE = "bla2"; 
+	private final static String REAC_IMPL_VENDOR = "bla3"; 
+	private final static String REAC_IMPL_ID = "bla4"; 
+
+    @Test public void testReactionSpecification_String_String_String_String() {
+		ReactionSpecification spec = new ReactionSpecification(
+            REAC_REF, REAC_IMPL_TITLE, REAC_IMPL_ID, REAC_IMPL_VENDOR
+		);
+		Assert.assertNotNull(spec);
+	}
+
+    @Test public void testGetImplementationVendor() {
+		ReactionSpecification spec = new ReactionSpecification(
+            REAC_REF, REAC_IMPL_TITLE, REAC_IMPL_ID, REAC_IMPL_VENDOR
+		);
+		Assert.assertEquals(REAC_IMPL_VENDOR, spec.getImplementationVendor());
+	}
+
+    @Test public void testGetSpecificationReference() {
+		ReactionSpecification spec = new ReactionSpecification(
+            REAC_REF, REAC_IMPL_TITLE, REAC_IMPL_ID, REAC_IMPL_VENDOR
+		);
+		Assert.assertEquals(REAC_REF, spec.getSpecificationReference());
+	}
+
+    @Test public void testGetImplementationIdentifier() {
+		ReactionSpecification spec = new ReactionSpecification(
+            REAC_REF, REAC_IMPL_TITLE, REAC_IMPL_ID, REAC_IMPL_VENDOR
+		);
+		Assert.assertEquals(REAC_IMPL_ID, spec.getImplementationIdentifier());
+	}
+
+    @Test public void testGetImplementationTitle() {
+		ReactionSpecification spec = new ReactionSpecification(
+            REAC_REF, REAC_IMPL_TITLE, REAC_IMPL_ID, REAC_IMPL_VENDOR
+		);
+		Assert.assertEquals(REAC_IMPL_TITLE, spec.getImplementationTitle());
+	}
+
+}
+
+
