e1bbd232077c8276db1ca8c59dda92d02c69746f
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-03-21 19:10:30 +0000

    Removed obsolete dir
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@10471 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- packages/windows/cdk/cdkconv.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Set CDK_HOME to the Jmol installation directory.
-if "%CDK_HOME%"=="" set CDK_HOME=.
-
-@set libDir=%CDK_HOME%\jar
-@java -cp "%libDir%\cdk-apps.jar" org.openscience.cdk.applications.FileConvertor %1 %2 %3 %4 %5 %6 %7 %8 %9
--- packages/windows/cdk/cdkview.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Set CDK_HOME to the Jmol installation directory.
-if "%CDK_HOME%"=="" set CDK_HOME=.
-
-@set libDir=%CDK_HOME%\jar
-@java -cp "%libDir%\cdk-apps.jar;%libDir%\jmol.jar" org.openscience.cdk.applications.Viewer %1 %2 %3 %4 %5 %6 %7 %8 %9
