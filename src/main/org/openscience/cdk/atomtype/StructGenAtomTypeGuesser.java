55b37713718c9dfafdd96df220c6bde5620cc424
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-04-30 14:15:26 +0000

    Fixed copyright years (cheap commit to test why Revision: updates are not working...)
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@10730 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/main/org/openscience/cdk/atomtype/StructGenAtomTypeGuesser.java
+++ src/main/org/openscience/cdk/atomtype/StructGenAtomTypeGuesser.java
@@ -1,6 +1,8 @@
-/* $Revision: 5855 $ $Author: egonw $ $Date: 2006-03-29 10:27:08 +0200 (Wed, 29 Mar 2006) $
+/* $Revision: 5855 $
+ * $Author: egonw $
+ * $Date: 2006-03-29 10:27:08 +0200 (Wed, 29 Mar 2006) $
  *
- * Copyright (C) 2005-2007  Egon Willighagen <egonw@users.sf.net>
+ * Copyright (C) 2005-2008  Egon Willighagen <egonw@users.sf.net>
  *
  * Contact: cdk-devel@lists.sourceforge.net
  *
