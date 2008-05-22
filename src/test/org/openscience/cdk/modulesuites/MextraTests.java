c706bb09065baa5524af07f4caee4543e4f2f5e8
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-05-22 04:25:55 +0000

    Removed an obsolete class
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@11111 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/test/org/openscience/cdk/modulesuites/MextraTests.java
+++ src/test/org/openscience/cdk/modulesuites/MextraTests.java
@@ -28,7 +28,6 @@ import org.openscience.cdk.CloneAtomContainerTest;
 import org.openscience.cdk.VariousTests;
 import org.openscience.cdk.applications.APIVersionTesterTest;
 import org.openscience.cdk.applications.swing.MoleculeListViewer;
-import org.openscience.cdk.atomtype.HybridizationMatcherTest;
 import org.openscience.cdk.atomtype.HybridizationStateATMatcherTest;
 import org.openscience.cdk.atomtype.ValencyMatcherTest;
 import org.openscience.cdk.fingerprint.ExtendedFingerprinterTest;
@@ -70,7 +69,6 @@ public class MextraTests {
         // from cdk.test.atomtype (unstable atom type stuff)
         suite.addTest(new JUnit4TestAdapter(ValencyMatcherTest.class));
         suite.addTest(new JUnit4TestAdapter(HybridizationStateATMatcherTest.class));
-        suite.addTest(new JUnit4TestAdapter(HybridizationMatcherTest.class));
         // from cdk.test.geometry
         suite.addTest(RDFCalculatorTest.suite());
         // from cdk.test.geometry.align
