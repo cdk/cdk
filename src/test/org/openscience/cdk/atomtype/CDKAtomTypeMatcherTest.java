9e030c7a1ba06a7ecc5b330b207a03fd7e498f33
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-06-06 20:21:02 +0000

    Added a unit test to check whether the typer can match the indole N as planar3 when the molecule is represeted as all sp2 atoms with all single bonds (to simulate the strcture obtained from the parsing the aromatic SMILES of indole, where no explicit H is given)
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@11292 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/test/org/openscience/cdk/atomtype/CDKAtomTypeMatcherTest.java
+++ src/test/org/openscience/cdk/atomtype/CDKAtomTypeMatcherTest.java
@@ -21,31 +21,21 @@
  */
 package org.openscience.cdk.atomtype;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
 import junit.framework.JUnit4TestAdapter;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.openscience.cdk.Atom;
-import org.openscience.cdk.Bond;
-import org.openscience.cdk.CDKConstants;
-import org.openscience.cdk.DefaultChemObjectBuilder;
-import org.openscience.cdk.Molecule;
-import org.openscience.cdk.PseudoAtom;
+import org.openscience.cdk.*;
 import org.openscience.cdk.config.AtomTypeFactory;
 import org.openscience.cdk.config.Symbols;
 import org.openscience.cdk.exception.CDKException;
-import org.openscience.cdk.interfaces.IAtom;
-import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IAtomType;
-import org.openscience.cdk.interfaces.IBond;
-import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.interfaces.*;
 import org.openscience.cdk.interfaces.IAtomType.Hybridization;
 import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilder;
 import org.openscience.cdk.templates.MoleculeFactory;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * This class tests the matching of atom types defined in the
@@ -1362,6 +1352,22 @@ public class CDKAtomTypeMatcherTest extends AbstractAtomTypeTest {
 		IMolecule molecule = MoleculeFactory.makeIndole();
     	assertAtomTypes(testedAtomTypes, expectedTypes, molecule);
 	}
+
+    @Test public void testAromaticIndole() throws Exception {
+		String[] expectedTypes = {
+			"C.sp2",
+			"C.sp2",
+			"C.sp2",
+			"C.sp2",
+			"C.sp2",
+			"C.sp2",
+			"C.sp2",
+			"C.sp2",
+			"N.planar3"
+		};
+		IMolecule molecule = MoleculeFactory.makeAromaticIndole();
+    	assertAtomTypes(testedAtomTypes, expectedTypes, molecule);
+    }
     
     @Test public void testPyrrole() throws Exception {
 		String[] expectedTypes = {
