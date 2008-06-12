a487ae1435d4df4ef6b419c1851ae801f00c7d23
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-06-12 15:42:46 +0000

    Updated Javadocs
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@11330 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/main/org/openscience/cdk/config/IsotopeFactory.java
+++ src/main/org/openscience/cdk/config/IsotopeFactory.java
@@ -50,7 +50,7 @@ import java.util.List;
  * <p>The use of this class is examplified as follows. To get information 
  * about the major isotope of hydrogen, one can use this code:
  * <pre>
- *   IsotopeFactory factory = IsotopeFactory.getInstance(new ChemObject().getBuilder());
+ *   IsotopeFactory factory = IsotopeFactory.getInstance(DefaultChemObjectBuilder.getInstance());
  *   Isotope major = factory.getMajorIsotope("H");
  * </pre> 
  *
