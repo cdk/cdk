86ce9aa08ed30e3a42f9e0e33c372f1ea7b21acc
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-06-21 11:44:17 +0000

    Uses the diff.tree.IDifference set up now
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@11419 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/main/org/openscience/cdk/tools/diff/tree/DoubleDifference.java
@@ -0,0 +1,61 @@
+/* $Revision$ $Author$ $Date$
+ * 
+ * Copyright (C) 2008  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * Contact: cdk-devel@lists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA. 
+ */
+package org.openscience.cdk.tools.diff.tree;
+
+/**
+ * Difference between two IChemObjects.
+ * 
+ * @author egonw
+ */
+public class DoubleDifference implements IDifference {
+
+    private final static double ERROR = 0.000000001;
+    
+    private String name;
+    private Double first;
+    private Double second;
+    
+    private DoubleDifference(String name, Double first, Double second) {
+        this.name = name;
+        this.first = first;
+        this.second = second;
+    }
+    
+    public static IDifference construct(String name, Double first, Double second) {
+        if (first == null && second == null) {
+            return null; // no difference
+        }
+        if (first == null || second == null) {
+            return new DoubleDifference(name, first, second);
+        }
+        if (Math.abs(first - second) < ERROR) {
+            return null; // no difference
+        }
+        return new DoubleDifference(name, first, second);
+    }
+    
+    public String toString() {
+        return name + ":" + 
+            (first == null ? "NA" : first) +
+            "/" +
+            (second == null ? "NA" : second);        
+    }
+}
