4a49e085682e89e212aff64de709be140160e5a1
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-07-14 04:51:14 +0000

    Whitespace cleanup
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@11576 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/test/org/openscience/cdk/modeling/builder3d/TemplateHandler3DTest.java
+++ src/test/org/openscience/cdk/modeling/builder3d/TemplateHandler3DTest.java
@@ -1,28 +1,24 @@
-/*
- *  $RCSfile$
- *  $Author: egonw $
- *  $Date: 2007-01-31 18:46:38 +0100 (Wed, 31 Jan 2007) $
+/* $Revision: 11555 $ $Author: egonw $ $Date: 2008-07-12 20:31:17 +0200 (Sat, 12 Jul 2008) $
  *
- *  Copyright (C) 1997-2007  The Chemistry Development Kit (CDK) project
+ * Copyright (C) 2004-2007  The Chemistry Development Kit (CDK) project
  *
- *  Contact: cdk-devel@list.sourceforge.net
+ * Contact: cdk-devel@list.sourceforge.net
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2.1
- *  of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 package org.openscience.cdk.modeling.builder3d;
-
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -33,9 +29,9 @@ import org.openscience.cdk.NewCDKTestCase;
 /**
  * @cdk.module test-builder3d
  *
- *@author      chhoppe
- *@author      Christoph Steinbeck
- *@cdk.created 2004-11-04
+ * @author      chhoppe
+ * @author      Christoph Steinbeck
+ * @cdk.created 2004-11-04
  */
 public class TemplateHandler3DTest extends NewCDKTestCase {
 		
@@ -45,6 +41,6 @@ public class TemplateHandler3DTest extends NewCDKTestCase {
 		// need to trigger a load of the templates
 		th3d.mapTemplates(new NNAtomContainer(), 0);
 		Assert.assertEquals(10751, th3d.getTemplateCount());
-	
 	}
+
 }
