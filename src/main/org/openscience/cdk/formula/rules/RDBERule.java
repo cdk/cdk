2651df43a6dfb3cc6ef97f2173b26f3305e291a7
miguelrojasch <miguelrojasch@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-07-20 13:31:49 +0000

    Removed unnecessary print line
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@11667 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/main/org/openscience/cdk/formula/rules/RDBERule.java
+++ src/main/org/openscience/cdk/formula/rules/RDBERule.java
@@ -31,11 +31,11 @@ import java.util.Map;
 
 import org.openscience.cdk.CDKConstants;
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.formula.MolecularFormulaManipulator;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IIsotope;
 import org.openscience.cdk.interfaces.IMolecularFormula;
 import org.openscience.cdk.tools.LoggingTool;
-import org.openscience.cdk.tools.manipulator.MolecularFormulaManipulator;
 /**
  * <p>Ring Double Bond Equivalents (RDBE) or 
  * Double Bond Equivalents (DBE) are calculated from valence values of 
@@ -321,8 +321,6 @@ public class RDBERule implements IRule{
             index = new int[m];
             for (int i = 0; i < m; i++)
                 index[0] = 0;
-                for (int j = 0; j < index.length; j++)
-            System.out.println();
         }
         /**
         * @return true, unless we have already returned the last combination.
