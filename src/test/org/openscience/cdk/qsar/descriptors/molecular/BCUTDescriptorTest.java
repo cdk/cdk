da8cd4a0050ac93bcf048c2a18ec6e58b2833882
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-09-03 21:20:38 +0000

    Added a test case to check for H addition
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@12156 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/test/org/openscience/cdk/qsar/descriptors/molecular/BCUTDescriptorTest.java
+++ src/test/org/openscience/cdk/qsar/descriptors/molecular/BCUTDescriptorTest.java
@@ -27,6 +27,7 @@ import org.openscience.cdk.ChemObject;
 import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.io.HINReader;
 import org.openscience.cdk.io.ISimpleChemObjectReader;
 import org.openscience.cdk.qsar.DescriptorValue;
@@ -147,6 +148,13 @@ public class BCUTDescriptorTest extends MolecularDescriptorTest {
         for (int i = 0; i < result1.length(); i++) {
             Assert.assertEquals("element " + i + " does not match", result1.get(i), result2.get(i), 0.01);
         }
+    }
+
+    public void testHAddition() throws Exception {
+        SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+        IMolecule mol = sp.parseSmiles("C=1C=CC(=CC1)CNC2=CC=C(C=C2N(=O)=O)S(=O)(=O)C(Cl)(Cl)Br");
+        DoubleArrayResult result1 = (DoubleArrayResult) descriptor.calculate(mol).getValue();
+        for (int i = 0; i < result1.length(); i++) Assert.assertTrue( result1.get(i) != Double.NaN);
     }
 }
 
