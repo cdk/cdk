5a8ac79928630fed0db4c80c13dc35c49a4a271b
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-09-05 12:03:34 +0000

    documentation
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@12180 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/main/org/openscience/cdk/modeling/builder3d/TemplateExtractor.java
+++ src/main/org/openscience/cdk/modeling/builder3d/TemplateExtractor.java
@@ -54,6 +54,8 @@ import org.openscience.cdk.tools.manipulator.RingSetManipulator;
 
 /**
  * Helper class that help setup a template library of CDK's Builder3D.
+ * shk3: I found the right combination of paramters for getting fingerprints which 
+ * work with the current implementation of TemplateHandler3D is infile outfile true false
  * 
  * @author     Christian Hoppe.
  * @cdk.module builder3dtools
