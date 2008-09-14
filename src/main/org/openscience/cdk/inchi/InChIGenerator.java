ddbd439e87bcb93f4a521acf91131053e9d82b04
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-09-14 04:16:08 +0000

    Updated some test annotations. Also converted a protected method to private since it's only used within this class, and shouldn't be called directly anyway from some other class
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/branches/cdk-1.2.x@12307 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/main/org/openscience/cdk/inchi/InChIGenerator.java
+++ src/main/org/openscience/cdk/inchi/InChIGenerator.java
@@ -100,6 +100,7 @@ public class InChIGenerator {
      * @throws org.openscience.cdk.exception.CDKException if there is an
      * error during InChI generation
      */
+    @TestClass("testGetInchiFromChlorineAtom,testGetInchiFromLithiumIontestGetInchiFromChlorine37Atom")
     protected InChIGenerator(IAtomContainer atomContainer) throws CDKException {
         try {
             input = new JniInchiInput("");
@@ -159,7 +160,7 @@ public class InChIGenerator {
      * @param atomContainer      AtomContainer to generate InChI for.
      * @throws CDKException
      */
-    protected void generateInchiFromCDKAtomContainer(IAtomContainer atomContainer) throws CDKException {
+    private void generateInchiFromCDKAtomContainer(IAtomContainer atomContainer) throws CDKException {
         this.atomContainer = atomContainer;
         
         Iterator<IAtom> atoms = atomContainer.atoms().iterator();
