e187e1fa7873f20be2ffe0b3f64616a9f603c94e
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-09-22 15:20:36 +0000

    Completed copyright statement
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/branches/cdk-1.2.x@12378 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/main/org/openscience/cdk/tools/HOSECodeGenerator.java
+++ src/main/org/openscience/cdk/tools/HOSECodeGenerator.java
@@ -1,6 +1,6 @@
 /* $Revision$ $Author$ $Date$
  *
- * Copyright (C) 1997-2007  The Chemistry Development Kit (CDK) project
+ * Copyright (C) 1997-2007  Christoph Steinbeck <steinbeck@users.sf.net>
  *                    2008  Egon Willighagen <egonw@users.sf.net>
  *
  * Contact: cdk-devel@lists.sourceforge.net
