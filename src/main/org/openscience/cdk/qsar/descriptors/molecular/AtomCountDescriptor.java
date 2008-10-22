6485055653f1d0bfe3e2671a89d7990ca553b268
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-10-22 14:33:48 +0000

    Corrected an error message
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/branches/cdk-1.2.x@12764 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/main/org/openscience/cdk/qsar/descriptors/molecular/AtomCountDescriptor.java
+++ src/main/org/openscience/cdk/qsar/descriptors/molecular/AtomCountDescriptor.java
@@ -161,7 +161,7 @@ public class AtomCountDescriptor implements IMolecularDescriptor {
         if (container.getAtomCount() == 0) {
             return new DescriptorValue(getSpecification(), getParameterNames(), getParameters(),
                     new IntegerResult((int) Double.NaN), getDescriptorNames(),
-                    new CDKException("The supplied AtomContainer was NULL"));
+                    new CDKException("The supplied AtomContainer did not have any atoms"));
         }
 
         if (elementName.equals("*")) {
