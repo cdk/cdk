0fa64aad17c5bedc93e96ef64375c5e724a4d1d0
miguelrojasch <miguelrojasch@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-11-02 13:18:42 +0000

    Added test to ElementalPTFactory
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/branches/cdk-1.2.x@12926 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/test/org/openscience/cdk/config/ElementPTFactoryTest.java
+++ src/test/org/openscience/cdk/config/ElementPTFactoryTest.java
@@ -28,6 +28,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openscience.cdk.CDKTestCase;
 import org.openscience.cdk.PeriodicTableElement;
+import org.openscience.cdk.interfaces.IElement;
 
 /**
  * Checks the functionality of the ElementPTFactory
@@ -62,6 +63,20 @@ public class ElementPTFactoryTest extends CDKTestCase
 		Assert.assertNotNull(pte);
     }
     
+    @Test
+    public void testConfigure_PeriodicTableElement() throws Exception {
+		ElementPTFactory elefac = ElementPTFactory.getInstance();
+		PeriodicTableElement pte = elefac.configure(new PeriodicTableElement("H"));
+		Assert.assertNotNull(pte);
+    }
+    
+    @Test
+    public void testConfigureE_PeriodicTableElement() throws Exception {
+		ElementPTFactory elefac = ElementPTFactory.getInstance();
+		IElement element = elefac.configureE(new PeriodicTableElement("H"));
+		Assert.assertNotNull(element);
+    }
+    
     @Test public void testGetAtomicNumber_PeriodicTableElement() throws Exception {
 		ElementPTFactory elefac = ElementPTFactory.getInstance();
         Assert.assertEquals(1.0079760, elefac.getAtomicNumber(new PeriodicTableElement("H")), 0.01);
@@ -72,12 +87,12 @@ public class ElementPTFactoryTest extends CDKTestCase
         Assert.assertEquals("Hydrogen", elefac.getName(new PeriodicTableElement("H")));
     }
 
-    @Test public void testGetChemicalSerie_PeriodicTablePeriodicTableElement() throws Exception {
+    @Test public void testGetChemicalSerie_PeriodicTableElement() throws Exception {
 		ElementPTFactory elefac = ElementPTFactory.getInstance();
         Assert.assertEquals("Nonmetals", elefac.getChemicalSerie(new PeriodicTableElement("H")));
     }
     
-    @Test public void testGetPeriod_PeriodicTablePeriodicTableElement() throws Exception {
+    @Test public void testGetPeriod_PeriodicTableElement() throws Exception {
 		ElementPTFactory elefac = ElementPTFactory.getInstance();
         Assert.assertEquals(1, elefac.getPeriod(new PeriodicTableElement("H")), 0.1);
     }
@@ -87,27 +102,27 @@ public class ElementPTFactoryTest extends CDKTestCase
         Assert.assertEquals(1, elefac.getGroup(new PeriodicTableElement("H")), 0.1);
     }
     
-    @Test public void testGetPhase_PeriodicTablePeriodicTableElement() throws Exception {
+    @Test public void testGetPhase_PeriodicTableElement() throws Exception {
 		ElementPTFactory elefac = ElementPTFactory.getInstance();
         Assert.assertEquals("Gas", elefac.getPhase(new PeriodicTableElement("H")));
     }
     
-    @Test public void testGetVdwRadius_PeriodicTablePeriodicTableElement() throws Exception {
+    @Test public void testGetVdwRadius_PeriodicTableElement() throws Exception {
 		ElementPTFactory elefac = ElementPTFactory.getInstance();
         Assert.assertEquals(1.2, elefac.getVdwRadius(new PeriodicTableElement("H")), 0.1);
     }
     
-    @Test public void testGetCASid_PeriodicTablePeriodicTableElement() throws Exception {
+    @Test public void testGetCASid_PeriodicTableElement() throws Exception {
 		ElementPTFactory elefac = ElementPTFactory.getInstance();
         Assert.assertEquals("1333-74-0", elefac.getCASid(new PeriodicTableElement("H")));
     }
     
-    @Test public void testGetCovalentRadius_PeriodicTablePeriodicTableElement() throws Exception {
+    @Test public void testGetCovalentRadius_PeriodicTableElement() throws Exception {
 		ElementPTFactory elefac = ElementPTFactory.getInstance();
         Assert.assertEquals(0.37, elefac.getCovalentRadius(new PeriodicTableElement("H")), 0.1);
     }
     
-    @Test public void testGetPaulingEneg_PeriodicTablePeriodicTableElement() throws Exception {
+    @Test public void testGetPaulingEneg_PeriodicTableElement() throws Exception {
 		ElementPTFactory elefac = ElementPTFactory.getInstance();
         Assert.assertEquals(2.2, elefac.getPaulingEneg(new PeriodicTableElement("H")), 0.1);
     }
