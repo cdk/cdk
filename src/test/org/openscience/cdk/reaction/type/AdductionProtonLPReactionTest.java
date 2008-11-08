acba4926138f37032c970d1a0b32e8ea76c10716
eoc21 <eoc21@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-11-08 15:10:09 +0000

    Line 80: Automatically looks for the active centre Line 115: Manually tests for active centre
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/branches/eoc21-jj1802589@13050 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/test/org/openscience/cdk/reaction/type/AdductionProtonLPReactionTest.java
+++ src/test/org/openscience/cdk/reaction/type/AdductionProtonLPReactionTest.java
@@ -77,7 +77,7 @@ public class AdductionProtonLPReactionTest extends ReactionProcessTest {
 	/**
 	 * A unit test suite for JUnit for acetaldehyde. 
 	 * Reaction: O=C-C-H => O(H)-C=C. 
-	 * Automatic looking for active center.
+	 * Automatically looks for the active centre.
 	 *
 	 * @cdk.inchi InChI=1/C2H4O/c1-2-3/h2H,1H3
 	 * 
@@ -112,7 +112,7 @@ public class AdductionProtonLPReactionTest extends ReactionProcessTest {
 	/**
 	 * A unit test suite for JUnit for acetaldehyde. 
 	 * Reaction: O=C-C-H => O(H)-C=C. 
-	 * Manually putting for active center.
+	 * Manually tests for active centre.
 	 *
 	 * @cdk.inchi InChI=1/C2H4O/c1-2-3/h2H,1H3
 	 * 
