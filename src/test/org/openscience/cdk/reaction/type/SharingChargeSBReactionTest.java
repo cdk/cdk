9b05863fccc20f7c00c55421ce6df8c7458accd8
eoc21 <eoc21@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-11-08 16:17:31 +0000

    Added 'identifies
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/branches/eoc21-jj1802589@13066 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/test/org/openscience/cdk/reaction/type/SharingChargeSBReactionTest.java
+++ src/test/org/openscience/cdk/reaction/type/SharingChargeSBReactionTest.java
@@ -341,7 +341,7 @@ public class SharingChargeSBReactionTest extends ReactionProcessTest {
         return expected2;
 	}
 	/**
-	 * Test to recognize if a IMolecule matcher correctly the CDKAtomTypes.
+	 * Test to recognize if a IMolecule matcher correctly identifies the CDKAtomTypes.
 	 * 
 	 * @param molecule          The IMolecule to analyze
 	 * @throws CDKException
