fac88fc09b7d4a4b67df4215b7ec566414e4c745
rajarshi <rajarshi@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-12-16 19:07:01 +0000

    Added a test for bug #1535055, which now works fine
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/branches/cdk-1.2.x@13606 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/test/org/openscience/cdk/gui/smiles/SmilesGeneratorTest.java
+++ src/test/org/openscience/cdk/gui/smiles/SmilesGeneratorTest.java
@@ -20,33 +20,35 @@
  */
 package org.openscience.cdk.gui.smiles;
 
-import java.io.IOException;
-import java.io.InputStream;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openscience.cdk.*;
+import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
+import org.openscience.cdk.config.IsotopeFactory;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.graph.AtomContainerAtomPermutor;
+import org.openscience.cdk.graph.AtomContainerBondPermutor;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomType.Hybridization;
+import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.interfaces.IChemFile;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.io.CMLReader;
+import org.openscience.cdk.io.IChemObjectReader.Mode;
+import org.openscience.cdk.io.MDLReader;
+import org.openscience.cdk.layout.HydrogenPlacer;
+import org.openscience.cdk.layout.StructureDiagramGenerator;
+import org.openscience.cdk.nonotify.NNChemFile;
+import org.openscience.cdk.smiles.SmilesGenerator;
+import org.openscience.cdk.smiles.SmilesParser;
+import org.openscience.cdk.templates.MoleculeFactory;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
+import org.openscience.cdk.tools.manipulator.ChemFileManipulator;
 
 import javax.vecmath.Point2d;
 import javax.vecmath.Vector2d;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.openscience.cdk.Atom;
-import org.openscience.cdk.AtomContainer;
-import org.openscience.cdk.CDKConstants;
-import org.openscience.cdk.Molecule;
-import org.openscience.cdk.CDKTestCase;
-import org.openscience.cdk.PseudoAtom;
-import org.openscience.cdk.Reaction;
-import org.openscience.cdk.config.IsotopeFactory;
-import org.openscience.cdk.graph.AtomContainerAtomPermutor;
-import org.openscience.cdk.graph.AtomContainerBondPermutor;
-import org.openscience.cdk.interfaces.IBond;
-import org.openscience.cdk.interfaces.IAtomType.Hybridization;
-import org.openscience.cdk.io.MDLReader;
-import org.openscience.cdk.io.IChemObjectReader.Mode;
-import org.openscience.cdk.layout.HydrogenPlacer;
-import org.openscience.cdk.layout.StructureDiagramGenerator;
-import org.openscience.cdk.smiles.SmilesGenerator;
-import org.openscience.cdk.templates.MoleculeFactory;
-import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  *@author         steinbeck
@@ -958,6 +960,7 @@ public class SmilesGeneratorTest extends CDKTestCase {
 
 	/**
 	 *  A unit test for JUnit
+     * @cdk.bug 1089770
 	 */
 	@Test public void testSFBug1089770_1()
 	{
@@ -982,6 +985,7 @@ public class SmilesGeneratorTest extends CDKTestCase {
 
 	/**
 	 *  A unit test for JUnit
+     * @cdk.bug 1089770
 	 */
 	@Test public void testSFBug1089770_2()
 	{
@@ -1002,6 +1006,34 @@ public class SmilesGeneratorTest extends CDKTestCase {
 			Assert.fail(exc.getMessage());
 		}
 	}
+
+    /**
+     * @cdk.bug 1535055
+     */
+    @Test public void testBug1535055() throws CDKException {
+        SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+        IAtomContainer mol = sp.parseSmiles("COC(=O)c1ccc2c(c1)c1ccccc1[nH]2");
+        CDKHueckelAromaticityDetector.detectAromaticity(mol);
+        AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);
+        SmilesGenerator sg = new SmilesGenerator();
+        sg.setUseAromaticityFlag(true);
+        String s1 = sg.createSMILES((IMolecule) mol);
+
+        String filename = "data/cml/bug1535055.cml";
+        InputStream ins = this.getClass().getClassLoader().getResourceAsStream(filename);
+        CMLReader reader = new CMLReader(ins);
+        IChemFile chemFile = (IChemFile)reader.read(new NNChemFile());
+
+        // test the resulting ChemFile content
+        Assert.assertNotNull(chemFile);
+        IAtomContainer mol2 = ChemFileManipulator.getAllAtomContainers(chemFile).get(0);
+        CDKHueckelAromaticityDetector.detectAromaticity(mol2);
+        AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol2);
+        String s2 = sg.createSMILES((IMolecule) mol2);
+
+        Assert.assertTrue(s1.contains("[nH]"));
+        Assert.assertTrue(s2.contains("[nH]"));
+    }
 
 }
 
