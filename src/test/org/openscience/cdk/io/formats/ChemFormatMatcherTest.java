8e8376f806272bd78955cdb3ba3deed3fdd9e54f
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2009-01-28 09:54:07 +0000

    Added test class for the IChemFormatMatcher interface
    git-svn-id:
    https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/branches/cdk-1.2.x@12598 eb4e18e3-b210-0410-a6ab-dec725e4b171
    
    Author:    egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
    
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@13982 eb4e18e3-b210-0410-a6ab-dec725e4b171

---
+++ src/test/org/openscience/cdk/io/formats/ChemFormatMatcherTest.java
@@ -0,0 +1,56 @@
+/* $Revision$ $Author$ $Date$
+ *
+ * Copyright (C) 2008  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * Contact: cdk-devel@slists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openscience.cdk.io.formats;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @cdk.module test-io
+ */
+abstract public class ChemFormatMatcherTest extends ChemFormatTest {
+    
+    private IChemFormatMatcher matcher;
+    
+    public void setChemFormatMatcher(IChemFormatMatcher matcher) {
+        super.setChemFormat(matcher);
+        this.matcher = matcher;
+    }
+    
+    @Test public void testChemFormatMatcherSet() {
+        Assert.assertNotNull(
+            "You must use setChemFormatMatcher() to set the IChemFormatMatcher object.",
+            matcher
+        );
+    }
+
+    @Test public void testMatchesEmptyString() {
+        Assert.assertFalse(matcher.matches(1, ""));
+    }
+    
+    @Test public void testMatchesLoremIpsum() {
+        Assert.assertFalse(matcher.matches(1, "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam accumsan metus ut nulla."));
+    }
+}
