7585dfa5d04f1634eff188844383f8e2c4e0adab
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2009-01-30 17:28:08 +0000

    Some compile fixes for fails that sneaked in during the previous trunk-messed-up patch commits
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/trunk@14025 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/main/org/openscience/cdk/atomtype/StructGenMatcher.java
+++ src/main/org/openscience/cdk/atomtype/StructGenMatcher.java
@@ -66,16 +66,6 @@ public class StructGenMatcher implements IAtomTypeMatcher {
       return types;
   }
 
-  public IAtomType[] findMatchingAtomType(IAtomContainer atomContainer) throws CDKException {
-      IAtomType[] types = new IAtomType[atomContainer.getAtomCount()];
-      int typeCounter = 0;
-      for (IAtom atom : atomContainer.atoms()) {
-          types[typeCounter] = findMatchingAtomType(atomContainer, atom);
-          typeCounter++;
-      }
-      return types;
-  }
-
 	/**
 	 * Finds the AtomType matching the Atom's element symbol, formal charge and 
      * hybridization state.
