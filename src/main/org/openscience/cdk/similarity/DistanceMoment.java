31f634146babdc5d8da5121ab2a7d05b99339006
miguelrojasch <miguelrojasch@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2009-02-03 08:06:03 +0000

    Spelling: conidered to considered
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/branches/cdk-1.2.x@14053 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/main/org/openscience/cdk/similarity/DistanceMoment.java
+++ src/main/org/openscience/cdk/similarity/DistanceMoment.java
@@ -28,7 +28,7 @@ import java.util.Iterator;
  * <b>Note</b>: The methods of this class do not perform hydrogen removal. If you want to
  * do the calculations excluding hydrogens, you'll need to do it yourself. Also, if the molecule has
  * disconnected components, you should consider one (usually the largest), otherwise all components
- * are conidered in the calculation.
+ * are considered in the calculation.
  *
  * @author Rajarshi Guha
  * @cdk.created 2007-03-11
