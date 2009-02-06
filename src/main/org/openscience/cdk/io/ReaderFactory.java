650fdf1102ec7c57b907c03da82dc899d0932654
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2009-02-06 20:10:43 +0000

    Fixed NPE: when no format has been detected, the reader == null
    git-svn-id: https://cdk.svn.sourceforge.net/svnroot/cdk/cdk/branches/cdk-1.2.x@14103 eb4e18e3-b210-0410-a6ab-dec725e4b171

--- src/main/org/openscience/cdk/io/ReaderFactory.java
+++ src/main/org/openscience/cdk/io/ReaderFactory.java
@@ -255,12 +255,14 @@ public class ReaderFactory {
             format = guessFormat( istreamToRead );
         }
         reader = createReader(format);
-        try {
-            reader.setReader(input);
-        } catch ( CDKException e1 ) {
-            IOException wrapper = new IOException("Exception while setting the InputStream: " + e1.getMessage());
-            wrapper.initCause(e1);
-            throw wrapper;
+        if (reader != null) {
+            try {
+                reader.setReader(input);
+            } catch ( CDKException e1 ) {
+                IOException wrapper = new IOException("Exception while setting the InputStream: " + e1.getMessage());
+                wrapper.initCause(e1);
+                throw wrapper;
+            }
         }
         return reader;
     }
