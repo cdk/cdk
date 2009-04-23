054803c5905eb146765e0999766ceb8e2df0d6ef
Egon Willighagen <egonw@users.sourceforge.net>
2009-04-23 12:33:27 +0200

    Added set up for creating module HTML pages
    Signed-off-by: Rajarshi  Guha <rajarshi.guha@gmail.com>

--- createModulePages.groovy
+++ createModulePages.groovy
@@ -7,6 +7,8 @@ ant.mkdir(dir:"doc/modules")
 
 def basedir = new File("src/META-INF")
 files = basedir.listFiles().grep(~/.*cdkdepends$/)
+files.add(new File(basedir,"annotation.cdkdepends"))
+files.add(new File(basedir,"interfaces.cdkdepends"))
 files.each {
   file = it
   m = (file =~ ~/\/([-|\w]*)\.cdkdepends/)
@@ -20,7 +22,26 @@ files.each {
     } 
     body(){
       h1("CDK Module: " + module)
-    } 
+      h2("Depends")
+      p(){
+        if (file.exists()) {
+          b("CDK")
+          file.text.eachLine{
+            dependency = (it =~ ~/cdk-([-|\w]*)\.jar/)[0][1]
+            a(href:dependency+".html", dependency)
+          }
+        }
+      }
+      libdepends = new File(basedir, module + ".libdepends")
+      p(){
+        if (libdepends.exists()) {
+          b("Libraries")
+          libdepends.text.eachLine{
+            span(it)
+          }
+        }
+      }
+    }
   } 
   new File("doc/modules/" + module + ".html").write(writer.toString())
 }
