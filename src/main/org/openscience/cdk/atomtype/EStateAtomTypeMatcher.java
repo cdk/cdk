bb70e625f3bc075e20a6d6340bc65d6ea0eacbaa
Rajarshi Guha <rajarshi.guha@gmail.com>
2009-05-31 15:31:39 -0400

    Fixed a typo from the previous merge

--- src/main/org/openscience/cdk/atomtype/EStateAtomTypeMatcher.java
+++ src/main/org/openscience/cdk/atomtype/EStateAtomTypeMatcher.java
@@ -174,11 +174,7 @@ public class EStateAtomTypeMatcher  implements IAtomTypeMatcher {
     public static boolean inSameAromaticRing(IAtomContainer m, IAtom atom1,
 			IAtom atom2, IRingSet rs)
 	{
-<<<<<<< HEAD:src/main/org/openscience/cdk/atomtype/EStateAtomTypeMatcher.java
-		if (rs == null)
-=======
 		if (rs == null) 
->>>>>>> 3d7adae977dcaf251932959c2c065bd20dd6a403:src/main/org/openscience/cdk/atomtype/EStateAtomTypeMatcher.java
 			return false;
 		for (int i = 0; i <= rs.getAtomContainerCount() - 1; i++)
 		{
