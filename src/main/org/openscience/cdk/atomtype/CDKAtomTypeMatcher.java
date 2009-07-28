1714de2663b1592db89399ec5daac11c26c02ce7
Egon Willighagen <egonw@users.sourceforge.net>
2009-07-28 09:17:04 +0200

    Fixed perception of N.planar3 where N.sp2 was detected, by now taking into account the given hydrogen count.
    Signed-off-by: Rajarshi  Guha <rajarshi.guha@gmail.com>

--- src/main/org/openscience/cdk/atomtype/CDKAtomTypeMatcher.java
+++ src/main/org/openscience/cdk/atomtype/CDKAtomTypeMatcher.java
@@ -674,10 +674,18 @@ public class CDKAtomTypeMatcher implements IAtomTypeMatcher {
                 int connectedHeavyAtoms = atomContainer.getConnectedBondsCount(atom) - explicitHydrogens;
                 if (connectedHeavyAtoms == 2) {
                 	List<IBond> bonds = atomContainer.getConnectedBondsList(atom);
-                	if (bonds.get(0).getFlag(CDKConstants.ISAROMATIC) &&
-                			bonds.get(1).getFlag(CDKConstants.ISAROMATIC)) {
-                		IAtomType type = getAtomType("N.sp2");
-                		if (isAcceptable(atom, atomContainer, type)) return type;
+                    if (bonds.get(0).getFlag(CDKConstants.ISAROMATIC) &&
+                            bonds.get(1).getFlag(CDKConstants.ISAROMATIC)) {
+                        Integer hCount = atom.getHydrogenCount();
+                        if (hCount == CDKConstants.UNSET || hCount == 0) {
+                            IAtomType type = getAtomType("N.sp2");
+                            if (isAcceptable(atom, atomContainer, type))
+                                return type;
+                        } else if (hCount == 1) {
+                            IAtomType type = getAtomType("N.planar3");
+                            if (isAcceptable(atom, atomContainer, type))
+                                return type;
+                        }
                 	} else if (bothNeighborsAreSp2(atom, atomContainer) && isRingAtom(atom, atomContainer)) {
                 		// a N.sp3 which is expected to take part in an aromatic system
                 		IAtomType type = getAtomType("N.planar3");
