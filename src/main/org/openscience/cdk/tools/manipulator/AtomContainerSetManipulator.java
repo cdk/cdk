e0304bf4bdfe0b8f5304391acec6437a5fd4e0ee
Rajarshi  Guha <rajarshi.guha@gmail.com>
2009-08-02 21:01:26 -0400

    Fixed Javadocs error

--- src/main/org/openscience/cdk/tools/manipulator/AtomContainerSetManipulator.java
+++ src/main/org/openscience/cdk/tools/manipulator/AtomContainerSetManipulator.java
@@ -30,11 +30,16 @@ package org.openscience.cdk.tools.manipulator;
 import org.openscience.cdk.annotations.TestClass;
 import org.openscience.cdk.annotations.TestMethod;
 import org.openscience.cdk.graph.ConnectivityChecker;
-import org.openscience.cdk.interfaces.*;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomContainerSet;
+import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.interfaces.IChemObject;
+import org.openscience.cdk.interfaces.IElectronContainer;
+import org.openscience.cdk.interfaces.IMoleculeSet;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -239,7 +244,7 @@ public class AtomContainerSetManipulator {
      * Tells if an AtomContainerSet contains at least one AtomContainer with the
      * same ID as atomContainer. Note this checks getID() for equality, not pointers.
      * 
-     * @param relevantContainer The IAtomContainer to look for
+     * @param atomContainerSet The IAtomContainer to look for
      * @param atomContainerSet The collection of IAtomContainer objects
      */
     @TestMethod("testContainsByID_IAtomContainerSet_IAtomContainer")
