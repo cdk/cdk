0fbe0e02cb7bcc4806fa3bb74a8d1c9130b54d1a
Egon Willighagen <egonw@users.sourceforge.net>
2009-10-05 12:30:31 +0200

    Added framework to convert the CDK data model into a JEna Model

---
+++ src/main/org/openscience/cdk/libio/jena/Convertor.java
@@ -0,0 +1,63 @@
+/* Copyright (C) 2009  Egon Willighagen <egonw@users.sf.net>
+ *
+ * Contact: jchempaint-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openscience.cdk.libio.jena;
+
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.interfaces.IMolecule;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.RDF;
+
+/**
+ * @cdk.module       iordf
+ * @cdk.githash
+ * @cdk.keyword      Resource Description Framework
+ * @cdk.keyword      Jena
+ * @cdk.keyword      RDF
+ * @cdk.keyword      Web Ontology Language
+ * @cdk.keyword      OWL
+ */
+public class Convertor {
+
+    public static Model molecule2Model(IMolecule molecule) {
+        Model model = ModelFactory.createOntologyModel();
+        model.setNsPrefix("cdk", "http://cdk.sourceforge.net/model.owl#");
+        Resource subject = model.createResource(
+            "http://cdk.sf.net/model/molecule/" + 1
+        );
+        model.add(
+            subject, RDF.type,
+            model.createResource("cdk:Molecule")
+        );
+        return model;
+    }
+
+    public static IMolecule model2Molecule(Model model,
+        IChemObjectBuilder builder) {
+        IMolecule mol = builder.newMolecule();
+        return mol;
+    }
+
+}
