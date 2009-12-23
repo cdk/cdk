8dee40dc3d4760cc17d351a5dd743f72357d4c6a
Egon Willighagen <egonw@users.sourceforge.net>
2009-12-23 14:32:37 +0100

    Added missing copyright/license header

--- createModulePages.groovy
+++ createModulePages.groovy
@@ -1,5 +1,24 @@
 #!/usr/bin/groovy
 
+/* Copyright (C) 2009  Egon Willighagen <egonw@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 def nightly = "http://pele.farmbio.uu.se/nightly-1.2.x/"
 
 ant = new AntBuilder()
