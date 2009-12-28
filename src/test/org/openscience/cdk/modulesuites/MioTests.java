beae6f26e82ee1fd23fc1ec243dfc68506630644
Egon Willighagen <egonw@users.sourceforge.net>
2009-12-28 14:54:15 +0100

    Added missing unit testing for the SMILESWriter
    Signed-off-by: Rajarshi Guha <rajarshi.guha@gmail.com>

--- src/test/org/openscience/cdk/modulesuites/MioTests.java
+++ src/test/org/openscience/cdk/modulesuites/MioTests.java
@@ -53,6 +53,7 @@ import org.openscience.cdk.io.PMPReaderTest;
 import org.openscience.cdk.io.ReaderFactoryTest;
 import org.openscience.cdk.io.SDFReaderTest;
 import org.openscience.cdk.io.SDFWriterTest;
+import org.openscience.cdk.io.SMILESWriterTest;
 import org.openscience.cdk.io.ShelXReaderTest;
 import org.openscience.cdk.io.WriterFactoryTest;
 import org.openscience.cdk.io.XYZReaderTest;
@@ -98,6 +99,7 @@ import org.openscience.cdk.io.iterator.IteratingPCSubstancesXMLReaderTest;
     PDBWriterTest.class,
     PMPReaderTest.class,
     ShelXReaderTest.class,
+    SMILESWriterTest.class,
     XYZReaderTest.class,
     XYZWriterTest.class,
 
