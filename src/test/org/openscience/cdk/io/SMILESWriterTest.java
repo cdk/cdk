7d6a9b6c1fbb4379aa78e4a5ec3128eeffcedd63
Egon Willighagen <egonw@users.sourceforge.net>
2009-12-28 14:54:15 +0100

    Added missing unit testing for the SMILESWriter
    Signed-off-by: Rajarshi Guha <rajarshi.guha@gmail.com>

---
+++ src/test/org/openscience/cdk/io/SMILESWriterTest.java
@@ -0,0 +1,63 @@
+/* Copyright (C) 2009  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * Contact: cdk-devel@slists.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openscience.cdk.io;
+
+import java.io.StringWriter;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.MoleculeSet;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.templates.MoleculeFactory;
+
+/**
+ * TestCase for the writer for SMILES files.
+ *
+ * @cdk.module test-io
+ *
+ * @see org.openscience.cdk.io.SMILESWriter
+ */
+public class SMILESWriterTest extends ChemObjectIOTest {
+
+    @BeforeClass public static void setup() {
+        setChemObjectIO(new SMILESWriter());
+    }
+
+    @Test public void testAccepts() throws Exception {
+    	SMILESWriter reader = new SMILESWriter();
+    	Assert.assertTrue(reader.accepts(Molecule.class));
+        Assert.assertTrue(reader.accepts(MoleculeSet.class));
+    }
+
+    @Test public void testWriteSMILESFile() throws Exception {
+        StringWriter stringWriter = new StringWriter();
+        IMolecule benzene = MoleculeFactory.makeBenzene();
+        SMILESWriter smilesWriter = new SMILESWriter(stringWriter);
+        smilesWriter.write(benzene);
+        smilesWriter.close();
+        Assert.assertTrue(stringWriter.toString().contains("C=C"));
+    }
+
+}
