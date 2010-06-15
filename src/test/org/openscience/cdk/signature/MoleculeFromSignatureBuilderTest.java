020fb92508cde32e8fb690b440f5dff1a3fc20ae
Egon Willighagen <egonw@users.sourceforge.net>
2010-06-15 20:15:19 +0200

    Removed unused import

--- src/test/org/openscience/cdk/signature/MoleculeFromSignatureBuilderTest.java
+++ src/test/org/openscience/cdk/signature/MoleculeFromSignatureBuilderTest.java
@@ -29,7 +29,6 @@ import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilder;
-import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilderTest;
 
 import signature.AbstractVertexSignature;
 import signature.ColoredTree;
