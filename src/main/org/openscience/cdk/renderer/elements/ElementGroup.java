98dd2fbc24f8757e1f83336ffc922f6d40a12c82
maclean <gilleain.torrance@gmail.com>
2010-09-26 22:31:59 +0100

    Javadoc changes to ElementGroup

--- src/main/org/openscience/cdk/renderer/elements/ElementGroup.java
+++ src/main/org/openscience/cdk/renderer/elements/ElementGroup.java
@@ -20,90 +20,59 @@
  */
 package org.openscience.cdk.renderer.elements;
 
-import java.awt.Dimension;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
-
-import org.openscience.cdk.interfaces.IAtom;
-import org.openscience.cdk.interfaces.IAtomContainer;
+import java.util.List;
 
 /**
+ * A group of rendering elements, of any type.
+ * 
  * @cdk.module  renderbasic
  */
 public class ElementGroup implements IRenderingElement,
                                        Iterable<IRenderingElement> {
 
-    private final Collection<IRenderingElement> 
-    						elements = new ArrayList<IRenderingElement>();
-    double scalex = 10;
-    double scaley = 10;
-    Rectangle2D extent;
+    /**
+     * The elements in the group. 
+     */
+    private final List<IRenderingElement> elements;
+   
+    /**
+     * Create an empty element group.
+     */
+    public ElementGroup() {
+        elements = new ArrayList<IRenderingElement>();
+    }
     
+    /** {@inheritDoc} */
     public Iterator<IRenderingElement> iterator() {
         return elements.iterator();
     }
     
+    /**
+     * Add a new element to the group.
+     * 
+     * @param element the element to add to the group
+     */
     public void add(IRenderingElement element) {
     	if (element == null) return;
         elements.add(element);
     }
     
+    /**
+     * Visit the members of the group.
+     *  
+     * @param visitor the class that will be visiting each element
+     */
     public void visitChildren(IRenderingVisitor visitor) {
     	for (IRenderingElement child : this.elements) {
     		child.accept(visitor);
     	}
     }
-
-    public void setScale( double[] scale ) {
-        if( scale !=null && scale.length !=2) return;
-        scalex = Math.min( scale[0],scale[1]);
-        scaley = scalex;
-    }
     
-    public double[] getDimensions(IAtomContainer ac, Dimension size) {
-        
-        Rectangle2D aSize = getExtent( ac );
-        double[] scale = new double[]{ (size.getWidth()/(aSize.getWidth())),
-                             (size.getHeight()/(aSize.getHeight()))};
-        setScale( scale );
-        return scale;
-    }
-
-    public double getScale(IAtomContainer ac, Dimension size) {
-        double[] scale = getDimensions( ac, size );
-        return Math.min(scale[0],scale[1]);
-    }
-    
-    Rectangle2D getExtent(IAtomContainer ac) {
-        double xmin = Double.POSITIVE_INFINITY, xmax = Double.NEGATIVE_INFINITY, ymin =
-                Double.POSITIVE_INFINITY , ymax = Double.NEGATIVE_INFINITY;
-
-        for ( IAtom atom : ac.atoms() ) {
-            double x = atom.getPoint2d().x;
-            double y = atom.getPoint2d().y;
-            xmin = Math.min( xmin, x );
-            xmax = Math.max( xmax, x );
-            ymin = Math.min( ymin, y );
-            ymax = Math.max( ymax, y );
-        }
-        extent =  new Rectangle2D.Double(xmin,ymin,xmax-xmin,ymax-ymin);
-        return extent;
-    }
-    
+    /** {@inheritDoc} */
     public void accept( IRenderingVisitor v ) {
         v.visit( this );
     }
-    
-    public Point2D center(Dimension size) {
-
-        double aWidth = extent.getWidth()*scalex;
-        double aHeight = extent.getHeight()*-scaley;
-        double xDiff = size.getWidth()-aWidth;
-        double yDiff = size.getHeight()-aHeight;
-        return new Point2D.Float( (float)(-(extent.getX()*scalex)+xDiff/2),
-                                  (float)(-(extent.getY()*-scaley)+yDiff/2));
-    }
+   
 }
