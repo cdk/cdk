23c605098f2d7d117bff316cb0e60dc6be55380c
maclean <gilleain.torrance@gmail.com>
2010-09-26 19:24:48 +0100

    Javadocs for TextGroupElement

--- src/main/org/openscience/cdk/renderer/elements/TextGroupElement.java
+++ src/main/org/openscience/cdk/renderer/elements/TextGroupElement.java
@@ -23,26 +23,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * A group of text elements, particularly the element symbol (eg: "C") 
+ * surrounded by other annotations such as mass number, charge, etc. These 
+ * annotation elements are the 'children' of the parent text element.
+ *   
  * @cdk.module renderbasic
  */
 public class TextGroupElement extends TextElement {
     
+    /**
+     * Compass-point positions for text element annotation children.
+     *
+     */
     public enum Position { NW, SW, SE, NE, S, N, W, E }; 
     
+    /**
+     * A string of text that should be shown around the parent. 
+     *  
+     * @author maclean
+     *
+     */
     public class Child {
         
+        /**
+         * The text of this child.
+         */
         public final String text;
         
+        /**
+         * A subscript (if any) for the child.
+         */
         public final String subscript;
         
+        /**
+         * The position of the child relative to the parent.
+         */
         public final Position position;
         
+        /**
+         * Make a child element with the specified text and position.
+         * 
+         * @param text the child's text
+         * @param position the position of the child relative to the parent
+         */
         public Child(String text, Position position) {
             this.text = text;
             this.position = position;
             this.subscript = null;
         }
         
+        /**
+         * Make a child element with the specified text, subscript, and position.
+         * @param text the child's text
+         * @param subscript a subscript for the child
+         * @param position the position of the child relative to the parent
+         */
         public Child(String text, String subscript, Position position) {
             this.text = text;
             this.position = position;
@@ -51,21 +86,48 @@ public class TextGroupElement extends TextElement {
         
     }
 
+    /**
+     * The child text elements.
+     */
     public final List<Child> children;
     
+    /**
+     * Make a text group at (x, y) with the text and color given.
+     * 
+     * @param x the x-coordinate of the center of the text
+     * @param y the y-coordinate of the center of the text
+     * @param text the text to render
+     * @param color the color of the text
+     */
     public TextGroupElement(double x, double y, String text, Color color) {
         super(x, y, text, color);
         this.children = new ArrayList<Child>();
     }
     
+    /**
+     * Add a child text element.
+     * 
+     * @param text the child text to add
+     * @param position the position of the child relative to this parent
+     */
     public void addChild(String text, Position position) {
         this.children.add(new Child(text, position));
     }
     
+    /**
+     * Add a child text element with a subscript.
+     * 
+     * @param text the child text to add
+     * @param subscript a subscript for the child
+     * @param position the position of the child relative to the parent
+     */
     public void addChild(String text, String subscript, Position position) {
         this.children.add(new Child(text, subscript, position));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void accept(IRenderingVisitor v) {
         v.visit(this);
     }
