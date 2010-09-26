a0905f7d621554c16a994d58b519d99469929ad0
maclean <gilleain.torrance@gmail.com>
2010-09-26 19:33:50 +0100

    Javadocs on RingElement

--- src/main/org/openscience/cdk/renderer/elements/RingElement.java
+++ src/main/org/openscience/cdk/renderer/elements/RingElement.java
@@ -3,16 +3,28 @@ package org.openscience.cdk.renderer.elements;
 import java.awt.Color;
 
 /**
+ * A ring is just a circle - in other words, an oval whose width and height are
+ * the same.
+ * 
  * @cdk.module renderbasic
  */
 public class RingElement extends OvalElement implements IRenderingElement {
 
-	public RingElement(double x, double y, double radius, Color color) {
-		super(x, y, radius, false, color);
-	}
+    /**
+     * Make a ring element centered on (x, y) with radius and color given.
+     * 
+     * @param x the x-coordinate of the ring center
+     * @param y the y-coordinate of the ring center
+     * @param radius the radius of the circle
+     * @param color the color of the circle
+     */
+    public RingElement(double x, double y, double radius, Color color) {
+        super(x, y, radius, false, color);
+    }
 
-	public void accept(IRenderingVisitor v) {
-		v.visit(this);
-	}
+    /** {@inheritDoc} */
+    public void accept(IRenderingVisitor v) {
+        v.visit(this);
+    }
 
 }
