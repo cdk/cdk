3d20d9288725f9ebc80611311a87043a8dbf09e4
Egon Willighagen <egonw@users.sourceforge.net>
2010-09-26 14:51:51 +0200

    Added use of Groovy to test if the output compiles

--- src/test/org/openscience/cdk/io/CDKSourceCodeWriterTest.java
+++ src/test/org/openscience/cdk/io/CDKSourceCodeWriterTest.java
@@ -1,9 +1,8 @@
-/* $Revision$ $Author$ $Date$
+/* Copyright (C) 2004-2007  The Chemistry Development Kit (CDK) project
+ *                    2010  Egon Willighagen <egonw@users.sf.net>
  *
- * Copyright (C) 2004-2007  The Chemistry Development Kit (CDK) project
- * 
  * Contact: cdk-devel@slists.sourceforge.net
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2.1
@@ -12,17 +11,19 @@
  * - but is not limited to - adding the above copyright notice to the beginning
  * of your source code files, and to any copyright notice that you may distribute
  * with programs based on this work.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 package org.openscience.cdk.io;
+
+import groovy.lang.GroovyShell;
 
 import java.io.StringWriter;
 
@@ -62,5 +63,15 @@ public class CDKSourceCodeWriterTest extends ChemObjectIOTest {
         sourceWriter.write(molecule);
         String output = writer.toString();
         Assert.assertTrue(output.indexOf("IAtom a1 = mol.getNewBuilder().newInstance(IAtom.class,\"C\")") != -1);
+
+        GroovyShell shell = new GroovyShell();
+        shell.evaluate(
+            // import the classes used in the output
+            "import org.openscience.cdk.interfaces.*;" +
+            "import org.openscience.cdk.*;" +
+            // compensate for the write to wrap the output in { ... }
+            "if (true) " + 
+            output
+        );
     }
 }
