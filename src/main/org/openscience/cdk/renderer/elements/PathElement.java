ae31398b4a26a4c1ce2944ba7d6c6f84cadfef11
maclean <gilleain.torrance@gmail.com>
2010-09-26 19:56:12 +0100

    Javadocs for the Path Element

--- src/main/org/openscience/cdk/renderer/elements/PathElement.java
+++ src/main/org/openscience/cdk/renderer/elements/PathElement.java
@@ -19,7 +19,7 @@
 package org.openscience.cdk.renderer.elements;
 
 import java.awt.Color;
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.vecmath.Point2d;
 
@@ -27,20 +27,31 @@ import org.openscience.cdk.annotations.TestClass;
 import org.openscience.cdk.annotations.TestMethod;
 
 /**
+ * A path composed of points.
+ * 
  * @cdk.module renderbasic
  */
 @TestClass("org.openscience.cdk.renderer.elements.PathElementTest")
 public class PathElement implements IRenderingElement {
 
-    public final ArrayList<Point2d> points;
+    /** The points that make up the path **/
+    public final List<Point2d> points;
+    
+    /** The color of the path **/
     public final Color color;
     
+    /**
+     * Make a path from the list of points
+     * @param points
+     * @param color
+     */
     @TestMethod("testConstructor")
-    public PathElement(ArrayList<Point2d> points, Color color) {
+    public PathElement(List<Point2d> points, Color color) {
         this.points = points;
         this.color = color;
     }
     
+    /** {@inheritDoc} **/
     @TestMethod("testAccept")
     public void accept(IRenderingVisitor v) {
         v.visit(this);
