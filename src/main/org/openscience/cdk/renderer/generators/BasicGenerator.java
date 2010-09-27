8ae97a8e4a0745174f309c0d1efb141ee33096a3
maclean <gilleain.torrance@gmail.com>
2010-09-27 15:36:48 +0100

    Whitespace fixes for Basic Generator

--- src/main/org/openscience/cdk/renderer/generators/BasicGenerator.java
+++ src/main/org/openscience/cdk/renderer/generators/BasicGenerator.java
@@ -1,23 +1,23 @@
 /* $Revision$ $Author$ $Date$
-*
-*  Copyright (C) 2008 Gilleain Torrance <gilleain.torrance@gmail.com>
-*
-*  Contact: cdk-devel@list.sourceforge.net
-*
-*  This program is free software; you can redistribute it and/or
-*  modify it under the terms of the GNU Lesser General Public License
-*  as published by the Free Software Foundation; either version 2.1
-*  of the License, or (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU Lesser General Public License for more details.
-*
-*  You should have received a copy of the GNU Lesser General Public License
-*  along with this program; if not, write to the Free Software
-*  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+ *
+ *  Copyright (C) 2008 Gilleain Torrance <gilleain.torrance@gmail.com>
+ *
+ *  Contact: cdk-devel@list.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 package org.openscience.cdk.renderer.generators;
 
@@ -38,33 +38,33 @@ import org.openscience.cdk.renderer.elements.IRenderingElement;
  * @author maclean
  */
 public class BasicGenerator implements IGenerator<IAtomContainer> {
-	
+
     /** Holder for various parameters, such as background color */
     private BasicSceneGenerator sceneGenerator;
-    
+
     /** Generates elements for each atom in a container */
-	private BasicAtomGenerator atomGenerator;
-	
-	/** Generates elements for each bond in a container */
-	private BasicBondGenerator bondGenerator;
-	
-	/**
-	 * Make a basic generator that creates elements for atoms and bonds.
-	 */
-	public BasicGenerator() {
-		this.atomGenerator = new BasicAtomGenerator();
-		this.bondGenerator = new BasicBondGenerator();
+    private BasicAtomGenerator atomGenerator;
+
+    /** Generates elements for each bond in a container */
+    private BasicBondGenerator bondGenerator;
+
+    /**
+     * Make a basic generator that creates elements for atoms and bonds.
+     */
+    public BasicGenerator() {
+        this.atomGenerator = new BasicAtomGenerator();
+        this.bondGenerator = new BasicBondGenerator();
         this.sceneGenerator = new BasicSceneGenerator();
-	}
-	
-	/** {@inheritDoc} */
-	public IRenderingElement generate(IAtomContainer ac, RendererModel model) {
-		ElementGroup diagram = new ElementGroup();
+    }
+
+    /** {@inheritDoc} */
+    public IRenderingElement generate(IAtomContainer ac, RendererModel model) {
+        ElementGroup diagram = new ElementGroup();
         diagram.add(this.sceneGenerator.generate(ac, model));
-		diagram.add(this.bondGenerator.generate(ac, model));
-		diagram.add(this.atomGenerator.generate(ac, model));
-		return diagram;
-	}
+        diagram.add(this.bondGenerator.generate(ac, model));
+        diagram.add(this.atomGenerator.generate(ac, model));
+        return diagram;
+    }
 
     @Override
     /** {@inheritDoc} */
