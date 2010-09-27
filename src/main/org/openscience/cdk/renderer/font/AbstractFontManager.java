0c5a5b36d73298b4162c595fbf93fd85a5f50a7b
maclean <gilleain.torrance@gmail.com>
2010-09-27 17:35:29 +0100

    Javdocs for AbstractFontManager

--- src/main/org/openscience/cdk/renderer/font/AbstractFontManager.java
+++ src/main/org/openscience/cdk/renderer/font/AbstractFontManager.java
@@ -22,139 +22,232 @@ import java.util.Map;
 import java.util.TreeMap;
 
 /**
+ * Implements the common parts of the {@link IFontManager} interface.
+ * 
  * @cdk.module renderbasic
+ * @cdk.githash
  */
 public abstract class AbstractFontManager implements IFontManager {
-    
+
+    /** The default font family */
     private String fontName = "Arial";
-    
+
+    /** The font style - normal or bold */
     private IFontManager.FontStyle fontStyle;
 
-	private Map<Double, Integer> zoomToFontSizeMap;
+    /** The mapping between zoom levels and font sizes */
+    private Map<Double, Integer> zoomToFontSizeMap;
 
-	// these two values track the font position if it falls
-	// off the end of the array so that font and scale are always in synch
-	private int lowerVirtualCount;
+    // these two values track the font position if it falls
+    // off the end of the array so that font and scale are always in synch
+    private int lowerVirtualCount;
 
-	private int upperVirtualCount;
+    private int upperVirtualCount;
 
-	protected int currentFontIndex;
+    protected int currentFontIndex;
 
-	public AbstractFontManager() {
-		this.zoomToFontSizeMap = new TreeMap<Double, Integer>();
-	}
-	
-	protected abstract void makeFonts();
-	
-	public String getFontName() {
-	    return this.fontName;
-	}
-	
-	public void setFontName(String fontName) {
-	    if (this.fontName.equals(fontName)) {
-	        return;
-	    } else {
-	        this.fontName = fontName;
-	        makeFonts();
-	    }
-	}
-	
-	public void setFontStyle(IFontManager.FontStyle fontStyle) {
-	    if (this.fontStyle == fontStyle) {
-	        return;
-	    } else {
-	        this.fontStyle = fontStyle;
-	        makeFonts();
-	    }
-	}
-	
-	public IFontManager.FontStyle getFontStyle() {
-	    return this.fontStyle;
-	}
+    /**
+     * Call this in subclasses with the super() constructor.
+     */
+    public AbstractFontManager() {
+        this.zoomToFontSizeMap = new TreeMap<Double, Integer>();
+    }
 
-	public void registerFontSizeMapping(double zoom, int size) {
-		this.zoomToFontSizeMap.put(zoom, size);
-	}
+    /**
+     * Make widget-specific fonts.
+     */
+    protected abstract void makeFonts();
 
-	protected Integer getFontSizeForZoom(double zoom) {
-		double lower = -1;
-		for (double upper : this.zoomToFontSizeMap.keySet()) {
-			if (lower == -1) {
-				lower = upper;
-				if(zoom<=lower)
-				    return this.zoomToFontSizeMap.get(upper);
-				continue;
-			}
-			if (zoom > lower && zoom <= upper) {
-				return this.zoomToFontSizeMap.get(upper);
-			}
-			lower = upper;
-		}
+    /**
+     * Get the font family name used in this font manager.
+     * 
+     * @return the font name
+     */
+    public String getFontName() {
+        return this.fontName;
+    }
 
-		return this.zoomToFontSizeMap.get(lower);
-	}
+    /** {@inheritDoc} */
+    public void setFontName(String fontName) {
+        if (this.fontName.equals(fontName)) {
+            return;
+        } else {
+            this.fontName = fontName;
+            makeFonts();
+        }
+    }
 
-	public int getNumberOfFontSizes() {
-		return this.zoomToFontSizeMap.size();
-	}
+    /** {@inheritDoc} */
+    public void setFontStyle(IFontManager.FontStyle fontStyle) {
+        if (this.fontStyle == fontStyle) {
+            return;
+        } else {
+            this.fontStyle = fontStyle;
+            makeFonts();
+        }
+    }
 
-	public void resetVirtualCounts() {
-		this.lowerVirtualCount = 0;
-		this.upperVirtualCount = this.getNumberOfFontSizes() - 1;
-	}
+    /**
+     * Get the font style, defined in the {@link IFontManager} interface.
+     *  
+     * @return the font style
+     */
+    public IFontManager.FontStyle getFontStyle() {
+        return this.fontStyle;
+    }
 
-	public void toMiddle() {
-		this.currentFontIndex = this.getNumberOfFontSizes() / 2;
-	}
+    /**
+     * For a particular zoom level, register a font point-size so that this
+     * size of font will be used when the zoom is at this level. 
+     * 
+     * @param zoom the zoom level
+     * @param size the font size
+     */
+    public void registerFontSizeMapping(double zoom, int size) {
+        this.zoomToFontSizeMap.put(zoom, size);
+    }
 
-	public void increaseFontSize() {
-		// move INTO range if we have just moved OUT of lower virtual
-		if (this.inRange() || (this.atMin() && this.atLowerBoundary())) {
-			this.currentFontIndex++;
-		} else if (this.atMax()){
-			this.upperVirtualCount++;
-		} else if (this.atMin() && this.inLower()){
-			this.lowerVirtualCount++;
-		}
-	}
+    /**
+     * For a particular zoom, get the appropriate font size.
+     *  
+     * @param zoom the zoom level
+     * @return an integer font size
+     */
+    protected Integer getFontSizeForZoom(double zoom) {
+        double lower = -1;
+        for (double upper : this.zoomToFontSizeMap.keySet()) {
+            if (lower == -1) {
+                lower = upper;
+                if(zoom<=lower)
+                    return this.zoomToFontSizeMap.get(upper);
+                continue;
+            }
+            if (zoom > lower && zoom <= upper) {
+                return this.zoomToFontSizeMap.get(upper);
+            }
+            lower = upper;
+        }
 
-	public void decreaseFontSize() {
-		// move INTO range if we have just moved OUT of upper virtual
-		if (this.inRange() || (this.atMax() && this.atUpperBoundary())) {
-			this.currentFontIndex--;
-		} else if (this.atMin()) {
-			this.lowerVirtualCount--;
-		} else if (this.atMax() && this.inUpper()) {
-			this.upperVirtualCount--;
-		}
-	}
+        return this.zoomToFontSizeMap.get(lower);
+    }
 
-	public boolean inRange() {
-		return this.currentFontIndex > 0
-				&& this.currentFontIndex < this.getNumberOfFontSizes() - 1;
-	}
+    /**
+     * Get the number of font sizes used.
+     * 
+     * @return the size of the zoom to font map
+     */
+    public int getNumberOfFontSizes() {
+        return this.zoomToFontSizeMap.size();
+    }
 
-	public boolean atLowerBoundary() {
-		return this.lowerVirtualCount == 0;
-	}
+    /**
+     * Reset the virtual counts.
+     */
+    public void resetVirtualCounts() {
+        this.lowerVirtualCount = 0;
+        this.upperVirtualCount = this.getNumberOfFontSizes() - 1;
+    }
 
-	public boolean atUpperBoundary() {
-		return this.upperVirtualCount == this.getNumberOfFontSizes() - 1;
-	}
+    /**
+     * Set the font size pointer to the middle of the range. 
+     */
+    public void toMiddle() {
+        this.currentFontIndex = this.getNumberOfFontSizes() / 2;
+    }
 
-	public boolean inLower() {
-		return this.lowerVirtualCount < 0;
-	}
+    /**
+     * Move the font size pointer up. If this would move the pointer past
+     * the maximum font size, track this increase with a virtual size.
+     */
+    public void increaseFontSize() {
+        // move INTO range if we have just moved OUT of lower virtual
+        if (inRange() || (atMin() && atLowerBoundary())) {
+            currentFontIndex++;
+        } else if (atMax()){
+            upperVirtualCount++;
+        } else if (atMin() && inLower()){
+            lowerVirtualCount++;
+        }
+    }
 
-	public boolean inUpper() {
-		return this.upperVirtualCount > this.getNumberOfFontSizes() - 1;
-	}
+    /**
+     * Move the font size pointer down. If this would move the pointer past
+     * the minimum font size, track this increase with a virtual size.
+     */
+    public void decreaseFontSize() {
+        // move INTO range if we have just moved OUT of upper virtual
+        if (inRange() || (atMax() && atUpperBoundary())) {
+            currentFontIndex--;
+        } else if (atMin()) {
+            lowerVirtualCount--;
+        } else if (atMax() && inUpper()) {
+            upperVirtualCount--;
+        }
+    }
 
-	public boolean atMax() {
-		return this.currentFontIndex == this.getNumberOfFontSizes() - 1;
-	}
+    /**
+     * Check that the font pointer is in the range (0, numberOfFonts - 1). 
+     * 
+     * @return true if the current font index is between 0 and |fonts| - 1
+     */
+    public boolean inRange() {
+        return currentFontIndex > 0 
+            && currentFontIndex < getNumberOfFontSizes() - 1;
+    }
 
-	public boolean atMin() {
-		return this.currentFontIndex == 0;
-	}
+    /**
+     * Test the virtual font pointer to see if it is at the lower boundary of
+     * the font size range (0).
+     *  
+     * @return true if the lower virtual count is zero
+     */
+    public boolean atLowerBoundary() {
+        return this.lowerVirtualCount == 0;
+    }
+
+    /**
+     * Test the virtual font pointer to see if it is at the upper boundary of
+     * the font size range (|fonts| - 1).
+     * 
+     * @return true if the upper virtual count is |fonts| - 1
+     */
+    public boolean atUpperBoundary() {
+        return this.upperVirtualCount == this.getNumberOfFontSizes() - 1;
+    }
+
+    /**
+     * Test to see if the lower virtual pointer is in use.
+     * 
+     * @return true if the lower virtual count is less than zero
+     */
+    public boolean inLower() {
+        return this.lowerVirtualCount < 0;
+    }
+
+    /**
+     * Test to see if the upper virtual pointer is in use.
+     * 
+     * @return true if the upper virtual count is greater than |fonts| - 1
+     */
+    public boolean inUpper() {
+        return this.upperVirtualCount > this.getNumberOfFontSizes() - 1;
+    }
+
+    /**
+     * Check if the font pointer is as the maximum value.
+     * 
+     * @return true if the current font index is equal to |fonts| - 1
+     */
+    public boolean atMax() {
+        return this.currentFontIndex == this.getNumberOfFontSizes() - 1;
+    }
+
+    /**
+     * Check if the font pointer is as the minimum value.
+     * 
+     * @return true if the current font index is equal to zero
+     */
+    public boolean atMin() {
+        return this.currentFontIndex == 0;
+    }
 }
