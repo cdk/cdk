ac78d0a275adf2b7d744aa700fdd2f909967732b
maclean <gilleain.torrance@gmail.com>
2010-09-27 17:45:44 +0100

    Javadocs for AWTFontManager

--- src/main/org/openscience/cdk/renderer/font/AWTFontManager.java
+++ src/main/org/openscience/cdk/renderer/font/AWTFontManager.java
@@ -22,53 +22,65 @@ import java.awt.Font;
 import java.util.HashMap;
 
 /**
+ * AWT-specific font manager.
+ * 
  * @cdk.module renderbasic
  * @cdk.githash
  */
 public class AWTFontManager extends AbstractFontManager {
-	
-	private HashMap<Integer, Font> fontSizeToFontMap;
-	
-	private int minFontSize;
-	
-	private Font currentFont;
-	
-	public AWTFontManager() {
-		// apparently 9 pixels per em is the minimum
-		// but I don't know if (size 9 == 9 px.em-1)... 
-		this.minFontSize = 9;
-		this.makeFonts();
-		this.toMiddle();
-		this.resetVirtualCounts();
-	}
-	
-	protected void makeFonts() {
-		int size = this.minFontSize;
-		double scale = 0.5;
-		this.fontSizeToFontMap = new HashMap<Integer, Font>();
-		
-		for (int i = 0; i < 20; i++) {
-		    if (super.getFontStyle() == IFontManager.FontStyle.NORMAL) {
-    			this.fontSizeToFontMap.put(size,
-    					new Font(super.getFontName(), Font.PLAIN, size));
-		    } else {
-		        this.fontSizeToFontMap.put(size,
+
+    private HashMap<Integer, Font> fontSizeToFontMap;
+
+    private int minFontSize;
+
+    private Font currentFont;
+
+    /**
+     * Make a manager for fonts in AWT, with a minimum font size of 9.
+     */
+    public AWTFontManager() {
+        // apparently 9 pixels per em is the minimum
+        // but I don't know if (size 9 == 9 px.em-1)... 
+        this.minFontSize = 9;
+        this.makeFonts();
+        this.toMiddle();
+        this.resetVirtualCounts();
+    }
+
+    /** {@inheritDoc} */
+    protected void makeFonts() {
+        int size = this.minFontSize;
+        double scale = 0.5;
+        this.fontSizeToFontMap = new HashMap<Integer, Font>();
+
+        for (int i = 0; i < 20; i++) {
+            if (super.getFontStyle() == IFontManager.FontStyle.NORMAL) {
+                this.fontSizeToFontMap.put(size,
+                        new Font(super.getFontName(), Font.PLAIN, size));
+            } else {
+                this.fontSizeToFontMap.put(size,
                         new Font(super.getFontName(), Font.BOLD, size));
-		    }
-			this.registerFontSizeMapping(scale, size);
-			size += 1;
-			scale += 0.1;
-		}
-	}
-	
-	public void setFontForZoom(double zoom) {
-		int size = this.getFontSizeForZoom(zoom);
-		if (size != -1) {
-			this.currentFont = this.fontSizeToFontMap.get(size); 
-		}
-	}
-	
-	public Font getFont() {
-		return currentFont;
-	}
+            }
+            this.registerFontSizeMapping(scale, size);
+            size += 1;
+            scale += 0.1;
+        }
+    }
+
+    /** {@inheritDoc} */
+    public void setFontForZoom(double zoom) {
+        int size = this.getFontSizeForZoom(zoom);
+        if (size != -1) {
+            this.currentFont = this.fontSizeToFontMap.get(size); 
+        }
+    }
+
+    /**
+     * Get the current font.
+     * 
+     * @return the font at this zoom level
+     */
+    public Font getFont() {
+        return currentFont;
+    }
 }
