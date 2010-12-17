0ce634ee15b42f2ac425a34d9e2596932177e455
Egon Willighagen <egonw@users.sourceforge.net>
2010-12-17 15:27:11 +0100

    Fixed returning the monomer names when no strands are defined
    Signed-off-by: Rajarshi Guha <rajarshi.guha@gmail.com>

--- src/main/org/openscience/cdk/BioPolymer.java
+++ src/main/org/openscience/cdk/BioPolymer.java
@@ -197,7 +197,10 @@ public class BioPolymer extends Polymer implements java.io.Serializable, IBioPol
 	public Collection<String> getMonomerNames() {
 		Iterator<String> keys = strands.keySet().iterator();
 		Map<String, IMonomer> monomers = new Hashtable<String, IMonomer>();
-		
+
+        if (!keys.hasNext()) // no strands
+            return super.getMonomerNames();
+
 		while(keys.hasNext())	{
 			Strand oStrand = (Strand)strands.get(keys.next());
 			monomers.putAll(oStrand.getMonomers());
