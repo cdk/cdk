d15def190e79ae5baeb244a1e3baa59f56c144b6
Egon Willighagen <egon.willighagen@gmail.com>
2011-06-03 07:52:45 +0200

    Made class private to package

--- src/main/org/openscience/cdk/renderer/AverageBondLengthCalculator.java
+++ src/main/org/openscience/cdk/renderer/AverageBondLengthCalculator.java
@@ -36,7 +36,7 @@ import org.openscience.cdk.interfaces.IReactionSet;
  * @cdk.module renderbasic
  * @cdk.githash
  */
-public class AverageBondLengthCalculator {
+class AverageBondLengthCalculator {
 
     /**
      * Calculate the average bond length for the bonds in a reaction.
