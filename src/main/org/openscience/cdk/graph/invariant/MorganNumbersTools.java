1c5ba83b2d8a5624dd75a7b3294648d70eed1f8d
Thorsten Flügel <thorsten.fluegel@tu-dortmund.de>
2011-06-08 23:13:48 +0200

    made the creation of morgan numbers N times faster, where N is the number of atoms in the AtomContainer
    Change-Id: Ia7fd027490939f112ec268f0128a935d2c031f04
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

--- src/main/org/openscience/cdk/graph/invariant/MorganNumbersTools.java
+++ src/main/org/openscience/cdk/graph/invariant/MorganNumbersTools.java
@@ -20,6 +20,10 @@
  */
 package org.openscience.cdk.graph.invariant;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.openscience.cdk.annotations.TestClass;
 import org.openscience.cdk.annotations.TestMethod;
 import org.openscience.cdk.interfaces.IAtom;
@@ -46,27 +50,34 @@ public class MorganNumbersTools {
    */
   @TestMethod("testGetMorganNumbers_IAtomContainer")
   public static long[] getMorganNumbers(IAtomContainer atomContainer) {
-    long[] morganMatrix;
-    long[] tempMorganMatrix;
-    int N = atomContainer.getAtomCount();
-    morganMatrix = new long[N];
-    tempMorganMatrix = new long[N];
-    java.util.List<IAtom> atoms;
-    for (int f = 0; f < N; f++) {
-      morganMatrix[f] = atomContainer.getConnectedBondsCount(f);
-      tempMorganMatrix[f] = atomContainer.getConnectedBondsCount(f);
-    }
-    for (int e = 0; e < N; e++) {
-      for (int f = 0; f < N; f++) {
-        morganMatrix[f] = 0;
-        atoms = atomContainer.getConnectedAtomsList(atomContainer.getAtom(f));
-          for (IAtom atom : atoms) {
-              morganMatrix[f] += tempMorganMatrix[atomContainer.getAtomNumber(atom)];
-          }
-      }
-      System.arraycopy(morganMatrix, 0, tempMorganMatrix, 0, N);
-    }
-    return tempMorganMatrix;
+		long[] morganMatrix;
+		long[] tempMorganMatrix;
+		int N = atomContainer.getAtomCount();
+		morganMatrix = new long[N];
+		tempMorganMatrix = new long[N];
+		@SuppressWarnings("unchecked")
+		java.util.List<IAtom>[] atoms = new List[N];
+		@SuppressWarnings("unchecked")
+		Map<IAtom, Integer>[] atomIndices = new HashMap[N];
+		for (int f = 0; f < N; f++) {
+			morganMatrix[f] = atomContainer.getConnectedBondsCount(f);
+			tempMorganMatrix[f] = atomContainer.getConnectedBondsCount(f);
+			atoms[f] = atomContainer.getConnectedAtomsList(atomContainer.getAtom(f));
+			atomIndices[f] = new HashMap<IAtom, Integer>();
+			for (IAtom atom : atoms[f]) {
+				atomIndices[f].put(atom, atomContainer.getAtomNumber(atom));
+			}
+		}
+		for (int e = 0; e < N; e++) {
+			for (int f = 0; f < N; f++) {
+				morganMatrix[f] = 0;
+				for (IAtom atom : atoms[f]) {
+					morganMatrix[f] += tempMorganMatrix[atomIndices[f].get(atom)];
+				}
+			}
+			System.arraycopy(morganMatrix, 0, tempMorganMatrix, 0, N);
+		}
+		return tempMorganMatrix;
   }
 
 
