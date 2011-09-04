682d0a1ab52810c61f206a63908a1ec8f0a01fb7
Egon Willighagen <egonw@users.sourceforge.net>
2011-09-04 19:02:40 +0200

    Removed obsoleted code by Ni patch

--- src/main/org/openscience/cdk/atomtype/CDKAtomTypeMatcher.java
+++ src/main/org/openscience/cdk/atomtype/CDKAtomTypeMatcher.java
@@ -1459,20 +1459,6 @@ public class CDKAtomTypeMatcher implements IAtomTypeMatcher {
                 IAtomType type = getAtomType("Co.metallic");
                 if (isAcceptable(atom, atomContainer, type)) return type;
     		}
-    	} else if ("Ni".equals(atom.getSymbol())) {
-    		if (hasOneSingleElectron(atomContainer, atom)) {
-    			// no idea how to deal with this yet
-    			return null;
-    		} else if ((atom.getFormalCharge() != CDKConstants.UNSET &&
-    				atom.getFormalCharge() == +2)) {
-    			IAtomType type = getAtomType("Ni.2plus");
-    			if (isAcceptable(atom, atomContainer, type)) return type;
-    		} else if ((atom.getFormalCharge() != CDKConstants.UNSET &&
-    				atom.getFormalCharge() == 0) &&
-    				atomContainer.getConnectedAtomsCount(atom) <= 2) {
-    			IAtomType type = getAtomType("Ni");
-    			if (isAcceptable(atom, atomContainer, type)) return type;
-    		}
         } else if ("W".equals(atom.getSymbol())) {
             if (hasOneSingleElectron(atomContainer, atom)) {
                 // no idea how to deal with this yet
