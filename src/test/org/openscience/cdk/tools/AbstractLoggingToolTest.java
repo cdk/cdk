106e5ec7a133f9faf508d395e58d4cc891cb0fea
Egon Willighagen <egonw@users.sourceforge.net>
2011-12-26 13:49:54 +0100

    Work with the interface
    Signed-off-by: Rajarshi  Guha <rajarshi.guha@gmail.com>

--- src/test/org/openscience/cdk/tools/AbstractLoggingToolTest.java
+++ src/test/org/openscience/cdk/tools/AbstractLoggingToolTest.java
@@ -28,197 +28,197 @@ import org.openscience.cdk.CDKTestCase;
  */
 public abstract class AbstractLoggingToolTest extends CDKTestCase {
 	
-    public abstract LoggingTool getLoggingTool();
+    public abstract ILoggingTool getLoggingTool();
     
 	@Test public void testLoggingTool_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		Assert.assertNotNull(logger);
 	}
 
 	@Test public void testDebug_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.debug(this, this);
 	}
 
 	@Test public void testDebug_Object_int() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.debug(this, 1);
 	}
 
 	@Test public void testDebug_Object_double() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.debug(this, 1.0);
 	}
 
 	@Test public void testDebug_Object_boolean() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.debug(this, true);
 	}
 
 	@Test public void testDebug_Object_Object_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.debug(this, this, this, this, this);
 	}
 
 	@Test public void testDebug_Object_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.debug(this, this, this, this);
 	}
 
 	@Test public void testDebug_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.debug(this, this, this);
 	}
 
     @Test public void testDebug_ExceptionWithNullMessage() throws Exception {
-        LoggingTool logger = getLoggingTool();
+    	ILoggingTool logger = getLoggingTool();
         Exception exc = new Exception((String)null);
         logger.debug(exc);
     }
 
 	@Test public void testError_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.error(this);
 	}
 
 	@Test public void testError_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.error(this, this);
 	}
 
 	@Test public void testError_Object_int() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.error(this, 1);
 	}
 
 	@Test public void testError_Object_double() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.error(this, 1.0);
 	}
 
 	@Test public void testError_Object_boolean() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.error(this, true);
 	}
 
 	@Test public void testError_Object_Object_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.error(this, this, this, this, this);
 	}
 
 	@Test public void testError_Object_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.error(this, this, this, this);
 	}
 
 	@Test public void testError_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.error(this, this, this);
 	}
 
 	@Test public void testWarn_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.warn(this);
 	}
 
 	@Test public void testWarn_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.warn(this, this);
 	}
 
 	@Test public void testWarn_Object_int() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.warn(this, 1);
 	}
 
 	@Test public void testWarn_Object_double() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.warn(this, 1.0);
 	}
 
 	@Test public void testWarn_Object_boolean() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.warn(this, true);
 	}
 
 	@Test public void testWarn_Object_Object_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.warn(this, this, this, this, this);
 	}
 
 	@Test public void testWarn_Object_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.warn(this, this, this, this);
 	}
 
 	@Test public void testWarn_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.warn(this, this, this);
 	}
 
 	@Test public void testInfo_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.info(this);
 	}
 
 	@Test public void testInfo_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.info(this, this);
 	}
 
 	@Test public void testInfo_Object_int() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.info(this, 1);
 	}
 
 	@Test public void testInfo_Object_double() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.info(this, 1.0);
 	}
 
 	@Test public void testInfo_Object_boolean() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.info(this, true);
 	}
 
 	@Test public void testInfo_Object_Object_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.info(this, this, this, this, this);
 	}
 
 	@Test
     public void testInfo_Object_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.info(this, this, this, this);
 	}
 
 	@Test public void testInfo_Object_Object_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.info(this, this, this);
 	}
 
 	@Test public void testFatal_Object() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.fatal(this);
 	}
 
 	@Test public void testSetStackLength_int() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.setStackLength(20);
 	}
 
 	@Test public void testDumpClasspath() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.dumpClasspath();
 	}
 
 	@Test public void testDumpSystemProperties() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.dumpSystemProperties();
 	}
 
 	@Test public void testIsDebugEnabled() throws Exception {
-		LoggingTool logger = getLoggingTool();
+		ILoggingTool logger = getLoggingTool();
 		logger.isDebugEnabled();
 	}
 	
