b56e08b0e0c291cf1d9d4b554317c546e77ff2f7
John May <john.wilkinsonmay@gmail.com>
2012-03-20 12:04:42 +0000

    Removing redundant code
    Removes code that was used when writing the test and is not used any
    more
    
    Signed-off-by: Rajarshi Guha <rajarshi.guha@gmail.com>

--- src/test/org/openscience/cdk/io/iterator/IteratingMDLReaderTest.java
+++ src/test/org/openscience/cdk/io/iterator/IteratingMDLReaderTest.java
@@ -322,13 +322,8 @@ public class IteratingMDLReaderTest extends CDKTestCase {
 
         int count = 0;
 
-        java.util.ArrayList<Object> titles            = new java.util.ArrayList<Object>();
-        java.util.ArrayList<IAtomContainer> molecules = new java.util.ArrayList<IAtomContainer>();
-
         while(reader.hasNext()){
             IAtomContainer molecule = reader.next();
-            titles.add(molecule.getProperty(CDKConstants.TITLE));
-            molecules.add(molecule);
             count++;
         }
 
