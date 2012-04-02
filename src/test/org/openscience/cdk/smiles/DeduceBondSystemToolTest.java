2c1f76f830984e9f2d6353ece1b9ec19dd3c3a2f
Egon Willighagen <egonw@users.sourceforge.net>
2012-04-02 21:55:29 +0200

    Double bond orders are not properly assigned when using the SilentCOB
    Change-Id: I299374c6ad0bf75f1e97ace99de52cb813e5c221
    Signed-off-by: ngn <jeliazkova.nina@gmail.com>
    
    Conflicts:
    
    	src/META-INF/test-smiles.cdkdepends
    	src/test/org/openscience/cdk/smiles/DeduceBondSystemToolTest.java

--- src/test/org/openscience/cdk/smiles/DeduceBondSystemToolTest.java
+++ src/test/org/openscience/cdk/smiles/DeduceBondSystemToolTest.java
@@ -34,6 +34,7 @@ import org.openscience.cdk.interfaces.IBond.Order;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.ringsearch.AllRingsFinder;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.silent.Atom;
 import org.openscience.cdk.silent.AtomContainer;
 import org.openscience.cdk.silent.Bond;
@@ -71,6 +72,25 @@ public class DeduceBondSystemToolTest extends CDKTestCase {
 	public void testPyrrole() throws Exception {
         String smiles = "c2ccc3n([H])c1ccccc1c3(c2)";
         SmilesParser smilesParser = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+        IAtomContainer molecule = smilesParser.parseSmiles(smiles);
+        
+        molecule = dbst.fixAromaticBondOrders(molecule);
+        Assert.assertNotNull(molecule);
+
+        molecule = AtomContainerManipulator.removeHydrogens(molecule);
+        int doubleBondCount = 0;
+        for (int i = 0; i < molecule.getBondCount(); i++) {
+        	IBond bond = molecule.getBond(i);
+        	Assert.assertTrue(bond.getFlag(CDKConstants.ISAROMATIC));
+            if (bond.getOrder() == Order.DOUBLE) doubleBondCount++;
+        }
+        Assert.assertEquals(6, doubleBondCount);
+    }
+
+	@Test(timeout=1000) 
+	public void testPyrrole_Silent() throws Exception {
+        String smiles = "c2ccc3n([H])c1ccccc1c3(c2)";
+        SmilesParser smilesParser = new SmilesParser(SilentChemObjectBuilder.getInstance());
         IAtomContainer molecule = smilesParser.parseSmiles(smiles);
         
         molecule = dbst.fixAromaticBondOrders(molecule);
