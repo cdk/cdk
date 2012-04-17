090afa9d9a9f9d44ef3c027b93a9516fa3f1d90d
Rajarshi Guha <rajarshi.guha@gmail.com>
2012-04-17 11:27:00 -0400

    Updated TPSA descriptor to properly cast to IRing rather than different Ring implementations. Added unit test
    Change-Id: I613cdf7728b2f9f9f9ece9a2742104d0c012fa6b
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

--- src/main/org/openscience/cdk/qsar/descriptors/molecular/TPSADescriptor.java
+++ src/main/org/openscience/cdk/qsar/descriptors/molecular/TPSADescriptor.java
@@ -24,7 +24,6 @@
 package org.openscience.cdk.qsar.descriptors.molecular;
 
 import org.openscience.cdk.CDKConstants;
-import org.openscience.cdk.Ring;
 import org.openscience.cdk.annotations.TestClass;
 import org.openscience.cdk.annotations.TestMethod;
 import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
@@ -32,6 +31,7 @@ import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.interfaces.IRing;
 import org.openscience.cdk.interfaces.IRingSet;
 import org.openscience.cdk.qsar.DescriptorSpecification;
 import org.openscience.cdk.qsar.DescriptorValue;
@@ -257,9 +257,7 @@ public class TPSADescriptor implements IMolecularDescriptor {
         }
 
         // iterate over all atoms of ac
-        java.util.Iterator atoms = ac.atoms().iterator();
-        while (atoms.hasNext()) {
-            IAtom atom = (IAtom) atoms.next();
+        for (IAtom atom : ac.atoms()) {
             if (atom.getSymbol().equals("N") || atom.getSymbol().equals("O") ||
                     atom.getSymbol().equals("S") || atom.getSymbol().equals("P")) {
                 int singleBondCount = 0;
@@ -321,7 +319,7 @@ public class TPSADescriptor implements IMolecularDescriptor {
                 if (rs.contains(atom)) {
                     IRingSet rsAtom = rs.getRings(atom);
                     for (int ringSetIndex = 0; ringSetIndex < rsAtom.getAtomContainerCount(); ringSetIndex++) {
-                        Ring ring = (Ring) rsAtom.getAtomContainer(ringSetIndex);
+                        IRing ring = (IRing) rsAtom.getAtomContainer(ringSetIndex);
                         if (ring.getRingSize() == 3)
                             isIn3MemberRing = 1;
                     }
