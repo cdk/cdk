f3226e076fd2ee336fe48b9371b4bcbaaec7f609
Egon Willighagen <egonw@users.sourceforge.net>
2012-07-28 22:41:25 +0200

    Fixed the module assignment
    Change-Id: Ia4becf1b898fc5b0e72b745badccb941c3f71e0f

--- src/main/org/openscience/cdk/qsar/descriptors/atompair/PiContactDetectionDescriptor.java
+++ src/main/org/openscience/cdk/qsar/descriptors/atompair/PiContactDetectionDescriptor.java
@@ -65,7 +65,7 @@ import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
  *
  * @author         mfe4
  * @cdk.created    2004-11-03
- * @cdk.module     qsar-molecular
+ * @cdk.module     qsarmolecular
  * @cdk.githash
  * @cdk.set        qsar-descriptors
  * @cdk.dictref    qsar-descriptors:piContact
