a156b374d78b25bb9622a63c5c9d34bda8feb92b
John May <john.wilkinsonmay@gmail.com>
2012-09-03 12:51:31 +0100

    - StringBuilder instead of buffer - Separated the determination of the class name to a method - Separated the processing of the IOSetting to a method - Added default value as an extra column in the output - Added table headers for the output - Added closing </dd> - Replaced "/" with "File.Separator" - Added logging statements for null settings/no default constructors - Made toString(Tag[]) return empty string - Added @override/@inheritDoc (not really need)
    Change-Id: Id7f039669c339136eb5d6d7eb0c31a7c88569efd
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

--- src/main/net/sf/cdk/tools/doclets/CDKIOOptionsTaglet.java
+++ src/main/net/sf/cdk/tools/doclets/CDKIOOptionsTaglet.java
@@ -16,6 +16,7 @@
  */
 package net.sf.cdk.tools.doclets;
 
+import java.io.File;
 import java.util.Map;
 
 import org.openscience.cdk.io.IChemObjectIO;
@@ -35,41 +36,74 @@ import com.sun.tools.doclets.Taglet;
  * </pre>
  */
 public class CDKIOOptionsTaglet implements Taglet {
-    
+
     private static final String NAME = "cdk.iooptions";
-    
+
+
+    /**
+     * @inheritDoc
+     */
+    @Override
     public String getName() {
         return NAME;
     }
-    
+
+    /**
+     * @inheritDoc
+     */
+    @Override
     public boolean inField() {
         return false;
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     public boolean inConstructor() {
         return false;
     }
-    
+
+    /**
+     * @inheritDoc
+     */
+    @Override
     public boolean inMethod() {
         return false;
     }
-    
+
+    /**
+     * @inheritDoc
+     */
+    @Override
     public boolean inOverview() {
         return false;
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     public boolean inPackage() {
         return false;
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     public boolean inType() {
         return true;
     }
-    
+
+    /**
+     * @inheritDoc
+     */
+    @Override
     public boolean isInlineTag() {
         return false;
     }
-    
+
     public static void register(Map<String, CDKIOOptionsTaglet> tagletMap) {
     	CDKIOOptionsTaglet tag = new CDKIOOptionsTaglet();
     	Taglet t = (Taglet) tagletMap.get(tag.getName());
@@ -79,48 +113,99 @@ public class CDKIOOptionsTaglet implements Taglet {
     	tagletMap.put(tag.getName(), tag);
     }
 
+
+    /**
+     * Extract the full class name from the tag source position.
+     *
+     * @param tag the tag on a given source file
+     * @return the class name
+     */
+    private static String getClassName(Tag tag){
+
+        SourcePosition position = tag.position();
+        File file               = position.file();
+
+        String path = file.getPath().replaceAll(File.separator, ".");
+
+        return path.substring(path.indexOf("src.main") + 9,
+                              path.indexOf(".java"));
+
+    }
+
+
+    /**
+     * Constructs a HTML table row (tr) for the given IO Setting. The row
+     * consist of a cell for the setting name and a cell with the question
+     * associated with that setting.
+     *
+     * @param setting instance of an IO Setting (should not be null)
+     * @return the table row
+     *
+     */
+    private static String getRow(IOSetting setting){
+        StringBuilder row = new StringBuilder();
+        row.append("<tr>");
+        row.append("<td><b>").append(setting.getName()).append("</b></td>");
+        row.append("<td>").append(setting.getQuestion()).append("</td>");
+        row.append("<td>").append(setting.getDefaultSetting()).append("</td>");
+        row.append("</tr>");
+        return row.toString();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
     public String toString(Tag tag) {
+
     	// create a table with IOOptions
-    	StringBuffer tableContent = new StringBuffer();
-    	SourcePosition file = tag.position();
-    	String pathAndFile = file.file().toString();
-    	pathAndFile = pathAndFile.replaceAll("/", ".");
-    	pathAndFile = pathAndFile.substring(pathAndFile.indexOf("src.main") + 9);
-    	pathAndFile = pathAndFile.substring(0, pathAndFile.indexOf(".java"));
-    	try {
-			Class ioClass = Class.forName(pathAndFile);
-			Object ioInstance = ioClass.newInstance();
-			if (ioInstance instanceof IChemObjectIO) {
-				IChemObjectIO objectIO = (IChemObjectIO)ioInstance;
-				if (objectIO.getIOSettings().length == 0) return ""; 
+        StringBuilder tableContent = new StringBuilder();
+        String        name         = getClassName(tag);
+
+        try {
+
+            // get the class name and try invoking the default constructor
+            Class  c        = Class.forName(name);
+			Object instance = c.newInstance();
+
+            // ensures the tag is on an IChemObjectIO
+			if (instance instanceof IChemObjectIO) {
+                tableContent.append("<dt><b>IO options:</b><dd>");
+				IChemObjectIO objectIO = (IChemObjectIO)instance;
+				if (objectIO.getIOSettings().length == 0) return "";
 	    		tableContent.append("<table>");
+                tableContent.append("<tr><th>Name</th><th>Question</th><th>Default</th></tr>");
 	    		for (IOSetting setting : objectIO.getIOSettings()) {
-	    			tableContent.append("<tr>");
-	    			tableContent.append("<td><b>" + setting.getName() + "</b></td>");
-	    			tableContent.append(
-	    				"<td>" + setting.getQuestion() +
-	    				" [Default: " + setting.getDefaultSetting() + "]</td>"
-	    			);
-	    			tableContent.append("</tr>");
+                    if(setting != null) {
+                        tableContent.append(getRow(setting));
+                    } else {
+                        System.err.println("[IOOptionsTaglet] Null IOSetting in class: " + name);
+                    }
 	    		}
 	    		tableContent.append("</table>");
-			} else {
-				return "";
+                tableContent.append("</dd></dt>");
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			return "";
-		}
-    	return "<DT><B>IO options: </B><DD>"
-                + tableContent.toString() + "</DD>\n";
-    }
-    
-    public String toString(Tag[] tags) {
-        if (tags.length == 0) {
-            return null;
-        } else {
-            return toString(tags[0]);
+
+		} catch (ClassNotFoundException ex){
+            System.err.println("[IOOptionsTaglet] Unable to find: " + name);
+        } catch (InstantiationException ex) {
+			System.err.println("[IOOptionsTaglet] Unable to create an instance of: "
+                               + name + ". Does this class have a default constructor?");
+		} catch (IllegalAccessException ex){
+            System.err.println("[IOOptionsTaglet] Default constructor for " + name
+                               +" was not accessible from this package (i.e. private/protected)");
         }
+
+    	return tableContent.toString();
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String toString(Tag[] tags) {
+        return tags.length == 0 ? "" : toString(tags[0]);
     }
 
 }
