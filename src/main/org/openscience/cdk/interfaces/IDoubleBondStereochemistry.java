76577b4551ac71e3a2a8772febbdd72a3fb25a28
Egon Willighagen <egonw@users.sourceforge.net>
2012-09-19 08:20:38 +0200

    Added a data model for double bond stereochemistry
    Change-Id: I93c2f6d5855372fa0dacb5dfd07077991cc3781f

---
+++ src/main/org/openscience/cdk/interfaces/IDoubleBondStereochemistry.java
@@ -0,0 +1,68 @@
+/* Copyright (C) 2012  Egon Willighagen <egonw@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openscience.cdk.interfaces;
+
+/**
+ * Stereochemistry specification for double bond stereochemistry. The data model defines the double
+ * atoms and two ligands attached to those two atoms, linearly connected with the double bond in the
+ * middle. The {@link IBond}s that define the stereo element are defined as an array where the bonds
+ * are sorted according to the linear connectivity. Thus, the first and third bonds are the two
+ * bonds attached on either side of the double bond, and the second bond is the double bond.
+ * The stereo annotation then indicates if the ligand atoms are in the cis position
+ * ({@link Conformation#TOGETHER}) or in the trans position ({@link Conformation#OPPOSITE}), matching the
+ * orientation of the methyls in but-2-ene respectively as <i>Z</i> and <i>E</i>.
+ *
+ * @cdk.module interfaces
+ * @cdk.githash
+ */
+public interface IDoubleBondStereochemistry extends IStereoElement {
+
+    /**
+     * Enumeration that defines the two possible values for this stereochemistry type.
+     */
+    public enum Conformation {
+        TOGETHER,  //  as in Z-but-2-ene
+        OPPOSITE   //  as in E-but-2-ene
+    }
+
+    /**
+     * Returns an array of ligand bonds around the double bond.
+     *
+     * @return an array of two {@link IBond}s.
+     */
+    public IBond[] getBonds();
+
+    /**
+     * {@link IBond} that is the stereo center.
+     *
+     * @return the double {@link IBond}.
+     */
+    public IBond getStereoBond();
+
+    /**
+     * Defines the stereochemistry around the double bond.
+     *
+     * @return the {@link Conformation} for this stereo element.
+     */
+    public Conformation getStereo();
+}
---
+++ src/main/org/openscience/cdk/stereo/DoubleBondStereochemistry.java
@@ -0,0 +1,94 @@
+/* Copyright (C) 2012  Egon Willighagen <egonw@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openscience.cdk.stereo;
+
+import org.openscience.cdk.annotations.TestClass;
+import org.openscience.cdk.annotations.TestMethod;
+import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.interfaces.IDoubleBondStereochemistry;
+
+/**
+ * Stereochemistry specification for double bonds. See {@link IDoubleBondStereochemistry} for
+ * further details.
+ *
+ * @cdk.module core
+ *
+ * @see org.openscience.cdk.interfaces.IDoubleBondStereochemistry
+ */
+@TestClass("org.openscience.cdk.stereo.DoubleBondStereochemistryTest")
+public class DoubleBondStereochemistry implements IDoubleBondStereochemistry {
+
+	private Conformation stereo;
+	private IBond[] ligandBonds;
+	private IBond stereoBond;
+    private IChemObjectBuilder builder;
+
+	/**
+	 * Creates a new double bond stereo chemistry. The path of length three is defined by
+	 * <code>ligandBonds[0]</code>, <code>stereoBonds</code>, and <code>ligandBonds[1]</code>.
+	 */
+	public DoubleBondStereochemistry(IBond stereoBond, IBond[] ligandBonds, Conformation stereo) {
+		this.stereoBond = stereoBond;
+		this.ligandBonds = ligandBonds;
+		this.stereo = stereo;
+	}
+	
+    /**
+     * Sets a new {@link IChemObjectBuilder}.
+     *
+     * @param builder the new {@link IChemObjectBuilder} to be returned
+     * @see #getBuilder()
+     */
+    @TestMethod("testBuilder")
+    public void setBuilder(IChemObjectBuilder builder) {
+        this.builder = builder;
+    }
+
+	/** {@inheritDoc} */
+	@Override
+	public IChemObjectBuilder getBuilder() {
+		return this.builder;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public IBond[] getBonds() {
+		IBond[] arrayCopy = new IBond[2];
+        System.arraycopy(ligandBonds, 0, arrayCopy, 0, 2);
+        return arrayCopy;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public IBond getStereoBond() {
+		return this.stereoBond;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public Conformation getStereo() {
+		return this.stereo;
+	}
+
+}
