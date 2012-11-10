46d48f9a0d81ed6e97b1cdb36a117c09209a5b87
Egon Willighagen <egonw@users.sourceforge.net>
2012-11-10 22:27:36 +0100

    A few unit tests uncovering some issues with == comparisons
    Change-Id: Ib013c284e1f742a6d7655ec2c81ae01ca2757b07

--- src/test/org/openscience/cdk/IsotopeTest.java
+++ src/test/org/openscience/cdk/IsotopeTest.java
@@ -1,6 +1,5 @@
-/* $Revision$ $Author$ $Date$    
- * 
- * Copyright (C) 1997-2007  The Chemistry Development Kit (CDK) project
+/* Copyright (C) 1997-2007  The Chemistry Development Kit (CDK) project
+ *                    2012  Egon Willighagen <egonw@users.sf.net>
  * 
  * Contact: cdk-devel@lists.sourceforge.net
  * 
@@ -80,4 +79,43 @@ public class IsotopeTest extends AbstractIsotopeTest {
         Assert.assertEquals(80.0, i.getNaturalAbundance(), 0.001);
     }
 
+    @Test public void testCompare_MassNumber() {
+    	Isotope iso = new Isotope("C");
+    	iso.setMassNumber(12);
+    	Isotope iso2 = new Isotope("C");
+    	iso2.setMassNumber((int)12.0);
+        Assert.assertTrue(iso.compare(iso2));
+    }
+
+    @Test public void testCompare_MassNumberIntegers() {
+    	Isotope iso = new Isotope("C");
+    	iso.setMassNumber(new Integer(12));
+    	Isotope iso2 = new Isotope("C");
+    	iso2.setMassNumber(new Integer(12));
+        Assert.assertTrue(iso.compare(iso2));
+    }
+
+    @Test public void testCompare_MassNumberIntegers_ValueOf() {
+    	Isotope iso = new Isotope("C");
+    	iso.setMassNumber(Integer.valueOf(12));
+    	Isotope iso2 = new Isotope("C");
+    	iso2.setMassNumber(Integer.valueOf(12));
+        Assert.assertTrue(iso.compare(iso2));
+    }
+
+    @Test public void testCompare_ExactMass() {
+    	Isotope iso = new Isotope("C");
+    	iso.setExactMass(12.000000);
+    	Isotope iso2 = new Isotope("C");
+    	iso2.setExactMass(12.0);
+        Assert.assertTrue(iso.compare(iso2));
+    }
+
+    @Test public void testCompare_NaturalAbundance() {
+    	Isotope iso = new Isotope("C");
+    	iso.setNaturalAbundance(12.000000);
+    	Isotope iso2 = new Isotope("C");
+    	iso2.setNaturalAbundance(12.0);
+        Assert.assertTrue(iso.compare(iso2));
+    }
 }
