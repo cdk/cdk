7a811d7c27aa9883764fe300a01664463d770e9b
John May <john.wilkinsonmay@gmail.com>
2013-02-07 14:37:23 +0000

    an atom hash generator for seeding hash values
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

---
+++ src/main/org/openscience/cdk/hash/SeedGenerator.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2013. John May <jwmay@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+
+package org.openscience.cdk.hash;
+
+import org.openscience.cdk.annotations.TestClass;
+import org.openscience.cdk.annotations.TestMethod;
+import org.openscience.cdk.hash.seed.AtomEncoder;
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+/**
+ * Generate a seed value for each atom of a molecule. The provided {@link
+ * AtomEncoder} is used to encode invariant attributes of the atoms. This value
+ * is then modified by the size of the molecule and pseudorandomly distributed.
+ * The seed values should be used with another {@link AtomHashGenerator} which
+ * will differentiate atoms experiencing different environments, such as, {@link
+ * BasicAtomHashGenerator}.
+ *
+ * <blockquote><pre>
+ *
+ * // create a new seed generator
+ * AtomEncoder       encoder   = ConjugatedAtomEncoder.create(ATOMIC_NUMBER,
+ *                                                            MASS_NUMBER);
+ * AtomHashGenerator generator = new SeedGenerator(encoder);
+ *
+ * // generate six hash codes for each atom of benzene
+ * IAtomContainer benzene   = MoleculeFactory.makeBenzene();
+ * long[]         hashCodes = generator.generate(benzene);
+ * </pre></blockquote>
+ *
+ * @author John May
+ * @cdk.module hash
+ * @see BasicAtomHashGenerator
+ * @see org.openscience.cdk.hash.seed.ConjugatedAtomEncoder
+ */
+@TestClass("org.openscience.cdk.hash.SeedGeneratorTest")
+public final class SeedGenerator extends AbstractHashGenerator
+        implements AtomHashGenerator {
+
+    /* used to encode atom attributes */
+    private final AtomEncoder encoder;
+
+    /**
+     * Create a new seed generator using the provided {@link AtomEncoder}.
+     *
+     * @param encoder a method for encoding atom invariant properties
+     * @throws NullPointerException encoder was null
+     * @see org.openscience.cdk.hash.seed.ConjugatedAtomEncoder
+     */
+    @TestMethod("testConstruct_Null")
+    public SeedGenerator(AtomEncoder encoder) {
+        this(encoder, new Xorshift());
+    }
+
+    /**
+     * Create a new seed generator using the provided {@link AtomEncoder} and
+     * pseudorandom number generator.
+     *
+     * @param encoder      a method for encoding atom invariant properties
+     * @param pseudorandom number generator to randomise initial invariants
+     * @throws NullPointerException encoder or pseudorandom number generator was
+     *                              null
+     */
+    SeedGenerator(AtomEncoder encoder, Pseudorandom pseudorandom) {
+        super(pseudorandom);
+        if (encoder == null)
+            throw new NullPointerException("encoder cannot be null");
+        this.encoder = encoder;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @TestMethod("testGenerate,testGenerate_SizeSeeding")
+    @Override public long[] generate(IAtomContainer container) {
+
+        int n = container.getAtomCount();
+        int seed = n > 1 ? 9803 % n : 1;
+
+        long[] hashes = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            hashes[i] = distribute(seed * encoder.encode(container.getAtom(i),
+                                                         container));
+        }
+        return hashes;
+    }
+}
---
+++ src/test/org/openscience/cdk/hash/SeedGeneratorTest.java
@@ -0,0 +1,148 @@
+package org.openscience.cdk.hash;
+
+import org.junit.Test;
+import org.openscience.cdk.hash.seed.AtomEncoder;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author John May
+ * @cdk.module test-hash
+ */
+public class SeedGeneratorTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testConstruct_Null() {
+        new SeedGenerator(null);
+    }
+
+    @Test
+    public void testGenerate() throws Exception {
+
+        IAtomContainer container = mock(IAtomContainer.class);
+
+        AtomEncoder encoder = mock(AtomEncoder.class);
+        SeedGenerator generator = new SeedGenerator(encoder);
+
+        IAtom c1 = mock(IAtom.class);
+        IAtom c2 = mock(IAtom.class);
+        IAtom c3 = mock(IAtom.class);
+        IAtom c4 = mock(IAtom.class);
+        IAtom c5 = mock(IAtom.class);
+
+        when(container.getAtomCount()).thenReturn(5);
+        when(container.getAtom(0)).thenReturn(c1);
+        when(container.getAtom(1)).thenReturn(c2);
+        when(container.getAtom(2)).thenReturn(c3);
+        when(container.getAtom(3)).thenReturn(c4);
+        when(container.getAtom(4)).thenReturn(c5);
+
+        when(encoder.encode(c1, container)).thenReturn(42);
+        when(encoder.encode(c2, container)).thenReturn(42);
+        when(encoder.encode(c3, container)).thenReturn(42);
+        when(encoder.encode(c4, container)).thenReturn(42);
+        when(encoder.encode(c5, container)).thenReturn(42);
+
+        generator.generate(container);
+
+        verify(container, times(1)).getAtomCount();
+
+        verify(container, times(5)).getAtom(anyInt());
+
+        verify(encoder, times(1)).encode(c1, container);
+        verify(encoder, times(1)).encode(c2, container);
+        verify(encoder, times(1)).encode(c3, container);
+        verify(encoder, times(1)).encode(c4, container);
+        verify(encoder, times(1)).encode(c5, container);
+
+        verifyNoMoreInteractions(c1, c2, c3, c4, c5, container, encoder);
+    }
+
+    @Test
+    public void testGenerate_SizeSeeding() throws Exception {
+
+        IAtomContainer m1 = mock(IAtomContainer.class);
+        IAtomContainer m2 = mock(IAtomContainer.class);
+
+        AtomEncoder encoder = mock(AtomEncoder.class);
+        SeedGenerator generator = new SeedGenerator(encoder);
+
+        IAtom c1 = mock(IAtom.class);
+        IAtom c2 = mock(IAtom.class);
+        IAtom c3 = mock(IAtom.class);
+        IAtom c4 = mock(IAtom.class);
+        IAtom c5 = mock(IAtom.class);
+        IAtom c6 = mock(IAtom.class);
+
+        when(m1.getAtomCount()).thenReturn(5);
+        when(m1.getAtom(0)).thenReturn(c1);
+        when(m1.getAtom(1)).thenReturn(c2);
+        when(m1.getAtom(2)).thenReturn(c3);
+        when(m1.getAtom(3)).thenReturn(c4);
+        when(m1.getAtom(4)).thenReturn(c5);
+
+        when(m2.getAtomCount()).thenReturn(6);
+        when(m2.getAtom(0)).thenReturn(c1);
+        when(m2.getAtom(1)).thenReturn(c2);
+        when(m2.getAtom(2)).thenReturn(c3);
+        when(m2.getAtom(3)).thenReturn(c4);
+        when(m2.getAtom(4)).thenReturn(c5);
+        when(m2.getAtom(5)).thenReturn(c6);
+
+        when(encoder.encode(c1, m1)).thenReturn(42);
+        when(encoder.encode(c2, m1)).thenReturn(42);
+        when(encoder.encode(c3, m1)).thenReturn(42);
+        when(encoder.encode(c4, m1)).thenReturn(42);
+        when(encoder.encode(c5, m1)).thenReturn(42);
+
+        when(encoder.encode(c1, m2)).thenReturn(42);
+        when(encoder.encode(c2, m2)).thenReturn(42);
+        when(encoder.encode(c3, m2)).thenReturn(42);
+        when(encoder.encode(c4, m2)).thenReturn(42);
+        when(encoder.encode(c5, m2)).thenReturn(42);
+        when(encoder.encode(c6, m2)).thenReturn(42);
+
+        long[] v1 = generator.generate(m1);
+        long[] v2 = generator.generate(m2);
+
+        verify(m1, times(1)).getAtomCount();
+        verify(m2, times(1)).getAtomCount();
+
+        verify(m1, times(5)).getAtom(anyInt());
+        verify(m2, times(6)).getAtom(anyInt());
+
+        verify(encoder, times(1)).encode(c1, m1);
+        verify(encoder, times(1)).encode(c2, m1);
+        verify(encoder, times(1)).encode(c3, m1);
+        verify(encoder, times(1)).encode(c4, m1);
+        verify(encoder, times(1)).encode(c5, m1);
+
+        verify(encoder, times(1)).encode(c1, m2);
+        verify(encoder, times(1)).encode(c2, m2);
+        verify(encoder, times(1)).encode(c3, m2);
+        verify(encoder, times(1)).encode(c4, m2);
+        verify(encoder, times(1)).encode(c5, m2);
+        verify(encoder, times(1)).encode(c6, m2);
+
+        // check the value were different (due to molecule size)
+        assertThat(v1.length, is(5));
+        assertThat(v2.length, is(6));
+        for (int i = 0; i < v1.length; i++) {
+            assertThat(v1[i], is(not(v2[i])));
+        }
+
+        verifyNoMoreInteractions(m1, m2, c1, c2, c3, c4, c5, c6, encoder);
+
+    }
+
+}
