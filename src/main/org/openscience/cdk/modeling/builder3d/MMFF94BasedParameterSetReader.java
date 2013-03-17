18b53bfecc0ab9e3a098c98f1e0f52f5b424acdc
John May <john.wilkinsonmay@gmail.com>
2013-03-17 19:30:33 +0000

    grouping location fields and correcting value (leading slash)
    Change-Id: I6be03972ca3163f10fd044bf1e5c931a1b68d736
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

--- src/main/org/openscience/cdk/modeling/builder3d/MMFF94BasedParameterSetReader.java
+++ src/main/org/openscience/cdk/modeling/builder3d/MMFF94BasedParameterSetReader.java
@@ -55,7 +55,10 @@ import org.openscience.cdk.interfaces.IAtomType;
  */
 public class MMFF94BasedParameterSetReader {
 
-	private String configFile = "org/openscience/cdk/modeling/forcefield/data/mmff94.prm";
+	private static final String configFile = "/org/openscience/cdk/modeling/forcefield/data/mmff94.prm";
+    private static final String configFilevdW = "/org/openscience/cdk/modeling/forcefield/data/mmffvdw.prm";
+    private static final String configFileDFSB = "/org/openscience/cdk/modeling/forcefield/data/mmffdfsb.par";
+
 	private InputStream ins = null;
 	private Map<String,Object> parameterSet;
 	private List<IAtomType> atomTypes;
@@ -63,12 +66,11 @@ public class MMFF94BasedParameterSetReader {
 	private String key = "";
 	private String sid;
 
-	private String configFilevdW = "org/openscience/cdk/modeling/forcefield/data/mmffvdw.prm";
+
 	private InputStream insvdW = null;
 	private StringTokenizer stvdW;
 	private String sidvdW;
 
-	private String configFileDFSB = "org/openscience/cdk/modeling/forcefield/data/mmffdfsb.par";
 	private InputStream insDFSB;
 	private StringTokenizer stDFSB;
 
