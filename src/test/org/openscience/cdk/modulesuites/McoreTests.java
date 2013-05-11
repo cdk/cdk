7a852943994a41e58e41eea91a3efdbc9d0c5bcd
John May <john.wilkinsonmay@gmail.com>
2013-05-11 12:43:02 +0100

    Including new test classes in the module suite
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

--- src/test/org/openscience/cdk/modulesuites/McoreTests.java
+++ src/test/org/openscience/cdk/modulesuites/McoreTests.java
@@ -46,7 +46,11 @@ import org.openscience.cdk.exception.CDKExceptionTest;
 import org.openscience.cdk.exception.NoSuchAtomExceptionTest;
 import org.openscience.cdk.exception.NoSuchAtomTypeExceptionTest;
 import org.openscience.cdk.graph.AllPairsShortestPathsTest;
+import org.openscience.cdk.graph.BitMatrixTest;
+import org.openscience.cdk.graph.GreedyBasisTest;
+import org.openscience.cdk.graph.InitialCyclesTest;
 import org.openscience.cdk.graph.PathToolsTest;
+import org.openscience.cdk.graph.RelevantCyclesTest;
 import org.openscience.cdk.graph.ShortestPathsTest;
 import org.openscience.cdk.graph.SpanningTreeTest;
 import org.openscience.cdk.graph.matrix.AdjacencyMatrixTest;
@@ -121,5 +125,9 @@ import org.openscience.cdk.tools.periodictable.PeriodicTableTest;
     RegularCyclicVertexSearchTest.class,
     JumboCyclicVertexSearchTest.class,
     RingSearchTest.class,
+    BitMatrixTest.class,
+    InitialCyclesTest.class,
+    GreedyBasisTest.class,
+    RelevantCyclesTest.class
 })
 public class McoreTests {}
