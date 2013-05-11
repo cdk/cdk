3de8e6ba1cbe27947f8b88b3deb1d9c6e52b2469
John May <john.wilkinsonmay@gmail.com>
2013-05-11 12:41:21 +0100

    Extraction of a cycle basis using a greedy algorithm
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

---
+++ src/main/org/openscience/cdk/graph/GreedyBasis.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2013 John May <jwmay@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+package org.openscience.cdk.graph;
+
+import org.openscience.cdk.annotations.TestClass;
+import org.openscience.cdk.annotations.TestMethod;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Collections;
+import java.util.List;
+
+import static org.openscience.cdk.graph.InitialCycles.Cycle;
+
+/**
+ * Greedily compute a cycle basis from a provided set of initial cycles using
+ * gaussian elimination.
+ *
+ * @author John May
+ * @cdk.module core
+ * @cdk.githash
+ * @see RelevantCycles
+ */
+@TestClass("org.openscience.cdk.graph.GreedyBasisTest")
+final class GreedyBasis {
+
+    /** Cycles which are members of the basis */
+    private final List<Cycle> basis;
+
+    /** Edges of the current basis. */
+    private final BitSet edgesOfBasis;
+
+    /** Number of edges */
+    private final int m;
+
+    /**
+     * Create a new basis for the <i>potential</i> number of cycles and the
+     * <i>exact</i> number of edges. These values can be obtained from an {@link
+     * InitialCycles} instance.
+     *
+     * @param n potential number of cycles in the basis
+     * @param m number of edges in the graph
+     * @see org.openscience.cdk.graph.InitialCycles#numberOfCycles()
+     * @see org.openscience.cdk.graph.InitialCycles#numberOfEdges()
+     */
+    GreedyBasis(final int n, final int m) {
+        this.basis = new ArrayList<Cycle>(n);
+        this.edgesOfBasis = new BitSet(m);
+        this.m = m;
+    }
+
+    /**
+     * Access the members of the basis.
+     *
+     * @return cycles ordered by length
+     */
+    @TestMethod("unmodifiableMembers")
+    final List<Cycle> members() {
+        return Collections.unmodifiableList(basis);
+    }
+
+    /**
+     * Add a cycle to the basis.
+     *
+     * @param cycle new basis member
+     */
+    @TestMethod("add")
+    final void add(final Cycle cycle) {
+        basis.add(cycle);
+        edgesOfBasis.or(cycle.edgeVector());
+    }
+
+    /**
+     * Add all cycles to the basis.
+     *
+     * @param cycles new members of the basis
+     */
+    @TestMethod("addAll")
+    final void addAll(final Iterable<Cycle> cycles) {
+        for (final Cycle cycle : cycles)
+            add(cycle);
+    }
+
+    /**
+     * Check if all the edges of the <i>cycle</i> are present in the current
+     * <i>basis</i>.
+     *
+     * @param cycle an initial cycle
+     * @return any edges of the basis are present
+     */
+    @TestMethod("subsetOfBasis")
+    final boolean isSubsetOfBasis(final Cycle cycle) {
+        final BitSet edgeVector = cycle.edgeVector();
+        final int intersect = and(edgesOfBasis, edgeVector).cardinality();
+        return intersect == cycle.length();
+    }
+
+    /**
+     * Determine whether the <i>candidate</i> cycle is linearly
+     * <i>independent</i> from the current basis.
+     *
+     * @param candidate a cycle not in currently in the basis
+     * @return the candidate is independent
+     */
+    @TestMethod("independence")
+    final boolean isIndependent(final Cycle candidate) {
+
+        // simple checks for independence
+        if (basis.isEmpty() || !isSubsetOfBasis(candidate))
+            return true;
+
+        final BitMatrix matrix = new BitMatrix(m, basis.size() + 1);
+        for (Cycle cycle : basis)
+            matrix.add(cycle.edgeVector());
+        matrix.add(candidate.edgeVector());
+
+        // perform gaussian elimination
+        matrix.eliminate();
+
+        // if the last row (candidate) was eliminated it is not independent
+        return !matrix.eliminated(basis.size());
+    }
+
+    /** and <i>s</i> and <i>t</i> without modifying <i>s</i> */
+    private static final BitSet and(final BitSet s, final BitSet t) {
+        final BitSet u = (BitSet) s.clone();
+        u.and(t);
+        return u;
+    }
+}
---
+++ src/test/org/openscience/cdk/graph/GreedyBasisTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2013 John May <jwmay@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+package org.openscience.cdk.graph;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.BitSet;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.openscience.cdk.graph.BitMatrixTest.toBitSet;
+import static org.openscience.cdk.graph.InitialCycles.Cycle;
+
+/**
+ * @author John May
+ * @cdk.module test-core
+ */
+public class GreedyBasisTest {
+
+    @Test public void add() {
+        Cycle c1 = mock(Cycle.class);
+        Cycle c2 = mock(Cycle.class);
+        when(c1.edgeVector()).thenReturn(new BitSet());
+        when(c2.edgeVector()).thenReturn(new BitSet());
+        GreedyBasis basis = new GreedyBasis(2, 0);
+        assertTrue(basis.members().isEmpty());
+        basis.add(c1);
+        assertThat(basis.members(), hasItem(c1));
+        basis.add(c2);
+        assertThat(basis.members(), hasItems(c1, c2));
+    }
+
+    @Test public void addAll() {
+        Cycle c1 = mock(Cycle.class);
+        Cycle c2 = mock(Cycle.class);
+        when(c1.edgeVector()).thenReturn(new BitSet());
+        when(c2.edgeVector()).thenReturn(new BitSet());
+        GreedyBasis basis = new GreedyBasis(2, 0);
+        assertTrue(basis.members().isEmpty());
+        basis.addAll(Arrays.asList(c1, c2));
+        assertThat(basis.members(), hasItems(c1, c2));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void unmodifiableMembers() {
+        Cycle c1 = mock(Cycle.class);
+        when(c1.edgeVector()).thenReturn(new BitSet());
+        GreedyBasis basis = new GreedyBasis(2, 0);
+        basis.members().add(c1);
+    }
+
+    @Test public void subsetOfBasis() {
+        Cycle c1 = mock(Cycle.class);
+        Cycle c2 = mock(Cycle.class);
+        Cycle c3 = mock(Cycle.class);
+        when(c1.edgeVector()).thenReturn(toBitSet("111000000000"));
+        when(c2.edgeVector()).thenReturn(toBitSet("0001110000000"));
+        when(c3.edgeVector()).thenReturn(toBitSet("011110000000"));
+        when(c1.length()).thenReturn(3);
+        when(c2.length()).thenReturn(3);
+        when(c3.length()).thenReturn(4);
+        GreedyBasis basis = new GreedyBasis(3, 12);
+        assertFalse(basis.isSubsetOfBasis(c3));
+        basis.add(c1);
+        assertFalse(basis.isSubsetOfBasis(c3));
+        basis.add(c2);
+        assertTrue(basis.isSubsetOfBasis(c3));
+
+    }
+
+    @Test public void independence() {
+        Cycle c1 = mock(Cycle.class);
+        Cycle c2 = mock(Cycle.class);
+        Cycle c3 = mock(Cycle.class);
+        when(c1.edgeVector()).thenReturn(toBitSet("111000000000"));
+        when(c2.edgeVector()).thenReturn(toBitSet("000111000000"));
+        when(c3.edgeVector()).thenReturn(toBitSet("111111000000"));
+        when(c1.length()).thenReturn(3);
+        when(c2.length()).thenReturn(3);
+        when(c3.length()).thenReturn(6);
+        GreedyBasis basis = new GreedyBasis(3, 12);
+        assertTrue(basis.isIndependent(c1));
+        assertTrue(basis.isIndependent(c2));
+        assertTrue(basis.isIndependent(c3));
+        basis.add(c1);
+        assertTrue(basis.isIndependent(c2));
+        assertTrue(basis.isIndependent(c2));
+        basis.add(c2);
+        assertFalse(basis.isIndependent(c3));
+    }
+
+}
