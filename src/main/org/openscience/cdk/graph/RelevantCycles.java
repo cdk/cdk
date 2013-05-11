42205cca16af2389996a251776085606d8a5d8dd
John May <john.wilkinsonmay@gmail.com>
2013-05-11 12:42:00 +0100

    Concise algorithm for relevant cycles
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

---
+++ src/main/org/openscience/cdk/graph/RelevantCycles.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2013 John May <jwmay@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+
+package org.openscience.cdk.graph;
+
+import org.openscience.cdk.annotations.TestClass;
+import org.openscience.cdk.annotations.TestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.openscience.cdk.graph.InitialCycles.Cycle;
+
+/**
+ * Compute the relevant cycles (<i>C<sub>R</sub></i>) of a graph. A cycle is
+ * relevant if it cannot be represented as the &oplus;-sum (xor) of strictly
+ * shorter cycles {@cdk.cite Berger04}. This is the smallest set of short cycles
+ * which is <i>uniquely</i> defined for a graph. The set can also be thought of
+ * as the union of all minimum cycle bases. The set of cycles may be exponential
+ * in number but can be checked (see {@link #size()}) before construction
+ * {@cdk.cite Vismara97}.
+ *
+ * <blockquote><pre>
+ * // import static org.openscience.cdk.graph.GraphUtil.*;
+ * IAtomContainer m = ...;
+ *
+ * // compute on the whole graph
+ * RelevantCycles relevant = new RelevantCycles(toAdjList(m));
+ *
+ * // it is much faster to compute on the separate ring systems of the molecule
+ * int[][]    graph      = toAdjList(m);
+ * RingSearch ringSearch = new RingSearch(m, graph);
+ *
+ * // all isolated cycles are relevant
+ * for (int[] isolated : ringSearch.isolated()){
+ *     int[] path = cycle(graph, isolated);
+ * }
+ *
+ * // compute the relevant cycles for each system
+ * for (int[] fused : ringSearch.fused()){
+ *
+ *     int[][] subgraph = subgraph(graph, fused);
+ *     RelevantCycles relevant = new RelevantCycles(subgraph);
+ *
+ *     for(int[] path : relevant.paths()){
+ *         // convert the sub graph vertices back to the super graph indices
+ *         for(int i = 0; i < path.length; i++) {
+ *             path[i] = fused[path[i];
+ *         }
+ *     }
+ * }
+ *
+ * </pre></blockquote>
+ *
+ * @author John May
+ * @cdk.module core
+ * @cdk.keyword Relevant Cycles
+ * @cdk.keyword Relevant Rings
+ * @cdk.keyword R(G)
+ * @cdk.keyword Union of all Minimum Cycles Bases
+ * @cdk.keyword Cycle
+ * @cdk.keyword Ring
+ * @cdk.keyword Ring Perception
+ * @cdk.githash
+ * @see org.openscience.cdk.ringsearch.RingSearch
+ * @see org.openscience.cdk.ringsearch.SSSRFinder#findRelevantRings()
+ * @see GreedyBasis
+ */
+@TestClass("org.openscience.cdk.graph.RelevantCyclesTest")
+public final class RelevantCycles {
+
+    /** The relevant cycle basis. */
+    private final GreedyBasis basis;
+
+    /**
+     * Generate the relevant cycle basis for a graph.
+     *
+     * @param graph undirected adjacency list
+     * @see org.openscience.cdk.ringsearch.RingSearch#fused()
+     * @see GraphUtil#subgraph(int[][], int[])
+     */
+    @TestMethod("noGraph")
+    public RelevantCycles(final int[][] graph) {
+        this(new InitialCycles(graph));
+    }
+
+    /**
+     * Generate the relevant cycle basis from a precomputed set of initial
+     * cycles.
+     *
+     * @param initial set of initial cycles.
+     * @throws NullPointerException null InitialCycles provided
+     */
+    @TestMethod("noInitialCycles") RelevantCycles(final InitialCycles initial) {
+
+        if (initial == null)
+            throw new NullPointerException("no InitialCycles provided");
+
+        this.basis = new GreedyBasis(initial.numberOfCycles(),
+                                     initial.numberOfEdges());
+
+        // processing by size add cycles which are independent of smaller cycles
+        for (final int length : initial.lengths()) {
+            basis.addAll(independent(initial.cyclesOfLength(length)));
+        }
+    }
+
+    /**
+     * Given a list of cycles return those which are independent (&oplus;-sum)
+     * from the current basis.
+     *
+     * @param cycles cycles of a given length
+     * @return cycles which were independent
+     */
+    private List<Cycle> independent(final List<Cycle> cycles) {
+        final List<Cycle> independent = new ArrayList<Cycle>(cycles.size());
+        for (final Cycle cycle : cycles) {
+            if (basis.isIndependent(cycle))
+                independent.add(cycle);
+        }
+        return independent;
+    }
+
+    /**
+     * Reconstruct the paths of all relevant cycles.
+     *
+     * <blockquote><pre>
+     * RelevantCycles relevant = ...
+     *
+     * // ensure the number is manageable
+     * if(relevant.size() < 100){
+     *   for(int[] path : relevant.paths()){
+     *     // process the path
+     *   }
+     * }
+     * </pre></blockquote>
+     *
+     * @return array of vertex paths
+     */
+    @TestMethod("paths_bicyclo,paths_napthalene,paths_anthracene," +
+                        "paths_cyclophane_odd,paths_cyclophane_even")
+    public int[][] paths() {
+        final int[][] paths = new int[size()][0];
+        int i = 0;
+        for (final Cycle c : basis.members()) {
+            for (final int[] path : c.family())
+                paths[i++] = path;
+        }
+        return paths;
+    }
+
+    /**
+     * The number of the relevant cycles.
+     *
+     * @return size of relevant cycle set
+     */
+    @TestMethod("size_bicyclo,size_napthalene,size_anthracene," +
+                        "size_cyclophane_odd,size_cyclophane_even")
+    public int size() {
+        int size = 0;
+        for (final Cycle c : basis.members())
+            size += c.sizeOfFamily();
+        return size;
+    }
+}
---
+++ src/test/org/openscience/cdk/graph/RelevantCyclesTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2013 John May <jwmay@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+package org.openscience.cdk.graph;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.openscience.cdk.graph.InitialCyclesTest.anthracene;
+import static org.openscience.cdk.graph.InitialCyclesTest.bicyclo;
+import static org.openscience.cdk.graph.InitialCyclesTest.cyclophane_even;
+import static org.openscience.cdk.graph.InitialCyclesTest.napthalene;
+
+/**
+ * @author John May
+ * @cdk.module test-core
+ */
+public class RelevantCyclesTest {
+
+    @Test(expected = NullPointerException.class)
+    public void noGraph() {
+        new RelevantCycles((int[][]) null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void noInitialCycles() {
+        new RelevantCycles((InitialCycles) null);
+    }
+
+    @Test public void paths_bicyclo() {
+        int[][] bicyclo = bicyclo();
+        RelevantCycles relevant = new RelevantCycles(bicyclo);
+        int[][] paths = relevant.paths();
+        assertThat(paths.length, is(3));
+        int[][] expected = new int[][]{{5, 0, 1, 2, 3, 4},
+                                       {5, 0, 1, 2, 7, 6},
+                                       {5, 4, 3, 2, 7, 6}};
+        assertThat(paths, is(expected));
+    }
+
+    @Test public void paths_napthalene() {
+        int[][] napthalene = napthalene();
+        RelevantCycles relevant = new RelevantCycles(napthalene);
+        int[][] paths = relevant.paths();
+        assertThat(paths.length, is(2));
+        int[][] expected = new int[][]{{5, 0, 1, 2, 3, 4},
+                                       {5, 4, 7, 8, 9, 6}};
+        assertThat(paths, is(expected));
+    }
+
+    @Test public void paths_anthracene() {
+        int[][] anthracene = anthracene();
+        RelevantCycles relevant = new RelevantCycles(anthracene);
+        int[][] paths = relevant.paths();
+        assertThat(paths.length, is(3));
+        int[][] expected = new int[][]{{5, 0, 1, 2, 3, 4},
+                                       {9, 8, 10, 11, 12, 13},
+                                       {9, 6, 5, 4, 7, 8}};
+        assertThat(paths, is(expected));
+    }
+
+    @Test public void paths_cyclophane_even() {
+        int[][] cyclophane_even = cyclophane_even();
+        RelevantCycles relevant = new RelevantCycles(cyclophane_even);
+        int[][] paths = relevant.paths();
+        assertThat(paths.length, is(3));
+        int[][] expected = new int[][]{{3, 2, 1, 0, 5, 4},
+                                       {3, 6, 7, 8, 9, 10, 11, 0, 1, 2},
+                                       {3, 6, 7, 8, 9, 10, 11, 0, 5, 4}};
+        assertThat(paths, is(expected));
+    }
+
+    @Test public void paths_cyclophane_odd() {
+        int[][] cyclophane_even = cyclophane_even();
+        RelevantCycles relevant = new RelevantCycles(cyclophane_even);
+        int[][] paths = relevant.paths();
+        assertThat(paths.length, is(3));
+        int[][] expected = new int[][]{{3, 2, 1, 0, 5, 4},
+                                       {3, 6, 7, 8, 9, 10, 11, 0, 1, 2},
+                                       {3, 6, 7, 8, 9, 10, 11, 0, 5, 4}};
+        assertThat(paths, is(expected));
+    }
+
+    @Test public void size_bicyclo() {
+        int[][] bicyclo = bicyclo();
+        RelevantCycles relevant = new RelevantCycles(bicyclo);
+        assertThat(relevant.size(), is(3));
+    }
+
+    @Test public void size_napthalene() {
+        int[][] napthalene = napthalene();
+        RelevantCycles relevant = new RelevantCycles(napthalene);
+        assertThat(relevant.size(), is(2));
+    }
+
+    @Test public void size_anthracene() {
+        int[][] anthracene = anthracene();
+        RelevantCycles relevant = new RelevantCycles(anthracene);
+        assertThat(relevant.size(), is(3));
+    }
+
+    @Test public void size_cyclophane_even() {
+        int[][] cyclophane_even = cyclophane_even();
+        RelevantCycles relevant = new RelevantCycles(cyclophane_even);
+        assertThat(relevant.size(), is(3));
+    }
+
+    @Test public void size_cyclophane_odd() {
+        int[][] cyclophane_even = cyclophane_even();
+        RelevantCycles relevant = new RelevantCycles(cyclophane_even);
+        assertThat(relevant.size(), is(3));
+    }
+}
