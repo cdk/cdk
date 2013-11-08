3f7cb676718293c77cf4594e3c31d950cdd4e745
John May <john.wilkinsonmay@gmail.com>
2013-11-08 12:11:10 +0000

    Stream matching states as an iterator.
    Signed-off-by: Stephan Beisken <sbeisken@gmail.com>
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

---
+++ src/main/org/openscience/cdk/isomorphism/StateStream.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) 2013 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *  
+ * Contact: cdk-devel@lists.sourceforge.net
+ *  
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+
+package org.openscience.cdk.isomorphism;
+
+import org.openscience.cdk.annotations.TestMethod;
+
+import java.util.Iterator;
+
+/**
+ * Given a (subgraph-)isomorphism state this class can lazily iterate over the
+ * mappings in a non-recursive manner. The class currently implements and {@link
+ * Iterator} but is better suited to the {@code Stream} class (which will be
+ * available in JDK 8).
+ *
+ * @author John May
+ * @cdk.module isomorphism
+ */
+final class StateStream implements Iterator<int[]> {
+
+    /** A mapping state. */
+    private final State state;
+
+    /** The stack replaces the call-stack in a recursive matcher. */
+    private final CandidateStack stack;
+
+    /** Current candidates. */
+    private int n = 0, m = -1;
+
+    /** The next mapping. */
+    private int[] next;
+
+    /**
+     * Create a stream for the provided state.
+     *
+     * @param state the state to stream over
+     */
+    StateStream(final State state) {
+        this.state = state;
+        this.stack = new CandidateStack(state.nMax());
+        this.next = findNext(); // first-mapping
+    }
+
+    /** @inheritDoc */
+    @TestMethod("hasNext")
+    @Override public boolean hasNext() {
+        return next != null;
+    }
+
+    /** @inheritDoc */
+    @TestMethod("next")
+    @Override public int[] next() {
+        int[] ret = next;
+        next = findNext();
+        return ret;
+    }
+
+    /** @inheritDoc */
+    @TestMethod("remove")
+    @Override public void remove() {
+        throw new UnsupportedOperationException("a graph matching cannot be removed");
+    }
+
+    /**
+     * Finds the next mapping from the current state.
+     *
+     * @return the next state (or null if none)
+     */
+    private int[] findNext() {
+        while (map()) ;
+        if (state.size() == state.nMax())
+            return state.mapping();
+        return null;
+    }
+
+    /**
+     * Progress the state-machine - the function return false when a mapping is
+     * found on the mapping is done.
+     *
+     * @return the state is partial
+     */
+    private boolean map() {
+
+        // backtrack - we've tried all possible n or m, remove the last mapping 
+        if ((n == state.nMax() || m == state.mMax()) && !stack.empty())
+            state.remove(n = stack.popN(),
+                         m = stack.popM());
+
+
+        while ((m = state.nextM(m)) < state.mMax()) {
+            if (state.add(n, m)) {
+                stack.push(n, m);
+                n = state.nextN(-1);
+                m = -1;
+                return n < state.nMax();
+            }
+        }
+
+        return state.size() > 0 || m < state.mMax();
+    }
+
+    /**
+     * A fixed size stack to keep track of which vertices are mapped. This stack
+     * allows us to turn the recursive algorithms it to lazy iterating mappers.
+     * A reclusive call is usually implemented as call-stack which stores the
+     * variable in each subroutine invocation. For the mapping we actually only
+     * need store the candidates.
+     */
+    private final class CandidateStack {
+
+        /** Candidate storage. */
+        private final int[] ns, ms;
+
+        /** Size of each stack. */
+        private int nSize, mSize;
+
+        private CandidateStack(int capacity) {
+            ns = new int[capacity];
+            ms = new int[capacity];
+        }
+
+        /**
+         * Push a candidate mapping on to the stack.
+         *
+         * @param n query candidate
+         * @param m target candidate
+         */
+        void push(int n, int m) {
+            ns[nSize++] = n;
+            ms[mSize++] = m;
+        }
+
+        /**
+         * Pops the G1 candidate.
+         *
+         * @return the previous 'n' candidate
+         */
+        int popN() {
+            return ns[--nSize];
+        }
+
+        /**
+         * Pops the G2 candidate.
+         *
+         * @return the previous 'm' candidate
+         */
+        int popM() {
+            return ms[--mSize];
+        }
+
+        /**
+         * Is the stack empty - if so no candidates can be popped.
+         *
+         * @return
+         */
+        boolean empty() {
+            return nSize == 0 && mSize == 0;
+        }
+    }
+}
---
+++ src/test/org/openscience/cdk/isomorphism/StateStreamTest.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2013 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *  
+ * Contact: cdk-devel@lists.sourceforge.net
+ *  
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+
+package org.openscience.cdk.isomorphism;
+
+import org.junit.Test;
+import org.openscience.cdk.graph.GraphUtil;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.templates.TestMoleculeFactory;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author John May
+ * @cdk.module test-isomorphism
+ */
+public class StateStreamTest {
+
+    @Test public void hasNext() throws Exception {
+        VFSubState state = createNaphthaleneToBenzene(AtomMatcher.forAny(),
+                                                      BondMatcher.forAny());
+        Iterator<int[]> it = new StateStream(state);
+        assertFalse(it.hasNext());
+    }
+
+    @Test public void hasNext2() throws Exception {
+        VFSubState state = createBenzeneToNaphthalene(AtomMatcher.forAny(),
+                                                      BondMatcher.forAny());
+        int cnt = 0;
+        Iterator<int[]> it = new StateStream(state);
+        while (it.hasNext()) {
+            assertNotNull(it.next());
+            cnt++;
+        }
+        assertThat(cnt, is(24));
+    }
+
+    @Test public void next() throws Exception {
+        VFSubState state = createBenzeneToNaphthalene(AtomMatcher.forAny(),
+                                                      BondMatcher.forAny());
+        Iterator<int[]> it = new StateStream(state);
+        assertThat(it.next(), is(new int[]{0, 1, 2, 7, 8, 9}));
+        assertThat(it.next(), is(new int[]{0, 9, 8, 7, 2, 1}));
+        assertThat(it.next(), is(new int[]{1, 0, 9, 8, 7, 2}));
+        assertThat(it.next(), is(new int[]{1, 2, 7, 8, 9, 0}));
+        assertThat(it.next(), is(new int[]{2, 1, 0, 9, 8, 7}));
+        assertThat(it.next(), is(new int[]{2, 3, 4, 5, 6, 7}));
+        assertThat(it.next(), is(new int[]{2, 7, 6, 5, 4, 3}));
+        assertThat(it.next(), is(new int[]{2, 7, 8, 9, 0, 1}));
+        assertThat(it.next(), is(new int[]{3, 2, 7, 6, 5, 4}));
+        assertThat(it.next(), is(new int[]{3, 4, 5, 6, 7, 2}));
+        assertThat(it.next(), is(new int[]{4, 3, 2, 7, 6, 5}));
+        assertThat(it.next(), is(new int[]{4, 5, 6, 7, 2, 3}));
+        assertThat(it.next(), is(new int[]{5, 4, 3, 2, 7, 6}));
+        assertThat(it.next(), is(new int[]{5, 6, 7, 2, 3, 4}));
+        assertThat(it.next(), is(new int[]{6, 5, 4, 3, 2, 7}));
+        assertThat(it.next(), is(new int[]{6, 7, 2, 3, 4, 5}));
+        assertThat(it.next(), is(new int[]{7, 2, 1, 0, 9, 8}));
+        assertThat(it.next(), is(new int[]{7, 2, 3, 4, 5, 6}));
+        assertThat(it.next(), is(new int[]{7, 6, 5, 4, 3, 2}));
+        assertThat(it.next(), is(new int[]{7, 8, 9, 0, 1, 2}));
+        assertThat(it.next(), is(new int[]{8, 7, 2, 1, 0, 9}));
+        assertThat(it.next(), is(new int[]{8, 9, 0, 1, 2, 7}));
+        assertThat(it.next(), is(new int[]{9, 0, 1, 2, 7, 8}));     
+        assertThat(it.next(), is(new int[]{9, 8, 7, 2, 1, 0}));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void remove() throws Exception {
+        VFSubState state = createBenzeneToNaphthalene(AtomMatcher.forAny(),
+                                                      BondMatcher.forAny());
+        Iterator<int[]> it = new StateStream(state);
+        it.remove();
+    }
+
+    /**
+     * Create a sub state for matching benzene to naphthalene
+     *
+     *
+     * Benzene: InChI=1/C6H6/c1-2-4-6-5-3-1/h1-6H
+     *
+     * Naphthalene: InChI=1/C10H8/c1-2-6-10-8-4-3-7-9(10)5-1/h1-8H
+     */
+    VFSubState createBenzeneToNaphthalene(AtomMatcher atomMatcher,
+                                          BondMatcher bondMatcher) throws Exception {
+        IAtomContainer container1 = TestMoleculeFactory.makeBenzene();
+        IAtomContainer container2 = TestMoleculeFactory.makeNaphthalene();
+        GraphUtil.EdgeToBondMap bonds1 = GraphUtil.EdgeToBondMap.withSpaceFor(container1);
+        GraphUtil.EdgeToBondMap bonds2 = GraphUtil.EdgeToBondMap.withSpaceFor(container2);
+        int[][] g1 = GraphUtil.toAdjList(container1, bonds1);
+        int[][] g2 = GraphUtil.toAdjList(container2, bonds2);
+        return new VFSubState(container1, container2, g1, g2, bonds1, bonds2, atomMatcher, bondMatcher);
+    }
+
+    /**
+     * Create a sub state for matching naphthalene to benzene
+     *
+     * Benzene: InChI=1/C6H6/c1-2-4-6-5-3-1/h1-6H
+     *
+     * Naphthalene: InChI=1/C10H8/c1-2-6-10-8-4-3-7-9(10)5-1/h1-8H
+     */
+    VFSubState createNaphthaleneToBenzene(AtomMatcher atomMatcher,
+                                          BondMatcher bondMatcher) throws Exception {
+        IAtomContainer container1 = TestMoleculeFactory.makeNaphthalene();
+        IAtomContainer container2 = TestMoleculeFactory.makeBenzene();
+        GraphUtil.EdgeToBondMap bonds1 = GraphUtil.EdgeToBondMap.withSpaceFor(container1);
+        GraphUtil.EdgeToBondMap bonds2 = GraphUtil.EdgeToBondMap.withSpaceFor(container2);
+        int[][] g1 = GraphUtil.toAdjList(container1, bonds1);
+        int[][] g2 = GraphUtil.toAdjList(container2, bonds2);
+        return new VFSubState(container1, container2, g1, g2, bonds1, bonds2, atomMatcher, bondMatcher);
+    }
+}
