4b676f47df604a35e0c2a5414232e19213be639a
John May <john.wilkinsonmay@gmail.com>
2013-11-23 16:56:17 +0000

    A utility for ranking indicies by a seperate array of values. The ranking is built around a sorting procedure with the comparisson baked in.
    Signed-off-by: Stephan Beisken <sbeisken@gmail.com>
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

---
+++ src/main/org/openscience/cdk/graph/invariant/InvariantRanker.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2013 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *  
+ * Contact: cdk-devel@lists.sourceforge.net
+ *  
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+
+package org.openscience.cdk.graph.invariant;
+
+/**
+ * A utility for ranking indices by invariants. The ranking is built around
+ * a merge/insertion sort with the primary interaction through {@link #rank}.
+ * 
+ * @author John May
+ * @cdk.module standard
+ * @see <a href="http://algs4.cs.princeton.edu/22mergesort/">Mergesort</a>
+ * @see Canon
+ */
+final class InvariantRanker {
+
+    /** Auxiliary array for merge sort. */
+    private final int[] aux;
+
+    /**
+     * Length at which the sub-array should be sorted using insertion sort. As
+     * insertion sort is adaptive and in-place it's advantageous to use a high
+     * threshold for this use-case. Once we do the first sort, the invariants
+     * will always be 'almost' sorted which is the best case for the insertion
+     * sort.
+     */
+    private static final int INSERTION_SORT_THRESHOLD = 42;
+
+    /**
+     * Create an invariant ranker for {@code n} invariants.
+     * @param n number of values
+     */
+    InvariantRanker(int n) {
+        this.aux = new int[n];
+    }
+
+    /**
+     * Given an array of equivalent indices (currEq) and their values (curr)
+     * assign a rank to the values. The values are sorted using 'prev' and
+     * 'curr' invariants and once ranked the new ranks placed in 'prev'. The 
+     * values which are still equivalent are placed in 'nextEq' and terminated 
+     * by a '-1'.
+     *
+     * @param currEq currently equivalent vertices (initially identity)
+     * @param nextEq equivalent vertices (to refine) will be set by this method
+     * @param n      the number of currently equivalent vertices
+     * @param curr   the current invariants
+     * @param prev   the prev invariants (initially = curr) used to sort and
+     *               then store ranks (set by this method)
+     * @return the number of ranks
+     */
+    int rank(int[] currEq, int[] nextEq, int n, long[] curr, long[] prev) {
+
+        sortBy(currEq, 0, n, curr, prev);
+
+        // with the values sorted we now partition the values in to those
+        // which are unique and aren't unique.
+        
+        // we use the aux array memory but to make it easier to read we alias
+        // nu: number unique, nnu: number non-unique
+        int[] unique = aux, nonunique = nextEq;
+        int   nu     = 0,   nnu       = 0;
+        
+        int u  = currEq[0];
+        unique[nu++] = u;
+        for (int i = 1; i < n; i++) {
+            int v = currEq[i];
+            if (curr[v] == curr[u] && prev[v] == prev[u]) {
+                if (nnu == 0 || nonunique[nnu - 1] != u) {
+                    nonunique[nnu++] = u;
+                    nu--; // next unique will overwrite
+                }
+                nonunique[nnu++] = v;
+            } else {
+                unique[nu++] = v;
+            }
+            u = v;
+        }
+
+        // mark so the invoker knows to stop early
+        if (nnu != nonunique.length)
+            nonunique[nnu] = -1; 
+
+        // values are partitioned we now need to assign the new ranks. unique 
+        // values are assigned first then the non-unique ranks. we know which
+        // rank to start at by seeing how many have already been assigned. this
+        // is given by (|V| - |current non unique|).
+        int rank = 1 + curr.length - n;
+        
+        // assign new unique class ranks
+        for (int i = 0; i < nu; i++)
+            prev[unique[i]] = rank++;
+        
+        if (nnu == 0)
+            return rank - 1;
+        
+        // assign the new non-unique class ranks, before we assign the new rank
+        // we store the old rank so we can compare with the previous value. we
+        // again can do this is the auxiliary array but we alias again
+        int[] oldRank = aux;
+        
+        u           = nextEq[0];
+        oldRank[u]  = (int) prev[u];
+        prev[u] = rank;
+        for (int i = 1; i < nnu; i++) {
+            int v = nextEq[i];
+            if (curr[v] != curr[u] || prev[v] != oldRank[u])
+                rank++;
+            oldRank[v] = (int) prev[v];
+            prev[v]    = rank;
+            u = v;
+        }
+        
+        return rank;
+    }
+
+    /**
+     * Sort the values (using merge sort) in {@code vs} from {@code lo} (until
+     * {@code len}) by the {@code prev[]} and then {@code curr[]} invariants to
+     * determine rank. The values in {@code vs} are indices into the invariant
+     * arrays.
+     *
+     * @param vs   values (indices)
+     * @param lo   the first value to start sorting from
+     * @param len  the len of values to consider
+     * @param curr the current invariants
+     * @param prev the previous invariants
+     */
+    void sortBy(int[] vs, int lo, int len, long[] curr, long[] prev) {
+
+        if (len < INSERTION_SORT_THRESHOLD) {
+            insertionSortBy(vs, lo, len, curr, prev);
+            return;
+        }
+
+        int split = len / 2;
+
+        sortBy(vs, lo, split, curr, prev);
+        sortBy(vs, lo + split, len - split, curr, prev);
+        
+        // sub arrays already sorted, no need to merge
+        if (!less(vs[lo + split], vs[lo + split - 1], curr, prev))
+            return;
+        
+        merge(vs, lo, split, len, curr, prev);
+    }
+
+    /**
+     * Merge the values which are sorted between {@code lo} - {@code split} and
+     * {@code split} - {@code len}.
+     *
+     * @param vs    vertices
+     * @param lo    start index
+     * @param split the middle index (partition)
+     * @param len   the range to merge
+     * @param curr  the current invariants
+     * @param prev  the previous invariants
+     */
+    private void merge(int[] vs, int lo, int split, int len, long[] curr, long[] prev) {
+        System.arraycopy(vs, lo, aux, lo, len);
+
+        int i = lo, j = lo + split;
+        int iMax = lo + split, jMax = lo + len;
+        for (int k = lo, end = lo + len; k < end; k++) {
+            if (i == iMax)
+                vs[k] = aux[j++];
+            else if (j == jMax)
+                vs[k] = aux[i++];
+            else if (less(aux[i], aux[j], curr, prev))
+                vs[k] = aux[i++];
+            else
+                vs[k] = aux[j++];
+        }
+    }
+
+    /**
+     * Sort the values (using insertion sort) in {@code vs} from {@code lo}
+     * (until {@code len}) by the {@code prev[]} and then {@code curr[]}
+     * invariants to determine rank. The values in {@code vs} are indices into
+     * the invariant arrays.
+     *
+     * @param vs   values (indices)
+     * @param lo   the first value to start sorting from
+     * @param len  the len of values to consider
+     * @param curr the current invariants
+     * @param prev the previous invariants
+     */
+    static void insertionSortBy(int[] vs, int lo, int len, long[] curr, long[] prev) {
+        for (int j = lo + 1, hi = lo + len; j < hi; j++) {
+            int v = vs[j];
+            int i = j - 1;
+            while ((i >= lo) && less(v, vs[i], curr, prev))
+                vs[i + 1] = vs[i--];
+            vs[i + 1] = v;
+        }
+    }
+
+    /**
+     * Using the {@code prev} and {@code curr} invariants is value in index i
+     * less than j. Value i is less than j if it was previously less than j
+     * (prev[]) or it was equal and it is now (curr[]) less than j.
+     *
+     * @param i    an index
+     * @param j    an index
+     * @param curr current invariants
+     * @param prev previous invariants
+     * @return is the value in index i less than j
+     */
+    static boolean less(int i, int j, long[] curr, long[] prev) {
+        return prev[i] < prev[j] || prev[i] == prev[j] && curr[i] < curr[j];
+    }
+}
---
+++ src/test/org/openscience/cdk/graph/invariant/InvariantRankerTest.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2013 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *  
+ * Contact: cdk-devel@lists.sourceforge.net
+ *  
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+
+package org.openscience.cdk.graph.invariant;
+
+import com.google.common.primitives.Longs;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author John May
+ * @cdk.module test-standard
+ */
+public class InvariantRankerTest {
+
+    @Test public void rank() {
+
+        InvariantRanker ranker = new InvariantRanker(6);
+        long[] prev = new long[]{50, 100, 25, 100, 50, 90};
+        long[] curr = new long[]{50, 100, 25, 100, 50, 90};
+
+        // no we leave extra space
+        int[] vs = new int[]{0, 1, 2, 3, 4, 5};
+        int[] ws = new int[6];
+
+        int ranks = ranker.rank(vs, ws, 6, curr, prev);
+
+        assertThat(ranks, is(4));
+
+        // assigned ranks (note: unique assigned first)
+        assertThat(prev, is(new long[]{3, 4, 1, 4, 3, 2}));
+
+        // remaining non-unique vertices
+        assertThat(ws, is(new int[]{0, 4, 1, 3, -1, 0}));
+    }
+
+    @Test public void rank_all_equiv() {
+        
+        InvariantRanker ranker = new InvariantRanker(6);
+        long[] prev = new long[]{42, 42, 42, 42, 42, 42};
+        long[] curr = new long[]{42, 42, 42, 42, 42, 42};
+
+        // no we leave extra space
+        int[] vs = new int[]{0, 1, 2, 3, 4, 5};
+        int[] ws = new int[6];
+
+        int ranks = ranker.rank(vs, ws, 6, curr, prev);
+
+        assertThat(ranks, is(1));
+
+        // assigned ranks (note: unique assigned first)
+        assertThat(prev, is(new long[]{1, 1, 1, 1, 1, 1}));
+
+        // remaining non-unique vertices
+        assertThat(ws, is(new int[]{0, 1, 2, 3, 4, 5}));
+    }
+
+    @Test public void rank_all_unique() {
+        
+        InvariantRanker ranker = new InvariantRanker(7);
+        long[] prev = new long[]{7, 3, 1, 0, 91, 32, 67};
+        long[] curr = new long[]{7, 3, 1, 0, 91, 32, 67};
+
+        // no we leave extra space
+        int[] vs = new int[]{0, 1, 2, 3, 4, 5, 6};
+        int[] ws = new int[7];
+
+        int ranks = ranker.rank(vs, ws, 7, curr, prev);
+
+        assertThat(ranks, is(7));
+
+        // assigned ranks (note: unique assigned first)
+        assertThat(prev, is(new long[]{4, 3, 2, 1, 7, 5, 6}));
+
+        // no non-unique vertices
+        assertThat(ws, is(new int[]{-1, 0, 0, 0, 0, 0, 0}));
+    }
+
+    @Test public void mergeSort() {
+
+        int n = 100;
+
+        // random (unique) values in random order
+        Random rnd = new Random();
+        Set<Long> values = new HashSet<Long>();
+        while (values.size() < n)
+            values.add(rnd.nextLong());
+
+        long[] prev = Longs.toArray(values);
+
+        // ident array
+        int[] vs = new int[n];
+        for (int i = 0; i < n; i++)
+            vs[i] = i;
+
+        InvariantRanker invRanker = new InvariantRanker(n);
+        invRanker.sortBy(vs, 0, n, prev, prev);
+
+        // check they are sorted
+        for (int i = 1; i < n; i++)
+            assertTrue(prev[vs[i]] > prev[vs[i - 1]]);
+    }
+
+    @Test public void mergeSort_range() {
+
+        int n = 100;
+
+        // random (unique) values in random order
+        Random rnd = new Random();
+        Set<Long> values = new HashSet<Long>();
+        while (values.size() < n)
+            values.add(rnd.nextLong());
+
+        long[] prev = Longs.toArray(values);
+
+        // ident array
+        int[] vs = new int[n];
+        for (int i = 0; i < n; i++)
+            vs[i] = i;
+
+        InvariantRanker invRanker = new InvariantRanker(n);
+        invRanker.sortBy(vs, 10, n - 20, prev, prev);
+
+        // check they are sorted
+        for (int i = 11; i < (n - 20); i++)
+            assertTrue(prev[vs[i]] > prev[vs[i - 1]]);
+
+        // other values weren't touched
+        for (int i = 0; i < 10; i++)
+            assertThat(vs[i], is(i));
+        for (int i = n - 10; i < n; i++)
+            assertThat(vs[i], is(i));
+    }
+
+    @Test public void insertionSort() {
+        long[] prev = new long[]{11, 10, 9, 8, 7};
+        long[] curr = new long[]{11, 10, 9, 8, 7};
+        int[] vs = new int[]{0, 1, 2, 3, 4};
+        InvariantRanker.insertionSortBy(vs, 0, 5, curr, prev);
+        assertThat(vs, is(new int[]{4, 3, 2, 1, 0}));
+    }
+
+    @Test public void insertionSort_duplicate() {
+        long[] prev = new long[]{11, 10, 10, 9, 8, 7};
+        long[] curr = new long[]{11, 10, 10, 9, 8, 7};
+        int[] vs = new int[]{0, 1, 2, 3, 4, 5};
+        InvariantRanker.insertionSortBy(vs, 0, 6, curr, prev);
+        assertThat(vs, is(new int[]{5, 4, 3, 1, 2, 0}));
+    }
+
+    @Test public void insertionSort_range() {
+        long[] prev = new long[]{12, 11, 10, 9, 8, 7};
+        long[] curr = new long[]{12, 11, 10, 9, 8, 7};
+        int[] vs = new int[]{0, 1, 2, 3, 4, 5};
+        InvariantRanker.insertionSortBy(vs, 2, 3, curr, prev);
+        assertThat(vs, is(new int[]{0, 1, 4, 3, 2, 5}));
+    }
+
+    @Test public void less() throws Exception {
+        long[] prev = new long[]{1, 1, 2, 2};
+        long[] curr = new long[]{1, 1, 2, 2};
+        assertFalse(InvariantRanker.less(0, 1, curr, prev));
+        assertFalse(InvariantRanker.less(2, 3, curr, prev));
+        assertTrue(InvariantRanker.less(0, 2, curr, prev));
+        assertTrue(InvariantRanker.less(0, 3, curr, prev));
+        assertTrue(InvariantRanker.less(1, 2, curr, prev));
+        assertTrue(InvariantRanker.less(1, 3, curr, prev));
+    }
+
+    @Test public void lessUsingPrev() throws Exception {
+        long[] prev = new long[]{1, 1, 2, 2};
+        long[] curr = new long[]{1, 2, 1, 2};
+        // 0,1 and 2,3 are only less is we inspect the 'curr' invariants
+        assertTrue(InvariantRanker.less(0, 1, curr, prev));
+        assertTrue(InvariantRanker.less(2, 3, curr, prev));
+        // these values are only less inspecting the first invariants
+        assertTrue(InvariantRanker.less(0, 2, curr, prev));
+        assertTrue(InvariantRanker.less(0, 3, curr, prev));
+        assertTrue(InvariantRanker.less(1, 2, curr, prev));
+        assertTrue(InvariantRanker.less(1, 3, curr, prev));
+    }
+}
