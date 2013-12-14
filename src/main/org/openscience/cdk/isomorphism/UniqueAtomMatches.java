15602c79ff22d925d0e136f1a2b4f97b0c06c865
John May <john.wilkinsonmay@gmail.com>
2013-12-14 10:00:12 +0000

    A filter for unique atom matches.
    Signed-off-by: Stephan Beisken <sbeisken@gmail.com>
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

---
+++ src/main/org/openscience/cdk/isomorphism/UniqueAtomMatches.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2013 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *  
+ * Contact: cdk-devel@lists.sourceforge.net
+ *  
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+
+package org.openscience.cdk.isomorphism;
+
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Sets;
+
+import java.util.BitSet;
+import java.util.Set;
+
+/**
+ * A predicate for filtering atom-mapping results. This class is intended for
+ * use with {@link Pattern}.
+ * 
+ * <blockquote><pre>{@code
+ *     Pattern     pattern = Ullmann.findSubstructure(query);
+ *     List<int[]> unique  = FluentIterable.of(patter.matchAll(target))
+ *                                         .filter(new UniqueAtomMatches())
+ *                                         .toList();
+ * }</pre></blockquote>
+ * 
+ * @author John May
+ * @cdk.module isomorphism
+ */
+public class UniqueAtomMatches implements Predicate<int[]> {
+
+    /** Which mappings have we seen already. */
+    private final Set<BitSet> unique;
+
+    /**
+     * Create filter for the expected number of unique matches. The number
+     * of matches can grow if required.
+     *
+     * @param expectedHits expected number of unique matches
+     */
+    public UniqueAtomMatches(int expectedHits) {
+        this.unique = Sets.newHashSetWithExpectedSize(expectedHits);
+    }
+
+    /**
+     * Create filter for unique matches.
+     */
+    public UniqueAtomMatches() {
+        this(10);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public boolean apply(int[] input) {
+        return unique.add(toBitSet(input));
+    }
+
+    /**
+     * Convert a mapping to a bitset.
+     * 
+     * @param mapping an atom mapping
+     * @return a bit set of the mapped vertices (values in array)
+     */
+    private BitSet toBitSet(int[] mapping) {
+        BitSet hits = new BitSet();
+        for (int v : mapping)
+            hits.set(v);
+        return hits;
+    }
+}
---
+++ src/test/org/openscience/cdk/isomorphism/UniqueAtomMatchesTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2013 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *  
+ * Contact: cdk-devel@lists.sourceforge.net
+ *  
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ */
+
+package org.openscience.cdk.isomorphism;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author John May
+ * @cdk.module test-isomorphism
+ */
+public class UniqueAtomMatchesTest {
+
+    @Test public void testApply() throws Exception {
+        UniqueAtomMatches uam = new UniqueAtomMatches();
+        assertTrue(uam.apply(new int[]{1, 2, 3, 4}));
+        assertTrue(uam.apply(new int[]{1, 2, 3, 5}));
+        assertFalse(uam.apply(new int[]{4, 3, 2, 1}));
+        assertFalse(uam.apply(new int[]{1, 5, 2, 3}));
+    }
+}
