7bc38f8aa8db0a4e02e196d8f6b33af336ee04d7
John May <john.wilkinsonmay@gmail.com>
2014-01-19 20:26:19 +0000

    Annoyingly the AtomDiff reports there is a difference in the atom ID... when that's not what we are testing. As CML adds ids to atoms differences were being incorrectly reported. Adding an ID before we write the file resolves all but 1 failure.
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

--- src/test/org/openscience/cdk/io/cml/CDKRoundTripTest.java
+++ src/test/org/openscience/cdk/io/cml/CDKRoundTripTest.java
@@ -52,201 +52,221 @@ public class CDKRoundTripTest extends CDKTestCase {
     @Test public void testIElement_Symbol() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Test public void testIElement_AtomicNumber() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setAtomicNumber(6);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Ignore @Test public void testIIsotope_NaturalAbundance() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setNaturalAbundance(99.);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
     
     @Test public void testIIsotope_ExactMass() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setExactMass(12.);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Test public void testIIsotope_MassNumber() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setMassNumber(13);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Ignore @Test public void testIAtomType_Name() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setAtomTypeName("C.sp3");
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Ignore @Test public void testIAtomType_MaxBondOrder() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setMaxBondOrder(IBond.Order.TRIPLE);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Ignore @Test public void testIAtomType_BondOrderSum() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setBondOrderSum(4.);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Test public void testIAtomType_FormalCharge() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setFormalCharge(+1);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Ignore @Test public void testIAtomType_FormalNeighborCount() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setFormalNeighbourCount(4);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Ignore @Test public void testIAtomType_Hybridization() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setHybridization(IAtomType.Hybridization.SP3);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Ignore @Test public void testIAtomType_CovalentRadius() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setCovalentRadius(1.5d);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Ignore @Test public void testIAtomType_Valency() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setValency(4);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Test public void testIAtom_Charge() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setCharge(0.3);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Test public void testIAtom_HydrogenCount() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setImplicitHydrogenCount(4);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Test public void testIAtom_Point2d() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setPoint2d(new Point2d(1.0, 2.0));
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Test public void testIAtom_Point3d() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setPoint3d(new Point3d(1.0, 2.0, 3.0));
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Test public void testIAtom_FractionalPoint3d() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setFractionalPoint3d(new Point3d(1.0, 2.0, 3.0));
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Test public void testIAtom_Point8d() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setPoint2d(new Point2d(0.0, 0.0));
         atom.setPoint3d(new Point3d(-1.0, -2.0, -3.0));
         atom.setFractionalPoint3d(new Point3d(1.0, 2.0, 3.0));
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
     @Ignore @Test public void testIAtom_StereoParity() throws Exception {
         IAtomContainer mol = builder.newInstance(IAtomContainer.class);
         IAtom atom = builder.newInstance(IAtom.class,"C");
+        atom.setID("a1");
         atom.setStereoParity(-1);
         mol.addAtom(atom);
         IAtomContainer copy = CMLRoundTripTool.roundTripMolecule(convertor, mol);
-        String difference = AtomDiff.diff(atom, copy.getAtom(0));;
+        String difference = AtomDiff.diff(atom, copy.getAtom(0));
         Assert.assertEquals("Found non-zero diff: " + difference, 0, difference.length());
     }
 
