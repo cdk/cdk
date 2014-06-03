bd25e18b1b44945d7d82e2d80c60bf92fe7a457f
Mark Williamson <mw529@cam.ac.uk>
2014-06-03 11:19:12 +0100

    Remove unused imports
    Signed-off-by: John May <john.wilkinsonmay@gmail.com>

--- storage/inchi/src/test/java/org/openscience/cdk/inchi/InChIGeneratorTest.java
+++ storage/inchi/src/test/java/org/openscience/cdk/inchi/InChIGeneratorTest.java
@@ -23,7 +23,6 @@ import net.sf.jniinchi.INCHI_RET;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openscience.cdk.*;
-import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
@@ -33,22 +32,17 @@ import org.openscience.cdk.interfaces.ITetrahedralChirality;
 import org.openscience.cdk.interfaces.IBond.Order;
 import org.openscience.cdk.interfaces.ITetrahedralChirality.Stereo;
 import org.openscience.cdk.io.MDLV2000Reader;
-import org.openscience.cdk.smiles.SmilesGenerator;
 import org.openscience.cdk.stereo.DoubleBondStereochemistry;
 import org.openscience.cdk.stereo.ExtendedTetrahedral;
 import org.openscience.cdk.stereo.TetrahedralChirality;
-import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import javax.vecmath.Point2d;
 import javax.vecmath.Point3d;
 
-import java.io.IOException;
 import java.util.Collections;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.openscience.cdk.interfaces.IBond.Stereo.E_OR_Z;
 
 /**
  * TestCase for the InChIGenerator.
