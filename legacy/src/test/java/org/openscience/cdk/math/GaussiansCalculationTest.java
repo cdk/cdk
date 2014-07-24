4a3966dd81735657d58f9acb5380a9f559f890db
John May <john.wilkinsonmay@gmail.com>
2014-07-24 22:51:21 +0100

    Move all 'cdk-qm' code to 'cdk-legacy'. The custom PMD rulesets (no removed) seem no different from '/pmd/custom-data.xml' and was not specific to 'qm' code.
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>
    
    Conflicts:
    	legacy/src/test/java/org/openscience/cdk/math/GaussiansCalculationTest.java
    	misc/qm/src/test/java/org/openscience/cdk/coverage/QmCoverageTest.java
    	misc/qm/src/test/java/org/openscience/cdk/modulesuites/MqmTests.java
    
    Change-Id: Icf6b051d50a0931e23fa16ea08b4a039a6eac219

--- legacy/src/test/java/org/openscience/cdk/math/GaussiansCalculationTest.java
+++ legacy/src/test/java/org/openscience/cdk/math/GaussiansCalculationTest.java
@@ -1,31 +1,27 @@
-/* Copyright (C) 2001-2007  Stephan Michels <stephan@vern.chem.tu-berlin.de>
- * 
+/*
+ * Copyright (c) 2014 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *   
  * Contact: cdk-devel@lists.sourceforge.net
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- * All we ask is that proper credit is given for our work, which includes
- * - but is not limited to - adding the above copyright notice to the beginning
- * of your source code files, and to any copyright notice that you may distribute
- * with programs based on this work.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
+ *   
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *  
  */
-package org.openscience.cdk.math.qm;
- 
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.util.Iterator;
+package org.openscience.cdk.math;
 
 import org.openscience.cdk.ChemFile;
 import org.openscience.cdk.ChemObject;
@@ -34,17 +30,21 @@ import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IChemModel;
 import org.openscience.cdk.interfaces.IChemSequence;
 import org.openscience.cdk.io.CMLReader;
+import org.openscience.cdk.io.IChemObjectReader.Mode;
 import org.openscience.cdk.io.ISimpleChemObjectReader;
 import org.openscience.cdk.io.MDLReader;
 import org.openscience.cdk.io.XYZReader;
-import org.openscience.cdk.io.IChemObjectReader.Mode;
 import org.openscience.cdk.math.qm.ClosedShellJob;
 import org.openscience.cdk.math.qm.GaussiansBasis;
 import org.openscience.cdk.math.qm.Orbitals;
 import org.openscience.cdk.math.qm.SimpleBasisSet;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 import org.openscience.cdk.tools.manipulator.ChemModelManipulator;
- 
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.Iterator;
+
 /**
  * Demonstration of the quantum mechanical capabilities of CDK.
  * This application takes a XYZ, CML or MDL mol file, calculates
