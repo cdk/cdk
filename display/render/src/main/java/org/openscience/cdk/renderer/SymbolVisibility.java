a93f32d0b2555e905d2b922827b232794fd89828
John May <john.wilkinsonmay@gmail.com>
2014-08-06 15:43:10 +0100

    A predicate class that defines whether an atom symbol is visible. Could be reused in other generators.
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

---
+++ display/render/src/main/java/org/openscience/cdk/renderer/SymbolVisibility.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2014 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *   
+ * Contact: cdk-devel@lists.sourceforge.net
+ *   
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.openscience.cdk.renderer;
+
+import org.openscience.cdk.config.Elements;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IBond;
+
+import javax.vecmath.Point2d;
+import javax.vecmath.Vector2d;
+import java.util.List;
+
+/**
+ * Predicate that defines whether an atom symbol is displayed in a structure diagram.
+ *
+ * <pre>{@code
+ * SymbolVisibility visibility = SymbolVisibility.iupacRecommendations();
+ * }</pre>
+ * 
+ * @author John May
+ */
+public abstract class SymbolVisibility {
+
+    /**
+     * Determine if an atom with the specified bonds is visible.
+     *
+     * @param atom      an atom
+     * @param neighbors neighboring bonds
+     * @return whether the atom symbol is visible
+     */
+    public abstract boolean visible(IAtom atom, List<IBond> neighbors);
+
+    /**
+     * All atom symbols are visible.
+     *
+     * @return visibility that displays all symbols
+     */
+    public static SymbolVisibility all() {
+        return new SymbolVisibility() {
+            @Override public boolean visible(IAtom atom, List<IBond> neighbors) {
+                return true;
+            }
+        };
+    }
+
+    /**
+     * Displays a symbol based on the preferred representation from the IUPAC guidelines (GR-2.1.2)
+     * {@cdk.cite Brecher08}. Carbons are unlabeled unless they have abnormal valence, parallel
+     * bonds, or are terminal (i.e. methyl).
+     *
+     * @return symbol visibility instance
+     */
+    public static SymbolVisibility iupacRecommendations() {
+        return new IupacVisibility(true);
+    }
+
+    /**
+     * Displays a symbol based on the acceptable representation from the IUPAC guidelines (GR-2.1.2)
+     * {@cdk.cite Brecher08}. Carbons are unlabeled unless they have abnormal valence, parallel
+     * bonds. The recommendations note that it is acceptable to leave methyl groups unlabelled.
+     *
+     * @return symbol visibility instance
+     */
+    public static SymbolVisibility iupacRecommendationsWithoutMethyl() {
+        return new IupacVisibility(false);
+    }
+
+    /**
+     * Visibility following IUPAC guidelines.
+     */
+    private static final class IupacVisibility extends SymbolVisibility {
+
+        private boolean terminal = false;
+
+        private IupacVisibility(boolean terminal) {
+            this.terminal = terminal;
+        }
+
+        /**
+         * @inheritDoc
+         */
+        @Override public boolean visible(IAtom atom, List<IBond> bonds) {
+
+            final Elements element = Elements.ofNumber(atom.getAtomicNumber());
+
+            // all non-carbons are displayed
+            if (element != Elements.Carbon)
+                return true;
+            
+            // methane
+            if (bonds.size() == 0)
+                return true;
+            
+            // methyl (optional)
+            if (bonds.size() == 1 && terminal)
+                return true;
+
+            // abnormal valence, could be due to charge or unpaired electrons
+            if (!isFourValent(atom, bonds))
+                return true;
+
+            // no kink between bonds to imply the presence of a carbon and it must
+            // be displayed
+            if (hasParallelBonds(atom, bonds)) {
+                // TODO only when both bonds are single?
+                return true;
+            }
+
+            // ProblemMarker ?
+
+            return false;
+        }
+
+        /**
+         * Check the valency of the atom.
+         *
+         * @param atom  an atom
+         * @param bonds bonds connected to the atom
+         * @return whether the atom is four valent
+         */
+        private static boolean isFourValent(IAtom atom, List<IBond> bonds) {
+            Integer valence = atom.getImplicitHydrogenCount();
+            if (valence == null)
+                return true;
+            for (final IBond bond : bonds) {
+                valence += bond.getOrder().numeric();
+            }
+            return valence == 4;
+        }
+
+        /**
+         * Check whether the atom has only two bonds connected and they are (or close to) parallel.
+         *
+         * @param atom  an atom
+         * @param bonds bonds connected to the atom
+         * @return whether the atom has parallele bonds
+         */
+        private static boolean hasParallelBonds(IAtom atom, List<IBond> bonds) {
+            if (bonds.size() != 2) return false;
+            final double thetaInRad = getAngle(atom, bonds.get(0), bonds.get(1));
+            final double thetaInDeg = Math.toDegrees(thetaInRad);
+            final double delta = Math.abs(thetaInDeg - 180);
+            return Double.isNaN(delta) || delta < 8;
+        }
+
+        /**
+         * Determine the angle between two bonds of one atom.
+         *
+         * @param atom  an atom
+         * @param bond1 a bond connected to the atom
+         * @param bond2 another bond connected to the atom
+         * @return the angle (in radians)
+         */
+        private static double getAngle(IAtom atom, IBond bond1, IBond bond2) {
+            final Point2d pA = atom.getPoint2d();
+            final Point2d pB = bond1.getConnectedAtom(atom).getPoint2d();
+            final Point2d pC = bond2.getConnectedAtom(atom).getPoint2d();
+            final Vector2d u = new Vector2d(pB.x - pA.x,
+                                            pB.y - pA.y);
+            final Vector2d v = new Vector2d(pC.x - pA.x,
+                                            pC.y - pA.y);
+            return u.angle(v);
+        }
+    }
+}
---
+++ display/render/src/test/java/org/openscience/cdk/renderer/SymbolVisibilityTest.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2014 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *   
+ * Contact: cdk-devel@lists.sourceforge.net
+ *   
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.openscience.cdk.renderer;
+
+import org.junit.Test;
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IBond;
+
+import javax.vecmath.Point2d;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SymbolVisibilityTest {
+
+    @Test public void anyAtom() {
+        assertTrue(SymbolVisibility.all().visible(null, null));
+    }
+
+    @Test public void iupacOxygen() {
+        assertTrue(SymbolVisibility.iupacRecommendations().visible(new Atom("O"),
+                                                                   Collections.<IBond>emptyList()));
+    }
+
+    @Test public void iupacNitrogen() {
+        assertTrue(SymbolVisibility.iupacRecommendations().visible(new Atom("N"),
+                                                                   Collections.<IBond>emptyList()));
+    }
+
+    @Test public void iupacMethane() {
+        assertTrue(SymbolVisibility.iupacRecommendations().visible(new Atom("C"),
+                                                                   Collections.<IBond>emptyList()));
+    }
+
+    @Test public void iupacMethylPreferred() {
+        IAtom a1 = new Atom("C");
+        IAtom a2 = new Atom("C");
+        IBond bond = new Bond(a1, a2);
+        a1.setPoint2d(new Point2d(0, 0));
+        a2.setPoint2d(new Point2d(0, 0));
+        assertTrue(SymbolVisibility.iupacRecommendations().visible(a1,
+                                                                   Arrays.asList(bond)));
+    }
+
+    @Test public void iupacMethylAcceptable() {
+        IAtom a1 = new Atom("C");
+        IAtom a2 = new Atom("C");
+        IBond bond = new Bond(a1, a2);
+        a1.setPoint2d(new Point2d(0, 0));
+        a2.setPoint2d(new Point2d(0, 0));
+        assertFalse(SymbolVisibility.iupacRecommendationsWithoutMethyl().visible(a1,
+                                                                                 Arrays.asList(bond)));
+    }
+
+    @Test public void iupacUnlabelledCarbon() {
+        IAtom a1 = new Atom("C");
+        IAtom a2 = new Atom("C");
+        IAtom a3 = new Atom("C");
+        
+        a1.setPoint2d(new Point2d(0, 0));
+        a2.setPoint2d(new Point2d(0.5, -0.5));
+        a3.setPoint2d(new Point2d(0.5, 0.5));
+
+        IBond bond1 = new Bond(a1, a2);
+        IBond bond2 = new Bond(a1, a3);
+
+        a1.setImplicitHydrogenCount(2);
+        a2.setImplicitHydrogenCount(3);
+        a3.setImplicitHydrogenCount(3);
+        
+        assertFalse(SymbolVisibility.iupacRecommendations().visible(a1,
+                                                                    Arrays.asList(bond1, bond2)));
+    }
+
+    @Test public void iupacCarbonIon() {
+        IAtom a1 = new Atom("C");
+        IAtom a2 = new Atom("C");
+        IAtom a3 = new Atom("C");
+
+        a1.setPoint2d(new Point2d(0, 0));
+        a2.setPoint2d(new Point2d(0.5, -0.5));
+        a3.setPoint2d(new Point2d(0.5, 0.5));
+
+        IBond bond1 = new Bond(a1, a2);
+        IBond bond2 = new Bond(a1, a3);
+        
+        a1.setFormalCharge(+1);
+        a1.setImplicitHydrogenCount(1);
+        a2.setImplicitHydrogenCount(3);
+        a3.setImplicitHydrogenCount(3);
+
+        assertTrue(SymbolVisibility.iupacRecommendations().visible(a1,
+                                                                   Arrays.asList(bond1, bond2)));
+    }
+
+    @Test public void iupacCarbonParallel() {
+        IAtom a1 = new Atom("C");
+        IAtom a2 = new Atom("C");
+        IAtom a3 = new Atom("C");
+
+        a1.setPoint2d(new Point2d(0, 0));
+        a2.setPoint2d(new Point2d(0, -0.5));
+        a3.setPoint2d(new Point2d(0, 0.5));
+
+        IBond bond1 = new Bond(a1, a2);
+        IBond bond2 = new Bond(a1, a3);
+
+        a1.setImplicitHydrogenCount(2);
+        a2.setImplicitHydrogenCount(3);
+        a3.setImplicitHydrogenCount(3);
+
+        assertTrue(SymbolVisibility.iupacRecommendations().visible(a1,
+                                                                   Arrays.asList(bond1, bond2)));
+    }
+
+    // produces an NaN internally
+    @Test public void iupacCarbonCornerCase() {
+        IAtom a1 = new Atom("C");
+        IAtom a2 = new Atom("C");
+        IAtom a3 = new Atom("C");
+
+        a1.setPoint2d(new Point2d(0, 0));
+        a2.setPoint2d(new Point2d(0, 0));
+        a3.setPoint2d(new Point2d(0, 0));
+
+        IBond bond1 = new Bond(a1, a2);
+        IBond bond2 = new Bond(a1, a3);
+
+        a1.setImplicitHydrogenCount(2);
+        a2.setImplicitHydrogenCount(3);
+        a3.setImplicitHydrogenCount(3);
+
+        assertTrue(SymbolVisibility.iupacRecommendations().visible(a1,
+                                                                   Arrays.asList(bond1, bond2)));
+    }
+
+}
