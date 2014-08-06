01d3c593c4be9f91c82481e0ccba75790ec10a04
John May <john.wilkinsonmay@gmail.com>
2014-08-06 14:53:37 +0100

    An atom symbol for storing a displayed symbol.
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

---
+++ display/renderbasic/src/main/java/org/openscience/cdk/renderer/generators/standard/AtomSymbol.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (c) 2014 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *   
+ * Contact: cdk-devel@lists.sourceforge.net
+ *   
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.openscience.cdk.renderer.generators.standard;
+
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Intermediate between an
+ * {@link org.openscience.cdk.renderer.elements.IRenderingElement} and the atom
+ * data. The atom symbol represents a visible atom element with zero of more
+ * adjuncts. The adjuncts are hydrogen count, charge, and mass. The atom symbol
+ * is immutable and modifying a property or transforming the symbol makes a new
+ * instance.
+ * 
+ * @author John May
+ */
+final class AtomSymbol {
+
+    /**
+     * The element symbol.
+     */
+    private final TextOutline       element;
+
+    /**
+     * Adjuncts to the symbol, hydrogen count, charge, and mass.
+     */
+    private final List<TextOutline> adjuncts;
+
+    /**
+     * Desired alignment of the symbol.
+     */
+    private final SymbolAlignment   alignment;
+
+    /**
+     * The convex hull of the entire atom symbol.
+     */
+    private final ConvexHull        hull;
+
+    /**
+     * Alignment of symbol, left aligned symbols are centered on the first
+     * character, right aligned on the last character.
+     */
+    enum SymbolAlignment {
+        Left, Center, Right
+    }
+
+    /**
+     * Create a new atom symbol with the specified adjuncts.
+     * 
+     * @param element the element symbol (e.g. 'N' in 'NH4+')
+     * @param adjuncts the adjuncts
+     */
+    AtomSymbol(TextOutline element, List<TextOutline> adjuncts) {
+        this.element = element;
+        this.adjuncts = adjuncts;
+        this.alignment = SymbolAlignment.Center;
+        this.hull = ConvexHull.ofShapes(getOutlines());
+    }
+
+    /**
+     * Internal constructor provides the attributes.
+     * 
+     * @param element
+     * @param adjuncts
+     * @param alignment
+     * @param hull
+     */
+    private AtomSymbol(TextOutline element, List<TextOutline> adjuncts, SymbolAlignment alignment, ConvexHull hull) {
+        this.element = element;
+        this.adjuncts = adjuncts;
+        this.alignment = alignment;
+        this.hull = hull;
+    }
+
+    /**
+     * Create a new atom symbol (from this symbol) but with the specified
+     * alignment.
+     * 
+     * @param alignment element alignment
+     * @return new atom symbol
+     */
+    AtomSymbol alignTo(SymbolAlignment alignment) {
+        return new AtomSymbol(element, adjuncts, alignment, hull);
+    }
+
+    /**
+     * Access the center point of the symbol. The center point is determined by
+     * the alignment.
+     * 
+     * @return center point
+     */
+    Point2D getAlignmentCenter() {
+        if (alignment == SymbolAlignment.Left) {
+            return element.getFirstGlyphCenter();
+        } else if (alignment == SymbolAlignment.Right) {
+            return element.getLastGlyphCenter();
+        } else {
+            return element.getCenter();
+        }
+    }
+
+    /**
+     * Access the Java 2D shape text outlines that display the atom symbol.
+     * 
+     * @return shapes
+     */
+    List<Shape> getOutlines() {
+        List<Shape> shapes = new ArrayList<Shape>();
+        shapes.add(element.getOutline());
+        for (TextOutline adjunct : adjuncts)
+            shapes.add(adjunct.getOutline());
+        return shapes;
+    }
+
+    /**
+     * Access the convex hull of the whole atom symbol.
+     * 
+     * @return convex hull
+     */
+    ConvexHull getConvexHull() {
+        return hull;
+    }
+
+    /**
+     * Transform the position and orientation of the symbol.
+     * 
+     * @param transform affine transform
+     * @return the transformed symbol (new instance)
+     */
+    AtomSymbol transform(AffineTransform transform) {
+        List<TextOutline> transformedAdjuncts = new ArrayList<TextOutline>(adjuncts.size());
+        for (TextOutline adjunct : adjuncts)
+            transformedAdjuncts.add(adjunct.transform(transform));
+        return new AtomSymbol(element.transform(transform), transformedAdjuncts, alignment, hull.transform(transform));
+    }
+
+    /**
+     * Convenience function to resize an atom symbol.
+     * 
+     * @param scaleX x-axis scaling
+     * @param scaleY y-axis scaling
+     * @return the resized symbol (new instance)
+     */
+    AtomSymbol resize(double scaleX, double scaleY) {
+        Point2D center = element.getCenter();
+        AffineTransform transform = new AffineTransform();
+        transform.translate(center.getX(), center.getY());
+        transform.scale(scaleX, scaleY);
+        transform.translate(-center.getX(), -center.getY());
+        return transform(transform);
+    }
+
+    /**
+     * Convenience function to center an atom symbol on a specified point. The
+     * centering depends on the symbol alignment.
+     * 
+     * @param x x-axis location
+     * @param y y-axis location
+     * @return the centered symbol (new instance)
+     */
+    AtomSymbol center(double x, double y) {
+        Point2D center = getAlignmentCenter();
+        return translate(x - center.getX(), y - center.getY());
+    }
+
+    /**
+     * Convenience function to translate an atom symbol on a specified point.
+     * 
+     * @param x x-axis location
+     * @param y y-axis location
+     * @return the translated symbol (new instance)
+     */
+    AtomSymbol translate(double x, double y) {
+        return transform(AffineTransform.getTranslateInstance(x, y));
+    }
+}
---
+++ display/renderbasic/src/test/java/org/openscience/cdk/renderer/generators/standard/AtomSymbolTest.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2014 European Bioinformatics Institute (EMBL-EBI)
+ *                    John May <jwmay@users.sf.net>
+ *   
+ * Contact: cdk-devel@lists.sourceforge.net
+ *   
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version. All we ask is that proper credit is given
+ * for our work, which includes - but is not limited to - adding the above 
+ * copyright notice to the beginning of your source code files, and to any
+ * copyright notice that you may distribute with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.openscience.cdk.renderer.generators.standard;
+
+import org.junit.Test;
+
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+import static org.openscience.cdk.renderer.generators.standard.AtomSymbol.SymbolAlignment.Center;
+import static org.openscience.cdk.renderer.generators.standard.AtomSymbol.SymbolAlignment.Left;
+import static org.openscience.cdk.renderer.generators.standard.AtomSymbol.SymbolAlignment.Right;
+
+public class AtomSymbolTest {
+
+    private final Font font = new Font("Verdana", Font.PLAIN, 12);
+
+    @Test
+    public void alignToCenter() throws Exception {
+        TextOutline outline = new TextOutline("Cl", font);
+        AtomSymbol symbol = new AtomSymbol(outline, Collections.<TextOutline> emptyList());
+        assertCloseTo(outline.getCenter(), symbol.alignTo(Center).getAlignmentCenter(), 0.01);
+    }
+
+    @Test
+    public void alignToLeft() throws Exception {
+        TextOutline outline = new TextOutline("Cl", font);
+        AtomSymbol symbol = new AtomSymbol(outline, Collections.<TextOutline> emptyList());
+        assertCloseTo(outline.getFirstGlyphCenter(), symbol.alignTo(Left).getAlignmentCenter(), 0.01);
+    }
+
+    @Test
+    public void alignToRight() throws Exception {
+        TextOutline outline = new TextOutline("Cl", font);
+        AtomSymbol symbol = new AtomSymbol(outline, Collections.<TextOutline> emptyList());
+        assertCloseTo(outline.getLastGlyphCenter(), symbol.alignTo(Right).getAlignmentCenter(), 0.01);
+    }
+
+    @Test
+    public void testGetOutlines() throws Exception {
+        TextOutline outline = new TextOutline("Cl", font);
+        AtomSymbol symbol = new AtomSymbol(outline, Collections.<TextOutline> emptyList());
+        Rectangle outlineBounds = outline.getOutline().getBounds();
+        Rectangle symbolBounds = symbol.getOutlines().get(0).getBounds();
+        assertThat(outlineBounds.getX(), closeTo(symbolBounds.getX(), 0.01));
+        assertThat(outlineBounds.getY(), closeTo(symbolBounds.getY(), 0.01));
+        assertThat(outlineBounds.getMaxX(), closeTo(symbolBounds.getMaxX(), 0.01));
+        assertThat(outlineBounds.getMaxY(), closeTo(symbolBounds.getMaxY(), 0.01));
+    }
+
+    @Test
+    public void testGetOutlinesWithAdjunct() throws Exception {
+        TextOutline outline = new TextOutline("Cl", font);
+        TextOutline adjunct = new TextOutline("H", font);
+        AtomSymbol symbol = new AtomSymbol(outline, Arrays.asList(adjunct));
+        Rectangle outlineBounds = adjunct.getOutline().getBounds();
+        Rectangle symbolBounds = symbol.getOutlines().get(1).getBounds();
+        assertThat(outlineBounds.getX(), closeTo(symbolBounds.getX(), 0.01));
+        assertThat(outlineBounds.getY(), closeTo(symbolBounds.getY(), 0.01));
+        assertThat(outlineBounds.getMaxX(), closeTo(symbolBounds.getMaxX(), 0.01));
+        assertThat(outlineBounds.getMaxY(), closeTo(symbolBounds.getMaxY(), 0.01));
+    }
+
+    @Test
+    public void testGetConvexHull() throws Exception {
+        TextOutline outline = new TextOutline("Cl", font);
+        AtomSymbol symbol = new AtomSymbol(outline, Collections.<TextOutline> emptyList());
+        ConvexHull outlineHull = ConvexHull.ofShape(outline.getOutline());
+        ConvexHull symbolHull = symbol.getConvexHull();
+
+        Rectangle2D outlineBounds = outlineHull.outline().getBounds2D();
+        Rectangle2D symbolBounds = symbolHull.outline().getBounds2D();
+
+        assertThat(outlineBounds.getX(), closeTo(symbolBounds.getX(), 0.01));
+        assertThat(outlineBounds.getY(), closeTo(symbolBounds.getY(), 0.01));
+        assertThat(outlineBounds.getMaxX(), closeTo(symbolBounds.getMaxX(), 0.01));
+        assertThat(outlineBounds.getMaxY(), closeTo(symbolBounds.getMaxY(), 0.01));
+    }
+
+    @Test
+    public void testResize() throws Exception {
+        TextOutline outline = new TextOutline("Cl", font);
+        AtomSymbol symbol = new AtomSymbol(outline, Collections.<TextOutline> emptyList());
+        AtomSymbol transformed = symbol.resize(2, 2);
+        Rectangle2D newBounds = transformed.getOutlines().get(0).getBounds2D();
+        assertThat(newBounds.getX(), closeTo(-4.27, 0.01));
+        assertThat(newBounds.getY(), closeTo(-13.75, 0.01));
+        assertThat(newBounds.getMaxX(), closeTo(15.52, 0.01));
+        assertThat(newBounds.getMaxY(), closeTo(4.79, 0.01));
+    }
+
+    @Test
+    public void testCenter() throws Exception {
+        TextOutline outline = new TextOutline("Cl", font);
+        AtomSymbol symbol = new AtomSymbol(outline, Collections.<TextOutline> emptyList());
+        AtomSymbol transformed = symbol.center(2, 2);
+        Rectangle2D newBounds = transformed.getOutlines().get(0).getBounds2D();
+        assertThat(newBounds.getX(), closeTo(-2.95, 0.01));
+        assertThat(newBounds.getY(), closeTo(-2.63, 0.01));
+        assertThat(newBounds.getMaxX(), closeTo(6.95, 0.01));
+        assertThat(newBounds.getMaxY(), closeTo(6.63, 0.01));
+    }
+
+    @Test
+    public void testTranslate() throws Exception {
+        TextOutline outline = new TextOutline("Cl", font);
+        AtomSymbol symbol = new AtomSymbol(outline, Collections.<TextOutline> emptyList());
+        AtomSymbol transformed = symbol.translate(4, 2);
+        Rectangle2D orgBounds = symbol.getOutlines().get(0).getBounds2D();
+        Rectangle2D newBounds = transformed.getOutlines().get(0).getBounds2D();
+        assertThat(newBounds.getX(), closeTo(orgBounds.getX() + 4, 0.01));
+        assertThat(newBounds.getY(), closeTo(orgBounds.getY() + 2, 0.01));
+        assertThat(newBounds.getMaxX(), closeTo(orgBounds.getMaxX() + 4, 0.01));
+        assertThat(newBounds.getMaxY(), closeTo(orgBounds.getMaxY() + 2, 0.01));
+    }
+
+    @Test
+    public void testTranslateAdjunct() throws Exception {
+        TextOutline outline = new TextOutline("Cl", font);
+        TextOutline adjunct = new TextOutline("H", font);
+        AtomSymbol symbol = new AtomSymbol(outline, Arrays.asList(adjunct));
+        AtomSymbol transformed = symbol.translate(4, 2);
+        Rectangle2D orgBounds = symbol.getOutlines().get(0).getBounds2D();
+        Rectangle2D newBounds = transformed.getOutlines().get(0).getBounds2D();
+        assertThat(newBounds.getX(), closeTo(orgBounds.getX() + 4, 0.01));
+        assertThat(newBounds.getY(), closeTo(orgBounds.getY() + 2, 0.01));
+        assertThat(newBounds.getMaxX(), closeTo(orgBounds.getMaxX() + 4, 0.01));
+        assertThat(newBounds.getMaxY(), closeTo(orgBounds.getMaxY() + 2, 0.01));
+    }
+
+    void assertCloseTo(Point2D actual, Point2D expected, double epsilon) {
+        assertThat(actual.getX(), closeTo(expected.getX(), epsilon));
+        assertThat(actual.getY(), closeTo(expected.getY(), epsilon));
+    }
+}
