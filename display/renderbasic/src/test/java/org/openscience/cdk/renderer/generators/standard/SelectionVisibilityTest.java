315e6bfbe748e1a9d3c9be8071cb1f8bb6696597
John May <john.wilkinsonmay@gmail.com>
2014-08-27 12:20:14 +0100

    Update tests to new API.
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

--- display/renderbasic/src/test/java/org/openscience/cdk/renderer/generators/standard/SelectionVisibilityTest.java
+++ display/renderbasic/src/test/java/org/openscience/cdk/renderer/generators/standard/SelectionVisibilityTest.java
@@ -7,6 +7,7 @@ import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.interfaces.IChemObject;
+import org.openscience.cdk.renderer.RendererModel;
 import org.openscience.cdk.renderer.SymbolVisibility;
 import org.openscience.cdk.silent.Atom;
 
@@ -33,7 +34,7 @@ public class SelectionVisibilityTest {
         methyl.addBond(0, 3, IBond.Order.SINGLE);
         methyl.addBond(0, 4, IBond.Order.SINGLE);
         SymbolVisibility visibility = SelectionVisibility.all(SymbolVisibility.iupacRecommendations());
-        assertFalse(visibility.visible(methyl.getAtom(0), methyl.getConnectedBondsList(methyl.getAtom(0))));
+        assertFalse(visibility.visible(methyl.getAtom(0), methyl.getConnectedBondsList(methyl.getAtom(0)), new RendererModel()));
     }
 
     @Test public void withHighlight() {
@@ -49,7 +50,7 @@ public class SelectionVisibilityTest {
         methyl.addBond(0, 3, IBond.Order.SINGLE);
         methyl.addBond(0, 4, IBond.Order.SINGLE);
         SymbolVisibility visibility = SelectionVisibility.all(SymbolVisibility.iupacRecommendations());
-        assertTrue(visibility.visible(methyl.getAtom(0), methyl.getConnectedBondsList(methyl.getAtom(0))));
+        assertTrue(visibility.visible(methyl.getAtom(0), methyl.getConnectedBondsList(methyl.getAtom(0)), new RendererModel()));
     }
 
     @Test public void isolated() {
@@ -65,7 +66,7 @@ public class SelectionVisibilityTest {
         methyl.addBond(0, 4, IBond.Order.SINGLE);
         methyl.getAtom(0).setProperty(StandardGenerator.HIGHLIGHT_COLOR, Color.RED);
         SymbolVisibility visibility = SelectionVisibility.disconnected(SymbolVisibility.iupacRecommendations());
-        assertTrue(visibility.visible(methyl.getAtom(0), methyl.getConnectedBondsList(methyl.getAtom(0))));
+        assertTrue(visibility.visible(methyl.getAtom(0), methyl.getConnectedBondsList(methyl.getAtom(0)), new RendererModel()));
     }
 
     @Test public void unIsolated() {
@@ -82,13 +83,13 @@ public class SelectionVisibilityTest {
         methyl.getAtom(0).setProperty(StandardGenerator.HIGHLIGHT_COLOR, Color.RED);
         methyl.getBond(0).setProperty(StandardGenerator.HIGHLIGHT_COLOR, Color.RED);
         SymbolVisibility visibility = SelectionVisibility.disconnected(SymbolVisibility.iupacRecommendations());
-        assertFalse(visibility.visible(methyl.getAtom(0), methyl.getConnectedBondsList(methyl.getAtom(0))));
+        assertFalse(visibility.visible(methyl.getAtom(0), methyl.getConnectedBondsList(methyl.getAtom(0)), new RendererModel()));
     }
 
     @Test public void highlightIsSelected() {
         IChemObject chemObject = mock(IChemObject.class);
         when(chemObject.getProperty(StandardGenerator.HIGHLIGHT_COLOR)).thenReturn(Color.RED);
-        assertTrue(SelectionVisibility.isSelected(chemObject));
+        assertTrue(SelectionVisibility.isSelected(chemObject, new RendererModel()));
     }
 
     static IAtom atomAt(String symb, Point2d p) {
