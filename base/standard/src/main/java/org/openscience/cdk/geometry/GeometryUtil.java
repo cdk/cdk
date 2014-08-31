39b58c3e9c90408d337eae43caea8b861a7da0ab
John May <john.wilkinsonmay@gmail.com>
2014-08-31 17:17:48 +0100

    Correct license header.
    Signed-off-by: Egon Willighagen <egonw@users.sourceforge.net>

--- base/standard/src/main/java/org/openscience/cdk/geometry/GeometryUtil.java
+++ base/standard/src/main/java/org/openscience/cdk/geometry/GeometryUtil.java
@@ -1,25 +1,24 @@
-/*
- * Copyright (c) 2014 European Bioinformatics Institute (EMBL-EBI)
- *                    John May (ORCID:0000-0001-7730-2646)
+/*  Copyright (C) 1997-2014  The Chemistry Development Kit (CDK) project
  *
- * Contact: cdk-devel@lists.sourceforge.net
- *   
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or (at
- * your option) any later version. All we ask is that proper credit is given
- * for our work, which includes - but is not limited to - adding the above 
- * copyright notice to the beginning of your source code files, and to any
- * copyright notice that you may distribute with programs based on this work.
+ *  Contact: cdk-devel@lists.sourceforge.net
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
- * License for more details.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package org.openscience.cdk.geometry;
