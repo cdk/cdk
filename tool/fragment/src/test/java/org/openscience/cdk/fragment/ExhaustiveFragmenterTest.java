f812bb55bfab7b7778bc1d8577f26223be8e6270
maclean <gilleain.torrance@gmail.com>
2014-12-30 21:24:37 +0000

    Use the hasItems matcher instead of is to match a list, avoiding ordering problems

--- tool/fragment/src/test/java/org/openscience/cdk/fragment/ExhaustiveFragmenterTest.java
+++ tool/fragment/src/test/java/org/openscience/cdk/fragment/ExhaustiveFragmenterTest.java
@@ -88,7 +88,7 @@ public class ExhaustiveFragmenterTest extends CDKTestCase {
         fragmenter.generateFragments(mol);
         String[] frags = fragmenter.getFragments();
         Assert.assertNotNull(frags);
-        Assert.assertThat(frags, is(new String[]{"c1ccc(cc1)C", "c1ccccc1"}));
+        Assert.assertThat(Arrays.asList(frags), hasItems("c1ccc(cc1)C", "c1ccccc1"));
         Assert.assertNotNull(fragmenter.getFragmentsAsContainers());
         Assert.assertEquals(2, fragmenter.getFragmentsAsContainers().length);
 
