6e2179589220090170486a5e3e030f95a5da82c7
John May <john@nextmovesoftware.com>
2015-06-24 22:57:15 +0100

    Revert "Added the literature reference"
    This reverts commit a84db31bc1d2c8ab88ea1fc3a3f10709dd090fea.

--- descriptor/qsarsubstance/src/main/java/org/openscience/cdk/qsar/descriptors/substance/OxygenAtomCountDescriptor.java
+++ descriptor/qsarsubstance/src/main/java/org/openscience/cdk/qsar/descriptors/substance/OxygenAtomCountDescriptor.java
@@ -29,7 +29,7 @@ import org.openscience.cdk.qsar.result.IntegerResultType;
 
 /**
  * Descriptor that returns the number of oxygens in the chemical
- * formula. Originally aimed at metal oxide nanoparticles {@cdk.cite Liu2011}.
+ * formula. Originally aimed at metal oxide nanoparticles.
  *
  * @author      egonw
  * @cdk.githash
