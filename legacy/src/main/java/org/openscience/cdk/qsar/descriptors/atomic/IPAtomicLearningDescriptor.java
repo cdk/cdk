227fcd98d330145cde0d60586e90ba19eb2c11c5
Egon Willighagen <egon.willighagen@gmail.com>
2016-08-10 13:51:56 +0200

    Removed @cdk.set JavaDocs tags (addresses proj maintenance task #70, https://sourceforge.net/p/cdk/project-maintainance-tasks/70/)

--- legacy/src/main/java/org/openscience/cdk/qsar/descriptors/atomic/IPAtomicLearningDescriptor.java
+++ legacy/src/main/java/org/openscience/cdk/qsar/descriptors/atomic/IPAtomicLearningDescriptor.java
@@ -57,7 +57,6 @@ import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
  * @cdk.created  2006-05-26
  * @cdk.module   qsarionpot
  * @cdk.githash
- * @cdk.set      qsar-descriptors
  * @cdk.dictref  qsar-descriptors:ionizationPotential
  */
 @Deprecated
