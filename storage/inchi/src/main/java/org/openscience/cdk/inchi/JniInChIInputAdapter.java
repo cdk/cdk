825225b10dc7742598a21cb1f8ad06f3a1d45d07
John May <john@nextmovesoftware.com>
2016-10-05 09:09:54 +0100

    InChI KET and 15T options.

--- storage/inchi/src/main/java/org/openscience/cdk/inchi/JniInChIInputAdapter.java
+++ storage/inchi/src/main/java/org/openscience/cdk/inchi/JniInChIInputAdapter.java
@@ -86,6 +86,14 @@ public class JniInChIInputAdapter extends JniInchiInput {
                 if (tok.hasMoreTokens()) {
                     sbOptions.append(" ");
                 }
+            }
+            // 1,5 tautomer option
+            else if ("15T".equals(op)) {
+                sbOptions.append('-').append("15T");
+            }
+            // keto-enol tautomer option
+            else if ("KET".equals(op)) {
+                sbOptions.append('-').append("KET");
             } else {
                 throw new JniInchiException("Unrecognised InChI option");
             }
