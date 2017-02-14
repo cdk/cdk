cbcaca780108bc8ef0b1f0f938518a2e3fba03d9
John May <john@nextmovesoftware.com>
2017-02-14 14:56:02 +0000

    Revert automatic license header changes - over zealous IDEA.

--- legacy/src/main/java/org/openscience/cdk/normalize/Normalizer.java
+++ legacy/src/main/java/org/openscience/cdk/normalize/Normalizer.java
@@ -1,25 +1,27 @@
 /*  
- * Copyright (C) 2004-2010  The Chemistry Development Kit (CDK) project
- *                    2014  Mark B Vine (orcid:0000-0002-7794-0426)
+ *  Copyright (C) 2004-2010  The Chemistry Development Kit (CDK) project
+ *                     2014  Mark B Vine (orcid:0000-0002-7794-0426)
  *
- * Contact: cdk-devel@lists.sourceforge.net
+ *  Contact: cdk-devel@lists.sourceforge.net
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or (at
- * your option) any later version. All we ask is that proper credit is given
- * for our work, which includes - but is not limited to - adding the above
- * copyright notice to the beginning of your source code files, and to any
- * copyright notice that you may distribute with programs based on this work.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
- * License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 U
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
 package org.openscience.cdk.normalize;
 
