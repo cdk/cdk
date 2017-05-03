9e5411e50c98db83e58787c99646ac96b412b049
John Mayfield <john@nextmovesoftware.com>
2017-05-03 14:52:39 +0100

    Clean up TemplateHandler3D

--- tool/builder3d/src/main/java/org/openscience/cdk/modeling/builder3d/TemplateHandler3D.java
+++ tool/builder3d/src/main/java/org/openscience/cdk/modeling/builder3d/TemplateHandler3D.java
@@ -24,10 +24,8 @@
  */
 package org.openscience.cdk.modeling.builder3d;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.Iterator;
@@ -39,11 +37,8 @@ import javax.vecmath.Point3d;
 
 import org.openscience.cdk.CDKConstants;
 import org.openscience.cdk.exception.CDKException;
-import org.openscience.cdk.fingerprint.FingerprinterTool;
-import org.openscience.cdk.fingerprint.HybridizationFingerprinter;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IAtomContainerSet;
 import org.openscience.cdk.interfaces.IChemObjectBuilder;
 import org.openscience.cdk.interfaces.IRingSet;
 import org.openscience.cdk.io.iterator.IteratingSDFReader;
@@ -59,29 +54,26 @@ import org.openscience.cdk.tools.manipulator.RingSetManipulator;
  * Helper class for ModelBuilder3D. Handles templates. This is
  * our layout solution for 3D ring systems
  *
- * @author      cho
- * @author      steinbeck
+ * @author cho
+ * @author steinbeck
+ * @author John Mayfield
  * @cdk.created 2004-09-21
- * @cdk.module  builder3d
+ * @cdk.module builder3d
  * @cdk.githash
  */
 public class TemplateHandler3D {
 
-    private static final IChemObjectBuilder builder                    = SilentChemObjectBuilder.getInstance();
-    private static final ILoggingTool       logger                     = LoggingToolFactory
-                                                                               .createLoggingTool(TemplateHandler3D.class);
+    private static final IChemObjectBuilder builder       = SilentChemObjectBuilder.getInstance();
+    public static final  String             TEMPLATE_PATH = "data/ringTemplateStructures.sdf.gz";
 
-    IAtomContainer                          molecule;
-    IRingSet                                sssr;
-    IAtomContainerSet                       templates                  = null;
-    private boolean                         templatesLoaded            = false;
+    private final List<IAtomContainer> templates     = new ArrayList<>();
+    private final List<IAtomContainer> anonTemplates = new ArrayList<>();
 
-    private static TemplateHandler3D        self                       = null;
+    private static TemplateHandler3D self = null;
 
-    private UniversalIsomorphismTester      universalIsomorphismTester = new UniversalIsomorphismTester();
+    private UniversalIsomorphismTester universalIsomorphismTester = new UniversalIsomorphismTester();
 
     private TemplateHandler3D() {
-        templates = builder.newInstance(IAtomContainerSet.class);
     }
 
     public static TemplateHandler3D getInstance() throws CDKException {
@@ -91,40 +83,33 @@ public class TemplateHandler3D {
         return self;
     }
 
+    private void addTemplateMol(IAtomContainer mol) {
+        final IAtomContainer anon = AtomContainerManipulator.anonymise(mol);
+        templates.add(mol);
+        anonTemplates.add(anon);
+    }
+
     /**
-     * Loads all existing templates into memory.
-     * Template file is a mdl file. Creates a Object Set of Molecules
+     * Load ring template
+     *
      * @throws CDKException The template file cannot be loaded
      */
     private void loadTemplates() throws CDKException {
-        logger.debug("Loading templates...");
-        IteratingSDFReader imdl;
-        InputStream ins;
-        BufferedReader fin;
-
-        try {
-            ins = this.getClass().getClassLoader()
-                    .getResourceAsStream("org/openscience/cdk/modeling/builder3d/data/ringTemplateStructures.sdf.gz");
-            fin = new BufferedReader(new InputStreamReader(new GZIPInputStream(ins)));
-            imdl = new IteratingSDFReader(fin, builder);
-        } catch (IOException exc1) {
-            throw new CDKException("Problems loading file ringTemplateStructures.sdf.gz", exc1);
+        try (InputStream gin = getClass().getResourceAsStream(TEMPLATE_PATH);
+             InputStream in = new GZIPInputStream(gin);
+             IteratingSDFReader sdfr = new IteratingSDFReader(in, builder)) {
+            while (sdfr.hasNext()) {
+                final IAtomContainer mol = sdfr.next();
+                addTemplateMol(mol);
+            }
+        } catch (IOException e) {
+            throw new CDKException("Could not load ring templates", e);
         }
-        IAtomContainer molecule;
-        while (imdl.hasNext()) {
-            molecule = (IAtomContainer) imdl.next();
-            templates.addAtomContainer(molecule);
-        }
-        molecule = null;
-        try {
-            imdl.close();
-        } catch (Exception exc2) {
-            System.out.println("Could not close Reader due to: " + exc2.getMessage());
-        }
-        templatesLoaded = true;
     }
 
-    public static BitSet getBitSetFromFile(StringTokenizer st) throws Exception {
+
+    public static BitSet getBitSetFromFile(StringTokenizer st) throws
+                                                               Exception {
         BitSet bitSet = new BitSet(1024);
         while (st.hasMoreTokens()) {
             bitSet.set(Integer.parseInt(st.nextToken()));
@@ -135,13 +120,13 @@ public class TemplateHandler3D {
     /**
      * Returns the largest (number of atoms) ring set in a molecule.
      *
-     *@param  ringSystems  RingSystems of a molecule
-     *@return              The largestRingSet
+     * @param ringSystems RingSystems of a molecule
+     * @return The largestRingSet
      */
     public IRingSet getLargestRingSet(List<IRingSet> ringSystems) {
-        IRingSet largestRingSet = null;
-        int atomNumber = 0;
-        IAtomContainer container = null;
+        IRingSet       largestRingSet = null;
+        int            atomNumber     = 0;
+        IAtomContainer container      = null;
         for (int i = 0; i < ringSystems.size(); i++) {
             container = getAllInOneContainer(ringSystems.get(i));
             if (atomNumber < container.getAtomCount()) {
@@ -153,8 +138,8 @@ public class TemplateHandler3D {
     }
 
     private IAtomContainer getAllInOneContainer(IRingSet ringSet) {
-        IAtomContainer resultContainer = ringSet.getBuilder().newInstance(IAtomContainer.class);
-        Iterator<IAtomContainer> containers = RingSetManipulator.getAllAtomContainers(ringSet).iterator();
+        IAtomContainer           resultContainer = ringSet.getBuilder().newInstance(IAtomContainer.class);
+        Iterator<IAtomContainer> containers      = RingSetManipulator.getAllAtomContainers(ringSet).iterator();
         while (containers.hasNext()) {
             resultContainer.add((IAtomContainer) containers.next());
         }
@@ -165,8 +150,9 @@ public class TemplateHandler3D {
      * @deprecated Use {@link #mapTemplates(org.openscience.cdk.interfaces.IAtomContainer, int)}
      */
     @Deprecated
-    public void mapTemplates(IAtomContainer ringSystems, double numberOfRingAtoms) throws CDKException,
-            CloneNotSupportedException {
+    public void mapTemplates(IAtomContainer ringSystems,
+                             double numberOfRingAtoms) throws CDKException,
+                                                              CloneNotSupportedException {
         mapTemplates(ringSystems, (int) numberOfRingAtoms);
     }
 
@@ -175,40 +161,42 @@ public class TemplateHandler3D {
      * Molecule. If so, it assigns the coordinates from the template to the
      * respective atoms in the Molecule.
      *
-     * @param ringSystems       AtomContainer from the ring systems.
+     * @param mol       AtomContainer from the ring systems.
      * @param numberOfRingAtoms Number of atoms in the specified ring
      * @throws CloneNotSupportedException The atomcontainer cannot be cloned.
      */
-    public void mapTemplates(IAtomContainer ringSystems, int numberOfRingAtoms) throws CDKException,
-            CloneNotSupportedException {
-        if (!templatesLoaded) self.loadTemplates();
+    public void mapTemplates(IAtomContainer mol, int numberOfRingAtoms)
+        throws CDKException, CloneNotSupportedException {
+        if (templates.isEmpty()) {
+            loadTemplates();
+        }
 
         //logger.debug("Map Template...START---Number of Ring Atoms:"+numberOfRingAtoms);
-        IAtomContainer ringSystemAnyBondAnyAtom = AtomContainerManipulator.anonymise(ringSystems);
+        final IAtomContainer anon = AtomContainerManipulator.anonymise(mol);
 
         boolean flagMaxSubstructure = false;
-        boolean flagSecondbest = false;
-        for (int i = 0; i < templates.getAtomContainerCount(); i++) {
-            IAtomContainer template = templates.getAtomContainer(i);
+        boolean flagSecondbest      = false;
+        for (int i = 0; i < templates.size(); i++) {
+
+            IAtomContainer template     = templates.get(i);
+            IAtomContainer anonTemplate = anonTemplates.get(i);
+
             //if the atom count is different, it can't be right anyway
-            if (template.getAtomCount() != ringSystems.getAtomCount()) {
+            if (template.getAtomCount() != mol.getAtomCount()) {
                 continue;
             }
-            //we compare the fingerprint with any atom and any bond
-            IAtomContainer templateAnyBondAnyAtom = AtomContainerManipulator.anonymise(template);
             //we do the exact match with any atom and any bond
-            if (universalIsomorphismTester.isSubgraph(ringSystemAnyBondAnyAtom, templateAnyBondAnyAtom)) {
+            if (universalIsomorphismTester.isSubgraph(anon, anonTemplate)) {
                 //if this is the case, we keep it as a guess, but look if we can do better
-                List<RMap> list = universalIsomorphismTester.getSubgraphAtomsMap(ringSystemAnyBondAnyAtom,
-                        templateAnyBondAnyAtom);
+                List<RMap> list = universalIsomorphismTester.getSubgraphAtomsMap(anon, anonTemplate);
                 boolean flagwritefromsecondbest = false;
                 if ((numberOfRingAtoms == list.size())
-                        && templateAnyBondAnyAtom.getBondCount() == ringSystems.getBondCount()) {
+                    && anonTemplate.getBondCount() == mol.getBondCount()) {
                     //so atom and bond count match, could be it's even an exact match,
                     //we check this with the original ring system
-                    if (universalIsomorphismTester.isSubgraph(ringSystems, template)) {
+                    if (universalIsomorphismTester.isSubgraph(mol, template)) {
                         flagMaxSubstructure = true;
-                        list = universalIsomorphismTester.getSubgraphAtomsMap(ringSystems, template);
+                        list = universalIsomorphismTester.getSubgraphAtomsMap(mol, template);
                     } else {
                         //if it isn't we still now it's better than just the isomorphism
                         flagSecondbest = true;
@@ -217,23 +205,20 @@ public class TemplateHandler3D {
                 }
 
                 if (!flagSecondbest || flagMaxSubstructure || flagwritefromsecondbest) {
-                    for (int j = 0; j < list.size(); j++) {
-                        RMap map = (RMap) list.get(j);
-                        IAtom atom1 = ringSystems.getAtom(map.getId1());
+                    for (RMap aList : list) {
+                        RMap  map   = aList;
+                        IAtom atom1 = mol.getAtom(map.getId1());
                         IAtom atom2 = template.getAtom(map.getId2());
                         if (atom1.getFlag(CDKConstants.ISINRING)) {
                             atom1.setPoint3d(new Point3d(atom2.getPoint3d()));
                         }
-                    }//for j
+                    }
                 }
-
                 if (flagMaxSubstructure) {
                     break;
                 }
-
-            }//if subgraph
-
-        }//for i
+            }
+        }
         if (!flagMaxSubstructure) {
             System.out.println("WARNING: Maybe RingTemplateError!");
         }
@@ -245,16 +230,16 @@ public class TemplateHandler3D {
      * @return The templateCount value
      */
     public int getTemplateCount() {
-        return templates.getAtomContainerCount();
+        return templates.size();
     }
 
     /**
-     *  Gets the templateAt attribute of the TemplateHandler object.
+     * Gets the templateAt attribute of the TemplateHandler object.
      *
-     *@param  position  Description of the Parameter
-     *@return The templateAt value
+     * @param position Description of the Parameter
+     * @return The templateAt value
      */
     public IAtomContainer getTemplateAt(int position) {
-        return templates.getAtomContainer(position);
+        return templates.get(position);
     }
 }
