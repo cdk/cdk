d54950b951abde6867de235ebb0f20c8f0c4fd5e
John Mayfield <john@nextmovesoftware.com>
2017-06-25 18:09:45 +0100

    Consider double bond configs when choosing macro position.

--- tool/sdg/src/main/java/org/openscience/cdk/layout/StructureDiagramGenerator.java
+++ tool/sdg/src/main/java/org/openscience/cdk/layout/StructureDiagramGenerator.java
@@ -43,6 +43,7 @@ import org.openscience.cdk.interfaces.IPseudoAtom;
 import org.openscience.cdk.interfaces.IReaction;
 import org.openscience.cdk.interfaces.IRing;
 import org.openscience.cdk.interfaces.IRingSet;
+import org.openscience.cdk.interfaces.IStereoElement;
 import org.openscience.cdk.isomorphism.AtomMatcher;
 import org.openscience.cdk.isomorphism.BondMatcher;
 import org.openscience.cdk.isomorphism.Pattern;
@@ -52,6 +53,7 @@ import org.openscience.cdk.sgroup.Sgroup;
 import org.openscience.cdk.sgroup.SgroupBracket;
 import org.openscience.cdk.sgroup.SgroupKey;
 import org.openscience.cdk.sgroup.SgroupType;
+import org.openscience.cdk.stereo.DoubleBondStereochemistry;
 import org.openscience.cdk.tools.ILoggingTool;
 import org.openscience.cdk.tools.LoggingToolFactory;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
@@ -737,6 +739,34 @@ public class StructureDiagramGenerator {
             // to each other either as bridged ringsystems, fused rings or via
             // spiro connections.
             ringSystems = RingPartitioner.partitionRings(sssr);
+
+            // set the in-ring db stereo
+            for (IStereoElement se : molecule.stereoElements()) {
+                if (se.getConfigClass() == IStereoElement.CisTrans) {
+                    IBond stereoBond    = (IBond) se.getFocus();
+                    IBond firstCarrier  = (IBond) se.getCarriers().get(0);
+                    IBond secondCarrier = (IBond) se.getCarriers().get(0);
+                    for (IRingSet ringSet : ringSystems) {
+                        for (IAtomContainer ring : ringSet.atomContainers()) {
+                            if (ring.contains(stereoBond)) {
+                                List<IBond> begBonds = ring.getConnectedBondsList(stereoBond.getBegin());
+                                List<IBond> endBonds = ring.getConnectedBondsList(stereoBond.getEnd());
+                                begBonds.remove(stereoBond);
+                                endBonds.remove(stereoBond);
+                                // something odd wrong, just skip it
+                                if (begBonds.size() != 1 || endBonds.size() != 1)
+                                    continue;
+                                boolean flipped = begBonds.contains(firstCarrier) ^ endBonds.contains(secondCarrier);
+                                int cfg = flipped ? se.getConfig() : se.getConfig() ^ (IStereoElement.TOGETHER | IStereoElement.OPPOSITE);
+                                ring.addStereoElement(new DoubleBondStereochemistry(stereoBond,
+                                                                                    new IBond[]{begBonds.get(0), endBonds.get(0)},
+                                                                                    cfg));
+                            }
+                        }
+                    }
+
+                }
+            }
         } else {
             sssr = molecule.getBuilder().newInstance(IRingSet.class);
             ringSystems = new ArrayList<>();
@@ -1616,7 +1646,7 @@ public class StructureDiagramGenerator {
      * @return ring is a macro cycle
      */
     private boolean isMacroCycle(IRing ring, IRingSet rs) {
-        if (ring.getAtomCount() < 10)
+        if (ring.getAtomCount() < 8)
             return false;
         for (IBond bond : ring.bonds()) {
             boolean found = false;
