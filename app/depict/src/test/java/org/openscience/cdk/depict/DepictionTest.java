f2936280e097b54369f19333cbaea31152fd537b
Saulius Gražulis <grazulis@ibt.lt>
2018-03-09 10:41:26 +0200

    Adding one more EPS output test.

--- app/depict/src/test/java/org/openscience/cdk/depict/DepictionTest.java
+++ app/depict/src/test/java/org/openscience/cdk/depict/DepictionTest.java
@@ -38,4 +38,17 @@ public class DepictionTest {
         assertEquals("%%BoundingBox: 0 0 28 35", lines[1]);
     }
 
+    @Test
+    public void depictAsEps2() throws CDKException {
+        DepictionGenerator dg = new DepictionGenerator();
+        SmilesParser sp = new SmilesParser(SilentChemObjectBuilder.getInstance());
+        IAtomContainer ac = sp.parseSmiles("C1CCCCC1CCCCC");
+        Depiction d = dg.depict(ac);
+        String eps = d.toEpsStr();
+        String nl = System.getProperty("line.separator");
+        String lines[] = eps.split(nl,3);
+        assertEquals("%!PS-Adobe-3.0 EPSF-3.0", lines[0]);
+        assertEquals("%%BoundingBox: 0 0 92 33", lines[1]);
+    }
+
 }
