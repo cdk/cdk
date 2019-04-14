9099a881a40fa3439d88bfb8d6dfeb75a070259b
John Mayfield <john@nextmovesoftware.com>
2019-04-14 18:52:32 +0100

    Missing import

--- tool/formula/src/main/java/org/openscience/cdk/tools/manipulator/MolecularFormulaManipulator.java
+++ tool/formula/src/main/java/org/openscience/cdk/tools/manipulator/MolecularFormulaManipulator.java
@@ -40,6 +40,7 @@ import org.openscience.cdk.tools.LoggingToolFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
