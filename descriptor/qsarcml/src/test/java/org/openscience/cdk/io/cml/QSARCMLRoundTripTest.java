154ab6727386f2e4029983144169c85a57089f98
John Mayfield <john@nextmovesoftware.com>
2021-12-22 20:45:00 +0000

    Avoid test-jar dependency for qsarcml - we only need the roundtrip function.

--- descriptor/qsarcml/src/test/java/org/openscience/cdk/io/cml/QSARCMLRoundTripTest.java
+++ descriptor/qsarcml/src/test/java/org/openscience/cdk/io/cml/QSARCMLRoundTripTest.java
@@ -24,10 +24,16 @@
 
 package org.openscience.cdk.io.cml;
 
+import nu.xom.Element;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomContainerSet;
+import org.openscience.cdk.interfaces.IChemFile;
+import org.openscience.cdk.interfaces.IChemModel;
+import org.openscience.cdk.interfaces.IChemSequence;
+import org.openscience.cdk.io.CMLReader;
 import org.openscience.cdk.io.CMLWriter;
 import org.openscience.cdk.libio.cml.Convertor;
 import org.openscience.cdk.libio.cml.QSARCustomizer;
@@ -39,10 +45,11 @@ import org.openscience.cdk.templates.TestMoleculeFactory;
 import org.openscience.cdk.tools.ILoggingTool;
 import org.openscience.cdk.tools.LoggingToolFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.StringWriter;
 
 /**
- * @author John May
+ * @author John Mayfield
  * @cdk.module test-qsarcml
  */
 public class QSARCMLRoundTripTest {
@@ -57,6 +64,34 @@ public class QSARCMLRoundTripTest {
         convertor.registerCustomizer(new QSARCustomizer());
     }
 
+    // See also CMLRoundTripTool
+    public static IAtomContainer roundTripMolecule(Convertor convertor, IAtomContainer mol) throws Exception {
+        String cmlString = "<!-- failed -->";
+        Element cmlDOM = convertor.cdkAtomContainerToCMLMolecule(mol);
+        cmlString = cmlDOM.toXML();
+
+        IAtomContainer roundTrippedMol = null;
+        logger.debug("CML string: ", cmlString);
+        CMLReader reader = new CMLReader(new ByteArrayInputStream(cmlString.getBytes()));
+
+        IChemFile file = (IChemFile) reader.read(new org.openscience.cdk.ChemFile());
+        reader.close();
+        Assert.assertNotNull(file);
+        Assert.assertEquals(1, file.getChemSequenceCount());
+        IChemSequence sequence = file.getChemSequence(0);
+        Assert.assertNotNull(sequence);
+        Assert.assertEquals(1, sequence.getChemModelCount());
+        IChemModel chemModel = sequence.getChemModel(0);
+        Assert.assertNotNull(chemModel);
+        IAtomContainerSet moleculeSet = chemModel.getMoleculeSet();
+        Assert.assertNotNull(moleculeSet);
+        Assert.assertEquals(1, moleculeSet.getAtomContainerCount());
+        roundTrippedMol = moleculeSet.getAtomContainer(0);
+        Assert.assertNotNull(roundTrippedMol);
+
+        return roundTrippedMol;
+    }
+
     @Test
     public void testDescriptorValue_QSAR() throws Exception {
         IAtomContainer molecule = TestMoleculeFactory.makeBenzene();
@@ -65,7 +100,7 @@ public class QSARCMLRoundTripTest {
         DescriptorValue originalValue = null;
         originalValue = descriptor.calculate(molecule);
         molecule.setProperty(originalValue.getSpecification(), originalValue);
-        IAtomContainer roundTrippedMol = CMLRoundTripTool.roundTripMolecule(convertor, molecule);
+        IAtomContainer roundTrippedMol = roundTripMolecule(convertor, molecule);
 
         Assert.assertEquals(1, roundTrippedMol.getProperties().size());
         Object object = roundTrippedMol.getProperties().keySet().toArray()[0];
