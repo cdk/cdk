ab90f91cde5821c955246cf62a66e3d0ee1e2f1b
John Mayfield <john@nextmovesoftware.com>
2022-02-05 11:28:32 +0000

    Relicense header as LGPL to be inline with rest of CDK

--- tool/structgen/src/test/java/org/openscience/cdk/structgen/maygen/SmiOutputConsumer.java
+++ tool/structgen/src/test/java/org/openscience/cdk/structgen/maygen/SmiOutputConsumer.java
@@ -1,7 +1,19 @@
 /*
- * =====================================
- *  Copyright (c) 2022 NextMove Software
- * =====================================
+ * Copyright (c) 2021 Mehmet Aziz Yirik <mehmetazizyirik@outlook.com> <0000-0001-7520-7215@orcid.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package org.openscience.cdk.structgen.maygen;
