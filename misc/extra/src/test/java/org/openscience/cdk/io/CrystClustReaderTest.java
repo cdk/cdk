5540f07d4aa24d4cb39287479dca9286b9ebd957
Egon Willighagen <egon.willighagen@gmail.com>
2024-06-11 19:53:13 +0200

    Removed an unused import

--- misc/extra/src/test/java/org/openscience/cdk/io/CrystClustReaderTest.java
+++ misc/extra/src/test/java/org/openscience/cdk/io/CrystClustReaderTest.java
@@ -29,7 +29,6 @@ import javax.vecmath.Vector3d;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openscience.cdk.AtomContainer;
 import org.openscience.cdk.ChemFile;
 import org.openscience.cdk.ChemObject;
 import org.openscience.cdk.interfaces.IAtomContainer;
