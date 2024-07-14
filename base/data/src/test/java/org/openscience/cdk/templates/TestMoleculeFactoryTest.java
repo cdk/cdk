68ba5387866371b81935277e7e6935c11005e5d1
Egon Willighagen <egon.willighagen@gmail.com>
2024-07-14 14:17:41 +0200

    Removed the redundant 'public' qualifier

--- base/data/src/test/java/org/openscience/cdk/templates/TestMoleculeFactoryTest.java
+++ base/data/src/test/java/org/openscience/cdk/templates/TestMoleculeFactoryTest.java
@@ -26,23 +26,23 @@ import org.junit.jupiter.api.Test;
  */
 public class TestMoleculeFactoryTest {
 
-	@Test public void makePhenylAmine() { Assertions.assertNotNull(TestMoleculeFactory.makePhenylAmine()); }
-	@Test public void makeFusedRings() { Assertions.assertNotNull(TestMoleculeFactory.makeFusedRings()); }
-	@Test public void makePyrroleAnion() { Assertions.assertNotNull(TestMoleculeFactory.makePyrroleAnion()); }
-	@Test public void makePyrazole() { Assertions.assertNotNull(TestMoleculeFactory.makePyrazole()); }
-	@Test public void make124Triazole() { Assertions.assertNotNull(TestMoleculeFactory.make124Triazole()); }
-	@Test public void makeOxazole() { Assertions.assertNotNull(TestMoleculeFactory.makeOxazole()); }
-	@Test public void makeIsoxazole() { Assertions.assertNotNull(TestMoleculeFactory.makeIsoxazole()); }
-	@Test public void makeIsothiazole() { Assertions.assertNotNull(TestMoleculeFactory.makeIsothiazole()); }
-	@Test public void makeThiadiazole() { Assertions.assertNotNull(TestMoleculeFactory.makeThiadiazole()); }
-	@Test public void makeOxadiazole() { Assertions.assertNotNull(TestMoleculeFactory.makeOxadiazole()); }
-	@Test public void makePyridazine() { Assertions.assertNotNull(TestMoleculeFactory.makePyridazine()); }
-	@Test public void makeTriazine() { Assertions.assertNotNull(TestMoleculeFactory.makeTriazine()); }
-	@Test public void makeSingleRing() { Assertions.assertNotNull(TestMoleculeFactory.makeSingleRing()); }
-	@Test public void makePiperidine() { Assertions.assertNotNull(TestMoleculeFactory.makePiperidine()); }
-	@Test public void makeTetrahydropyran() { Assertions.assertNotNull(TestMoleculeFactory.makeTetrahydropyran()); }
-	@Test public void makeAnthracene() { Assertions.assertNotNull(TestMoleculeFactory.makeAnthracene()); }
-	@Test public void makeCyclophaneLike() { Assertions.assertNotNull(TestMoleculeFactory.makeCyclophaneLike()); }
-	@Test public void makeGappedCyclophaneLike() { Assertions.assertNotNull(TestMoleculeFactory.makeGappedCyclophaneLike()); }
+	@Test void makePhenylAmine() { Assertions.assertNotNull(TestMoleculeFactory.makePhenylAmine()); }
+	@Test void makeFusedRings() { Assertions.assertNotNull(TestMoleculeFactory.makeFusedRings()); }
+	@Test void makePyrroleAnion() { Assertions.assertNotNull(TestMoleculeFactory.makePyrroleAnion()); }
+	@Test void makePyrazole() { Assertions.assertNotNull(TestMoleculeFactory.makePyrazole()); }
+	@Test void make124Triazole() { Assertions.assertNotNull(TestMoleculeFactory.make124Triazole()); }
+	@Test void makeOxazole() { Assertions.assertNotNull(TestMoleculeFactory.makeOxazole()); }
+	@Test void makeIsoxazole() { Assertions.assertNotNull(TestMoleculeFactory.makeIsoxazole()); }
+	@Test void makeIsothiazole() { Assertions.assertNotNull(TestMoleculeFactory.makeIsothiazole()); }
+	@Test void makeThiadiazole() { Assertions.assertNotNull(TestMoleculeFactory.makeThiadiazole()); }
+	@Test void makeOxadiazole() { Assertions.assertNotNull(TestMoleculeFactory.makeOxadiazole()); }
+	@Test void makePyridazine() { Assertions.assertNotNull(TestMoleculeFactory.makePyridazine()); }
+	@Test void makeTriazine() { Assertions.assertNotNull(TestMoleculeFactory.makeTriazine()); }
+	@Test void makeSingleRing() { Assertions.assertNotNull(TestMoleculeFactory.makeSingleRing()); }
+	@Test void makePiperidine() { Assertions.assertNotNull(TestMoleculeFactory.makePiperidine()); }
+	@Test void makeTetrahydropyran() { Assertions.assertNotNull(TestMoleculeFactory.makeTetrahydropyran()); }
+	@Test void makeAnthracene() { Assertions.assertNotNull(TestMoleculeFactory.makeAnthracene()); }
+	@Test void makeCyclophaneLike() { Assertions.assertNotNull(TestMoleculeFactory.makeCyclophaneLike()); }
+	@Test void makeGappedCyclophaneLike() { Assertions.assertNotNull(TestMoleculeFactory.makeGappedCyclophaneLike()); }
 
 }
