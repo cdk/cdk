2a8dcba11e888274707f431ea27043f522e995fe
Jonas Schaub <44881147+JonasSchaub@users.noreply.github.com>
2024-08-30 16:16:33 +0200

    Remove unused import

--- tool/fragment/src/main/java/org/openscience/cdk/fragment/FunctionalGroupsFinder.java
+++ tool/fragment/src/main/java/org/openscience/cdk/fragment/FunctionalGroupsFinder.java
@@ -38,7 +38,6 @@ import org.openscience.cdk.interfaces.ISingleElectron;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
