ce801b4d5486ce7b6d80fda09f6d7ddefa7c0687
Uli <98144814+uli-f@users.noreply.github.com>
2024-12-05 18:27:14 +1100

    add license header and class-level javadoc

--- storage/rinchi/src/main/java/org/openscience/cdk/rinchi/RInChIException.java
+++ storage/rinchi/src/main/java/org/openscience/cdk/rinchi/RInChIException.java
@@ -1,8 +1,33 @@
+/* Copyright (C) 2024 Uli Fechner
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.openscience.cdk.rinchi;
 
 import org.openscience.cdk.exception.CDKException;
 
-public class RInChIException extends CDKException {
+/**
+ * Exception specifically thrown by classes residing in the module cdk-rinchi.
+ *
+ * @author Uli Fechner
+ * @cdk.module rinchi
+ * @cdk.githash
+ */
+public final class RInChIException extends CDKException {
     public RInChIException(String message) {
         super(message);
     }
--- storage/rinchi/src/main/java/org/openscience/cdk/rinchi/RInChIGenerator.java
+++ storage/rinchi/src/main/java/org/openscience/cdk/rinchi/RInChIGenerator.java
@@ -58,7 +58,6 @@ import static org.openscience.cdk.rinchi.RInChIConstants.RINCHI_WEB_KEY_HEADER;
  * @cdk.module rinchi
  * @cdk.githash
  */
-
 public final class RInChIGenerator extends StatusMessagesOutput {
     private static final ILoggingTool LOGGER = LoggingToolFactory.createLoggingTool(RInChIGenerator.class);
     private static final int NUMBER_OF_COMPONENTS = 3;
